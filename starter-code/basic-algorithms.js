c3bd4c676acb87c57f6fe3cd544f9e8b4a4e0b8e
Juanma Perez <juanmaperezvar@gmail.com>
2017-04-10 19:20:29 +0200

    Done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,5 @@
 console.log("I'm ready");
 
-//   Variables
-
 hacker1 = "James";
 
 console.log("The driver's name is " + hacker1);
@@ -10,8 +8,6 @@ hacker2 = prompt("What's the navigator name?");
 
 console.log("The navigator's name is " + hacker2);
 
-
-//   Conditionals
 
 
 if (hacker1.length > hacker2.length){
@@ -26,7 +22,6 @@ if (hacker1.length > hacker2.length){
 
 
 
-//   Loops
 
 function splitAndUpper(str){
   var name = "";
@@ -38,4 +33,77 @@ function splitAndUpper(str){
   console.log(name.toUpperCase());
 }
 
-splitAndUpper("hacker1");
+splitAndUpper(hacker2);
+
+
+
+
+function reverse(name){
+  var reverse = " ";
+  var times = name.length-1;
+
+  for(i=times; i>=0; i--){
+    reverse += name[i];
+  }
+  console.log(reverse);
+}
+
+reverse(hacker2);
+
+
+
+function orderWords(name1, name2){
+  var letters = "abcdefghijklmnñopqrstuvwxyz";
+  name1 = name1.toLowerCase();
+  name2 = name2.toLowerCase();
+
+  var times = name1.length;
+
+  if(name2.length < name1.length){
+    times = name2.length;
+  }
+
+  var orderName1 = "";
+  var orderName2 = "";
+
+  for(i=0;i<=times-1; i++){
+      orderName1 += (letters.indexOf(name1[i]) +".");
+      orderName2 += (letters.indexOf(name2[i])+".");
+  }
+
+  if(orderName1 > orderName2){
+    console.log("Alphabetically " + name2 + " go first");
+  }else if(orderName1 < orderName2){
+    console.log("Alphabetically " + name1 + " go first");
+  }else if(orderName1 === orderName2){
+    console.log("Really? You have the same name");
+  }
+}
+
+orderWords(hacker1, hacker2);
+
+
+
+function isPalindrome(){
+  var str = prompt("Insert a Palindrome");
+  str = str.replace(/([.*,' ?¿!¡])/gi, "");
+  var times = Math.floor(str.length/2);
+
+  var first = "";
+  var second = "";
+
+  for (i=0; i<times; i++ ){
+    first += str[i];
+    second += str[(str.length-1) - i];
+
+  }
+
+  if(first.toLowerCase() === second.toLowerCase()){
+    console.log(str + " is a palindrome");
+  } else {
+    console.log(str + " is not a palindrome");
+  }
+
+}
+
+isPalindrome();
