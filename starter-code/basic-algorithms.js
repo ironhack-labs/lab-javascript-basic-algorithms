576ff9185f06c516db7cbe94bfb9382efe394f32
Carlos del Prado <carlos.delprado@11paths.com>
2017-10-23 22:42:09 +0200

    feat: loops

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -2,7 +2,7 @@
 var hacker1 = "Carlos";
 console.log("The driver's name is " + hacker1);
 
-var hacker2 = prompt("What is the navigator's name?");
+var hacker2 = prompt("What is the navigator's name?", "");
 console.log("The navigator's name is " + hacker2);
 
 //Conditionals
@@ -14,4 +14,44 @@ if (hacker1.length > hacker2.length) {
     console.log("wow, you both got equally long names, " + hacker1.length + " characters!!");
 }
 
-// Lorem ipsum generator
+// Loops
+var capitalized = "";
+for (var i = 0; i < hacker1.length; i++) {
+    capitalized += hacker1[i].toUpperCase();
+    if (i < hacker1.length - 1) {
+        capitalized += " ";
+    }
+    
+}
+console.log(capitalized);
+
+var reversed = "";
+for (var i = hacker2.length - 1; i >= 0; i--) {
+    reversed += hacker2[i];
+}
+console.log(reversed);
+
+var first = "";
+var minSize = Math.min(hacker1.length, hacker2.length);
+for (var i = 0; i < minSize; i++) {
+    if (hacker1[i] < hacker2[i]) {
+        first = hacker1;
+        break;
+    } else if (hacker1[i] > hacker2[i]) {
+        first = hacker2;
+        break;
+    } else if (i == minSize - 1) {
+        first = false;
+    }
+}
+
+switch (first) {
+    case hacker1:
+        console.log("The driver's name goes first");
+        break;
+    case hacker2:
+        console.log("Yo, the navigator goes first definitely");
+        break;
+    default:
+        console.log("What?! You both got the same name?");
+}
