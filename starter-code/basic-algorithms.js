b3f671de72df4f49602a377d79712ea1051c57a8
Carlos del Prado <carlos.delprado@11paths.com>
2017-10-23 23:15:17 +0200

    feat: bonus

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -55,3 +55,23 @@ switch (first) {
     default:
         console.log("What?! You both got the same name?");
 }
+
+// Bonus
+var word = prompt("Enter a message for palindrome evaluation!", "");
+word = word.replace(/[\s,.!¡¿?:;']/g, '').toLowerCase();
+console.log(word);
+isPalindrome = true;
+var i = 0;
+while (isPalindrome && i < Math.floor(word.length / 2)) {
+    isPalindrome = word[i] === word[word.length - i - 1];
+    i++;
+}
+if (isPalindrome) {
+    console.log("Palindrome!");
+} else {
+    console.log("nope :(");
+}
+
+var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vestibulum ipsum nibh, tincidunt non consectetur quis, elementum sed nisl.Suspendisse aliquet nibh sed tellus gravida luctus.Nam in elementum nisl.Maecenas posuere mauris eget urna blandit tempor.Integer aliquam eros eu est scelerisque, nec vehicula ipsum dictum.Nunc id ante in risus molestie pellentesque.Sed quis egestas nisl.Fusce mattis lobortis eros, non placerat erat feugiat et.Cras dignissim dolor libero, ut placerat sapien auctor ac. Maecenas vel augue purus.Etiam posuere ultricies turpis eget commodo.Sed vulputate auctor eros, in dignissim nisl.Maecenas scelerisque, metus id auctor ornare, lorem nisl egestas nibh, sed porttitor elit mi sed sem.Mauris quis porttitor enim, et lobortis nisl.Nullam ac semper nunc.Ut accumsan facilisis neque mollis sollicitudin.Pellentesque sollicitudin augue non vulputate laoreet.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Vivamus quis fermentum dolor.Fusce quis pulvinar neque. Nulla quis pulvinar metus, a tristique ligula.Maecenas elementum at eros vel dignissim.Fusce a est augue.Donec tempor accumsan ipsum a pulvinar.Cras pharetra, quam ac euismod ultrices, urna nibh bibendum sapien, a mattis ex metus et magna.Integer sed commodo justo.Donec at tellus tempor, suscipit enim a, placerat tellus.Quisque sed nibh imperdiet, hendrerit turpis ac, fermentum sapien.Quisque luctus mi at lorem placerat eleifend.Nam fringilla sed sapien eget tincidunt.Sed rhoncus tortor non viverra dignissim.Morbi interdum massa eu nulla fermentum scelerisque.Quisque sit amet lorem elementum neque commodo sagittis.Aliquam vel faucibus eros.";
+console.log("Words: " + text.split(' ').length);
+console.log("Et matchs: " + (text.match(/et/g) || []).length);
