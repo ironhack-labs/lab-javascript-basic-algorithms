b2c2fd983304ee65fae380a17b3963db4820377e
Carlos del Prado <carlos.delprado@11paths.com>
2017-10-23 22:14:09 +0200

    feat: names and input

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,9 @@
 // Names and Input
+var hacker1 = "Carlos";
+console.log("The driver's name is " + hacker1);
 
+var hacker2 = prompt("What is the navigator's name?");
+console.log("The navigator's name is " + hacker2);
 
 //Conditionals
 
