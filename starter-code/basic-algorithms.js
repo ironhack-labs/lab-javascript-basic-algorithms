b08d213c2534b02b662809bc0b821be93bd0b38e
Carlos del Prado <carlos.delprado@11paths.com>
2017-10-23 22:14:32 +0200

    feat: conditionals

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -6,6 +6,12 @@ var hacker2 = prompt("What is the navigator's name?");
 console.log("The navigator's name is " + hacker2);
 
 //Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log("The Driver has the longest name, it has " + hacker1.length + " characters");
+} else if (hacker1.length < hacker2.length) {
+    console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters");
+} else {
+    console.log("wow, you both got equally long names, " + hacker1.length + " characters!!");
+}
 
 // Lorem ipsum generator
