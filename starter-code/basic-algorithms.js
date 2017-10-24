c773eb048441b5e2b0ccf10f1a089ab9ebf15d9f
diana-91 <diana.hervasmoya@gmail.com>
2017-10-24 21:38:46 +0200

    feat: exercise 1

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,9 @@
 // Names and Input
+var hacker1 = "pepe";
+console.log("The driver's name is "+ hacker1);
 
+var hacker2 = prompt("name:");
+console.log("The navigato's is " + hacker2);
 
 //Conditionals
 
