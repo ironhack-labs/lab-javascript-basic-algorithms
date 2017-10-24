1a27f68cadaead6214a53832e82933bcd39a647c
asalria <ameliasalria@gmail.com>
2017-10-24 21:20:26 +0200

    feat: Condicionales

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,20 @@
 // Names and Input
+console.log("I'm Ready!");
+var hacker1 = "Pepe";
+console.log("The driver's name is " + hacker1);
+var hacker2 = window.prompt("What's the navigator's name?","Navigator's name");
+console.log("The navigator's name is " + hacker2);
+
 
 
 //Conditionals
-
+if(hacker1.length>hacker2.length)
+{
+  console.log("The Driver has the longest name, it has " + hacker1.length + " characters");
+}
+else if(hacker2.length>hacker1.length)
+{
+  console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters");
+}
 
 // Lorem ipsum generator
