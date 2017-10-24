0d5a0d97a951791d809bb77ed283f88917631bdd
Cesar Aparicio <contacto@cesaraparicio.es>
2017-10-24 21:40:01 +0200

    done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,4 @@
-// Names and Input
-
-
-//Conditionals
-
-
-// Lorem ipsum generator
+var hacker1 = "Juancho";
+console.log("The driver's name is " + hacker1);
+var hacker2 = prompt("What's the navigator name?");
+console.log ("The navigator's name is " + hacker2);
