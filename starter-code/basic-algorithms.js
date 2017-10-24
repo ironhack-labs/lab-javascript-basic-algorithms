6c3a1d817b53ce035815740355a2ff20ac132a18
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2017-10-24 21:42:47 +0200

    initial commit

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,7 @@
-// Names and Input
-
+var hacker1 = prompt("What's your name?")// Names and Input
+console.log("The driver's name is" + " " + hacker1);
+var hacker2 = prompt("What's your name?")// Names and Input
+console.log("The navigator's name is" + " " + hacker2);
 
 //Conditionals
 
