ab9ad0e89544be1e7406e2ac5179ee0cdd38592f
Marcos Monzon <marco238cuba@gmail.com>
2017-10-25 06:03:31 +0200

    feat: comments added

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,10 +1,15 @@
 // Names and Input
+//1
 var hacker1 = "Marco";
+//2
 console.log("The driver's name is " + hacker1);
+//3
 var hacker2 = prompt("The navigator's name is:");
+//4
 console.log("The navigator's name is " + hacker2);
 
 //Conditionals
+//5
 var len1 = hacker1.length;
 var len2 = hacker2.length;
 
@@ -17,6 +22,7 @@ if(len1 > len2) {
 }
 
 //Loops
+//6
 var uCaseDriver = "";
 for( i=0; i < len1; i++){
   uCaseDriver += hacker1.toUpperCase()[i] + " ";
@@ -24,8 +30,10 @@ for( i=0; i < len1; i++){
 uCaseDriver = uCaseDriver.slice(0, uCaseDriver.length-1);
 console.log(uCaseDriver);
 
+//7
 console.log(hacker2.split("").reverse().join(""));
 
+//8
 if(hacker1.localeCompare(hacker2) < 0){
   console.log("The driver's name goes first");
 } else if(hacker1.localeCompare(hacker2) > 0){
