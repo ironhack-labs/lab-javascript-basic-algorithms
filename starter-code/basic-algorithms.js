856051044abde14d10bd603774bebd02682d16fb
Bart Smet <bartsmet30@gmail.com>
2017-10-25 20:19:31 +0200

    feat: Variables and first two loops

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,33 @@
-// Names and Input
+// variables
+var hacker1 = "Bart";
+var hacker2 = window.prompt("What's your name?");
+console.log("The driver's name is " + hacker1);
+console.log("The navigator's name is " + hacker2);
+
+// conditionals
+if (hacker1.length > hacker2.length) {
+  console.log("The Driver has the longest name, it has " + hacker1.length + " characters");
+} else if (hacker1.length < hacker2.length) {
+  console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters");
+} else {
+  console.log("Wow, you both got equally long names, " + hacker1.length + " characters");
+}
+
+// loops
+var newString ="";
+
+for (var i = 0; i < hacker1.length; i++) {
+  newString += hacker1[i] + " ";
+}
+
+var upperCaseString = newString.toUpperCase();
+console.log(upperCaseString);
 
 
-//Conditionals
+var reverseString="";
 
+for (var i = hacker2.length - 1; i >= 0; i--) {
+  reverseString += hacker2[i];
+}
 
-// Lorem ipsum generator
+console.log(reverseString);
