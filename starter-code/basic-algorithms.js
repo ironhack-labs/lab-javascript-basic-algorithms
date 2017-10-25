5e9ae2c2ec55ab6995593867d52b69539bf31578
Miguel Manzano <miguelmanzan@gmail.com>
2017-10-25 18:35:56 +0200

    Ejercicio Resuelto

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,18 @@
 // Names and Input
-
+var hacker1 = "Miguel";
+console.log("The driver's name is " + hacker1);
+//var hacker2 = prompt("Whats your name");
+console.log("The navigator's name is " + hacker2);
 
 //Conditionals
-
+if( hacker1.length > hacker2.length ) {
+   console.log("The Driver has the longest name, it has " + hacker1.lenght + " characters");
+}
+else if( hacker1.length < hacker2.length ) {
+  console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters");
+}
+else{
+  console.log("wow, you both got equally long names, " + hacker2.length + " characters!!");
+}
 
 // Lorem ipsum generator
