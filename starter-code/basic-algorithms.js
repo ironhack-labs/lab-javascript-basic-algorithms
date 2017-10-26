ef63f21de492175b5ef8925a28311c920c457709
Rak <lovetacirupeca@gmail.com>
2017-10-26 09:08:54 +0200

    feat: add teamwork

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,20 @@
 // Names and Input
+var hacker1 = "Marco";
 
+console.log("The driver's name is " + hacker1);
 
+var hacker2 = prompt("The navigator's name is:");
+
+console.log("The navigator's name is " + hacker2);
+
+var len1 = hacker1.length;
+var len2 = hacker2.length;
 //Conditionals
-
-
+if(len1 > len2) {
+  console.log("The Driver has the longest name, it has " + len1 + " characters");
+} else if(len1 < len2){
+  console.log("Yo, navigator got the longest name, it has " + len2 + " characters");
+} else {
+  console.log("wow, you both got equally long names, " + len1 + " characters!!");
+}
 // Lorem ipsum generator
