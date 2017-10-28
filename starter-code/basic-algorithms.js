5cbc6a381e1ef4689dbf608e3f0790fb1c3adb40
Rak <lovetacirupeca@gmail.com>
2017-10-28 08:41:32 +0200

    feat: add compare and try palindrome

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -8,10 +8,10 @@ var hacker2 = prompt("The navigator's name is:");
 
 console.log("The navigator's name is " + hacker2);
 
+//Conditionals
+
 var len1 = hacker1.length;
 var len2 = hacker2.length;
-
-//Conditionals
 
 if(len1 > len2) {
   console.log("The Driver has the longest name, it has " + len1 + " characters");
@@ -28,3 +28,16 @@ var driverName1 = driverName0.split("").join(" ");
 console.log(driverName1);
 
 console.log(hacker2.split("").reverse().join(""));
+
+var compare = hacker1.localeCompare(hacker2);
+
+if (compare <= -1){
+  console.log("The driver's name goes first");
+} else if (compare >= 1) {
+  console.log("Yo, the navigator goes first definitely");
+} else console.log("What?! You both got the same name?");
+
+// Bonus Time!
+
+var tryPalindrome = prompt("Write a text, we check if it is a palindrome");
+console.log("Sorry, we are working on this function");
