b87c41856b7da7e67cfd784af5ad39a87e57fd89
Gyasik Nunez <gyasiknunez@gmail.com>
2018-01-22 15:12:49 -0500

    In class progress, first commit

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,29 @@
 // Names and Input
 
+var hacker1 = "john";
+var hacker2 = (prompt("What is your name?"));
+
 
 //Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log("The Driver has the longest name, it has XX characters");
 
+} else if (hacker2.length > hacker1.length) {
+    console.log("Yo, navigator got the longest name, it has XX characters");
+
+} else {
+    console.log("wow, you both got equally long names, XX characters!!");
+}
+
+
+for (var i = 0; i <= hacker1.length; i++) {
+    (hacker1.length[i]);
+    console.log(hacker1.toUpperCase().split('').join(' '));
+}
+
+
+
+ 
 // Lorem ipsum generator
+
