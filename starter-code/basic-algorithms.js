ba6222f401141c005933b12d7d86719830ad972c
Alexinder Virdee <alex.virdee08@gmail.com>
2018-01-22 15:08:11 -0500

    first commit

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,47 @@
-// Names and Input
+var hacker1 = "alex";
+
+console.log("The driver's name is " + hacker1);
+
+console.log("--------")
+
+var hacker2 = prompt("Please enter the navigator's name");
+
+console.log("The navigator's name is " + hacker2);
+
+console.log("--------")
+
+if (hacker1.length > hacker2.length) {
+  console.log("The driver has the longest name, it has " + hacker1.length + " characters") 
+} else if (hacker2.length > hacker1.length) {
+    console.log("Yo, navigator has the longest name, it has " + hacker2.length + " characters")
+  } else {
+    console.log("wow, you both have equally long names, " + hacker1.length || hacker2.length + " characters!!")
+  }
+  
+  console.log("--------");
+  
+  for (var i = 0; i < hacker1.length; i++) {
+    console.log(hacker1[i].toUpperCase() + " ")
+  }
+  
+  
+  console.log("--------");
+  
+  
+for (var i = hacker2.length - 1; i >= 0; i--) {
+  console.log(hacker2[i]);
+}
+
+console.log("--------");
+
+for (var i = 0; i < hacker1.length && hacker2.length; i++) {
+    return hacker1.split('').sort().join('');
+}
 
 
-//Conditionals
 
 
-// Lorem ipsum generator
+
+
+
+
