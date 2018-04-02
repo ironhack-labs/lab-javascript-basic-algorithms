7dddd2cb5451b3960da9a67c8abea7e5de267fb3
Stephen Valido <37198107+istephenzz@users.noreply.github.com>
2018-04-02 15:32:44 -0400

    Update basic-algorithms.js

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,23 @@
-// Names and Input
+var hacker1 = "Stephen";
+var hacker2 = "Jason";
 
+console.log("The driver\'s name is" + " " + hacker1);
+console.log("The navigator\'s name is" + " " + hacker2);
 
-//Conditionals
+if (hacker1.length === hacker2.length){
+  console.log("Wow, you both equally have long names, " + hacker1.length + " characters");
+}
+else if (hacker1.length > hacker2.length){
+  console.log("The driver has the longest name, it has " + hacker1.length + " characters");
+}
+else{
+  console.log("Yo, the navigator got the longest name, it has " + hacker2.length + " characters" );
+}
 
+var result = "";
 
-// Lorem ipsum generator
+for (var i = 0; i < hacker1.length; i++){
+  result = result + hacker1[i] + " ";
+
+}
+ console.log(result.toUpperCase());
