aa118aa045f73279ec8e1f7d5d9192b0685439b0
Jens Kai <jkaih7@gmail.com>
2018-04-02 15:56:02 -0400

    assignment complete

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,27 @@
-// Names and Input
+var hacker1 = "Crystal";
 
+var hacker2 = "Jens";
 
-//Conditionals
+console.log(hacker2);
 
+if (hacker1.length > hacker2.length) {
+  console.log("Driver has longer name, it has 7 characters");
+}
 
-// Lorem ipsum generator
+var result = "";
+
+for (var i = 0; i < hacker1.length; i += 1) {
+  result += hacker1[i] + " ";
+  result = result.toUpperCase();
+}
+
+console.log(result);
+
+var result2 = ""; 
+
+for (var i = 0; i < hacker2.length; i++) {
+  // result2 += hacker2[i];
+  result2 = hacker2.split("").reverse(" ").join("");
+}
+
+console.log(result2);
