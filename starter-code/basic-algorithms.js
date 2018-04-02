65fb8a032c0491d4883296d2c7ddab618e145aef
Summer Emison <summeremison@Summers-MacBook-Pro.local>
2018-04-02 16:00:45 -0400

    first assignment

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,3 +1,37 @@
+var hacker1;
+hacker1 = "claudia";
+console.log("the driver's name is" + " " + hacker1 );
+var hacker2;
+hacker2 = "summer";
+console.log("the navigator's name is" + " " + hacker2);
+hacker1.length = 7;
+hacker2.length = 6;
+if (hacker1< hacker2) {
+  console.log("The driver's name is longer")
+}
+else if (hacker2 > hacker1) {
+  console.log("The navigaor has the longest name")
+}
+else if (hacker1 === hacker2) {
+  console.log("both names are long");
+}
+console.log(hacker1.toUpperCase().split('').join(' '));
+var array;
+function reverseString(str) {
+ array = str.split("");
+ array.reverse();
+ str = array.join('');
+ return str;
+}
+reverseString("Summer");
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first")
+}
+else if (hacker1 < hacker2){
+  console.log("Yo, the navigator goes first definitely")
+}else{
+  console.log("What?! You both have the same name")
+}
 // Names and Input
 
 
