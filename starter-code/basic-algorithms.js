21c18ebc2c1bcb2ea37ab4a1910311df398be035
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-08 16:27:13 -0500

    Iteration 1 done, names and input

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,8 @@
 // Names and Input
 
+const hacker1 = 'Marilyn';
+console.log(`the drivers name is ${hacker1}`);
+const hacker2 = prompt('what is your name?');
+console.log(`the navigators name is ${hacker2}`);
 
-//Conditionals
 
-
-// Lorem ipsum generator
