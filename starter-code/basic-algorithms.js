916b2fb8a982b3b4084bca3a4193263986941ff7
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-10 13:00:18 -0500

    changed to functions insted of loops

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -7,37 +7,35 @@ const hacker2 = prompt('what is your name?');
 console.log(`the navigators name is ${hacker2}`);
 
 const longestName = (driver, navigator) => {
+  let message;
   if (driver.length > navigator.length) {
-    alert(`The driver ${driver} has the longest name, it has ${driver.length} characters`);
+    message = `The driver ${driver} has the longest name, it has ${driver.length} characters`;
   } else if (navigator.length > driver.length) {
-    alert(`Yo, navigator ${navigator} got the longest name, it has ${navigator.length} characters`);
+    message = `Yo, navigator ${navigator} got the longest name, it has ${navigator.length} characters`;
   } else if (driver.length === navigator.length) {
-    alert('What?! You both got the same name?');
+    message = 'What?! You both got the same name?';
   }
+  return message;
 };
 
-longestName(hacker1, hacker2);
+alert(longestName(hacker1, hacker2));
 
 const reverseName = hacker2.split('').reverse('').join('');
-
 const nameInCapitals = hacker1.toUpperCase().split('').join(' ');
 
 alert(`${nameInCapitals} ${reverseName}`);
 
 
-// 8 .Depending on the lexicographic order of the strings, print:
-
-// The driver's name goes first
-// Yo, the navigator goes first definitely
-// What?! You both got the same name?
-
 const lexicographicOrder = (driver, navigator) => {
+  let message;
   if (driver > navigator) {
-    alert('The driver\'s name goes first');
-  } else if (driver > navigator) {
-    alert('Yo, the navigator goes first definitely');
+    message = `The driver's name goes first,${driver}`;
+  } else if (driver < navigator) {
+    message = `Yo, the navigator goes first, ${navigator}`;
   } else {
-    alert('What?! You both got the same name?');
+    message = 'What?! You both got the same name?';
   }
+  return message;
 };
-lexicographicOrder(hacker1, hacker2);
+
+alert(lexicographicOrder(hacker1, hacker2));
