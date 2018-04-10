0e7d786d32c34362b4c9700401ad234712a3663e
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-10 12:16:01 -0500

    madechanges in conditionals

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -6,13 +6,17 @@ console.log(`the drivers name is ${hacker1}`);
 const hacker2 = prompt('what is your name?');
 console.log(`the navigators name is ${hacker2}`);
 
-if (hacker1.length > hacker2.length) {
-  alert(`The driver ${hacker1} has the longest name, it has ${hacker1.length} characters`);
-} else if (hacker2.length > hacker1.length) {
-  alert(`Yo, navigator ${hacker2} got the longest name, it has ${hacker2.length} characters`);
-} else if (hacker1.length === hacker2.length) {
-  alert('What?! You both got the same name?')
-}
+const longestName = (driver, navigator) => {
+  if (driver.length > navigator.length) {
+    alert(`The driver ${driver} has the longest name, it has ${driver.length} characters`);
+  } else if (navigator.length > driver.length) {
+    alert(`Yo, navigator ${navigator} got the longest name, it has ${navigator.length} characters`);
+  } else if (driver.length === navigator.length) {
+    alert('What?! You both got the same name?');
+  }
+};
+
+longestName(hacker1, hacker2);
 
 const reverseName = hacker2.split('').reverse('').join('');
 
