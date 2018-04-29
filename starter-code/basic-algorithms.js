99232749bab03c8ebc77d6cc666fee1918ed564c
Uriel Camargo <cf_uriel@hotmail.com>
2018-04-29 15:34:40 -0500

    almost

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,16 @@
 // Names and Input
+var hacker1 = "";
+var hacker2= window.prompt(message, "¿Cual es tu nombre?");
 
+console.log("The drive is XXXX");
+console.log("The navigator's name is YYYY");
 
 //Conditionals
-
-
+if(hacker1.length > hacker2.length){
+    console.log("The Driver has the longest name, it has" + hacker1.length +" characters");
+}else if(hacker1.length < hacker2.length){
+  console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters");
+}else if(hacker1.length === hacker2.length){
+  console.log("wow, you both got equally long names, "+ hacker1.length +" characters!!")
+}
 // Lorem ipsum generator
