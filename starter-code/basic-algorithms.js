dece7d9eacf088c8894ca4afa57905e5afce2a3b
eloji9 <eloji9@hotmail.com>
2018-05-29 08:56:59 +0200

    done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -33,7 +33,7 @@ strCompare.sort();
 if (hacker1 < hacker2) {
   console.log("The driver's name goes first");
 } else if (hacker2 < hacker1) {
-  console.log("The navigator's name goes first");
+  console.log("The navigator's name goes first.");
 } else {
   console.log("What?! You both got the same name?");
 }
@@ -50,3 +50,11 @@ if (palindrome = strPalindrome) {
 }
 
 // Lorem ipsum generator
+var lorem = ("Nulla porttitor eros in ante sagittis bibendum sed molestie purus. Donec elementum, quam ut euismod suscipit, turpis lorem pretium odio, nec maximus nunc odio eget nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc eu sodales diam, sit amet gravida justo. Morbi vitae metus quis nisi ornare rutrum. Aenean vel augue dignissim tortor malesuada scelerisque ut quis dolor. Vivamus posuere commodo egestas. Vestibulum efficitur molestie lorem, ut fringilla ipsum luctus at. Mauris commodo mauris sed iaculis dignissim. Vivamus hendrerit sapien ut tellus lacinia, et molestie ipsum semper. Vivamus tristique ante quis dictum finibus. Vivamus interdum metus magna, quis pellentesque tortor pellentesque id. Suspendisse tristique lorem orci, id efficitur nibh aliquam efficitur.\n Aliquam non convallis turpis. Ut hendrerit lorem sed elit suscipit aliquam. Maecenas tempus mauris at sodales aliquam. Cras metus erat, varius nec leo ut, molestie sollicitudin mi. Sed fringilla cursus magna, ut elementum sem finibus nec. Cras justo turpis, tristique ut ligula eget, imperdiet imperdiet leo. Donec dignissim rhoncus malesuada. Nullam porta turpis nisl, euismod aliquam dui mollis nec.\n Aenean hendrerit interdum consectetur. Nulla at libero auctor leo porta maximus. Morbi efficitur auctor nisl eget eleifend. Nullam ultrices efficitur nunc, vitae elementum ligula tristique et. Duis auctor elementum vestibulum. Fusce lobortis sapien sed dolor varius condimentum. Nullam dapibus diam ut ligula imperdiet scelerisque. Pellentesque cursus rutrum sem nec bibendum. Proin non tellus a lacus efficitur venenatis.");
+
+function wordCount(lorem) {
+  return lorem.split(" ").length;
+}
+console.log(wordCount(lorem));
+
+console.log(lorem.indexOf(" et "));
