28c697f53c804ead7010c860536afd0d26212ea1
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-06-11 16:55:24 +0200

    done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,44 @@
-// Names and Input
+var hacker1 = "Julien";
+
+console.log('The driver\'s name is ' + hacker1);
+
+var hacker2 = 'Andrea'; 
+
+console.log('The driver\'s name is' + hacker2);
+
+var length1 = hacker1.length;
+var length2 = hacker2.length;
+
+if (length1 > length2) {
+  console.log('The Driver has the longest name, it has ' + length1 + ' characters');
+} 
+else if (length2 > length1) {
+  console.log('Yo, navigator got the longest name, it has ' + length2 + ' characters');
+}
+else if (length1 === length2){
+  console.log('wow, you both got equally long names, ' + length1 + ' characters');
+}
 
 
-//Conditionals
+var letters = hacker1.split('').join(' ');
+
+console.log(letters.toLocaleUpperCase());
 
 
-// Lorem ipsum generator
+function reverse (hacker) {
+  var array = hacker.split("");
+
+  array = array.reverse();
+  array = array.join("");
+
+  console.log(array);
+  return array;
+}
+
+var rev = reverse(hacker2);
+
+console.log(rev);
+
+
+
+
