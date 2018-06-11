38d69dc1342bdec6b1f4f830ae5264d9bdd43e7f
Gabriel Garcia <gabogarciam71@gmail.com>
2018-06-11 16:57:14 +0200

    First exercise Sebastian and Gabriel Team

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,20 @@
 // Names and Input
-
+var hacker1 = 'Sebastian';
+console.log(hacker1);
+var hacker2 = 'Gabriel';
+console.log('The navigators name is '+hacker2)
 
 //Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log('The Driver has the longest name, it has '+hacker1.length+' characters');
+} else if (hacker1.length < hacker2.length) {
+    console.log('Yo, navigator got the longest name, it has '+hacker2.length+' characters');
+} else {
+    console.log('wow, you both got equally long names, '+hacker1.length+' characters!!');
+}
 
-
+var aux = hacker1.toUpperCase();
+for (var i = 0; i < aux.length; i++) {
+    console.log(aux[i]+' ');    
+}
 // Lorem ipsum generator
