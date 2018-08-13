13da5070554926f88c4d8c67cf993ec2fa6ab315
Douglas DeJong <DougDeJong@Douglass-MacBook-Pro.local>
2018-08-13 14:33:09 -0400

    Doug and Tony's Project Final

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,33 @@
-// Names and Input
+let hacker1 = 'Doug'
+let hacker2 = prompt("Who is the Navigator?")
+let drivLetters = ""
+let navLetters = ""
+let names = [hacker1, hacker2]
 
+console.log('The driver\'s name is', hacker1)
+console.log('The navigator\'s name is', hacker2)
 
-//Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log ('The Driver has the longest name, it\'s',hacker1.length,' characters')
+} else if (hacker1.length === hacker2.length) {
+  console.log('wow, you both got equally long names,',hacker1.length,'characters!')
+} else {
+  console.log('Yo, navigator got the longest name, it has',hacker2.length,'characters!')
+}
 
+for (var i = 0; i < hacker1.length; i++){
+  drivLetters+= hacker1.toUpperCase()[i];
+  drivLetters+= " ";
+}
+console.log(drivLetters)
 
-// Lorem ipsum generator
+for (var i = hacker2.length -1; i >=0; i--) {
+  navLetters+= hacker2[i];
+}
+console.log(navLetters)
+
+sort.names
+
+if (names0 === hacker1){
+  console.log('The Driver\'s name goes first')
+}
