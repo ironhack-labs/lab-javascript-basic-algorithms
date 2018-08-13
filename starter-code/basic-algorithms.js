672b41ef28967acdd570ec341da7ceddee34b019
levijskal <levijskal@icloud.com>
2018-08-13 14:37:36 -0400

    initial commit

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,13 @@
-// Names and Input
+//driver
+let hacker1 = "Arthur"
+console.log("Drivers name is " + hacker1.toUpperCase())
+let hacker2 = prompt("your name please?");
+console.log("navigator is " + hacker2)
 
-
-//Conditionals
-
-
-// Lorem ipsum generator
+if (hacker1.length > hacker2.length) {
+  console.log("driver has a longer name " + hacker1.length)
+} else if (hacker1.length < hacker2.length) {
+  console.log("other has a longer name " + hacker2.length)
+} else {
+  console.log("both have same length " + hacker1.length)
+}
