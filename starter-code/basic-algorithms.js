9a577d101ef07e9553bdb1426ddee843dca6262d
nickborbe <nickborbe@gmail.com>
2018-08-13 14:18:21 -0400

    checks for palindrome in driver name

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -5,5 +5,5 @@
 
 
 // Lorem ipsum generator
-console.log("super important change being made right here")
+console.log("super important change being made here")
 // ;jnve;f ovijef vjef voefj v[oejv]
