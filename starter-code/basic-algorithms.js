a0e3aca46c49003af8837af367fa3425a8f7f233
Arthur Etmekdjian <AEtmekdjian7@gmail.com>
2018-08-13 14:32:49 -0400

    first two itterations of challenge

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,27 @@
 // Names and Input
+let hacker1 = "arthur";
+let hacker2 = "levijs";
+var n = hacker1.length;
 
+console.log("the drivers name is " + hacker1);
+let hacker2 = prompt("what is your name hacker 2");
+console.log("the navigators name is" + hacker2);
 
 //Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log("the driver has the longest name it has" + n + "characters")
+}
+else if (hacker1.length < hacker2.length) {
+    console.log("navigator has the longest name")
+}
+
+else {
+    console.log("they both have the same length")
+}
+
 
 
 // Lorem ipsum generator
+
+
+console.log(hacker1.toUpperCase())
