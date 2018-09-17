2a796a49282bbb1d305bc83b468151b57781d96c
Ester Diaz <ester.diaz.sola@gmail.com>
2018-09-17 18:03:01 +0200

    okey hasta el 6

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,27 @@
 // Names and Input
+var hacker1 = "Ester";
+console.log("The driver's name is " + hacker1);
 
+var hacker2 = "Jose";
+console.log("The navigator's name is " + hacker2);
 
 //Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log("The Driver has the longest name, it has " + hacker1.length + " characters");
+} else if (hacker2.length > hacker1.length) {
+  console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters");
+} else if (hacker2.length === hacker1.length) {
+  console.log("wow, you both got equally long names, " + hacker2.length + " characters!!");
+}
 
-
+//loops
+var nombreSeparado="";
+for(var i=0; i< hacker1.length; i++){
+ if (i===hacker1.length -1){
+  nombreSeparado += hacker1[i].toUpperCase();
+ } else {
+  nombreSeparado += hacker1[i].toUpperCase() + " ";
+ }
+};
+console.log(nombreSeparado);
 // Lorem ipsum generator
