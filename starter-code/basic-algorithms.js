fa0655655b421182e8b688ffbbd2504da74956f5
m-qm <mireiaquerol@gmail.com>
2018-09-18 21:26:21 +0200

    finished exercises

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,6 +1,6 @@
-// Names and Input
+var hacker1 = "Mireia";
 
-var hacker1 = 'Mireia'
+console.log("The driver's name is " + hacker1);
 
 var hacker2 = prompt('What is the Navigators name?')
 
@@ -8,54 +8,52 @@ var hacker2 = prompt('What is the Navigators name?')
 
     console.log('The navigators name is ' + hacker2)
 
+if(hacker1.length > hacker2.length) {
 
-//Conditionals
+  
+    console.log("The driver has the longest name. It has " + hacker1.length + " characters.");
+} else if(hacker2.length > hacker1.length) {
 
-if (hacker1.length > hacker2.length)  {
-    console.log('The driver has the longest name, it has ' + hacker1.length + ' characters');
-
-} else if (hacker2.length > hacker1.length) {
-    console.log('Yo, navigator got the longest name, it has ' + hacker2.length + ' characters')
-
+    console.log("Yo, navigator got the longest name. It has " + hacker2.length + " characters.");
 } else {
-    console.log('wow you both equally long names ' + hacker1.length + ' characters')
+    
+    console.log("Wow! You both got equally long names! They have " + hacker1.length + " characters.");
 }
 
-//Loops
+var theName = []
+for(var i=0; i < hacker1.length; i++) {
+  theName.push(hacker1[i].toUpperCase());
+}
 
-var space1="";
+var theJoinedName = theName.join(' ');
+console.log(theJoinedName);
 
-for (var i = 0; i<hacker1.length; i++) {
-    space1 += hacker1[i].toUpperCase() + " ";
+var hackerReverse = hacker2.split("");
+var reversedStr = hackerReverse.reverse().join("");
+console.log(reversedStr);
 
-        console.log(space1);
+var lowercaseDriverName = hacker1.toLowerCase();
+var lowercaseNavName = hacker2.toLowerCase();
 
-var space2="";
-    for (var i = hacker2.length-1; i>=0; i--){
-        space2 += hacker2[i];
+if(lowercaseDriverName < lowercaseNavName) {
+  console.log("The driver's name goes first.");
+} else if (lowercaseNavName < lowercaseDriverName) {
+  console.log('Yo, the navigator goes first definitely.');
+} else {
+  console.log("What?! You both got the same name.");
+}
+
+if(checkPalindrome(prompt("Is this a palindrome?").replace(/[^a-zA-Z]/g, ""))) {
+  console.log("This is a palindrome");
+} else {
+  console.log("This is NOT a palindrome");
+}
+  
+function checkPalindrome(str) {
+  for(j = 0; j < str.length; j++) {
+    if(str[j] !== str[str.length - j - 1]) {
+      return false;
     }
-    console.log(space2)
-
-var hacker1Length = 0;
-
-for (var i=0; i<hacker1.length; i++) {
-    if (hacker1[i] === hacker2[i]);
-    hacker1.length++;
-    if (hacker1Length==hacker1.length); {
-        console.log('What?! You both got the same name?')
-    }
-        } elseif (hacker1[i] < hacker2[i]); {
-            console.log('The drivers name goes first')
-
-        } elseif (hacker1[i] > hacker2[i]); {
-            console.log('Yo, the navigator goes first definitely')
-        }
-
-    }
-// Lorem ipsum generator
-
-var newString = prompt('Insert a string here')
-
-for (var i = newString.lenght-1; i>=0; i--){
-    console.log(newString)
+  }
+  return true;
 }
