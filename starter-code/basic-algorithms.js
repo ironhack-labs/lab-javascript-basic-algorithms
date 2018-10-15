3ad5a646a7a0e850040502440fc75bec1d2677f2
Romulo Franca <romulo2franca@gmail.com>
2018-10-15 20:09:26 -0300

    v2.0

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -3,10 +3,13 @@ let hacker1 = "Romulo";
 console.log("The driver's name is " + hacker1);
 let hacker2 = process.argv[2];
 console.log("The navigator's name is " + hacker2);
+let stringInit = process.argv[3];
+let isPalindrome = "";
 let hacker1Size = hacker1.length;
 let hacker2Size = hacker2.length;
 let drive = "";
 let navigator = "";
+
 //Conditionals
 if (hacker1Size > hacker2Size) {
   console.log(
@@ -46,5 +49,28 @@ if (hacker1[0] === hackerSort[0] && hacker1[0] != hacker2[0]) {
 } else {
   console.log("What?! You both got the same name?");
 }
-
+for (var i = 0; i < stringInit.length; i++) {
+  if (stringInit[i].charCodeAt() >= 65 && stringInit[i].charCodeAt() <= 90) {
+    isPalindrome += stringInit[i];
+  } else if (
+    stringInit[i].charCodeAt() >= 97 &&
+    stringInit[i].charCodeAt() <= 122
+  ) {
+    isPalindrome += stringInit[i];
+  }
+}
+var tmp = "";
+for (var i = isPalindrome.length - 1; i >= 0; i--) {
+  tmp += isPalindrome[i];
+}
+if (tmp.toLowerCase() == isPalindrome.toLowerCase()) {
+  console.log(isPalindrome.toLowerCase());
+}
 // Lorem ipsum generator
+let paragraph =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et lectus eget quam finibus vehicula. Mauris mollis volutpat elit eu aliquam. Aliquam elementum augue lacus, nec interdum urna luctus non. Suspendisse auctor dolor enim, et placerat justo dapibus a. Praesent mi mi, suscipit in tincidunt ac, auctor sed quam. Nunc ut diam vel dui pharetra commodo ac vel turpis. Nulla vitae metus porttitor, laoreet dui in, vulputate dolor. Nulla ut nisl sed libero pretium volutpat. Nam tincidunt libero ligula, eget euismod elit accumsan at.Nulla facilisi. Quisque varius viverra pulvinar. Integer suscipit volutpat consectetur. Donec dapibus nisi elit, sit amet facilisis lectus feugiat at. Morbi sit amet lobortis turpis. Praesent aliquam sed velit eget consequat. Vivamus et elementum orci. Fusce sed tellus lacinia sapien elementum dapibus nec sit amet leo. Donec posuere ipsum nec ligula porttitor suscipit non quis ex. Cras malesuada sagittis mattis. Integer eleifend orci diam, non hendrerit erat tincidunt non. Mauris at dui sit amet nulla viverra porta in ut magna. Aenean pretium molestie sapien, quis venenatis lacus pellentesque quis. Ut tempor quam justo, a cursus leo accumsan vestibulum. Nulla eget mollis lectus, nec facilisis ante. Donec vestibulum enim sed ipsum accumsan, non rhoncus tellus bibendum. Phasellus non tempor sapien, ac ultrices risus. Phasellus et feugiat massa. Nullam placerat tellus lectus, et viverra turpis aliquet at. Quisque et eleifend velit. Curabitur placerat fermentum velit, quis suscipit metus luctus non. Aliquam accumsan rhoncus odio ultrices fringilla. Nam id feugiat metus.";
+
+console.log("Qt de palavras " + paragraph.split(" ").length);
+
+let count = (paragraph.match(/et/g) || []).length;
+console.log("Quantidade de et: "+ count);
