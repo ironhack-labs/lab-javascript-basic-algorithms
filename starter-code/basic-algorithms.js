02d758c165a27cc7625dc7f799a2d333519dce47
Romulo Franca <romulo2franca@gmail.com>
2018-10-15 18:47:20 -0300

    v1.1

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -3,32 +3,32 @@ let hacker1 = "Romulo";
 console.log("The driver's name is " + hacker1);
 let hacker2 = process.argv[2];
 console.log("The navigator's name is " + hacker2);
+let hacker1Size = hacker1.length;
+let hacker2Size = hacker2.length;
+let drive = "";
+let navigator = "";
 //Conditionals
-if (hacker1.length > hacker2.length) {
+if (hacker1Size > hacker2Size) {
   console.log(
-    "The Driver has the longest name, it has " + hacker1.length + " character"
+    "The Driver has the longest name, it has " + hacker1Size + " character"
   );
-} else if (hacker1.length < hacker2.length) {
+} else if (hacker1Size < hacker2Size) {
   console.log(
-    "Yo, navigator got the longest name, it has " +
-      hacker2.length +
-      " character"
+    "Yo, navigator got the longest name, it has " + hacker2Size + " character"
   );
 } else {
   console.log(
-    "wow, you both got equally long names" + hacker1.length + " character!!"
+    "wow, you both got equally long names" + hacker1Size + " character!!"
   );
 }
 
-let drive = "";
-let navigator = "";
-for (var i = 0; i < hacker1.length; i++) {
+for (var i = 0; i < hacker1Size; i++) {
   drive += hacker1[i].toUpperCase();
   drive += " ";
 }
 console.log(drive);
 
-for (var i = hacker2.length - 1; i >= 0; i--) {
+for (var i = hacker2Size - 1; i >= 0; i--) {
   navigator += hacker2[i];
 }
 console.log(navigator);
@@ -37,8 +37,14 @@ let hackersFirstLetter = [];
 hackersFirstLetter[0] = hacker1[0];
 hackersFirstLetter[1] = hacker2[0];
 
+let hackerSort = hackersFirstLetter.sort();
 
-console.log(hackersFirstLetter.sort());
-
+if (hacker1[0] === hackerSort[0] && hacker1[0] != hacker2[0]) {
+  console.log("The driver's name goes first");
+} else if (hacker2[0] === hackerSort[0] && hacker1[0] != hacker2[0]) {
+  console.log("Yo, navigators's goes first definitely");
+} else {
+  console.log("What?! You both got the same name?");
+}
 
 // Lorem ipsum generator
