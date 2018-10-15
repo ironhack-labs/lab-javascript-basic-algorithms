302ae63d4f851e3cb392429d77d03a94475b4208
priyaprafful <priyankamaheshwari.2021@gmail.com>
2018-10-15 17:15:42 +0300

    done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,4 +1,22 @@
 // Names and Input
+var hacker1 = "filippo";
+console.log("The driver's name is " + hacker1);
+var hacker2 = prompt("what is navigator's name");
+console.log ("The navigator's name is " + hacker2);
+if(hacker1.length > hacker2.length){
+  console.log("The Driver has the longest name,it has " +hacker1.length+" characters");
+
+}
+else if (hacker1.length < hacker2.length){
+  console.log("Yo navigator got the longest name, it has" +hacker2.length+" characters");
+}
+else {
+  console.log("wow,you both got equally long names,"+hacker1.length+" characters");
+}
+console.log(hacker2.toUpperCase().split("").join(' '));
+console.log(hacker2.split("").reverse().join(""));
+
+
 
 
 //Conditionals
