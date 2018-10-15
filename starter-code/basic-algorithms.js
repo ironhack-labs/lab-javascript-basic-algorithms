d9a2c9e180ebab24876ccbd23b7dbeed461945ef
Gabriel Sicuto <gabrielsicuto@gmail.com>
2018-10-15 19:46:19 -0300

    Final da atividade sem bonus

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,5 @@
 // Names and Input
-let driver ='Gabriel Sicuto';
+let driver ='Gabriel';
 console.log('The driver\'s name is: ' + driver);
 //console.log('Digite o nome do navegador:');
 let navigator =  process.argv[2];
@@ -14,16 +14,50 @@ if(driver.length > navigator.length){
 else if (driver.length < navigator.length){
   console.log('The navigator has the longest name, it has ' + navigator.length+ ' characters.');
 }
-else console.log('wow, you both got equally long names, ' + driver.length + 'characters.');
+else console.log('wow, you both got equally long names, ' + driver.length + ' characters.');
 
-let capitalDriver = [];
+//UpperCase driver name
+
+let capitalDriver = new Array();
 
 for(i = 0 ; i < driver.length ; i += 1){
+  //driver = driver.toUpperCase();
   if (driver[i]!== ' '){
-    driver = driver.toUpperCase();
-    capitalDriver.push(driver[i] + ' ');
+
+    capitalDriver.push(driver[i]);
+    
   } 
 }
+capitalDriver = capitalDriver.join(' ');
+console.log(capitalDriver.toUpperCase());
 
-console.log(capitalDriver.toString());
+// Navigator name reverse
+reverseNavigator = navigator.split('').reverse().join('');
+
+console.log(reverseNavigator);
+//lexicographic order
+
+if(driver === navigator){
+
+  console.log('What?! You both got the same name?');
+
+}
+else{
+  for(i=0 ; i < driver.length && i < navigator.length ; i+=1)
+  {
+    if(driver[i].toUpperCase() < navigator[i].toUpperCase()){
+      console.log('The driver\'s name goes first');
+      break;
+    }
+    if(driver[i].toUpperCase() > navigator[i].toUpperCase()){
+      console.log('Yo, the navigator goes first definitely');
+      break;
+    }
+    
+  }
+}
+
+
+
+
 // Lorem ipsum generator
