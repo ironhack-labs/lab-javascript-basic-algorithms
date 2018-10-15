f398f3762b6440d0e291f22a8380b64ba93767a6
Ismael P. Paiva <ismael.paiva@fatec.sp.gov.br>
2018-10-15 18:00:20 -0300

    primeiro commit

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,29 @@
 // Names and Input
+let hacker1 = 'Ismael';
+console.log("The driver's name is ",hacker1);
 
+let hacker2 = process.argv[2];
+console.log("The navigator's name is ",hacker2);
 
 //Conditionals
+if (hacker1.length > hacker2.length){
+    console.log( "The driver has the longest name, it has ", hacker1.length, "characters");
+} else if(hacker2.length > hacker1.length) {
+    console.log("Yo, the navigator got the longest name, it has", hacker2.length, "characters");
+} else {
+    console.log("wow, you both got equally long names",hacker1.length, "characters");
+} 
 
+let capitals = [];
 
+for(i=0; hacker1.length > i; i += 1){
+    capitals.push(hacker1[i], ' ');
+}
+console.log(capitals);
+
+let reverse = [];
+for(i=0; hacker1.length > i; i += 1){
+    reverse.unshift(hacker2[i]);
+}
+console.log(reverse);
 // Lorem ipsum generator
