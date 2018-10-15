14fc4e4ec7f85ce6e014220214cb577f000b4ba6
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-10-15 17:09:32 +0200

    done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,15 @@
-// Names and Input
+console.log("WHILE LOOP ---------");
+var number = 99;
 
+while (number >= 1) {
+  console.log(number);
 
-//Conditionals
+  number -= 1;
+}
 
+// --------------------------------------------
 
-// Lorem ipsum generator
+console.log("FOR LOOP ---------");
+for (var number = 99; number >= 1; number -= 1) {
+  console.log(number);
+}
