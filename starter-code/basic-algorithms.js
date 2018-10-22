1b35e81c148bc9e69e5b8f06aab108a1ce62b08b
Travis Grubbs <flygrubbs@yahoo.com>
2018-10-22 15:44:38 -0400

    pairing asgn

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,27 @@
-// Names and Input
 
+
+// Names and Input
+let hacker1 = "Travis"
+console.log("The drivers name is " +hacker1)
+let navigator = "Javier"
+console.log("The navigators name is " +navigator)
 
 //Conditionals
+if (hacker1.length > navigator.length) {
+  console.log(`The driver has the longest name, it has ` + hacker.length + 'characters!');
+} else if (hacker1.length < navigator.length) {
+  console.log('The Navigator has the longest name, it has ' + navigator.length + 'characters');
+} else if (hacker1.length === navigator.length) {
+  console.log('Wow! you both got equally long names, ' +hacker1.length + ' characters');
+}
 
 
 // Lorem ipsum generator
+
+console.log(hacker1.toUpperCase().split(''))
+
+function reverse(str){
+  return str.split("").reverse().join("");
+}
+console.log(reverse(navigator));
+
