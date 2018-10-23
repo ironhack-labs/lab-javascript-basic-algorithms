ef2608febc5f9c4b1deb9daf298a1ea7236ef6f5
Kamila Purymska <kamilapurymska@MacBook-Air-Kamila.local>
2018-10-23 09:24:05 +0200

    added files

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,23 @@
-// Names and Input
+var hacker1= "Tom"
+console.log("The driver's name is " + hacker1)
+
+var hacker2=window.prompt('What is your name?');
+console.log("The navigator's name is " + hacker2)
+
+if (hacker1.length > hacker2.length){
+    console.log('The Driver has the longest name, it has ' + hacker1.length + ' characters');
+} else if (hacker1.length < hacker2.length) {
+    console.log('Yo, navigator got the longest name, it has ' + hacker2.length +' characters');
+} else if (hacker1.length === hacker2.length); {
+    console.log("wow, you both got equally long names, " + hacker1.length + " characters!!");
+};
 
 
-//Conditionals
+for (var i=0; i<hacker1.length; i++);
+console.log(hacker1.toUpperCase());
+console.log()
 
 
-// Lorem ipsum generator
+for (var i=0; i<hacker1.length; i--);
+debugger;
+
