68cc63635dfd765c74fc2ecfd8424f0300f2b9f0
cecilia <blasterkat101@gmail.com>
2018-10-23 03:55:01 -0300

    what i managed to finish, names-input-conditionals-loop-firstpart

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,22 @@
 // Names and Input
+var hacker1 = 'Cecilia';
+var hacker2 = window.prompt('What\'s the navigator\'s name?');
 
+console.log('The driver\'s name is ' + hacker1);
+console.log('The navigator\'s name is ' + hacker2);
 
-//Conditionals
+// Conditionals
+if (hacker1.length > hacker2.length ) {
+    console.log('The driver has the longest name, it has ' + hacker1.length + ' characters');
+} else if (hacker1.length < hacker2.length ) {
+    console.log('Yo, navigator got the longest name, it has ' + hacker2.length + ' characters');
+} else if (hacker1.length === hacker2.length) {
+    console.log('Wow, you both got equally long names, ' + hacker2.length + ' characters!!');
+};
 
+// Loops
+for (var i = 0; i < hacker1.length; i++) {
+    console.log(hacker1[i].toUpperCase());
+}
 
 // Lorem ipsum generator
