c6bfe486eabe4ad141a87de127796bc1899a1234
Robby van Rheenen <robbyvanrheenen@gmail.com>
2018-10-23 09:30:08 +0200

    first attempt

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,31 @@
-// Names and Input
+'use strict';
+
+// Names and input
+var hacker1 = 'Robby';
+
+console.log("The driver's name is " + hacker1 + ".");
+
+var hacker2 = window.prompt("What's the navigator's name?");
+
+console.log("The navigator's name is " + hacker2 + ".");
+
+// Conditionals
 
 
-//Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log('The Driver has the longest name, it has ' + hacker1.length + ' characters');
+} else if (hacker2.length > hacker.length) {
+  console.log('Yo, navigator got the longest name, it has ' + hacker2.Length + ' characters');
+} else {
+  console.log('wow, you both got equally long names, ' + hacker1.length + ' characters!!');
+}
 
+// Loops
+var capitalized = '';
 
-// Lorem ipsum generator
+for (i = 0; i < hacker1.length; i++) {
+  capitalized = capitalized + hacker1[i] + ' ';
+}
+capitalized.toUpperCase();
+
+console.log(capitalized);
