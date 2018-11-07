f37f8fac1e7480ec2aca6a920dd2e9b86f6eaaea
Mariana Macedo Rodrigues da Silva <mariana.macedo@anbima.com>
2018-11-07 20:24:43 -0200

    falta só o da ordenação

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -13,21 +13,27 @@ if (driver.length > navigator.length) {
 }
 
 // Loops
+//Space and capitals letter
 var spacedDriver = '';
 for (var i = 0; i < driver.length; i++) {
     spacedDriver += `${driver[i]} `;
 }
 console.log(spacedDriver.toUpperCase());
 
+//Name reverse
 var reverseNavigator = '';
 for (var i = navigator.length - 1; i >= 0; i--) {
     reverseNavigator += navigator[i];
 }
 console.log(reverseNavigator);
 console.log(" ");
+
+//Lexicographic order
+
+
 //Bonus
 //Palindrome
-var str = 'Al lets Della call Ed “Stella.”';
+var str = `No 'x' in Nixon`;
 var wrapStr = '';
 for (var i = 0; i < str.length; i++) {
     if (str[i].match(/[a-z]/gi)) {
@@ -46,18 +52,26 @@ if (wrapStr === reverseStr) {
 }
 console.log(" ");
 //Ex. 10
-var lorem = `A população ela precisa da Zona Franca de Manaus, porque na Zona franca de Manaus, 
-não é uma zona de exportação, é uma zona para o Brasil. 
-Portanto ela tem um objetivo, ela evita o desmatamento, que é altamente lucravito. 
-Derrubar arvores da natureza é muito lucrativo! et`;
+var lorem = `Suspendisse at ipsum tempor, laoreet nisi nec, sodales turpis. 
+Phasellus tempus vestibulum sollicitudin. Nulla facilisi. 
+Donec pulvinar et orci at elementum. Nulla aliquet, felis fringilla eleifend tempor, 
+purus libero ultrices odio, eu laoreet leo quam id neque. Donec convallis luctus purus, 
+eu tincidunt nulla lobortis eu. Praesent fringilla sed turpis vitae molestie. Etiam iaculis 
+nunc quis nibh fermentum hendrerit vitae id felis. Morbi efficitur, nibh vel vulputate rutrum, 
+turpis ex accumsan turpis, sit amet finibus mi eros et est. Donec ipsum arcu, fermentum eget massa
+ sit amet, efficitur blandit quam. Fusce at sollicitudin nibh, in vehicula enim.`;
 
-var loremSplitada = lorem.split(" ");
+var noSpace = lorem.toLowerCase().split(" ");
 var count = 0;
-for(var i = 0; i<loremSplitada.length; i++){
+var count2 = 0;
+for (var i = 0; i < noSpace.length; i++) {
     count += 1;
+    if (noSpace[i].includes('et')) {
+        count2 += 1;
+    }
 }
-console.log(` blafd ${countLatin}`);
 console.log(`The string: 
 "${lorem}" 
-has ${count} words`);
-
+has ${count} words.`);
+console.log(" ");
+console.log(`Latin word 'et' appears ${count2}.`);
