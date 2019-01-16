4f8a20e70dae5e2b623c5df9cf217327614a3018
rpernicone <roberto.pernicone@gmail.com>
2019-01-15 22:22:43 -0600

    done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,10 @@
 // Names and Input
 
+var hacker1 = 'Roberto';
+console.log('The hacker name is ', hacker1);
+
+var hacker2 = 'Dylan';
+console.log('The navigators name is', hacker2);
 
 //Conditionals
 
