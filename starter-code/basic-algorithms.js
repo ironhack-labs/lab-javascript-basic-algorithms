e785a841f59a1b77cc814f277d5cd068190b95c6
dannbleu <d_stm@hotmail.com>
2019-01-17 14:53:29 -0600

    falto cambiar el orden de hacker2 y ordenar alfabeticamente
    exit
    q

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,25 @@
 // Names and Input
+var hacker1= 'Daniel';
+console.log("The driver's name is:", hacker1);
 
+var hacker2=window.prompt("Type your name please:", "Iron Hacker");
+console.log("The navigator's name is:", hacker2);
 
 //Conditionals
 
+if(hacker1.length==hacker2.length){
+    console.log("What! you have the same name?");
+  }
+  
+  else if(hacker1.length<hacker2.length){
+    console.log("The navigator's name is the longest");
+  }
+  else{
+    console.log("The driver's name is the longest")
+  }
 
 // Lorem ipsum generator
+
+console.log(hacker1.toUpperCase())
+
+console.log(hacker2)
