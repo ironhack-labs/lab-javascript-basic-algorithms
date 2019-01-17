31f6026af26a849a27e6df3011f7e514fc7b4865
ReyAzucar <reyazucar777@gmail.com>
2019-01-17 17:26:31 -0600

    dia1

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,42 @@
 // Names and Input
+var hacker1 = "Marcos"
+console.log("the drivers name is",hacker1)
+var hacker2 = "Firefox"
+console.log("The navigator's name is", hacker2)
 
+ 
+the drivers name is Marcos
+The navigator's name is Firefox
+=> undefined
 
 //Conditionals
-
+if(hacker1 === hacker2){
+    console.log("The Driver has the longest name, it has 6 characters")
+    }else if (hacker2 = hacker1){
+      console.log("Navigator got the longest name, it has 7 characters")
+    }else {
+      console.log("wow, you both got equally long names, 7 characters!!")
+    }
+    
+Navigator got the longest name, it has 7 characters
 
 // Lorem ipsum generator
+var hacker1 = "M A R C O S"
+console.log(hacker1)
+var hacker2 = "xoferif"
+console.log(hacker2)
+
+switch("Firefox"){
+  case "Marcos":
+  console.log("The driver's name goes first")
+  break;
+  case "Firefox":
+  console.log("the navigator goes first definitely")
+  break;
+  default:
+  console.log("What?! You both got the same name?")
+}
+
+M A R C O S
+xoferif
+the navigator goes first definitely
