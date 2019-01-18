afb340da0c53f220df71de71b3e89fa308080297
Ricardo <unrhs@hotmail.com>
2019-01-17 18:57:21 -0600

    Termine trabajo

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,37 @@
 // Names and Input
+var hacker1;
+hacker = "The driver's name is";
+var _XXXX;
 
+_XXXX = "Ricardo"; 
+
+console.log(hacker + ' ' + _XXXX);
+
+var hacker2; 
+hacker2 = "Alberto"
+let asking = prompt("What's your name?", "The navigator name is" + ' '+ hacker2);
 
 //Conditionals
 
+var edadD = 32;
+var edadN = 38;
+var char = "characters";
+longName1 = "The Driver has the longest name, it has";
+longName2 = "The navigator got the longest name, it has";
+longName3 = "wow, you both got equally long names,";
+
+if(edadD === edadN){
+  console.log(longName3 + ' ' + edadN + ' ' + char)
+}else{
+  if(edadD > edadN){
+    console.log(longName1 + ' ' + edadD + ' ' + char)
+  }else{console.log(longName2 + ' ' + edadN + ' ' + char)}
+}
 
 // Lorem ipsum generator
+
+var spz = " "
+
+
+console.log(_XXXX.toUpperCase() [0] + spz + _XXXX.toUpperCase() [1] + spz + _XXXX.toUpperCase() [2] + spz + _XXXX.toUpperCase() [3] + spz + _XXXX.toUpperCase() [4] + spz + _XXXX.toUpperCase() [5] + spz + _XXXX.toUpperCase() [6]);
+
