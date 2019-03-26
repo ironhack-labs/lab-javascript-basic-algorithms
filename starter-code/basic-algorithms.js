06f1e14b41a3067ebdb252ecf6c937bbdaaf910f
FrankIsmael <ismaelfcom93@gmail.com>
2019-03-25 18:00:00 -0600

    Ejercicio terminado

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,33 @@
-// Names and Input
+let driverName='Ismael'
+console.log(`The drivers is ${driverName}`)
+const navigatorName = prompt("Dime tu nombre")
+console.log(`The navigators name is: ${navigatorName}`)
 
+if(driverName.length > navigatorName.length){
+  console.log(`The Driver has the longest name, it has ${driverName.length} characters`)
+} else if(driverName.length < navigatorName.length){
+  console.log(`Yo, navigator got the longest name, it has ${navigatorName.length} characters`)
+} else{
+  console.log(`wow, you both got equally long names, ${driverName.length} characters!!`)
+}
 
-//Conditionals
+let nuevoName = '';
+for (i =0; i < driverName.length; i++){
+  nuevoName += driverName[i] + ' '
+}
 
+console.log(nuevoName.toUpperCase())
 
-// Lorem ipsum generator
+let nuevoN=''
+for(j=1;j <= navigatorName.length ; j++){
+  nuevoN += navigatorName[navigatorName.length - j]
+}
+console.log(nuevoN)
+
+if(driverName < navigatorName){
+  console.log(`The driver's name goes first`)
+} else if(driverName > navigatorName){
+  console.log(`Yo, the navigator goes first definitely`)
+} else{
+  console.log(`What?! You both got the same name?`)
+}
