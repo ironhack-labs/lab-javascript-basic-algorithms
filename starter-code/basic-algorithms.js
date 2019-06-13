fd1b80077fec02db2cf03ae10449c62a9027eabc
nadia nami abe <abe.nami0510@gmail.com>
2019-06-12 22:17:03 -0300

    exercicio pair programming

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,49 @@
 // Names and Input
+let hacker1 = 'Nami';
+console.log(`The driver's name is ${hacker1}`);
 
+let hacker2 = ('Ricardo');
+console.log(`The navigator's name is ${hacker2}`);
 
 //Conditionals
+if (hacker1.length > hacker2.length){
+    console.log(`The Driver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker1.length < hacker2.length){
+    console.log(`Yo, navigator got the longest name, it has ${hacker2.length} characters`)
+} else {
+    console.log(`wow, you both got equally long names, ${hacker2.length} characters!!`)
+}
 
+let hacker1Upper = "";
 
+for (let i = 0; i < hacker1.length; i++){
+    hacker1Upper += hacker1[i] + " ";
+}
+
+console.log(hacker1Upper.toUpperCase());
+
+let hacker2back = "";
+for (let i = hacker2.length - 1; i >= 0; i--){
+    hacker2back += hacker2[i];
+}
+
+console.log(hacker2back);
+
+if (hacker1Upper.localeCompare(hacker2back)){
+    console.log("The driver's name goes first");
+} else if (hacker2back.localeCompare(hacker1Upper)){
+    console.log("Yo, the navigator goes first definitely");
+} else {
+    console.log("What?! You both got the same name?");
+}
+
+hacker3 = 'Ana';
+
+hacker3.toLowerCase();
+let hacker3Update = "";
+for (let i = hacker3.length - 1; i >= 0; i--){
+    hacker3Update += hacker3[i];
+}
+
+console.log
 // Lorem ipsum generator
