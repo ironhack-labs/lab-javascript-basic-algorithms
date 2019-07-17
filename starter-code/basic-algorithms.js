dc8798731a3d06d9c9e5df1a76a20497dc35642f
Victor Flores <victorflorescastellanos@gmail.com>
2019-07-17 10:17:17 -0500

    Agregue ejercicio de palindromos

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -7,10 +7,6 @@ console.log ("The navigator's name is", hacker2);
 
 var hacker1Length = hacker1.length;
 var hacker2Length = hacker2.length;
-
-
-
-
 //Conditionals
 
 if (hacker1Length > hacker2Length ){
@@ -23,5 +19,32 @@ if (hacker1Length > hacker2Length ){
   else {
     console.log("wow, you both got equally long names,", hacker1Length, "characters!!")
   }
+
+
+// Palindrome Checker
+
+var palindromo = prompt("Please enter a palindrome");
+
+var revPal = palindromo.split("").reverse().join("");
+
+/* 
+  Intenté usar la función .equals pero no supe que estaba haciendo mal entonces mejor puse un switch
+
+  if(palindromo.equals(revPal) == true) {
+    console.log("Palindrome"); 
+  } else {
+    console.log("Not Pelindrome");
+  } 
+
+*/
+
+switch (palindromo) {
+  case revPal:
+  console.log("its a palindrome");
+  break;
+  default:
+  console.log("not a palindrome");
+}
+
 
 // Lorem ipsum generato 
