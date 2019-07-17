dccdb8112d138afc3105593309e4212f727d1445
Victor Flores <victorflorescastellanos@gmail.com>
2019-07-16 22:14:31 -0500

    Terminado

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,27 @@
 // Names and Input
+var hacker1 = "Pancho";
+console.log (hacker1);
+
+var hacker2 = prompt("Please enter the navigator's name");
+console.log ("The navigator's name is", hacker2);
+
+var hacker1Length = hacker1.length;
+var hacker2Length = hacker2.length;
+
+
 
 
 //Conditionals
 
+if (hacker1Length > hacker2Length ){
+    console.log ("The Driver has the longest name, it has", hacker1Length, "characters")
+  }
+    else if (hacker2Length > hacker1Length) {
+      console.log ("Yo, navigator got the longest name, it has ", hacker2Length, "characters")
+    }
+  
+  else {
+    console.log("wow, you both got equally long names,", hacker1Length, "characters!!")
+  }
 
-// Lorem ipsum generator
+// Lorem ipsum generato
