341858b37ac9e94a7c87fbf65cdfe96a3735ecda
Victor Flores <victorflorescastellanos@gmail.com>
2019-07-17 13:49:07 -0500

    Agregué Lorem Ipsum

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -48,3 +48,24 @@ switch (palindromo) {
 
 
 // Lorem ipsum generato 
+
+// Lorem ipsum generator
+
+
+
+var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed finibus quam. Nullam metus nunc, pretium a lectus eu, semper pellentesque nisi. Praesent maximus enim est, et ornare ligula iaculis quis. Phasellus euismod magna dui, ac dictum tortor elementum et. Suspendisse faucibus sapien nec ante eleifend lobortis. Morbi cursus tempor convallis. Donec rutrum erat arcu, at rhoncus dui ultricies eget. Cras a imperdiet velit. \n \n Nam quis eros in enim sagittis consequat. Nunc vitae nulla vitae nisl vestibulum porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam congue gravida leo, posuere condimentum sapien rhoncus eget. Praesent odio lacus, consequat eget rhoncus sed, ullamcorper at felis. Sed euismod, diam vulputate placerat cursus, nunc lectus iaculis augue, a consequat lorem diam a ex. Vestibulum interdum orci purus, eu imperdiet tortor sodales in. Aliquam quis ultricies enim, in lacinia massa. Vivamus rhoncus at erat ac ornare. Maecenas turpis lorem, feugiat sed turpis at, tincidunt rutrum dolor. Curabitur non dapibus metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas lectus lorem, euismod quis commodo at, elementum et elit. \n \n Sed ultricies, diam eget rutrum pharetra, nunc erat tempus orci, eu vulputate lorem ipsum a est. Fusce eget orci facilisis, placerat tellus a, mollis arcu. Morbi rutrum malesuada felis, eu sodales purus congue in. Donec et est ut mi fermentum sodales. Quisque lobortis at eros ac posuere. Donec accumsan mi sed tellus aliquam interdum. Pellentesque ac mauris imperdiet, varius libero ut, euismod libero. Integer ut sapien volutpat, bibendum tellus quis, rutrum metus. Quisque quis enim bibendum, bibendum ante ac, consectetur enim. In tincidunt, enim a tincidunt vestibulum, ligula nibh efficitur neque, non consequat lectus libero ac velit. Ut suscipit eros sed tellus pharetra faucibus."
+
+
+var wordsInTheString = loremIpsum.split(" ").length;
+var paragraphsInTheString = 1 + (loremIpsum.match(/ \n /g) || []).length;;
+var words = wordsInTheString - (paragraphsInTheString - 1) * 2
+var count = (loremIpsum.match(/ et /g) || []).length;
+
+
+if (confirm('want to create a 3 paragraph lorem ipsum?')) {
+    console.log('\n', loremIpsum);
+    console.log('\n','\n',"Generated", paragraphsInTheString,  "paragraphs,", words, "words, 2002 bytes of Lorem Ipsum");
+    console.log('\n', "latin word et appears", count, "times")
+} else {
+    console.log("bye bye")
+}
