6f19f20beddd3ea75f23dce4c1c11a0493b99d31
Ritrex <roalretre@gmail.com>
2019-07-18 20:13:25 -0500

    No entendi el comparelocale

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,44 @@
-// Names and Input
+let nav=hacker2
+let pmenosl=""
+if(driver.length>nav.length){
+console.log("The Driver has the longest name, it has "+driver.length+" characters")
+pmenosl=nav
+} else if(driver.length<nav.length){
+  console.log("Yo, navigator got the longest name, it has "+nav.length+" characters")
+  pmenosl=driver
+} else{
+  console.log("wow, you both got equally long names, "+driver.length+" characters!!")
+  pmenosl=nav
+}
+
+var cap=""
+for(var j=0;j<driver.length;j++)
+  cap+=" "+driver[j].toUpperCase()
+console.log(cap)
 
 
-//Conditionals
+var rev=""
+for(var i=0;i<nav.length;i++)
+  rev+=nav[nav.length-1-i]
 
+let alpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+console.log(rev)
+let d=n=""
+for(l=0;l<driver.length;l++)
+  if(driver[l].match(/[a-z,A-Z]/i))
+    d+=driver[l]
 
-// Lorem ipsum generator
+for(m=0;m<nav)
+
+for(k=0;k<pmenosl.length;k++)
+  if()
+//Bonus
+resb=true 
+for(var h=0;h<palabra.length;h++)
+  if(palabra[palabra.length-1-h]!==plabra[h])
+      
+
+if(resb)
+console.log("la palabra es palindromo")
+else
+console.log("la palabra no es palindromo")
