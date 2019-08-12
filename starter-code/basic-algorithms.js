65ba04e9feb18cb8d46e9599a52828852814db42
Larry Nobles <larrynobles@Larrys-MacBook-Pro.local>
2019-08-12 18:22:54 -0500

    PP FT MEX LARRY LARA

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,24 @@
 // Names and Input
 
+let hacker1 = 'Larry'
+console.log('The driver\'s name is ' + hacker1)
+let hacker2 = prompt('What is your name?')
+console.log('The navigator\'s name is ' + hacker2)
+
+if (hacker1.length > hacker2.length) {
+  console.log('The Driver ' + hacker1 +', has the longest name, it has ' + hacker1.length + ' characters.')
+ } else if (hacker1.length < hacker2.length) {
+   console.log('Yo, navigator ' + hacker2 + ', got the longest name, it has ' + hacker2.length + ' characters.')
+ } else {console.log('Wow, you both got equally long names, ' + hacker1.length + ' characters!!')
+ }
+  
+let hackerUC = hacker1.toUpperCase()
+hackerUC = hacker1.split('').join(' ')
+console.log(hackerUC)
+
+let hackerrev = hacker2.split('').reverse().join('')
+console.log(hackerrev)
+
 
 //Conditionals
 
