e31763e53e7e27c0d5c19e624ecbb6d67a3b72bd
Camilosorio <tech@camilo.fr>
2019-10-16 17:32:18 +0200

    Lab JS basic Algo - done - Nouhad & Camilo - oct 2019

---
+++ Lab-JS-Basic-Algorithms-Nouhad-Camilo.js
@@ -0,0 +1,77 @@
+// Nouhad et Camilo - Iron Hack - oct 19
+
+//Iteration 1 Iteration 1: Names and Input
+
+//==> First driver
+var hacker1 = "Nouhad";
+
+//console.log(`The driver's name is ${hacker1}`);
+
+//==> Second driver
+var hacker2 = "Nouhad";
+
+//console.log(`The driver's name is ${hacker2}`);
+
+//Iteration 2: Conditionals
+
+// if (hacker1.length > hacker2.length) {
+//     console.log(`The driver ${hacker1} has the longest name, it has ${hacker1.length} characters`)
+// } else if (hacker2.length > hacker1.length){
+// console.log(`The driver ${hacker2} has the longest name, it has ${hacker2.length} characters`)
+// } else if (hacker2.length = hacker1.length){
+// console.log(`Wow, you both got equally long names, ${hacker2.length} characters! `)
+// }
+
+//Iteration 3: Loops
+// var str = ""; 
+
+// for (let i = 0; i < hacker1.length; i++) {
+//     str += hacker1.toUpperCase()[i] + " ";
+// }
+
+// console.log(str);
+
+// Iteration 3 - 2e
+// var splitString = hacker2.split(""); 
+
+// var reverseArray = splitString.reverse(); 
+
+// var joinArray = reverseArray.join(""); 
+
+// console.log(joinArray);
+
+//Iteration 3 - 3e
+
+// if (hacker1 > hacker2) {
+//     console.log(`${hacker1} goes first`)
+// } else if (hacker2 > hacker1) {
+//     console.log(`${hacker2} goes first`)
+// } else if (hacker2 = hacker1) {
+//     console.log(`What?! You both got the same name?`)
+// }
+
+//Bonus Time!
+
+var ipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis sem quam. Maecenas sem ex, venenatis vitae risus suscipit, tincidunt gravida neque. Suspendisse semper tempus nunc, vel finibus tellus facilisis vel. Curabitur malesuada dolor felis, ac commodo purus lacinia at. Etiam finibus est sit amet turpis sagittis sagittis. Proin lacinia elementum purus, et dictum leo faucibus finibus. Donec ex quam, aliquet porttitor justo sed, ultricies efficitur dui.
+
+Nullam mauris metus, malesuada a quam eu, ultrices ultricies lorem. Aenean pharetra fringilla facilisis. Donec varius fringilla massa bibendum efficitur. Pellentesque libero risus, imperdiet non sem nec, fringilla scelerisque orci. Aliquam tincidunt ultrices tellus, sit amet pharetra elit volutpat vel. Nunc eu arcu est. In efficitur aliquet diam sed ullamcorper. Fusce pretium urna eget lobortis molestie. Duis a nisl leo. Nam eu auctor odio. Nullam sit amet ex ut lorem sodales laoreet dictum id felis. Donec bibendum tortor magna, ut ullamcorper metus posuere sed.
+
+Phasellus nec consequat libero. Aliquam id viverra ligula. In vitae elit venenatis, interdum tortor eget, egestas dolor. Morbi dignissim viverra consectetur. Quisque eros enim, commodo sed elit vel, congue ultrices urna. Nulla lobortis lacus ac arcu sollicitudin mollis. In at pretium libero. Nullam tempor eleifend dui sed pretium. Ut finibus magna eget diam gravida, vitae facilisis mi ultrices. Vestibulum rhoncus dui turpis, vel vulputate purus vehicula eget. Aenean id massa a ligula hendrerit dignissim. Vestibulum at eleifend massa. Pellentesque ac est in sapien molestie feugiat in in arcu. Curabitur pellentesque, urna et euismod interdum, nisi metus tempor turpis, quis lacinia mauris eros non est. Proin sodales mi eros, ut posuere lectus congue eget. Phasellus eros turpis, convallis sed mattis sed, faucibus eu lectus.`
+
+// Bonus - 1
+
+// function countTotalNumberOfWord() {
+//     return ipsum.split(" ").length-1;
+// }
+
+// console.log(countTotalNumberOfWord())
+
+// Bonus - 2
+
+// function countTotalNumberOfWord() {
+//     return ipsum.split(" et ").length-1;
+// }
+
+// console.log(countTotalNumberOfWord())
+
+//end
