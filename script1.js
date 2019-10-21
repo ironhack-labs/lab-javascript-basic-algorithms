ec5ac55637e648920890853d0ece48391132168e
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-21 14:24:59 -0400

    1.3 Create a variable hacker2 with the navigator's name.

--- script1.js
+++ script1.js
@@ -3,3 +3,6 @@ let hacker1 = "Juan";
 
 // 1.2 Print "The driver's name is XXXX".
 console.log("The driver's name is " + hacker1);
+
+// 1.3 Create a variable hacker2 with the navigator's name.
+let hacker2 = "Snow";
