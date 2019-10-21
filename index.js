a7afbf8291fd06f75139577b74b90c3a5eb61a7d
Daphne <daphne.feresin@gmail.com>
2019-10-21 14:57:06 -0400

    more changes to js file

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const hacker1 = "Daphne";
 const hacker2 = "Mike";
 
 console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+
+if(hacker1.length > hacker2.length){
+    console.log(`The Driver has the longest name, it has ${hacker1.length} characters`);
+}
+else if (hacker1.length === hacker2.length){
+    console.log(`Wow, you both got equally long names, ${hacker1.length} characters!`);
+}
+else{
+    console.log(`Yo, navigator got the longest name, it has ${hacker2.length} characters`);
+
+}
