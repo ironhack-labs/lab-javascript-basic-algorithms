2fa9b5d0d43abb8158e050643ad5329187cf77fd
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-21 14:26:33 -0400

    1.4 Print The navigator's name is YYYY.

--- script1.js
+++ script1.js
@@ -6,3 +6,6 @@ console.log("The driver's name is " + hacker1);
 
 // 1.3 Create a variable hacker2 with the navigator's name.
 let hacker2 = "Snow";
+
+// 1.4 Print "The navigator's name is YYYY".
+console.log("The driver's name is " + hacker2);
