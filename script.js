43b89e606b9b6b415a504109ba978da506505db2
robbiesanchez <rfsanchez004@gmail.com>
2019-10-21 15:52:13 -0400

    hello

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ else if (hacker2.length > hacker1.length) {
 else {
   console.log('Wow, you both got equally long names, ' + hacker1.length + ' characters!')
 }
+
+let hack = '';
+for (let i=0; i < hacker1.length; i++) {
+  let upper = hacker1[i].toUpperCase();
+  hack += upper + ' ';
+}
+console.log(hack);
+
+let hack2 = '';
+for (let i = hacker2.length-1; i >= 0; i--) {
+  let lower = hacker2[i];
+  hack2 += lower;
+}
+console.log(hack2);
+
+if (hacker1.localeCompare(hacker2) < 0 ) {
+  console.log(hacker1 + "'s name goes first.");
+} else if (hacker1.localeCompare(hacker2) > 0) {
+  console.log("Yo, " + hacker2 + " goes first definetly.");
+} else ("What?! You both got the same name?")
