1ccf17823d5add1bf9e941d19744e55f1a7dcecd
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-10-21 15:22:38 -0400

    fixing the file

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("I'm ready.");
+const hacker1 = "Gil";
+console.log(`The driver name is ${hacker1}.`);
+const hacker2 = "Gil";
+console.log(`The navigator name is ${hacker2}`);
+
+hacker1.length > hacker2.length
+  ? console.log(`The driver has the longest name`)
+  : hacker2.length > hacker1.length
+  ? console.log(`The navigator name is longer.`)
+  : console.log(`Equal`);
+
+console.log([...hacker1].join(" "));
+console.log([...hacker2].reverse().join(""));
+
+hacker1 > hacker2
+  ? console.log(hacker1)
+  : hacker2 > hacker1
+  ? console.log(hacker2)
+  : console.log(`equal`);
+
+let par1 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+let words = 0;
+par1.split(" ").forEach(word => {
+  words += 1;
+});
+console.log(words);
+
+let etTimes = 0;
+par1.split(" ").map(word => {
+  if (word === "et") etTimes += 1;
+});
+console.log(etTimes);
