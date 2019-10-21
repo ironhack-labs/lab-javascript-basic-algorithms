ae9b181e3394d1e54045aa7659d7317070fcc1f0
Robbie Sanchez <robbiesanchez@Mellys-MacBook-Air.local>
2019-10-21 14:37:41 -0400

    hello

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('hello')
+
+for(let i=1; i<5; i++) {
+    console.log(i);
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+let hacker1 = 'Robbie';
+
+console.log('The drivers name is ' + hacker1);
+
+let hacker2 = 'Nick';
+
+console.log('The navigators name is ' + hacker2);
