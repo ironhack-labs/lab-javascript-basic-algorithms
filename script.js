5f044d09dfa4c86b7a8df7631e1dac3d23fe2f5b
robbiesanchez <rfsanchez004@gmail.com>
2019-10-21 14:50:47 -0400

    hello

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("I'm ready!");
+
 let hacker1 = 'Robbie';
 
 console.log('The drivers name is ' + hacker1);
@@ -5,3 +7,13 @@ console.log('The drivers name is ' + hacker1);
 let hacker2 = 'Nick';
 
 console.log('The navigators name is ' + hacker2);
+
+if (hacker1.length > hacker2.length) {
+  console.log(hacker1 + ' has the longest name, it has ' + hacker1.length + ' characters')
+}
+else if (hacker2.length > hacker1.length) {
+  console.log('Yo, ' + hacker2 + ' got the longest name, it has ' + hacker2.length + ' characters')
+}
+else {
+  console.log('Wow, you both got equally long names, ' + hacker1.length + ' characters!')
+}
