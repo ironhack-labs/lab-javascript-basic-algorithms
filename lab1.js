45d7443aa3debd730f9dc09c2cfe4d895ff53c24
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-10-25 12:58:54 -0400

    Step-2 - Create conditional statements to check for longest characters

--- lab1.js
+++ lab1.js
@@ -6,3 +6,16 @@ console.log(`The driver's name is ${hacker1}`);
 
 const hacker2 = "Ashraf";
 console.log(`The navigator's name is ${hacker2}`);
+
+
+// //Iteration 2: Conditionals
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, ${hacker1.length} characters`);
+  } else if (hacker2.length > hacker1.length) {
+    console.log(`Yo, navigator got the longest name, it has ${hacker2.length} characters`);
+  } else {
+    console.log(`Wow, you both got equally long names, ${hacker1.length} characters`);
+  }
+
+  
