779352684deefba2cbe5e8550d53ff16b082bf08
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-10-25 12:28:34 -0400

    1st Step - Creating names and print to the console

---
+++ lab1.js
@@ -0,0 +1,8 @@
+// //Lab-1
+
+// //Iteration 1: Names and Input
+const hacker1 = "Ashraf";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Kevin";
+console.log(`The navigator's name is ${hacker2}`);
