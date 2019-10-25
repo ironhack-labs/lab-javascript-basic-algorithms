65b306928da8a03677eb6bf17b496abe9c4b8165
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-10-25 15:31:16 -0400

    Step-4 - Create a progrom to count a word in the given string

--- lab1.js
+++ lab1.js
@@ -45,3 +45,19 @@ if (lexi === -1) console.log("The driver's name goes first");
  else if (lexi === 1) console.log("The navigator's name goes first");
  else if (lexi === 0) console.log("What?! You both have the same name?");
 // so we get: "The navigator's name goes first".
+
+// // 4. Bonus Time!
+//Create a program that counts word "et" in the string.
+//1.Create a string:
+let text = "";
+//2.Ganerate lorem and insert to the string:
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a mi ante. Pellentesque sit amet erat neque. Maecenas interdum augue ut euismod iaculis. Fusce luctus tellus ut nisi aliquet semper sit amet ultrices metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et viverra risus. Aenean vel rutrum massa. Curabitur sed nibh nec nisl luctus pharetra sed eu neque. Fusce quis tortor metus. Quisque metus ipsum, semper in neque a, vehicula sollicitudin ex. Morbi sed est posuere, scelerisque quam consequat, mattis ex. Integer bibendum scelerisque dui. Vestibulum consequat dapibus semper. Cras fermentum orci eget ante facilisis lacinia. Maecenas augue elit, pellentesque at urna quis, tristique ultricies eros. Mauris ac est eros."
+//3.Create loop to count "et":
+let count = 0;
+for(let i = 0; i < text.length; i++){
+  let wordCount = text[i] + text[i+1];
+  if (wordCount === "et"){
+    count++;
+  }
+}
+console.log(`The number of word 'et' in the string is: ${count}`);
