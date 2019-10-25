ed9ea1d9d84e46458bc25e38408daa82cedf2f5c
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-10-25 17:53:46 -0400

    Test for self work branch

---
+++ self.js
@@ -0,0 +1,26 @@
+
+// Ask the user for a new string and check if it's a *palindrome*. 
+// Examples of palindromes:
+
+// "A man, a plan, a canal, Panama!"
+// "Amor, Roma"
+// "race car"
+// "stack cats"
+// "step on no pets"
+// "taco cat"
+// "put it up"
+// "Was it a car or a cat I saw?" and "No 'x' in Nixon".
+
+let str ="race car";
+let restr = str.split('').reverse().join('');
+console.log(restr);
+
+if(str === restr) console.log(true);
+else console.log(false);
+
+str ="step on no pets";
+restr = str.split('').reverse().join('');
+console.log(restr);
+
+if(str === restr) console.log(true);
+else console.log(false);
