b24f34bc7a267285f9eda8a63cf9f4169703f679
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-10-25 12:33:56 -0400

    Edit step-1

--- lab1.js
+++ lab1.js
@@ -1,8 +1,8 @@
 // //Lab-1
 
 // //Iteration 1: Names and Input
-const hacker1 = "Ashraf";
+const hacker1 = "Kevin";
 console.log(`The driver's name is ${hacker1}`);
 
-const hacker2 = "Kevin";
+const hacker2 = "Ashraf";
 console.log(`The navigator's name is ${hacker2}`);
