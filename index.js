270ec9adb55386c8e1936326edc94bf5060c7b0e
Carlos Osorio <carlos.osorio005@gmail.com>
2019-10-25 20:18:57 -0400

    Iteration 2 complete

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let hacker1 = 'Humberto';
 
 let hacker2 = 'Carlos';
 // console.log(`The navigator's name is ${hacker2}`);
+
+if (hacker1.length > hacker2.length)
+  {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+  }
+  else if (hacker1.length < hacker2.length)
+  {
+    console.log(`Yo, navigator got the longest name, it has ${hacker2.length} characters`);
+  }
+  else if (hacker1.length = hacker2.length)
+  {
+    console.log(`Wow, you both got equally long names, ${hacker1.length} characters!`);
+  }
