d391045d3fa6b4d59cb3627b7c009f9b507e24b9
Veronica <veronica1506@gmail.com>
2019-10-25 21:58:23 -0400

    Add Iteration-3 Loops

--- practice-lab.js
+++ practice-lab.js
@@ -1,17 +1,48 @@
 // Iteration 1: Names and Input
 
+// 1.1 
 let hacker1 = "Veronica";
-let hacker2 = "Michel";
-
+// 1.2
 console.log(`The driver's name is ${hacker1}`)
+// 1.3
+let hacker2 = "Michel";
+// 1.4
 console.log(`The navigator name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
+// 2.1
 if (hacker1.length > hacker2.length) console.log(`The driver's name goes first`) 
 else if (hacker2.length > hacker1.length) console.log(`Yo, the navigator goes first definitely`);
 else console.log(`What you got the same name?`)
 
+// Iteration 3: Loops
+
+// 3.1
+let name = "Veronica"
+let newName = "";
+console.log(name.split('').join(' ').toUpperCase())
+// 3.2 
+console.log(name.split('').reverse().join(''))
+for (let i = name.length ; i >= 0; i--) {
+  newName += name.charAt(i)
+}
+console.log(newName)
+// 3.3
+let name1 = "Veronica";
+let name2 = "Michel";
+
+let namesArray = [];
+namesArray.push(name1);
+namesArray.push(name2);
+namesArray.sort()
+if (namesArray[0] === namesArray[1]) {
+  console.log(`What?! You both got the same name?`)
+} else if (namesArray[0] === name1) {
+  console.log(`The drivers name goes first`)
+} else if (namesArray[0] === name2) {
+  console.log(`Yo, the navigator goes first definitely`)
+  
 // Bonus Time!
 
 let pharagraphs = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at luctus enim, et rutrum augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus nisl eros, fermentum eget magna vel, auctor iaculis urna. Integer nec feugiat velit, nec placerat augue. Fusce finibus mauris ac semper elementum. Nullam dignissim orci quis leo cursus malesuada. Aenean euismod, justo sit amet venenatis iaculis, orci odio imperdiet sem, eget lacinia dolor leo vel sapien. Ut in aliquam mauris. Integer est purus, euismod ac mattis in, volutpat eget nisi. Vivamus rhoncus sagittis lobortis.
