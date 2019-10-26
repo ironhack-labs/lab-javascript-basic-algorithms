4bdf837dfb5b7aafee3df3df925972b785a4a3c3
esrfree <esrfree@gmail.com>
2019-10-25 21:07:28 -0400

    Iteration 1: Names and Input

---
+++ index.js
@@ -0,0 +1,6 @@
+// Iteration 1: Names and Input
+let hacker1 = 'Delio';  //driver
+let hacker2 = 'Erick';  //navigator
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
