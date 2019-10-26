882887cfd0ca01dde800019b18e9229d6254ebc0
Humberto Buniotto <hbuniotto@gmail.com>
2019-10-26 09:59:27 -0400

    Iteration 1: Numers and Input

---
+++ index.js
@@ -0,0 +1,15 @@
+let hacker1 = 'Humberto';
+// console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = 'Carlos';
+// console.log(`The navigator's name is ${hacker2}`);
+
+  if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+  }
+  else if (hacker1.length < hacker2.length) {
+    console.log(`Yo, navigator got the longest name, it has ${hacker2.length} characters`);
+  }
+  else if (hacker1.length = hacker2.length) {
+    console.log(`Wow, you both got equally long names, ${hacker1.length} characters!`);
+  }
