9558390447b05abe27c73c704d48e9a6d14a92e9
Humberto Buniotto <hbuniotto@gmail.com>
2019-10-27 16:52:06 -0400

    Iteration 3: Loops

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ let hacker2 = 'Carlos';
   else if (hacker1.length = hacker2.length) {
     console.log(`Wow, you both got equally long names, ${hacker1.length} characters!`);
   }
+
+// Make letters uppercase and add space between letters  
+console.log (hacker1.toUpperCase().split('').join(' '));
+
+// Print navigator's name backwards
+console.log (hacker2.split('').reverse().join(''));
+
+// lexicographic order
+let compare = hacker1.localeCompare(hacker2);
+
+if (compare == 1) {
+  console.log (`Yo, the navigator goes first definitely.`);
+}
+else if (compare == -1) {
+  console.log (`The driver's name goes first.`);
+}
+else {
+  console.log (`What?! You both got the same name?`);
+}
