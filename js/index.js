68c40079e85f84c233274d4a30bce5d6261e757e
pelinski <teresapelinski@gmail.com>
2019-11-06 23:41:22 +0100

    iteration1 finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 // Iteration 1: Names and Input
+var hacker1 = "Teresa";
+console.log("The driver's name is",hacker1);
+var hacker2 = "Pepita";
+console.log("The navigator's name is",hacker2);
 
 
 // Iteration 2: Conditionals
