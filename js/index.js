d84fcfacf80b4e913b2917d3d32426dbff5b939e
Luismi Feijoo <luism.feijoo26@gmail.com>
2019-11-06 22:52:04 +0100

    Finish iteration 2

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ if(hacker1.length > hacker2.length) {
   console.log("The driver has the longest name, it has" ,hacker1.length ,"characters.")
 } else if (hacker1.length < hacker2.length){
   console.log("It seems that the navigator has the longest name, it has", hacker2.length ,"characters.")
+} else {
+  console.log("Wow, you both have equally long names,", hacker1.length,  "characters!")
 }
-
 // Iteration 3: Loops
