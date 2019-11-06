15b050b83cbb4ba339c7bc4234deda8c78e84ae7
David Garcia <dgarciabravo13@gmail.com>
2019-11-06 21:14:52 +0100

    print the names

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 // Iteration 1: Names and Input
+console.log("I'm ready!");
+let haker1 = "Pepito";
+console.log("The driver's name is",haker1);
+let haker2 = "David";
+console.log("The navigator's name is",haker2);
 
 
 // Iteration 2: Conditionals
