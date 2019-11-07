623ee0469166322021234a9dae1cc3b1e174a462
Victor <vicham01@ucm.es>
2019-11-07 20:14:33 +0100

    Iteration 2 done

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@
 
 
 // Iteration 2: Conditionals
-
+let msg;
+if (hacker1.length > hacker2.length) {
+  msg = `The driver has the longest name, it has ${hacker1.length} characters.`
+} else if (hacker1.length < hacker2.length) {
+  msg = `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+}
+else {
+  msg = `Wow, you both have equally long names, ${hacker1.length} characters!`
+}
 
 // Iteration 3: Loops
