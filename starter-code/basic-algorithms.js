518a88f0802fdeadac36ad2738471be440399cbb
Javi L <jlopezro@gmail.com>
2019-11-08 00:55:37 +0100

    Iteration 7 done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -5,8 +5,15 @@
 
 // Loops
 
+  //Iteration 6
+
 let upperCase = hacker1.split(/(?=[a-z])/).join(" ");
 
 console.log (upperCase.toUpperCase());
+
+  //Iteration 7
+
+console.log(hacker2.split(/(?=[a-z])/).reverse().join(''));
+
 
 // Lorem ipsum generator
