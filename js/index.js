0426a7be34d97ee3934f775c51ef9c0e1b198845
Román Méndez <mendezbeck@gmail.com>
2019-11-07 02:11:03 +0100

    iteration1 done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = "Nacho";
+let hacker2 = "Roman";
 
+console.log("The driver's name is", hacker1);
+console.log("The navigator's name is", hacker2);
 
 // Iteration 2: Conditionals
 
