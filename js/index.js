179cf26bad2f97b3724f52decc292d40ec12f173
Ana Segarra <anasegarraber@gmail.com>
2019-11-07 16:51:26 +0100

    Refactor bonus 1 and add prompt in bonus 2

--- js/index.js
+++ js/index.js
@@ -37,35 +37,42 @@ console.log(navigator);
 // 3.3
 if (hacker1 < hacker2) {
 	console.log("The driver's name goes first.");
-} else if (hacker1 > hacker2) {
+} else if (hacker2 < hacker1) {
 	console.log('Yo, the navigator goes first definitely.');
 } else {
 	console.log('What?! You both have the same name?');
 }
 
 // Bonus 1: Lorem ipsum
-// 1.1 Number of words
 const loremText =
 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ligula erat, mattis non mattis a, imperdiet eu est. Suspendisse sed augue quam. Aenean eleifend purus et nisl mollis, eget dictum nulla pharetra. Duis massa orci, cursus eget aliquam ac, ullamcorper ac mauris. Nunc volutpat neque vitae ipsum sollicitudin, a lobortis lectus dapibus. Vivamus tempus lectus a orci tempus molestie. Integer a dictum justo. Cras dapibus facilisis eros cursus luctus.\nDuis vel eros mauris. Etiam sed placerat leo. Maecenas sollicitudin nunc posuere volutpat ultrices. Proin odio libero, consectetur id eros in, rutrum facilisis neque. Etiam nisl orci, viverra eu sapien pharetra, volutpat commodo odio. Aliquam erat volutpat. Phasellus feugiat fermentum lectus, at luctus magna interdum id. Mauris vestibulum tincidunt nisi at congue. Proin est purus, hendrerit et molestie at, imperdiet ac libero. Donec vel tempor lectus, eu dictum leo. Vivamus quis faucibus lectus,at efficitur leo.\nSuspendisse iaculis ante vehicula, tristique purus at, luctus nibh. Curabitur at tempor erat, nec maximus diam. Aliquam libero risus, efficitur malesuada mauris sit amet, mollis mattis massa. Vestibulum consectetur leo nec massa iaculis ultricies. Nulla rhoncus quam magna, at efficitur purus congue vel. Praesent scelerisque justo nec mauris tincidunt, eu tincidunt nulla sollicitudin. Nullam at varius purus. Morbi turpis libero, aliquam id felis sit amet, vulputate porta purus. In iaculis massa ipsum, at vehicula arcu sollicitudin vitae. Maecenas ac felis vel augue porta euismod.';
-let numOfWords = loremText.split(' ').length;
-console.log(numOfWords);
+let loremArr = loremText.split(' ');
+
+// 1.1 Number of words
+console.log(`The string has ${loremArr.length} words.`);
 
 // 1.2 Count of 'et' word
-let arrLorem = loremText.split(' ');
 let counter = 0;
-for (let i = 0; i < loremText.length; i++) {
-	if (arrLorem[i] === 'et' || arrLorem[i] === 'Et') {
+for (let i = 0; i < loremArr.length; i++) {
+	if (loremArr[i] === 'et' || loremArr[i] === 'Et') {
 		counter++;
 	}
 }
-console.log(counter);
+console.log(`The word 'et' appears ${counter} times in the string`);
 
 // Bonus 2: Palindromes
+let input = prompt('Type something you think might be a palindrome');
 function isPalindrome(str) {
 	let arr = str.toLowerCase().replace(/\W/g, '').split('');
 	let arrReversed = [];
 	arr.forEach(item => {
 		arrReversed.unshift(item);
 	});
-	return arr.join('') === arrReversed.join('') ? true : false;
+
+	if (arr.join('') === arrReversed.join('')) {
+		alert("That's a palindrome!");
+	} else {
+		alert('Not a palindrome, try again');
+	}
 }
+isPalindrome(input);
