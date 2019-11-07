6ada498d78ca0c1780e00d03dbb76871d2174350
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-11-07 22:28:44 +0100

    iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Rubén";
+let hacker2 = "Pedro";
+
+console.log("The driver's name is", hacker1);
+console.log("The driver's name is", hacker2);
 
 // Iteration 2: Conditionals
 
