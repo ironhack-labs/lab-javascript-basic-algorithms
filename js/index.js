dc663dec843ae7454c43c115c908fef79768fb05
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-11-08 01:59:15 +0100

    bonus1

--- js/index.js
+++ js/index.js
@@ -47,3 +47,27 @@ arrayOrden.sort(function (a,b) {
   console.log('What?! You both have the same name?')
   return;
 });
+
+//Bonus 1
+
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin arcu eros, lobortis sed eros nec, auctor euismod massa. Cras in tincidunt enim. Integer at leo dui. Vestibulum ac lobortis turpis. Maecenas elementum malesuada leo, quis viverra urna efficitur ut. Nulla facilisi. Proin vel dui ligula. Phasellus non massa ut nibh rhoncus ultrices sit amet ut risus. Phasellus mollis nibh volutpat sapien vulputate, eu dapibus purus varius. Morbi suscipit accumsan diam a pulvinar. Nunc eu interdum mi, ac egestas erat. Mauris nunc est, imperdiet et quam auctor, posuere fermentum ipsum. Sed eget aliquet metus. Proin dapibus purus in massa interdum fermentum. Curabitur consequat consequat urna a porta. Curabitur aliquam velit eros, vel rhoncus erat viverra quis. Suspendisse nec nunc nisl. Duis velit purus, egestas quis hendrerit malesuada, mollis eu diam. Vestibulum nulla nibh, blandit ac tristique id, cursus a nunc. Quisque non quam in dui scelerisque volutpat ac sed ante. Etiam rutrum vehicula felis, ac pellentesque justo molestie quis. In tincidunt magna sed sem consequat, nec suscipit nulla tristique. Fusce justo sapien, feugiat nec velit vitae, convallis euismod tortor. Integer scelerisque ipsum vel sapien finibus, non vestibulum risus feugiat. Nunc egestas metus ac risus maximus, in porttitor justo gravida. Fusce eleifend, lacus in ullamcorper commodo, tortor odio posuere metus, vel maximus felis orci eu mauris. Nam sagittis odio lacinia nisi maximus efficitur. Vivamus luctus, dolor a vulputate dapibus, ex ipsum imperdiet arcu, eget dapibus lorem neque id arcu. In volutpat at ex nec elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris urna orci, pulvinar vel molestie in, placerat et nibh. Duis a eros eget dui tempus tincidunt at sed urna. In hac habitasse platea dictumst. Ut accumsan, risus a elementum placerat, sapien erat pharetra nibh, id imperdiet elit risus eget ante. Nullam quis ex sit amet ipsum blandit scelerisque non sed lectus. Donec scelerisque, lectus eu lacinia tristique, lorem dui suscipit arcu, quis hendrerit massa eros nec felis. Cras ac sollicitudin mi, non auctor tellus. Curabitur aliquam, libero vitae consectetur luctus, augue tellus ornare tellus, eget tempor metus velit vel lectus. Mauris eu auctor leo. Integer venenatis ante vitae aliquet vehicula. Donec ultrices nunc lectus, sit amet faucibus urna blandit sed. Aliquam erat volutpat."
+
+console.log('Numero de palabras:', lorem.split(' ').length);
+
+let arrayEt = [];
+for(let i=0; i < lorem.length ; i++){
+  var x = lorem.indexOf('et', i);
+  if(!arrayEt.includes(x) && x != -1){
+    arrayEt.push(x);
+  }
+}
+
+console.log(arrayEt.length);
+
+//Bonus 2
+
+let examplePalindrome = "A man, a plan, a canal, Panama!";
+
+examplePalindrome.forEach(function(e){
+  console.log(e)
+})
