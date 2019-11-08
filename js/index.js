ec3babd35936fa472f51914631d80e6449a90a83
Gonzalo Hernández <gonzalohernandez@icloud.com>
2019-11-08 10:03:39 +0100

    code refactoring

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ console.log(navigatorReverse);
 
 if (hacker1 < hacker2){
   console.log("The driver's name goes first");
-} else if (hacker2 < hacker1){
+} else if (hacker1 > hacker2){
   console.log("Yo, the navigator goes first definitely");
 } else {
   console.log("What?! You both have the same name?");
