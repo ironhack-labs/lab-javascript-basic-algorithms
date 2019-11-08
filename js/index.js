cf87785746057e133d5a755aae12a88ecf94f1c3
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2019-11-08 16:42:13 +0100

    Iterations Complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 // Iteration 1: Names and Input
+console.log("I´m ready");
 
+let hacker1 = "Igna";
+
+console.log("The driver´s name is " + hacker1);
+
+let hacker2 = "Pepe";
+
+console.log("The navigator´s name is " + hacker2);
+
+if (hacker1.length > hacker2.length) {
+  console.log(
+    "The driver has the longest name, it has " + hacker1.length + " characters"
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    "It seems that the navigator has the longest name " +
+      hacker2.length +
+      " characters"
+  );
+} else if (hacker1.length === hacker2.length) {
+  console.log(
+    "Wow, you both have equally long names, " + hacker1.length + " characters"
+  );
+}
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
