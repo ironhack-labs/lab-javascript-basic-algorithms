541ecd9c59b06f137cded1e0b8c1e0aa1d356f57
David Garcia <dgarciabravo13@gmail.com>
2019-11-08 07:08:30 +0100

    bonus 1, finish

--- js/index.js
+++ js/index.js
@@ -41,3 +41,15 @@ if (sort > 0) {
 } else if (sort < 0) {
   cosole.log("Yo, the navigator goes first definitely.");
 } else console.log("What?! You both have the same name?");
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in lacus felis. Etiam dolor quam, posuere vel blandit vel, gravida aliquet augue. Nulla facilisi. Nulla arcu lorem, cursus eget ante ut, vulputate rutrum justo. Fusce et lobortis libero, quis porttitor est. Etiam sem lacus, vulputate maximus dapibus sed, ullamcorper nec libero. Nunc venenatis finibus tincidunt. Proin finibus, elit a maximus eleifend, justo nunc malesuada urna, sed mollis ante massa nec turpis. Duis placerat mauris sed magna fermentum suscipit. Sed tincidunt vulputate mauris malesuada porta. Aenean tempus vestibulum pulvinar. Proin finibus luctus ex vel semper. Vivamus sed egestas sapien.Nam nec felis vel urna accumsan placerat sed eget velit. Maecenas consectetur mi ac diam imperdiet, ut pulvinar enim malesuada. Fusce eget lacus eget arcu rhoncus venenatis. Maecenas imperdiet felis vitae erat gravida, et ultricies quam convallis. Sed a porttitor ligula. Nam vitae porta felis. Nulla sit amet luctus mauris.Duis eu molestie neque. Sed consectetur, justo non consectetur faucibus, mauris odio suscipit lectus, vel luctus purus turpis eu erat. Nulla pulvinar, erat a auctor posuere, magna nibh tempor enim, in semper neque nunc ac orci. Donec ultricies eleifend ullamcorper. Aenean malesuada metus quam, ut dictum nisl tempor tincidunt. Sed non felis enim. Etiam vitae metus iaculis, luctus turpis vitae, fermentum ante. Donec hendrerit elit quis rutrum dignissim. Pellentesque sit amet neque felis. Vestibulum justo nulla, sollicitudin vitae ipsum sit amet, tempor iaculis neque. Duis porttitor ipsum posuere mi maximus, vitae blandit nulla congue. Cras a nibh eleifend, tempor purus nec, commodo ex. Sed lorem lacus, ornare ut enim quis, tempor placerat neque. Nullam placerat venenatis mattis. Sed laoreet varius tempor. Duis nisi ante, pharetra non urna ac, accumsan pharetra purus."
+
+let arrayLorem = lorem.split(" ");
+console.log(arrayLorem.length);
+count = 0;
+
+for (j = 0; j < arrayLorem.length;j++){
+  if(arrayLorem[j] === "et"){
+    count ++;
+  }
+}
+console.log(count);
