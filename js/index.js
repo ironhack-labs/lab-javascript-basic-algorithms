b092c1b867eaf41132fe2a8fd37e2df521359896
Grecia Patricia <grecia.busquets@gmail.com>
2019-11-08 20:40:03 +0100

    Resueltos los ejercicios 1 y 2, trabajando en el 3ero

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 // Iteration 1: Names and Input
 
-var hacker1 = "Grecia"
+var hacker1 = "Grecia";
+console.log ("The driver's name is " + hacker1 );
+var hacker2 = "Google Chrome";
+console.log("The navigator's name is " + hacker2);
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length){
+    console.log("The driver has the longest name, it has " +hacker1.length+  " characters.")
+  } else if (hacker1.length < hacker2.length){
+    console.log("It seems that the navigator has the longest name, it has " +hacker2.length+  " characters.")
+  } else if (hacker1.length == hacker2.length){
+    console.log("Wow, you both have equally long names, " +hacker1.length+  " characters!")
+  }
 
 // Iteration 3: Loops
+
+function mayusculas(x) {
+  return x.split("").join(" ");
+}
+mayusculas (hacker1.toUpperCase());
+
+function alReves(x) {
+  return x.split("").reverse().join("");
+}
+alReves(hacker1);
