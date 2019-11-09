6739fce55f3b4ebc60177d0834992df38c140a18
Peacock-Co <pavaoleonardo@hotmail.com>
2019-11-09 20:25:00 +0100

    Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 // Iteration 1: Names and Input
+let hacker1 = "Leonardo";
+console.log("Driver´s name: ", hacker1);
 
+let hacker2 = "Alberto";
+console.log("The navigator´s name is: ", hacker2);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
