2fbb9d49a8dfbe1629704ac9037b9d5fee39e074
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2019-11-10 12:12:34 +0100

    ejercicios 1 apartado

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
 // Iteration 1: Names and Input
+var hacker1 = "Alberto";
+console.log("The drive´s name is" , hacker1);
+
+var hacker2 = prompt("what´s your name");
+
+//var hacker2 = "Carlos";
+console.log("The navigator´s name is", hacker2);
+
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length)
+{
+console.log("The driver has the longest name, it has " + hacker1.length + " characters");
+}
+else if (hacker1.length < hacker2.length)
+{
+console.log("It seems that the navigator has the longest name, it has " + hacker2.length + " characters")
+}
+else 
+{
+  console.log("Wow, you both have equally long names, " + hacker1.length + " characters");
+} 
 
 
 //funciones
