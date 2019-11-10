9cc2914bdc2e80badb467979e64641466904ecad
Javi L <jlopezro@gmail.com>
2019-11-10 19:07:25 +0100

    retouching

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -21,31 +21,28 @@ console.log(
 
 //Iteration 8
 
-let h1Split = hacker1.split("");
-let h2Split = hacker2.split("");
-
 for (let i = 0; i < 1; i++) {
-  if (h1Split[0] < h2Split[0]) {
+  if (hacker1[0] < hacker2[0]) {
     console.log("The driver's name goes first");
-  } else if (h1Split[0] > h2Split[0]) {
+  } else if (hacker1[0] > hacker2[0]) {
     console.log("Yo, the navigator goes first definitely");
-  } else if (h1Split[1] < h2Split[1]) {
+  } else if (hacker1[1] < hacker2[1]) {
     console.log("The driver's name goes first");
-  } else if (h1Split[1] > h2Split[1]) {
+  } else if (hacker1[1] > hacker2[1]) {
     console.log("Yo, the navigator goes first definitely");
-  } else if (h1Split[2] < h2Split[2]) {
+  } else if (hacker1[2] < hacker2[2]) {
     console.log("The driver's name goes first");
-  } else if (h1Split[2] > h2Split[2]) {
+  } else if (hacker1[2] > hacker2[2]) {
     console.log("Yo, the navigator goes first definitely");
-  } else if (h1Split[3] < h2Split[3]) {
+  } else if (hacker1[3] < hacker2[3]) {
     console.log("The driver's name goes first");
-  } else if (h1Split[3] > h2Split[3]) {
+  } else if (hacker1[3] > hacker2[3]) {
     console.log("Yo, the navigator goes first definitely");
   } else if (hacker1 === hacker2) {
     console.log("What?! You both got the same name?");
-  } else if (h1Split[3] === h2Split[3] && h2Split.length > 4) {
+  } else if (hacker1[3] === hacker2[3] && hacker2.length > 4) {
     console.log("The driver's name goes first");
-  } else if (h1Split[0] === h2Split[0] && h2Split.length < 4) {
+  } else if (hacker1[0] === hacker2[0] && hacker2.length < 4) {
     console.log("Yo, the navigator goes first definitely");
   }
 }
