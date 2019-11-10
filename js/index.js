c8dc630ea13f6bbb677e3611d606da980c8d3813
zante <dza.devandcomputing@gmail.com>
2019-11-10 20:39:36 +0100

    first_bonus

--- js/index.js
+++ js/index.js
@@ -81,3 +81,51 @@ function compareLexicograficOrder(driversName, navigatorsName){
 }
 
 console.log(compareLexicograficOrder(hacker1,hacker2));
+
+//Bonus Time!
+
+    /*Bonus 1:
+        
+        Generate 3 paragraphs. Store the text in a variable type of string.
+        Make your program count the number of words in the string.
+        Make your program count the number of times the Latin word et appears.
+    */
+
+    //var randomText = "et et hey et yo et et"
+    var randomText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec laoreet nisl. Nulla a velit imperdiet, tempus mi non, euismod mauris. Curabitur sollicitudin viverra tellus. Mauris luctus placerat laoreet. Phasellus volutpat, est ac interdum pretium, ante lacus vehicula augue, eget porttitor ipsum ex vitae neque. Integer dictum, neque sit amet vulputate sagittis, dui urna faucibus elit, at pellentesque dui risus ac arcu. Aliquam sit amet lorem nec felis pulvinar rutrum sit amet vel sapien. " +
+
+    "Donec dapibus posuere semper. Pellentesque tempus egestas arcu, porta pellentesque nibh dapibus vel. Cras sem mauris, mollis quis scelerisque a, tempus auctor magna. Nam tincidunt posuere arcu, ut molestie nulla vestibulum quis. Praesent et turpis maximus magna venenatis accumsan eu id tortor. Nullam pulvinar at metus a bibendum. In vel condimentum dolor, sit amet viverra est. Quisque dignissim, urna ac elementum congue, ipsum ipsum luctus sapien, quis viverra arcu nisl molestie magna. Cras consequat purus hendrerit risus molestie hendrerit. Maecenas eleifend mollis sapien at laoreet. " +
+    
+    "Nunc at nisl sed nisi sollicitudin sodales. Phasellus sodales sollicitudin odio eu congue. Proin vehicula hendrerit pulvinar. Donec ex urna, vestibulum quis sem ut, eleifend faucibus magna. Proin aliquet risus quis lorem auctor, quis interdum quam porttitor. Donec lorem purus, tempor at leo eget, porta ornare leo. Sed lorem nisi, luctus non viverra in, sollicitudin pulvinar ante. Donec vitae mattis erat. Duis nec erat ante. Nunc sed neque quis risus tincidunt gravida quis ac tortor. Quisque et orci quis elit congue ultricies et et velit. Pellentesque tempus ligula ac ante accumsan cursus. Donec vel nisi non eros gravida finibus. Cras pretium interdum pretium.";
+
+    var words = randomText.split(" ");
+
+    console.log("The three Lore ipsum paragraphs contain",words.length,"words.");
+
+    var counter = 0;
+
+    for(let i = 0; i < words.length; i++){
+
+        if(words[i] === "et"){
+            counter++;
+        }
+
+    }
+
+    console.log("The word 'et' appears",counter,"times within the paragraphs.");
+
+
+    /*Bonus 2:
+
+        Ask the user for a new string and check if it's a Palindrome. Examples of palindromes:
+
+        "A man, a plan, a canal, Panama!"
+        "Amor, Roma"
+        "race car"
+        "stack cats"
+        "step on no pets"
+        "taco cat"
+        "put it up"
+        "Was it a car or a cat I saw?" and "No 'x' in Nixon".
+    */
+   
