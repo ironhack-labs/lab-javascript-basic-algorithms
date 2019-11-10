e39c11bafeb46884ef1452502b86f9c8aaa4a288
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2019-11-10 11:32:21 +0100

    limpieza de cagada

--- js/index.js
+++ js/index.js
@@ -1,72 +1,14 @@
 // Iteration 1: Names and Input
-//number
-var number = 0;
-//string
-var string = "Alberto";
-//bool
-var bool = true;
-//arrays
-var arrays = [];
-//objetos
-var objects = new objects();
-//not a number
-NaN 
-//undefined
-undefined
-//null
-null
+
 
 //funciones
-var a = 0;
-var b = 1;
 
-function funciones(a,b)
-{
 
-}
+
 
 // Iteration 2: Conditionals
- // if , else if ,else
- if (a > b)
- {
 
- }
- else if(a < b)
- {
-
- }
-else 
-{
-
-}
-//switch
-switch (8 < 5)
-{
-case a:
-    return 8;
-    break;
-case b:
-    default:
-}
 
 
 // Iteration 3: Loops
-//do while
-do {
-    i = i + 1;
-    result = result + i;
-  } while (i < 5);
 
-//while
-n = 1;
-x = 2;
-while (n < 3) {
-  n ++;
-  x += n;
-}
-
-//for
-for(var i = 0; i > 3 ; i++)
-{
-
-}
