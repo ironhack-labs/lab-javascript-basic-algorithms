afd5ea84165d4c9499a393a4bf63ccb0ba470c72
zante <dza.devandcomputing@gmail.com>
2019-11-10 18:11:10 +0100

    first_iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
 
+var hacker1 = "Dani";
+var hacker2 = "Berto";
+console.log("The driver's name is",hacker1);
+console.log("The navigator's name is",hacker2);
 
 // Iteration 2: Conditionals
 
