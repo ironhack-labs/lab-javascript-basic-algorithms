b0420e215baccaabdf5e81003c847e01e899905a
zante <dza.devandcomputing@gmail.com>
2019-11-10 18:39:42 +0100

    second_iteration

--- js/index.js
+++ js/index.js
@@ -7,5 +7,18 @@ console.log("The navigator's name is",hacker2);
 
 // Iteration 2: Conditionals
 
+var a = hacker1.length;
+var b = hacker2.length;
+
+/*console.log(a);
+console.log(b);*/
+
+if(a>b){
+    console.log("The driver has the longest name, it has",a,"characters.");
+}else if(a<b){
+    console.log("It seems that the navigator has the longest name, it has",b,"characters.");
+}else{
+    console.log("Wow, you both have equally long names,",b,"characters!");
+}
 
 // Iteration 3: Loops
