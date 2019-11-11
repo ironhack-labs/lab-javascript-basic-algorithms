5095e70d431e77fbf9ffae61a11e2eacfe805541
Javi L <jlopezro@gmail.com>
2019-11-11 16:15:21 +0100

    some fixing

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -21,8 +21,26 @@ console.log(
 
 //Iteration 8
 
+
 for (let i = 0; i < 1; i++) {
-  if (hacker1[0] < hacker2[0]) {
+
+  if (hacker1 === hacker2) {
+   console.log("What?! You both got the same name?"); break;
+ } else if (hacker1[0] <= hacker2[0]) {
+   i++
+  
+   console.log("The driver's name goes first");
+ } else if (hacker1[0] >= hacker2[0]) {
+   i++
+
+   console.log("Yo, the navigator goes first definitely");
+ }
+}
+
+ //Este for anterior tiene el bug de que si el nombre del navigator empieza también por "J", por "Ja" o por "Jav", siempre devuelve mensaje de que el driver va primero, incluso cuando no, por ejemplo poniendo "Jave" como navigator name.
+ //No me ha dado tiempo a solucionarlo. Aunque con el código comentado que sigue el bug no ocurre pero no está dentro de un loop, no he sabido meterlo dentro de un loop sin que dé ningún bug.
+
+/*  if (hacker1[0] < hacker2[0]) {
     console.log("The driver's name goes first");
   } else if (hacker1[0] > hacker2[0]) {
     console.log("Yo, the navigator goes first definitely");
@@ -45,6 +63,8 @@ for (let i = 0; i < 1; i++) {
   } else if (hacker1[0] === hacker2[0] && hacker2.length < 4) {
     console.log("Yo, the navigator goes first definitely");
   }
-}
+
+  */
+
 
 // Lorem ipsum generator
