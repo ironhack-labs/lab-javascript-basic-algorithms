b2c59c79f0f458fa3f5a82223fadfa528384409a
Javi L <jlopezro@gmail.com>
2019-11-11 16:20:16 +0100

    Identation

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,15 +1,13 @@
 // Names and Input
 
-
 //Conditionals
 
 let l1 = hacker1.length;
 let l2 = hacker2.length;
 
 if (l1 > l2) {
- console.log("The Driver has the longest name, it has", l1, "characters");
-}
-else if (l1 < l2) {
+  console.log("The Driver has the longest name, it has", l1, "characters");
+} else if (l1 < l2) {
   console.log("Yo, navigator got the longest name, it has", l2, "characters");
 } else {
   console.log("wow, you both got equally long names", l1, "characters!!");
