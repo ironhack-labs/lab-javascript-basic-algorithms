e670dd1a24b8558e5058e12a01f5f161138d026b
Borja del Amo Olivares <54556686+borja115@users.noreply.github.com>
2019-11-11 18:29:45 +0100

    ejercico

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // Iteration 1: Names and Input
-
+var hacker1 = "XXXX";
+var hacker2 = "AAAA";
+console.log("The driver's name is",hacker1);
+console.log("The navigator's name is",hacker2);
 
 // Iteration 2: Conditionals
+var a = hacker1.length;
+var b = hacker2.length;
 
 
 // Iteration 3: Loops
