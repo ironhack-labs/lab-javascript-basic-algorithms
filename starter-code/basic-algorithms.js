bcb6d6247f3e508f8fbd7374a03c3880ef9f3b41
Javi L <jlopezro@gmail.com>
2019-11-11 18:42:05 +0100

    conflits solving

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,19 +1,13 @@
 // Names and Input
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 let hacker1 = "Javi";
 console.log("The driver name is", hacker1);
 
 let hacker2 = prompt("What's your name?");
 console.log("The navigator name is", hacker2);
->>>>>>> names_and_input
 
 //Conditionals
 
-=======
 //Conditionals
 
 let l1 = hacker1.length;
@@ -26,8 +20,7 @@ if (l1 > l2) {
 } else {
   console.log("wow, you both got equally long names", l1, "characters!!");
 }
->>>>>>> conditionals
-=======
+
 //Conditionals
 
 // Loops
@@ -63,35 +56,5 @@ for (let i = 0; i < 1; i++) {
     console.log("Yo, the navigator goes first definitely");
   }
 }
-
-//Este for anterior tiene el bug de que si el nombre del navigator empieza también por "J", por "Ja" o por "Jav", siempre devuelve mensaje de que el driver va primero, incluso cuando no, por ejemplo poniendo "Jave" como navigator name.
-//No me ha dado tiempo a solucionarlo. Aunque con el código comentado que sigue el bug no ocurre pero no está dentro de un loop, no he sabido meterlo dentro de un loop sin que dé ningún bug.
-
-/*  if (hacker1[0] < hacker2[0]) {
-    console.log("The driver's name goes first");
-  } else if (hacker1[0] > hacker2[0]) {
-    console.log("Yo, the navigator goes first definitely");
-  } else if (hacker1[1] < hacker2[1]) {
-    console.log("The driver's name goes first");
-  } else if (hacker1[1] > hacker2[1]) {
-    console.log("Yo, the navigator goes first definitely");
-  } else if (hacker1[2] < hacker2[2]) {
-    console.log("The driver's name goes first");
-  } else if (hacker1[2] > hacker2[2]) {
-    console.log("Yo, the navigator goes first definitely");
-  } else if (hacker1[3] < hacker2[3]) {
-    console.log("The driver's name goes first");
-  } else if (hacker1[3] > hacker2[3]) {
-    console.log("Yo, the navigator goes first definitely");
-  } else if (hacker1 === hacker2) {
-    console.log("What?! You both got the same name?");
-  } else if (hacker1[3] === hacker2[3] && hacker2.length > 4) {
-    console.log("The driver's name goes first");
-  } else if (hacker1[0] === hacker2[0] && hacker2.length < 4) {
-    console.log("Yo, the navigator goes first definitely");
-  }
-
-  */
->>>>>>> loops
 
 // Lorem ipsum generator
