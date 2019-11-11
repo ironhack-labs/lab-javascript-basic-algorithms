64347ff9ea7260e3fb81ff475607cd24c4e3e6f9
Javi L <jlopezro@gmail.com>
2019-11-11 16:19:36 +0100

    Identation

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,9 +1,9 @@
 // Names and Input
 
 let hacker1 = "Javi";
-console.log ("The driver name is", hacker1);
+console.log("The driver name is", hacker1);
 
-let hacker2 = prompt ("What's your name?");
+let hacker2 = prompt("What's your name?");
 console.log("The navigator name is", hacker2);
 
 //Conditionals
