53bdeb6dd96b969b6d1d3fcd2100cda742b76f57
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2019-11-18 17:05:47 +0100

    Iteration 1 und 2

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
 // Iteration 1: Names and Input
+console.log("I'am ready");
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<< Iteration 1 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const hacker1 = "Frank";
+const hacker2 = "Juliane";
+
+console.log(`The drivers name is ${hacker1}`);
+console.log(`The navigators name is ${hacker2}`);
+
+// <<<<<<<<<<<<<<<<<<<<<<<< Iteration 2 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length === hacker2.length) {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length}  characters!`
+  );
+} else {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${
+      hacker2.length
+    } characters.`
+  );
+}
+
+
+
+
 
 
 // Iteration 2: Conditionals
