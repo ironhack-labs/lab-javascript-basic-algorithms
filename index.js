f67e129703003aef07e5dd0827db92118fbba765
Gooss <gooss@media-saturn.com>
2019-11-18 17:19:41 +0100

    Second iteration done

--- index.js
+++ index.js
@@ -9,15 +9,24 @@ console.log(`The driver´s name is ${hacker1}`);
 
 //1.3 Create a variable hacker2 with the navigator's name.//
 
-let hacker2 = "Alina";
-let hacker3 = "Sergej";
+let hacker2 = "AlinaSergej";
 
 //1.4 Print "The navigator's name is YYYY".//
 
-console.log(`The navigators are ${hacker2}, ${hacker3}`);
+console.log(`The navigator´s name is ${hacker2}`);
 
 /*Iteration 2: Conditionals
 2.1. Depending on which name is longer, print: - The Driver has the longest name, it has XX characters or - Yo, navigator got the longest name, it has XX characters or - Wow, you both got equally long names, XX characters!*/
+
+if (hacker1.length >=hacker2.length){
+  if (hacker1.length == hacker2.length){
+    console.log(`Wow, you both got equally long names, ${hacker1.length} characters!`)
+  } else {
+    console.log(`The Driver has the longest name, it has ${hacker1.length} characters`)
+  } 
+} else {
+  console.log(`Yo, navigator got the longest name, it has ${hacker2.length} characters`)
+}
 
 /*Iteration 3: Loops
 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"*/
