5d08ab178a4b62a96795254dd5ddd7010b0f89b2
Rochowski <rochowskim@media-saturn.com>
2019-11-18 17:07:04 +0000

    Iteration3 finished

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ console.log(`The driver's name is ${hacker1}`);
 let hacker2 = "Anastasia";
 console.log(`The navigators's name is ${hacker2}`);
 
-
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
@@ -18,6 +17,36 @@ if (hacker1.length > hacker2.length) {
 }
 
 
-
-
 // Iteration 3: Loops
+//console.log(hacker1.toUpperCase());
+
+hacker1 = hacker1.toUpperCase();
+
+let hacker1leer = "";
+
+
+for (let i=0; i<hacker1.length; i++) {
+    hacker1leer += (hacker1[i]+ " ");
+}
+console.log(hacker1leer);
+
+
+let hacker2reverse = "";
+
+for (let i = hacker2.length-1; i >= 0 ; i--) {
+    hacker2reverse += hacker2[i];
+}
+console.log(hacker2reverse);
+
+//let strings = [hacker1, hacker2];
+
+hacker1 = hacker1.toLowerCase;
+hacker2 = hacker2.toLowerCase;
+
+if(hacker1 < hacker2) {
+    console.log(`The driver's name goes first`);
+} else if (hacker2 < hacker1) {
+    console.log(`Yo, the navigator goes first definitely.`);
+} else {
+    console.log(`What?! You both have the same name?`);
+}
