2c3bbcb7bf7e4d4fba91c1aa40edef23dec41901
Rochowski <rochowskim@media-saturn.com>
2019-11-18 15:48:47 +0000

    Iteration1 finished

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Matthäus";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Anastasia";
+console.log(`The navigators's name is ${hacker2}`);
+
 
 // Iteration 2: Conditionals
 
