c294a31c54d50d47588f7b42e370b75370cd73d8
Bialas <bialaska@media-saturn.com>
2019-11-18 18:10:20 +0100

    finish third iteration

--- js/index.js
+++ js/index.js
@@ -16,4 +16,33 @@ if(lenHacker1>lenHacker2){
 }else{
   console.log(`Wow, you both have equally long names, ${lenHacker2} characters!`);
 }
+
+
 // Iteration 3: Loops
+let upperHacker1=hacker1[0];
+for(let i=1;i<lenHacker1;i++){
+  upperHacker1+=" "+hacker1[i].toUpperCase();
+}
+console.log(`"${upperHacker1}"`);
+
+let reverseHacker2="";
+for(let i=lenHacker2-1;i>=0;i--){
+  reverseHacker2+=hacker2[i];
+}
+console.log(`"${reverseHacker2}"`);
+
+/*
+reverseHacker2="";
+for(let i=0;i<lenHacker2;i++){
+  reverseHacker2+=hacker2[lenHacker2-i-1];
+}
+console.log(`"${reverseHacker2}"`);
+*/
+
+if(hacker1<hacker2){
+  console.log(`The driver's name goes first`);
+}else if(hacker1>hacker2){
+  console.log(`Yo, the navigator goes first definitely`);
+}else{
+  console.log(`What?! You both have the same name?`);
+}
