87c7cd12ed4c73142e1d4c06b1aa6f6ebc0b911d
Zahn <zahnk@media-saturn.com>
2019-11-18 16:51:47 +0100

    iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 // Iteration 1: Names and Input
+// Iteration 1: Names and Input
+/*1.1 Create a variable `hacker1` with the driver's name.
 
+1.2 Print `"The driver's name is XXXX"`.
+
+1.3 Create a variable `hacker2` with the navigator's name.
+
+1.4 Print `"The navigator's name is YYYY"`.*/
+
+const hacker1 = "Kai";
+console.log(hacker1);
+
+const hacker2 = " Tomasz";
+console.log(hacker2);
 
 // Iteration 2: Conditionals
 
