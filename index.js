8c32f5726def7cd13617fc554d5d4f4693fb22b2
Gooss <gooss@media-saturn.com>
2019-11-18 16:50:41 +0100

    First iteration done

---
+++ index.js
@@ -0,0 +1,34 @@
+/*Iteration 1: Names and Input
+1.1 Create a variable hacker1 with the driver's name.*/
+let hacker1 = "Juergen";
+
+
+//1.2 Print "The driver's name is XXXX".//
+
+console.log(`The driver´s name is ${hacker1}`);
+
+//1.3 Create a variable hacker2 with the navigator's name.//
+
+let hacker2 = "Alina";
+let hacker3 = "Sergej";
+
+//1.4 Print "The navigator's name is YYYY".//
+
+console.log(`The navigators are ${hacker2}, ${hacker3}`);
+
+/*Iteration 2: Conditionals
+2.1. Depending on which name is longer, print: - The Driver has the longest name, it has XX characters or - Yo, navigator got the longest name, it has XX characters or - Wow, you both got equally long names, XX characters!*/
+
+/*Iteration 3: Loops
+3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"*/
+
+//3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"//
+
+//3.3 Depending on the lexicographic order of the strings, print: - The driver's name goes first. - Yo, the navigator goes first definitely. - What?! You both got the same name?//
+
+/*Bonus Time!
+Go to lorem ipsum generator and:
+
+Generate 3 paragraphs. Store the text in a variable type of string.
+Make your program count the number of words in the string.
+Make your program count the number of times the Latin word et appears.*/
