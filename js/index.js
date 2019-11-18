181bf75574f180aad51b8929629ac0d816ff6718
MSHSusanneV <voglsus@mediamarktsaturn.com>
2019-11-18 16:41:39 +0100

    finished iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Susanne";
+console.log(`"The driver's name is ${hacker1}"`);
+let hacker1 = "Malte";
+console.log(`"The navigator's name is ${hacker2}"`);
 
 // Iteration 2: Conditionals
 
