c760d43573b163aac8e2cc87d002b8f278989114
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2019-11-18 18:21:01 +0100

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -28,19 +28,28 @@ if (hacker1.length > hacker2.length) {
 
 //<<<<<<<<<<<<<<<<<<<< Iteration 3 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+
+// Iteration 3: Loops
+
 let driver = "";
 for (let i = 0; i < hacker1.length; i++) {
   driver += `${hacker1[i].toUpperCase()} `;
 }
 console.log(driver.trim());
 
+let navigator = "";
+for (let i = hacker2.length -1 ; i>=0 ; i--){
+  navigator += hacker2[i];
+}
+
+console.log(navigator);
 
 
-
-
-
-
-// Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
+if (hacker1<hacker2){
+  console.log("The driver's name goes first")
+ } else if (hacker1.toLowerCase()===hacker2.toLowerCase()){
+      console.log("What?! You both have the same name?");
+    }else{
+      console.log("Yo, the navigator goes first definitely.")
+ }
+  
