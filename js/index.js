7d7be768ee0876ca4bda0be80b18b1ddd3faceab
Juan Espegel <juanespegel@gmail.com>
2019-12-16 14:47:20 +0100

    it3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Juan";
+console.log("The driver's name is " + hacker1);
+let hacker2 = "Pepe";
+console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length){
+    console.log("The driver has the longest name, it has " + hacker1.length + " characters.");
+} else if (hacker2.length > hacker1.length){
+    console.log("It seems that the navigator has the longest name, it has " + hacker2.length + " characters.");
+} else {
+    console.log("Wow, you both have equally long names, " + hacker1.length + " characters!")
+}
 
 // Iteration 3: Loops
+
+console.log(hacker1.split('').join(" ").toUpperCase());
+console.log(hacker2.split("").join(" ").toUpperCase());
+console.log(hacker1.split("").reverse().join(""));
+console.log(hacker2.split("").reverse().join(""));
+
+// Bonus
+
+let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id urna eget sapien auctor faucibus. Donec mattis urna ex, vel mattis enim congue sed. Donec mollis ut nulla nec egestas. Donec vitae neque est. Curabitur auctor risus ut tristique consequat. Nam convallis ut mi ut mattis. Fusce sed fringilla dui. Nam dignissim mi eu nisl venenatis, aliquam aliquam elit suscipit. Proin et convallis enim, vitae pulvinar enim. Maecenas justo elit, elementum ac interdum vitae, ornare at purus. Aenean tristique dolor ut molestie vestibulum. In elit magna, pharetra at turpis vitae, pretium facilisis turpis. Etiam auctor venenatis fringilla. Vivamus tincidunt eget ex et ultrices. Nunc odio nulla, interdum ut vulputate quis, aliquet non ex. In convallis ante eu ex auctor convallis. Donec ut vestibulum sem. Etiam aliquet nisi non orci scelerisque, lobortis auctor arcu commodo. Duis molestie libero sapien, at pellentesque metus volutpat ac. Mauris justo justo, vulputate sed vestibulum ut, tincidunt non nulla. Nulla ipsum purus, imperdiet nec hendrerit ac, dignissim vitae massa. Mauris ut felis nulla. Sed vel accumsan est. Maecenas aliquet felis ut ex imperdiet, convallis iaculis velit gravida. Quisque bibendum libero eget est pulvinar, vel sagittis ipsum faucibus. Phasellus ac consequat sapien. Suspendisse ac posuere mauris. Sed sed sagittis tortor, vel finibus nunc. Vivamus venenatis metus eget ante maximus, tincidunt dignissim tellus vehicula. Sed eget iaculis est. Cras ipsum risus, gravida et nunc nec, vulputate fermentum enim. Duis mollis accumsan pellentesque. Vestibulum bibendum malesuada neque, vitae posuere odio cursus et. Suspendisse quis nisi sed erat sagittis semper. Maecenas lacinia ultricies risus porta eleifend. Aenean fringilla commodo volutpat. Vivamus est libero, ultrices non lorem a, ornare consequat dolor. Morbi ultricies orci eget accumsan lacinia. Suspendisse ut ligula et lectus dignissim dignissim. Praesent finibus eu sapien ac commodo. Suspendisse finibus dignissim dictum. Nam at venenatis ante, quis egestas felis. Nullam at luctus orci. Maecenas gravida mollis nisl, ut ullamcorper augue imperdiet id. Etiam varius efficitur mauris, vel elementum turpis cursus vel.";
+
