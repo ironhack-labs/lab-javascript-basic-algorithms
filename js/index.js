bbf86e3d35e27fc09a161c975eaa70311972ebbe
Jaime <jaimemoravallejo@gmail.com>
2019-12-20 08:08:42 +0100

    Iterarion 3 done

--- js/index.js
+++ js/index.js
@@ -28,3 +28,29 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+let nameSpace = "";
+for (let i = 0; i < hacker1.length; i++) {
+    nameSpace += hacker2[i].toUpperCase() + ' ';
+}
+
+console.log(nameSpace);
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+let nameOrder = "";
+for (i = hacker2.length; i >= 0; i--) {
+    nameOrder += hacker2[i];
+}
+console.log(nameOrder);
+// 3.3 Depending on the lexicographic order of the strings, print:
+// - The driver's name goes first.
+// - Yo, the navigator goes first definitely.
+// - What?! You both have the same name?
+
+if (hacker1 > hacker2) {
+    console.log('The driver´s name goes first')
+} else if (hacker1 < hacker2) {
+    console.log('The navigator goes first definitely')
+} else {
+    console.log('What?! You both got the same name?')
+}
