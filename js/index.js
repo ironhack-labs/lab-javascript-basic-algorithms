7ee24677c79a35c039725b3313692f6589398bda
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2019-12-20 18:56:29 +0100

    complete

--- js/index.js
+++ js/index.js
@@ -16,5 +16,29 @@ if (hacker1.length > hacker2.length){
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
   }
 
-  
+
+
 // Iteration 3: Loops
+
+let newHacker1 = "";
+
+for (let i = 0; i < hacker1.length; i++){
+    newHacker1 += hacker1[i].toUpperCase() + " ";
+}
+console.log(newHacker1);
+
+let reverseHacker2 = "";
+
+for (let k = hacker2.length -1; k >= 0; k-=1){
+reverseHacker2 = reverseHacker2 + hacker2[k];
+}
+
+console.log(reverseHacker2);
+
+if (hacker1 > hacker2){
+  console.log('The driver´s name goes first')
+} else if(hacker1 < hacker2){
+  console.log('The navigator goes first definitely')
+} else{
+  console.log('What?! You both got the same name?')
+}
