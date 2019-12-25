b32e659cfcd65e92c0231acdc7560eec3e7665c1
Javier Varela <jvarela@strands.com>
2019-12-25 16:03:00 +0100

    iteration 3 loops

--- js/index.js
+++ js/index.js
@@ -6,16 +6,35 @@ console.log(`The navigator\'s name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 const hacker1 = 'PepeLu'
-console.log(`The driver\'s name is ${hacker1}`)
+console.log(`The driver\'s name is ${hacker1}`);
 const hacker2 = 'MariLo'
-console.log(`The navigator\'s name is ${hacker2}`)
+console.log(`The navigator\'s name is ${hacker2}`);
 
 if (hacker1.length > hacker2.length) {
-  console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
 } else if (hacker1.length < hacker2.length) {
-  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
 } else {
-  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
 
 // Iteration 3: Loops
+let formattedHacker1 = '';
+for (let i = 0; i < hacker1.length; i++) {
+   formattedHacker1 += `${hacker1[i].toUpperCase()} `;
+}
+console.log(formattedHacker1);
+
+let reverseHacker1 = '';
+for (var i = hacker1.length - 1; i >= 0; i--) { 
+  reverseHacker1 += hacker1[i]; 
+}
+console.log(reverseHacker1);
+
+if (hacker1.localeCompare(hacker2) < 0){
+  console.log('The driver\'s name goes first.');
+  } else if (hacker1.localeCompare(hacker2) > 0) {
+  console.log('Yo, the navigator goes first definitely.');
+  } else {
+  console.log('What?! You both have the same name?');
+}
