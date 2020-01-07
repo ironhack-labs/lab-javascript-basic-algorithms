ca0883652ebc595ff050a79b9ec0d4c52b10bacf
Edith LE <edith.lop.estrada@gmail.com>
2020-01-06 18:04:45 -0600

    done :)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 // Iteration 1: Names and Input
+let hacker1 = "richie";
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "edith";
+console.log(`The navigator's name is ${hacker2}`)
 
 
 // Iteration 2: Conditionals
 
 
+if (hacker1.length> hacker2.length){
+    console.log(`hacker1  have ${hacker1.length}`);
+  
+  }else if(hacker1.length<hacker2.length){
+      console.log(`hacker2  have ${hacker2.length}`)
+  
+  }else{
+    console.log(`Wow have equally characters ${hacker1.length}`)
+  }
+
 // Iteration 3: Loops
+
+let hck1= hacker1.toUpperCase().split('').join(' ');
+console.log(hck1)
+
+
+let hck2 = hacker2.split('').reverse().join()
+
+console.log(hck2)
