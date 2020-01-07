0ffb2aa0629670594e537a73837c81d994082684
mjarraya <mjarraya@student.42.fr>
2020-01-07 09:20:24 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 // Iteration 1: Names and Input
 
-
 // Iteration 2: Conditionals
 
-
 // Iteration 3: Loops
+const hacker1 = "Johnny";
+const hacker2 = "Ozhan";
+
+if (hacker1.localeCompare(hacker2) < 0) {
+  console.log("The driver's name goes first.");
+} else if (hacker1.localeCompare(hacker2) > 0) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
