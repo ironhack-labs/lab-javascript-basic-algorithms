112f43e07b9993bca9cd291500ec55e176dd0a57
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-01-08 17:32:29 +0100

    done until bonus 1

--- js/index.js
+++ js/index.js
@@ -41,3 +41,59 @@ if (hacker2.localeCompare(hacker1)) {
     console.log(`What?! You both have the same name?`);
 }
 
+// Bonus 1
+
+let paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lorem augue, placerat nec ex porttitor, hendrerit vulputate ligula. Vestibulum feugiat pretium ex vel varius. Proin non purus faucibus turpis maximus auctor. Aliquam eget tortor non massa fringilla rhoncus. Donec diam lorem, vestibulum in imperdiet ac, molestie et orci. Nulla blandit, nisl non molestie consectetur, tellus neque fringilla neque, in ultrices massa lacus vel justo. Donec eget vestibulum libero. Ut vel posuere augue, in suscipit erat. Fusce sed turpis mi. In hac habitasse platea dictumst. Donec ut tellus interdum ligula luctus vehicula. Ut varius vehicula libero, eget suscipit orci tempus a. Quisque eu ligula congue, consectetur orci sed, convallis mauris. Curabitur eget erat vitae augue aliquet viverra at a nunc. Sed id orci convallis, semper nisi non, blandit quam.
+Suspendisse potenti. Quisque lectus orci, venenatis sit amet metus sit amet, ultricies vestibulum est. In scelerisque dapibus mauris, id pharetra nunc dignissim a. Pellentesque tincidunt rhoncus fringilla. Vivamus aliquam mi sit amet magna pharetra auctor. Vivamus eu malesuada diam, vitae pellentesque augue. Aliquam erat volutpat. Integer leo ex, elementum nec lorem tincidunt, dapibus pharetra lectus.
+Nulla facilisi. Vivamus in blandit est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam imperdiet lacus ut felis porta, nec porta nibh mattis. Etiam accumsan pulvinar tortor, quis tempus leo molestie eu. Fusce placerat mauris in risus varius consequat. Praesent non porttitor ligula, commodo suscipit risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate placerat purus sit amet consectetur. Praesent rhoncus euismod felis, at bibendum augue ornare feugiat. Aenean ex urna, blandit nec sem sit amet, euismod pretium augue. Nunc consectetur, nulla cursus cursus auctor, nisl ante tristique nisi, id imperdiet mauris erat ut purus.`
+
+// Make your program count the number of words in the string.
+
+function Count(str) {
+    let total = 0;
+    for (let i=0; i<str.length; i++) {
+        if (str.charAt(i) === " ") {
+            total+=1;
+        }
+    }
+     return total + 1;
+}
+console.log(Count(paragraph));
+
+// OU
+
+let parArr = paragraph.split(" ");
+console.log(parArr.length)
+
+// Make your program count the number of times the Latin word et appears.
+
+let countWord = 0;
+paragraph.split(" ").forEach(word => {
+    if (word === "et") {
+        countWord += 1;
+    }
+})
+console.log(countWord)
+
+// OU
+
+function CountWords(str){
+    let count = 0;
+    let strArray = str.split(" ");
+    for (let i =0; i<strArray.length; i++){
+      if (strArray[i]=="et"){
+        count++;
+      }
+    }
+  return count;
+  }
+  console.log(CountWords(paragraph));
+
+  
+
+
+
+
+
+
+
