14314a75ca035061220a354fe5b13357b1a72bc2
Devgen <dev@generationyct.nl>
2020-01-09 21:07:52 +0100

    Done with the first 2 iterations.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 // Iteration 1: Names and Input
 
+const hackerOne = "Mark";
+
+console.log(`The driver's name is ${hackerOne}.`);
+
+const hackerTwo = "Jonas";
+
+console.log(`The navigator's name is ${hackerTwo}.`);
 
 // Iteration 2: Conditionals
 
+if (hackerOne.length > hackerTwo.length) {
+  console.log(
+    `The driver has the longest name, it has ${hackerOne.length} characters.`
+  );
+} else if (hackerOne.length < hackerTwo.length) {
+  console.log(
+    `The navigator had the longest name, it has ${hackerTwo.length} characters.`
+  );
+} else {
+  console.log(`Wow, you both have equally long names, ${hackerOne.length}`);
+}
 
 // Iteration 3: Loops
+
+for (let i = 0; i < hackerOne.length; i++){
+  console.log(hackerOne.charAt(i).toUpperCase())
+}
