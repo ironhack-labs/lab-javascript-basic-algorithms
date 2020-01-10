38ff17e050a1f676694e4b71674e016997feedcf
Boyuan Shi <sbyshjd@hotmail.com>
2020-01-10 11:52:13 +0100

    install lorem-ipsum

--- js/index.js
+++ js/index.js
@@ -17,17 +17,41 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 
-let hacker1Arr =[];
-for (let i=0; i<hacker1.length; i++) {
-  hacker1Arr.push(hacker1[i].toUpperCase());
-}
-console.log(hacker1Arr.join(' '))
+// let hacker1Arr =[];
+// for (let i=0; i<hacker1.length; i++) {
+//   hacker1Arr.push(hacker1[i].toUpperCase());
+// }
+// console.log(hacker1Arr.join(' '))
 
-let hacker2Arr =[];
-for (let i = hacker2.length-1; i>-1; i--) {
-  hacker2Arr.push(hacker2[i]);
+let newHacker1 = '';
+for (let i = 0; i<hacker1.length; i++) {
+  newHacker1 += hacker1[i].toUpperCase()+' ';
 }
-console.log(hacker2Arr.join(''));
+console.log(newHacker1);
+
+
+
+// let hacker2Arr =[];
+// for (let i = hacker2.length-1; i>-1; i--) {
+//   hacker2Arr.push(hacker2[i]);
+// }
+// console.log(hacker2Arr.join(''));
+
+let newHacker2 = '';
+for (let i = hacker2.length-1; i > -1; i--) {
+  newHacker2 += hacker2[i];
+}
+console.log(newHacker2);
+
+if (hacker1.localeCompare(hacker2) < 0) {
+  console.log(`The driver's name goes first.`);
+} else if (hacker1.localeCompare(hacker2) > 0) {
+  console.log(`Yo, the navigator goes first definitely.`);
+} else {
+  console.log(`What?! You both have the same name?`);
+}
+
+//#### Bonus1 !
 
 
 
