94f05acaf0c6f359634a3bd50a82ef1e12b3b981
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-01-10 09:06:31 +0100

    1st bonus assignment

--- js/index.js
+++ js/index.js
@@ -1,44 +1,50 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
-let hacker1 = "Pip";
-console.log(`"The driver's name is ${hacker1}"`);
+let hacker1 = "Pip"
+console.log(`"The driver's name is ${hacker1}"`)
 
-let hacker2 = "Pip";
-console.log(`"The navigator's name is ${hacker2}"`);
+let hacker2 = "Pip"
+console.log(`"The navigator's name is ${hacker2}"`)
 
 // Iteration 2: Conditionals
-if (hacker1.length > hacker2.length) {
-  console.log(
-    `The driver has the longest name, it has ${hacker1.length} characters.`
-  );
-} else if (hacker2.length > hacker1.length) {
-  console.log(
-    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
-  );
-} else {
-  console.log(
-    `Wow, you both have equally long names, ${hacker1.length} characters!`
-  );
+if (hacker1.length > hacker2.length ) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+  } else if (hacker2.length > hacker1.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  } else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
+
 
 // Iteration 3: Loops
-let driverName = "";
-let navName = "";
+let driverName = ""
+let navName = ""
 
 for (let i = 0; i < hacker1.length; i++) {
-  driverName = driverName + hacker1[i].toUpperCase() + " ";
+  driverName = driverName + hacker1[i].toUpperCase() + " "
 }
 
-for (let i = hacker2.length - 1; i >= 0; i--) {
-  navName += hacker2[i];
+for (let i = hacker2.length -1 ; i >= 0; i--) {
+  navName += hacker2[i]
 }
-console.log(navName);
+console.log(navName)
 
 if (hacker1.localeCompare(hacker2) === 1) {
-  console.log("The driver's name goes first. ");
+  console.log('The driver\'s name goes first. ')
 } else if (hacker2.localeCompare(hacker1) === 1) {
-  console.log("Yo, the navigator goes first definitely.");
+  console.log('Yo, the navigator goes first definitely.')
 } else {
-  console.log("What?! You both have the same name?");
+  console.log('What?! You both have the same name?')
 }
+
+loremIpsum = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce condimentum malesuada est, id auctor lectus maximus sit amet. Vivamus aliquam tellus vitae dui porttitor, quis rutrum nulla mollis. Fusce ultricies eros id viverra sodales. Nam id hendrerit nisi. Cras commodo rutrum nibh at interdum. Vivamus ac pellentesque ante, non sodales sapien. Aenean fermentum vestibulum eleifend. Nullam posuere sed lorem id hendrerit. Sed convallis ac turpis non hendrerit. Nulla iaculis eget metus a blandit. Proin rutrum at velit ac gravida. Duis finibus metus justo, non venenatis purus mollis vitae. In suscipit metus sit amet turpis dictum sagittis. Ut imperdiet convallis nulla a viverra. Nunc mattis varius est id mollis.
+
+Praesent consequat volutpat suscipit. Mauris ut tellus suscipit, facilisis odio sed, porta urna. Duis ac pharetra ligula. In non varius nibh. Curabitur pretium ex quis neque vulputate, vitae consequat enim tincidunt. Nam in nunc in dui tincidunt rhoncus id sit amet orci. Sed efficitur magna sollicitudin, tempor nunc id, volutpat lacus. In ullamcorper egestas ipsum porta feugiat. Suspendisse egestas nec ante non varius. Nam varius lectus sit amet consectetur sodales.
+
+Vivamus quis est massa. Vestibulum lobortis, libero nec fermentum ultrices, sem metus vulputate dolor, eget pharetra elit sapien quis velit. Nullam porta metus at iaculis hendrerit. Aliquam laoreet eros non dui venenatis dignissim. Maecenas quis metus quis arcu lobortis tempor. Sed lobortis nisi quis ligula dictum, nec consequat nisl dictum. Nullam suscipit porttitor nibh, eget varius lacus. Integer velit sapien, finibus in lorem quis, pellentesque venenatis quam. Quisque purus nunc, tincidunt et orci non, faucibus ullamcorper libero. Morbi euismod ac mauris vel ullamcorper. Aenean quis facilisis velit, vel laoreet diam. Nunc tincidunt est id nibh blandit, eget egestas mi pellentesque. Morbi accumsan et sem sed tempus. Nullam suscipit massa ac pharetra bibendum.`
+
+console.log(loremIpsum.split(' ').length)
+
+for (let i=0;i < loremImsu)
