23c16deeaa54a94aee6385fd406d8c48d9df0cbc
Rajmohan <rajmohankct2003@gmail.com>
2020-01-11 00:08:37 +0100

    Bonus 1,2 completed

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
-// Iteration 1: Names and Input
+console.log("Iteration 1 : ");   
 // 1.1 Create a variable `hacker1` with the driver's name.
 let hacker1 = 'Driver123'
 
 // 1.2 Print `"The driver's name is XXXX"`.
 console.log(`The driver\'s name is ${hacker1}`);
 
-// 1.3 Create a variable `hacker2` with the navigator's name.
-let hacker2 = 'Driver123';//'Navigator1';
+// 1.3 Create a variable `hacker2` with the navigator's name.ac
+let hacker2 = 'Navigator1';
 
 // 1.4 Print `"The navigator's name is YYYY"`.
 console.log(`The navigator's name is ${hacker2}`);
 
-// Iteration 2: Conditionals
+console.log("\nIteration 2: Conditionalas ");  
 // 2.1. Depending on which name is longer, print:
 // - The driver has the longest name, it has XX characters. or
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
+
 
 switch (true) {
   case hacker1.length > hacker2.length :
@@ -28,7 +29,7 @@ switch (true) {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
 }
 
-// Iteration 3: Loops
+console.log("\nIteration 3: Loops"); 
 // 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
 
 let word1 = "";
@@ -86,3 +87,32 @@ for (i = 0; (i < hacker1.length || i < hacker2.length); i++){
        break;
  }
 
+
+console.log("\nBonus 1 : ");
+
+let para1= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt ligula vel est laoreet, eget tincidunt urna ultricies. Nulla sit amet lacinia eros. Morbi tincidunt urna velit. Nulla faucibus metus at arcu scelerisque mollis. Aliquam erat volutpat. Proin eleifend felis et mi pharetra efficitur. Ut orci ipsum, semper vel fringilla eu, feugiat sit amet urna. Maecenas quis arcu vitae purus rhoncus auctor. Sed nibh lectus, vehicula ac tellus vel, tincidunt ornare ligula. Proin sapien augue, dignissim ac ipsum vel, ultrices hendrerit neque. Nam tristique lectus in ipsum accumsan, eu porta odio posuere. Nunc tincidunt placerat sapien vel dictum. Fusce metus ipsum, euismod at tellus non, condimentum aliquet nisi. Suspendisse nisl elit, porttitor nec tellus nec, sollicitudin egestas leo. Nulla facilisi. Phasellus congue sapien diam, vitae vulputate metus varius eu. Maecenas nulla dui, pretium ut venenatis quis, mattis quis erat. Cras at mauris posuere, pellentesque mi consequat, vehicula lectus. Vivamus nec mi mauris. Sed viverra pulvinar ante eget varius. Integer at scelerisque justo, et hendrerit metus. Donec lobortis, sem non tempus ultricies, felis neque tincidunt est, vel pulvinar massa sapien ac eros. Mauris eu mi varius, dignissim massa vitae, tincidunt velit. Nulla neque nisl, dignissim ut consequat sit amet, dictum at neque. Nam blandit sapien odio, eu dictum dui molestie quis. Ut consectetur lacus a libero scelerisque sollicitudin. Suspendisse molestie, ligula et imperdiet imperdiet, nulla ipsum pulvinar metus, vitae venenatis massa ligula ut lacus. Nullam ac vulputate lectus, accumsan venenatis elit. Sed at vulputate ipsum."
+
+console.log("No of words in the paragraph is : " + para1.split(" ").length);
+
+console.log("\nBonus 2 : ");
+let phraseToCheck = "Amor, Roma";
+let num2 = phraseToCheck.length - 1;
+let num1;
+for ( num1 = 0; num1 <= num2; ) {
+ if (!/^[a-zA-Z0-9]/.test(phraseToCheck[num1])) {
+      num1++;
+      continue;
+ } else if (!/^[a-zA-Z0-9]/.test(phraseToCheck[num2])){
+      num2--;
+      continue;
+ } else if(phraseToCheck[num1].toUpperCase() !== phraseToCheck[num2].toUpperCase()){
+   console.log(`${phraseToCheck} is not a palindrome`);
+   break;
+ } else {
+   num1++;
+   num2--;
+ }
+}
+if (num1 >= num2) {
+  console.log(`${phraseToCheck} is a palindrome`);
+}
