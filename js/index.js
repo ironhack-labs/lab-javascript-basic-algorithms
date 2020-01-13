a30976a9c93d2dcb9c2d06ea42f0f1a582951d46
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-13 20:24:22 +0100

    Create variable for navigator's name

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 var hacker1 = prompt("Write driver's name:");
 console.log("The driver's name is", hacker1);
 
+var hacker2 = prompt("Write navigator's name:");
+console.log("The navigator's name is", hacker2);
+
 // Iteration 2: Conditionals
 
 
