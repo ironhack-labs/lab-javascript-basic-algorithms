e4fd79dc98b3b8f05ca13b6e193988a50597a789
Andre <andre.talpo@gmail.com>
2020-01-16 20:47:48 -0300

    Bonus 1 roots

--- js/index.js
+++ js/index.js
@@ -45,7 +45,27 @@ if (hacker1 !== hacker2) {
 
 //Bonus
 let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultricies ipsum mi, eu laoreet tellus malesuada vel. Cras sed finibus purus. Etiam a justo quam. Donec tincidunt vel quam at tristique. Vivamus viverra dolor sem, quis imperdiet nulla fermentum sit amet. Etiam venenatis in quam et feugiat. Maecenas ut dignissim urna. Suspendisse auctor molestie nulla sit amet lacinia. Phasellus aliquam dignissim dolor tempor malesuada. Sed maximus hendrerit aliquam. Aenean a dui blandit, pellentesque metus eu, malesuada libero. Suspendisse in justo varius, tempus mi et, blandit quam. Aliquam sed aliquet quam. Donec posuere sagittis ante, ac imperdiet lacus bibendum aliquam. Fusce bibendum velit ut ex rutrum, a suscipit lorem tristique. Nulla elementum elit magna, nec feugiat est facilisis non. Sed pretium id arcu id ultrices. Ut ac semper felis. Praesent fermentum nunc sem, at cursus nunc ornare eget. Proin lacinia aliquet lacus, a auctor ante eleifend sed. Vivamus vel molestie ex. Proin vel libero eu mi pharetra congue. Nam ut sem rutrum, pellentesque libero a, luctus libero. Proin tincidunt odio massa, et lacinia massa gravida in. Ut dolor sapien, mollis vel eros non, maximus suscipit neque. Donec eget nulla faucibus, vulputate ipsum sit amet, semper lacus. Sed tempor velit et maximus iaculis. Curabitur dolor lectus, interdum id ligula sed, aliquam convallis ante. Donec id risus eget urna euismod dignissim. Sed porttitor pulvinar ex, in ultricies sem convallis non. Sed fermentum convallis imperdiet. Donec nunc metus, blandit at tincidunt eget, ornare at sapien."
-console.log(text.split(" ").length);
-console.log(text.match(/\set\W/gi).length);
 
-console.log("vai cavalo");
+let wordCount = 1;
+for (let i = 0; i < text.length; i++) {
+    if (text[i] === " ") {
+        wordCount++;
+    }
+}
+console.log(wordCount);
+
+let etCount = 0;
+for (let i = 0; i < text.length; i++) {
+    if (i > 1 
+        && i < text.length - 1 
+        && text[i] === "t" 
+        && text[i-1] === "e" 
+        && (text[i+1] === " " || text[i+1] === "." || text[i+1] === ",") 
+        && text[i-2] === " ") {
+        etCount++;
+    }
+}
+console.log(etCount);
+
+let phraseToCheck = "";
+console.log("veriricar palindromo");
