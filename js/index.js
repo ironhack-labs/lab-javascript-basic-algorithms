0187271296da8d5d1099271f0d7d99f763cea723
Andre <andre.talpo@gmail.com>
2020-01-16 00:27:23 -0300

    Finalizados exercicios necessarios

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 let hacker1 = "Andre";
 console.log(hacker1);
 
-let hacker2 = "Andru";
+let hacker2 = "Caio";
 console.log(hacker2);
 
 // Iteration 2: Conditionals
@@ -15,10 +15,10 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
-hacker1 = hacker1.toUpperCase();
+let nameUpper = hacker1.toUpperCase();
 let nameSpace = "";
 for (let i = 0; i < hacker1.length; i++) {
-    nameSpace = nameSpace + hacker1[i] + " ";
+    nameSpace = nameSpace + nameUpper[i] + " ";
 }
 console.log(nameSpace);
 
@@ -28,13 +28,13 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 }
 console.log(nameReverse);
 
-if (hacker1.toLowerCase() !== hacker2.toLowerCase()) {
-    for (let i = 0; i < hacker1.length; i++) {
-        console.log(i);
-        if (hacker1[i] < hacker2[i]) {
+let iLenght = hacker1.length > hacker2.length ? hacker1.length : hacker2.length;
+if (hacker1 !== hacker2) {
+    for (let i = 0; i < iLenght; i++) {
+        if (hacker1[i] < hacker2[i] || hacker1[i] === undefined) {
             console.log("The driver's name goes first.");
             break;
-        } else if (hacker1[i] > hacker2[i]) {
+        } else if (hacker1[i] > hacker2[i] || hacker2[i] === undefined) {
             console.log("Yo, the navigator goes first definitely.");
             break;
         }
