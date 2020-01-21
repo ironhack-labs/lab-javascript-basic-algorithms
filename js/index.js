fd5dd70349db0f9de34a618fd933fe17a1de68ab
ynnep <adwpenalva@gmail.com>
2020-01-21 12:32:58 +0000

    Solved the lab

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ console.log('I am ready!');
 let hacker1 = 'Antonio';
 console.log(`The driver's name is ${hacker1}.`);
 
+/* could be 1) console.log("The driver's name is " + hacker1);
+            2) console.log("The driver's name is %s", hacker1);
+*/
+
 let hacker2 = 'Diana';
 console.log(`The navigator's name is ${hacker2}`);
 
@@ -19,3 +23,42 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+
+let auxiliaryName = '';
+
+for (let i = 0; i < hacker1.length; i++) {
+  auxiliaryName += hacker1[i].toUpperCase();
+  if (i < hacker1.length - 1) {
+    auxiliaryName += ' ';
+  }
+}
+
+console.log(auxiliaryName);
+
+/* could be:
+let alternativeAuxiliaryName = hacker1.toUpperCase().split('').join(' ');
+console.log(alternativeAuxiliaryName);
+*/
+
+let auxiliaryReverseName = '';
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  auxiliaryReverseName += hacker2[i];
+}
+
+console.log(auxiliaryReverseName);
+
+/* could be:
+let alternativeAuxiliaryReverseName = hacker2.split('').reverse().join();
+console.log(alternativeAuxiliaryReverseName)
+*/
+
+const comparisonOrder = hacker1.localeCompare(hacker2); // MDN localecompare
+
+if (comparisonOrder < 0) {
+  console.log('Yo, the navigator goes first definitely.');
+} else if (comparisonOrder > 0) {
+  console.log("The driver's name goes first.");
+} else {
+  console.log('What?! You both have the same name?');
+}
