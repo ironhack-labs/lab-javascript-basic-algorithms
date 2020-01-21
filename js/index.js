9c21baa000bc3dcc4e2a8d8a63331d0fc4b516f3
Igor Zelenov <igorg.zelenov@gmail.com>
2020-01-21 18:15:34 -0500

    First commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 // Iteration 1: Names and Input
+let hacker1 = "Chaba";
+console.log(hacker1);
 
+let hacker2 = "Igor";
+console.log(hacker2);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log('The driver has a longest name ${hacker1.length}');
+  }
+  else if(hacker1.length < hacker2.length) {
+  console.log("It seems that the navigator has the longest name, it has ${hacker2.length} characters");
+  }
+  else {
+    console.log ("Wow, you both have equally lang names, ${hacker.1length} characters!.")
+  }
 
 // Iteration 3: Loops
+console.log(hacker1.toUpperCase().split('').join(''));
+
+console.log(hacker2.split("").reverse().join(""));
