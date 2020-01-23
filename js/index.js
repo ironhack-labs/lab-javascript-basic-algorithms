c6af870d0ba4018a0e85f355b29d6a39b2d6d4a7
Paulo Salles <plsalles@gmail.com>
2020-01-23 10:00:24 -0300

    Adjustments in the final version

--- js/index.js
+++ js/index.js
@@ -106,13 +106,13 @@ for(let j = 0; j<phrases.length; j++){
     }
   }
 
-  console.log(string1)
+  //console.log(string1)
 
   for(let i = string1.length-1; i >= 0; i-- ){
     string2 += string1[i];
   }
 
-  console.log(string2)
+  //console.log(string2)
 
   if(string1 === string2){
     console.log(`${phraseToCheck} Palíndromo!!!`)
