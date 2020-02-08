2bad6fee2046c714b14bd3ed5ffe29e912784acc
Ben Balderas <benbalderas3@gmail.com>
2020-02-07 18:51:14 -0600

    Iteration 3 Done

--- js/index.js
+++ js/index.js
@@ -18,3 +18,25 @@ if(hacker1Length > hacker2Length) {
 }
 
 // Iteration 3: Loops
+let hacker1Spaced = ""
+let hacker2Backward = ""
+
+for (const char of hacker1) {
+  hacker1Spaced = hacker1Spaced + char.toUpperCase() + " "
+}
+
+console.log(hacker1Spaced)
+
+for (const char of hacker2.split("").reverse()) {
+  hacker2Backward += char
+}
+
+console.log(hacker2Backward)
+
+if (hacker1.localeCompare(hacker2) === 1) {
+  console.log("The driver's name goes first.")
+} else if (hacker1.localeCompare(hacker2) === -1) {
+  console.log("Yo, the navigator goes first definitely.")
+} else {
+  console.log("What?! You both have the same name?")
+}
