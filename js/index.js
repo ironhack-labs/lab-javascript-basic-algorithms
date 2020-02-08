952ce5a9a8a9b70003decd9963ec76819af95b6c
Ben Balderas <benbalderas3@gmail.com>
2020-02-07 19:01:43 -0600

    Bonus 1 Done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,18 @@ if (hacker1.localeCompare(hacker2) === 1) {
 } else {
   console.log("What?! You both have the same name?")
 }
+
+//Bonus 1
+let someText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam egestas leo turpis, vel venenatis diam blandit vel. Pellentesque eu diam quis nisl eleifend imperdiet nec sed nisl. Sed mauris augue, lacinia sed eleifend eget, sodales ut erat. Nunc neque nulla, sodales ac suscipit posuere, fermentum at arcu. Pellentesque porta vehicula lacus et posuere. Proin metus urna, egestas in finibus in, hendrerit eget magna. Fusce hendrerit diam sit amet ex blandit, eu malesuada neque dapibus.
+
+Sed a sodales tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus rhoncus quam a dui efficitur luctus. Donec ut justo ut erat faucibus venenatis et fermentum sapien. Pellentesque imperdiet leo nec luctus interdum. Quisque efficitur, risus in pellentesque pellentesque, libero odio elementum massa, et ultricies massa massa consectetur orci. Donec non sagittis augue. Pellentesque blandit, ligula id placerat vehicula, enim purus pharetra augue, sit amet mollis est nibh vitae diam. Donec arcu enim, consequat eget lectus ac, porta eleifend diam. Suspendisse tristique vitae eros nec luctus. Aenean suscipit, erat condimentum sodales commodo, eros velit pulvinar augue, et fringilla nisi mi vitae velit. Nunc sed molestie est. Aenean eu leo lectus. Phasellus interdum elit nec vestibulum convallis. Quisque gravida massa quis sapien pulvinar egestas. Etiam urna odio, pharetra eu elementum nec, accumsan eget mauris.
+
+Etiam ipsum eros, suscipit sit amet nulla eu, commodo gravida ipsum. Cras vel consectetur felis. Quisque suscipit nibh non ipsum rutrum convallis. Vivamus vitae suscipit mauris. Nullam lorem ante, posuere quis ligula eget, commodo ornare neque. Aliquam condimentum, ante nec rutrum ultricies, ipsum arcu luctus leo, eget viverra eros velit non lacus. Aenean massa lectus, euismod non risus at, aliquet feugiat sapien. Praesent blandit leo a nisi consectetur, a imperdiet neque tristique. Curabitur blandit interdum convallis. Nullam vehicula elit a consectetur euismod.`
+
+let numOfWords = someText.split(" ").length;
+
+for (let i = 0; i <= numOfWords; i++) {
+ console.log(i);
+}
+
+console.log(`The text has ${numOfWords} words`)
