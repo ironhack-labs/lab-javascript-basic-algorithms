f0191203b0c1b491fec7e3e1f77b8d57e94626c8
migueltomarnogueira@mac.com <blink@LAPTOP-ATEBI2SM.localdomain>
2020-02-10 22:09:44 +0000

    First submit Lab 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Roadie";
+console.log("The driver's name is", hacker1);
+let hacker2 = "Passenger"
+console.log("The navigator's name is", hacker2);
 
 // Iteration 2: Conditionals
-
+let nameCount = (hacker2.length)
+console.log(`it seems the ${hacker2} has the biggest name, it has ${nameCount} characters!`)
 
 // Iteration 3: Loops
