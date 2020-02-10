05a4fc0ba53f917ec28f7d195db468558f4d12d2
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-02-10 22:05:21 +0000

    iteration 1/2 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 // Iteration 1: Names and Input
+const hacker1 = "Rui"
+console.log(`The driver's name is ${hacker1}`)
+//-------Iteration 1----------     
+const hacker1 = "Rui";
+console.log(`The driver's name is ${hacker1}`);
 
+const hacker2 = "Miguel";
+console.log(`The navigator name is ${hacker2}`)
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length){
+    console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
+  } else if (hacker1.length < hacker2.length) {
+    console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  } else {
+    console.log(`Wow, you both have equally long names, ${hacker1} characters!`)
+  }
 
 // Iteration 3: Loops
