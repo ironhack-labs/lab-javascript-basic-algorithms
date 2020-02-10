26c8779d34198e1348a8d43fee3af97e0e2bc032
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-02-10 22:05:42 +0000

    Work in progress - finishing iteration 3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Catarina";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "John";
+console.log(`The navigator's name is ${hacker2}`);
+
+
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length){
+    console.log(`The driver ${hacker1} has the longest name, it has ${hacker1.length} characters.`);
+  }else if(hacker1.length < hacker2.length){
+    console.log(`The driver ${hacker2} has the longest name, it has ${hacker2.length} characters.`);
+  }else{
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters.`);
+  }
+
+
 
 // Iteration 3: Loops
+
+let driversNameUpperCase = "";
+
+for (let i = 0; i < hacker1.length; i++){
+  driversNameUpperCase += hacker1[i].toUpperCase() + " ";
+}
+console.log(driversNameUpperCase);
