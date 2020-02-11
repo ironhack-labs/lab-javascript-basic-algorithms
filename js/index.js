275f76ecaeef020e80239e475e8717dd6ff0677b
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-02-11 20:11:08 +0000

    iteration 3 done

--- js/index.js
+++ js/index.js
@@ -18,3 +18,22 @@ if (hacker1.length > hacker2.length){
   }
 
 // Iteration 3: Loops
+let driverName = "";
+for(i=0; i<hacker1.length; i++){
+  driverName += hacker1.charAt(i) + ' ';
+}
+console.log(driverName.toUpperCase());
+
+let navigatorName = "";
+for (i = 5; i >= 0; i--){
+  navigatorName += hacker2[i] ;
+}
+console.log (navigatorName);
+
+if (hacker1.localeCompare(hacker2) < 0 ) {
+  console.log("The driver's name goes first.")
+} else if (hacker1.localeCompare(hacker2) > 0 ) {
+  console.log("Yo, the navigator goes first definitely.")
+} else {
+  console.log("What?! You both have the same name?")
+}
