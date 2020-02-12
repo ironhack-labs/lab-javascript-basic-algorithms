05538c757257890a2c9a9bec8ee5f6d348235f7e
Mihaela Mateescu <miha.mateescu@gmail.com>
2020-02-12 16:54:47 +0000

    Completed bonus 1 and cleaned up some of the previous code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 let hacker1 = "Peter";
 console.log(`The driver's name is ${hacker1}`);
 let hacker2 = "Bob";
-let hacker1Length = hacker1.length;
-let hacker2Length = hacker2.length;
-if (hacker1Length > hacker2Length){
+if (hacker1.length > hacker2.length){
   console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 }
-else if (hacker1Length < hacker2Length)
+else if (hacker1.length < hacker2.length)
 {
 console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 }
@@ -29,3 +27,8 @@ else if (hacker1.localeCompare(hacker2) === 1) {
 else {
   console.log("What?! You both have the same name?");
 }
+function WordCount(str) { 
+  return str.split(" ").length;
+}
+
+console.log(WordCount("lorem ipsum test test test"));
