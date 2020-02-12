68bf270e56d4d46571ebb4eb4c6e82675d2a1282
migueltomarnogueira@mac.com <blink@LAPTOP-ATEBI2SM.localdomain>
2020-02-12 18:32:50 +0000

    Second commit. Conditionals now proprely working

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 // Iteration 1: Names and Input
-let hacker1 = "Roadie";
+let hacker1 = "James";
 console.log("The driver's name is", hacker1);
-let hacker2 = "Passenger"
+let hacker2 = "Bruno"
 console.log("The navigator's name is", hacker2);
 
 // Iteration 2: Conditionals
-let nameCount = (hacker2.length)
-console.log(`it seems the ${hacker2} has the biggest name, it has ${nameCount} characters!`)
+if (hacker1.length > hacker2.length){
+  console.log(`The Driver has the longest name, it has ${hacker1.length} characters!`);
+} else if (hacker1.length < hacker2.length){
+   console.log(`It seems that the Navigator has the longest name, it has ${hacker2.length} characters!`);
+ } else{
+  console.log("Wow you both have equally long names");
+ }
+
 
 // Iteration 3: Loops
