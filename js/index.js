ff4ed7641992fac667d40ba38e478a0f83829ee4
Wilson Cruz <galatoide@gmail.com>
2020-02-12 17:48:22 +0000

    Iteration 3.3 changed for better, bonus 2 solution added.

--- js/index.js
+++ js/index.js
@@ -31,13 +31,23 @@ if(hacker1.length > hacker2.length){
     console.log(b);
 
     // 3.3 
-    if (hacker1>hacker2) {
-        console.log(`The driver's name goes first.`)
-    }else if (hacker1<hacker2){
-      console.log('Yo, the navigator goes first definitely.')
-    }else {
-        console.log('What?! You both have the same name?')
-    }
+            //this was my first try
+    // if (hacker1>hacker2) {
+    //     console.log(`The driver's name goes first.`)
+    // }else if (hacker1<hacker2){
+    //   console.log('Yo, the navigator goes first definitely.')
+    // }else {
+    //     console.log('What?! You both have the same name?')
+    // }
+            // after Paula's (My TA) hint of localeCompare
+        if (hacker1.localeCompare(hacker2)>0) {
+            console.log(`The driver's name goes first.`)
+        }else if (hacker1.localeCompare(hacker2)<0){
+          console.log('Yo, the navigator goes first definitely.')
+        }else {
+            console.log('What?! You both have the same name?')
+        }
+
 
 // Bonus 1 !!
 let lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum eros sit amet purus consequat, a egestas mauris maximus. Duis imperdiet, ante vel vulputate ornare, ante ante fringilla odio, eget iaculis mi turpis ac tortor. Curabitur quis commodo nisl. Aliquam rutrum neque gravida odio malesuada sagittis. Proin magna lorem, laoreet ut auctor non, lacinia vel lorem. Ut nec interdum quam, in dictum tortor. Proin aliquet, est at vehicula pharetra, orci leo pellentesque libero, a fermentum metus nisl nec diam. Nullam feugiat lacinia metus quis mollis.
@@ -68,3 +78,27 @@ console.log(countEt)
 // }
 
 // got stuck thinking about this one
+
+    // Paula's solution that I have to check
+            // //change around phraseToCheck to text results
+            // const phraseToCheck = "Amor, Roma";
+
+            // // 1st Remove spaces from String
+            // let removedSpaces = "";
+            // for(i= 0; i < phraseToCheck.length; i++){
+            //   if(phraseToCheck[i] === " "){
+            //     continue
+            //   } else {
+            //     removedSpaces += phraseToCheck[i].toLowerCase()
+            //   }
+            // }
+            // // 2nd compare every index from front with every index from end
+            // let output = ""
+            // for(i = 0; i < removedSpaces.length; i++) {
+            //   if(removedSpaces[i] === removedSpaces[removedSpaces.length-1-i]) {
+            //     output = `${phraseToCheck} is a Palindrome. :) `
+            //   } else {
+            //     output = `${phraseToCheck} is not a Palindrome. :( `
+            //     break }
+            // }
+            // console.log("Bonus 2: ",output)
