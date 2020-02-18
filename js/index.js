a2d32225a4d009308ceae6c168f31a844f69afc2
Zetzher <fyeah16@gmail.com>
2020-02-18 18:02:31 +0100

    Final

--- js/index.js
+++ js/index.js
@@ -41,3 +41,12 @@ if(hacker1.localeCompare(hacker2) == -1){
 } else {
   console.log('What?! You both have the same name?');
 }
+
+//Bonus
+
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius vitae risus vel consequat. Sed at mauris ac mi accumsan suscipit. Aliquam pharetra, mi at molestie aliquam, augue justo gravida nisi, vel rutrum dui ipsum ut enim. Cras ac molestie velit, vel efficitur odio. Ut ultricies tempor odio, sed scelerisque mauris ullamcorper ut. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec vel ante sit amet odio vehicula consequat. Ut rhoncus imperdiet quam nec blandit. Duis varius convallis nibh sed cursu. Etiam laoreet odio non urna tristique, ac interdum urna vulputate. Proin cursus, diam et feugiat luctus, magna eros interdum ante, et semper leo turpis ac lacus. Fusce consectetur finibus est et lobortis. Nam felis ante, maximus eget augue quis, suscipit interdum felis. Etiam nec rhoncus mauris. Nunc elementum vestibulum neque, vel venenatis dui. Vivamus lorem est, sollicitudin fermentum feugiat vitae, faucibus ultricies augue. Ut tempus lectus at tortor tristique molestie. Aenean non elit a dui rhoncus ultricies. Nullam tempus sagittis risus, ut pellentesque mi luctus eu. Donec a magna a mi volutpat ultricies. Morbi feugiat mauris in ultrices scelerisque. Integer aliquet leo vel magna imperdiet, ac bibendum libero cursus. Maecenas pretium efficitur varius. Duis porta a felis in fringilla. Etiam sollicitudin urna sapien, et malesuada nibh finibus vestibulum. Integer sodales, quam ut rutrum aliquam, odio metus accumsan ante, sed semper orci magna ut nulla. Mauris blandit in sem ac efficitur. Donec quis euismod libero. Ut vehicula nisi nec velit vehicula venenatis. Curabitur sit amet dui eros. Sed sapien elit, rutrum at condimentum imperdiet, sodales ut diam. In magna augue, finibus non neque eu, bibendum laoreet nulla. Suspendisse potenti. Maecenas bibendum mollis feugiat. Etiam at vestibulum justo. Cras lacinia nisi id eros tincidunt, quis molestie turpis auctor. Maecenas luctus elementum enim ut pellentesque. Curabitur id pellentesque nisl. Ut maximus interdum lectus, vel scelerisque mauris lacinia non.';
+
+//Bonus 1
+console.log(lorem.split(' ').length)
+
+console.log(lorem.split('et').length)
