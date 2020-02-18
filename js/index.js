9516584bdb9ed02439f5ee7d9624badfba4c5802
Sofia Santa Maria <yolaresofia@gmail.com>
2020-02-18 20:32:50 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,76 @@
 // Iteration 1: Names and Input
+let hacker1 = "Sofia";
+console.log(hacker1);
 
+let hacker2 = "Raul";
+console.log(hacker2);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters`);
+}
+
 
 // Iteration 3: Loops
+//1
+for (i = 0; i<hacker1.length; i++){
+    result += hacker1.charAt(i).toUpperCase() + " ";
+}
+
+console.log(result);
+
+console.log(
+    hacker1
+    .split("")
+    .join(" ")
+    .toUpperCase()
+);
+
+//2
+
+for(i=hacker1.length; i>=0; i--) {
+    result += hacker1.charAt(i);
+  }
+
+  console.log(result);
+
+  console.log(
+    hacker1
+    .split("")
+    .reverse()
+    .join("")
+);
+
+//3
+
+switch (hacker1.localeCompare(hacker2)) {
+    case 1:
+    console.log("The drivers name goes first");
+    break;
+    case -1:
+    console.log("Yo, the navigator goes first definitely.");
+    break;
+    default:
+    console.log("What?! You both have the same name?");
+}
+
+//BONUS 1
+
+let lorem = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Corporis harum eaque corrupti nostrum autem vitae, ab blanditiis molestiae,
+voluptatum totam beatae cupiditate quis hic tenetur quae optio sit sed aperiam.`;
+
+function countLorem(str){
+  let splitLorem=str.split(" ").length;
+  console.log("resultado:",splitLorem)
+}
+
+countLorem(lorem);
+
+
+
