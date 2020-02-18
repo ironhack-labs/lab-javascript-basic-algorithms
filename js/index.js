dfadf2f6f96d0b8ec9e4648537ca3f2632327f91
Cristina Garcia <cristinagarciagrillo@gmail.com>
2020-02-18 20:21:58 +0100

    Finished Iterations 1, 2 and 3

--- js/index.js
+++ js/index.js
@@ -16,7 +16,6 @@ if (hacker1.length > hacker2.length) {
 } else {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters.`)
 }
-;
 
 
 // Iteration 3: Loops
@@ -28,7 +27,7 @@ let separated = "";
 for (let i = 0; i < driverUpperCase.length; i++) {
     separated += driverUpperCase[i] + " ";
 }
-;
+
 
 console.log(separated);
 
@@ -36,11 +35,10 @@ console.log(separated);
 //3.2
 let reversed = "";
 
-console.log(navigator.length)
+
 for (let j = navigator.length - 1; j >= 0; j--) {
-    console.log("hola")
     reversed += navigator[j] + "";
-};
+}
 
 console.log(reversed);
 
@@ -51,7 +49,15 @@ if (hacker1 === hacker2) {
     console.log("The driver's name goes first");
 } else {
     console.log("Yo, the navigator goes first definitely");
-};
+}
 
+
+//BONUS 1
+
+let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a massa porta ipsum scelerisque efficitur. Mauris erat odio, egestas quis ullamcorper non, venenatis nec metus. Aliquam auctor, libero id rutrum vestibulum, lectus metus condimentum nibh, tincidunt suscipit sem elit sed est. Aliquam egestas mauris lectus. Nullam varius nulla eget tellus hendrerit, sit amet convallis augue blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pretium erat tempus velit blandit mollis. Praesent varius ex sit amet augue hendrerit ornare. Vivamus fringilla fermentum facilisis. Duis in egestas sem, ac blandit eros. Nullam id velit augue. Fusce consequat arcu non porttitor ultricies. Suspendisse non arcu non magna ornare congue. Vivamus in ligula at lorem aliquam tempus. Donec rutrum, justo sed venenatis egestas, felis tortor varius lacus, eu finibus ante sapien a urna. Etiam dui libero, porta non suscipit ac, venenatis non nisi. In commodo luctus consequat. Curabitur luctus diam et metus sodales, sit amet commodo ex tempor. Fusce et lobortis dui, ac tristique urna. Phasellus placerat sed orci a facilisis. Nam mollis, ligula eu iaculis hendrerit, ante elit tempus sapien, nec finibus metus metus at justo. Nam pharetra lectus ligula, sit amet tincidunt risus tempus vitae. Vivamus molestie nisl vitae nibh elementum, nec scelerisque ante commodo. Donec rhoncus metus in velit tincidunt, vel ullamcorper mi tempor. Vestibulum tristique tincidunt sapien, nec pretium neque venenatis dignissim. Nam tempor sapien et mauris molestie commodo. Proin quis interdum nibh. Cras semper nibh ipsum, nec porta elit porttitor sit amet. Fusce efficitur eros a mi finibus, aliquam fermentum mi viverra. Etiam ultrices justo libero. Maecenas aliquam ultrices quam ac sollicitudin.";
+
+console.log(text.split(" ").length);
+
+console.log(text.split("et").length);
 
 
