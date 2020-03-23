a423b441fc4d5720b3c7eed76f12967cbca67436
Jan Redmann <janredmann1@gmail.com>
2020-03-23 16:41:58 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 // Iteration 1: Names and Input
-const hacker1 = 'Jan';
-console.log(`The drivers's name is ${hacker1}`);
-const hacker2 = 'Patrick';
-console.log(`The navigator's name is ${hacker2}`);
-
+console.log('done');
 // Iteration 2: Conditionals
 
 // Iteration 3: Loops
