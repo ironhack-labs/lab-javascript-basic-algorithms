9a7742febcbfa6703ec184c5880d6a55ec9bcdd6
Lalib <louis.alib19@gmail.com>
2020-03-25 16:10:46 +0100

    Iteration3

--- js/index.js
+++ js/index.js
@@ -19,3 +19,32 @@ else {
 
 
 // Iteration 3: Loops
+
+
+let hacker1Spaced = "";
+for (let i = 0; i < hacker1.length ; i++) {
+    hacker1Spaced += hacker1[i] +  " ";
+}
+
+console.log(hacker1Spaced.toUpperCase());
+
+
+let hacker2Reversed = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    hacker2Reversed += hacker2[i];
+
+}
+
+console.log(hacker2Reversed);
+
+
+
+if (hacker1 < hacker2) {
+    console.log(`The driver's name goes first.`);
+
+} else if (hacker1 > hacker2) {
+    console.log(`Yo, the navigator goes first definitely.`);
+} else {
+    console.log(`What?! You both have the same name?`);
+}
+
