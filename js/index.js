c71c0098ce645c0a4edd3cae98a046bb2d866c8f
Maxime Paolone <mxpln@outlook.com>
2020-03-29 16:14:31 +0200

    Iteration 4

--- js/index.js
+++ js/index.js
@@ -43,21 +43,43 @@ console.log(res);
 //   i++;
 //   console.log(i);
 // }
-for (let i = 0; i < hacker1.length; i++) {
-  for (let j = 0; j < hacker2.length; j++) {
-    if (hacker1[i] > hacker2[j]) {
-      console.log("The driver's name goes first.");
-      break;
-    } else if (hacker1[i] < hacker2[j]) {
-      console.log("Yo, the navigator goes first definitely.");
-      break;
-    } else {
-      console.log("What?! You both have the same name?");
-      break;
-    }
-  }
-  console.log(i);
-}
+// for (let i = 0; i < hacker1.length; i++) {
+//   for (let j = 0; j < hacker2.length; j++) {
+//     if (hacker1[i] > hacker2[j]) {
+//       console.log("The driver's name goes first.");
+//       break;
+//     } else if (hacker1[i] < hacker2[j]) {
+//       console.log("Yo, the navigator goes first definitely.");
+//       break;
+//     } else {
+//       console.log("What?! You both have the same name?");
+//       break;
+//     }
+//   }
+//   console.log(i);
+// }
 // The driver's name goes first.
 // Yo, the navigator goes first definitely.
 // What?! You both have the same name?
+
+// Iteration 4
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit dolor eget convallis consectetur. Donec posuere elementum justo, quis fringilla dui ornare sit amet. Nullam nisi dolor, semper sit amet magna sed, sollicitudin tincidunt nunc. Integer porta elit id gravida semper. Phasellus porta massa dapibus ligula placerat sollicitudin. Donec et rhoncus lorem. Curabitur vestibulum venenatis est, quis tristique sapien molestie congue. Vivamus posuere sagittis lorem. Aenean vulputate nunc in risus interdum, ut sollicitudin tortor tempus. Integer vel metus nisi. Morbi cursus, nisl bibendum malesuada feugiat, risus felis auctor felis, non tincidunt odio lectus nec neque. Integer elit mauris, placerat at nulla nec, tempus pretium orci. Sed id nibh quis dui pretium volutpat sed at mauris. Vivamus venenatis eu mi nec posuere. Nullam vel velit id nisl auctor congue quis at magna. Nunc dignissim cursus ex, non fermentum dui hendrerit non. Aenean eget lobortis massa, eu sollicitudin urna. Vestibulum ut massa nec risus eleifend eleifend. Fusce vehicula pretium gravida. Proin convallis volutpat feugiat. Sed tincidunt, nulla mattis dictum iaculis, mauris enim pretium nisl, a luctus purus purus vel neque. Quisque sed malesuada mi. Donec mollis lectus in nibh pretium rhoncus. Suspendisse blandit lacus nec imperdiet pharetra. Cras facilisis nunc eu nisi fringilla, a posuere magna blandit. Duis vitae diam metus. Nullam iaculis turpis eget magna dignissim, sed mattis nisi porttitor. Maecenas ut arcu quis nunc ullamcorper vehicula. Curabitur scelerisque mi aliquet nisi sollicitudin, eu fringilla nibh condimentum. Sed a sapien gravida, blandit lectus luctus, porta quam. Pellentesque sollicitudin velit et mattis luctus. Sed laoreet dapibus feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque scelerisque lobortis luctus. Praesent vel purus tempus, fringilla velit a, viverra tellus. Aliquam posuere porttitor velit et vulputate. Mauris eget egestas elit. Nullam tincidunt leo quis lectus vulputate, nec porttitor ante tincidunt. Praesent congue tortor nulla.";
+
+function howManyLatinWords(latinSentence) {
+  let withoutPonct = latinSentence.replace(",", "");
+  let arrayLatin = withoutPonct.split(" ");
+  let wordsCount = 0;
+  let etCount = 0;
+  for (i = 0; i < arrayLatin.length; i++) {
+    if (arrayLatin[i] == "et") {
+      wordsCount += 1;
+      etCount += 1;
+    } else {
+      wordsCount += 1;
+    }
+  }
+  return `The sentence has ${wordsCount} words, and among them ${etCount} "et".`;
+}
+
+console.log(howManyLatinWords(lorem));
