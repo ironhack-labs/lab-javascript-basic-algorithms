04cc541f0fb93072557e49ef02c2c3c9311b1951
Alba Gustems <albagustems@gmail.com>
2020-03-29 16:08:24 +0200

    Bonus of the exercises

--- js/index.js
+++ js/index.js
@@ -55,3 +55,36 @@ for(let i = 0; i < alphabet.length; i++){
     continue;
   }
 };
+
+// Bonus 1: Make your program count the number of times the Latin word et appears
+let string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris erat augue, vestibulum eget urna quis, finibus suscipit metus. Nam vulputate laoreet lorem, sed euismod dui ornare ut. Nunc at aliquam mauris, sed hendrerit augue. Vestibulum vitae ante tincidunt diam vestibulum auctor id vel augue. Nullam auctor, ipsum non finibus interdum, nunc turpis feugiat leo, vel tincidunt tortor odio vestibulum leo. Praesent ullamcorper sapien non convallis aliquet. Vestibulum ac diam in nulla tempus sagittis eget sed enim. Quisque sem sapien, rhoncus eu blandit vel, fermentum sed lectus.
+
+Maecenas ac neque facilisis, egestas eros quis, dictum libero. Aliquam scelerisque vulputate quam, volutpat posuere risus imperdiet in. Praesent pulvinar id dolor et luctus. Maecenas posuere ac nisl sed convallis. Fusce id porttitor ante. Nulla facilisi. Etiam sollicitudin eros in dictum venenatis. Sed euismod, nisi sed tincidunt suscipit, leo ante luctus nisl, sed sollicitudin augue orci nec erat. Nullam non odio id lectus aliquam aliquam. Donec fermentum porttitor massa et vestibulum. Etiam et leo lectus. Nulla pellentesque libero ut odio tempor mollis. Cras ultrices lectus sed rhoncus suscipit.
+
+Fusce varius pharetra mauris iaculis ullamcorper. Mauris neque felis, scelerisque nec tempus ut, fringilla ac erat. Praesent vel sagittis libero, a luctus turpis. Nulla quis tincidunt velit, et tempus dolor. Etiam scelerisque sem quis arcu aliquam rutrum. Aenean tortor urna, finibus quis nisi a, feugiat rhoncus enim. Sed congue turpis in nunc pulvinar, nec pharetra felis efficitur.`;
+
+let searchFor = 'et';
+let count = 0;
+let pos = string.indexOf(searchFor);
+
+while (pos > -1) {
+    ++count;
+    pos = string.indexOf(searchFor, ++pos);
+}
+
+console.log(count);
+
+//Bonus 2: Write a code that will check if the value we assigned to this variable is a Palindrome. 
+let phraseToCheck = "Amor, Roma";
+let palindromeLeft = phraseToCheck.split(" ").join('').replace(',', '').toLowerCase();
+
+let palindromeRight = "";
+for (let i = palindromeLeft.length-1; i >= 0; i--) {
+  palindromeRight += palindromeLeft[i].toLowerCase();
+}
+
+if (palindromeLeft === palindromeRight){
+  console.log(true);
+} else {
+  console.log(false);
+};
