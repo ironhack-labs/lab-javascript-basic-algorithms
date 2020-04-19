2bbc9ad6cebbffc0992937da9ea57dbd527e4fc3
Sofia <anasofia.costa90@gmail.com>
2020-04-19 16:26:28 +0100

    Bonus1, bonus2 not finished

--- js/index.js
+++ js/index.js
@@ -52,3 +52,29 @@ if (hacker1.toUpperCase() > hacker2.toUpperCase()) {
 } else {
   console.log("What?! You both have the same name?");
 }
+
+// Bonus 1
+
+let bonus1 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit quis dui sit amet eleifend. Nulla sollicitudin iaculis convallis. Cras ac tempor nulla, at tristique purus. Nunc mollis a nunc vitae feugiat. Aliquam et elit in velit auctor ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit, sapien lacinia fermentum tincidunt, nulla enim pulvinar erat, ac dictum odio sapien a metus.
+
+Ut ornare orci erat, id pellentesque quam gravida eu. Praesent tincidunt suscipit ipsum quis pretium. Etiam dui risus, ullamcorper eu tincidunt et, egestas sed magna. Nullam tortor nisl, mollis eu malesuada eget, venenatis a sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed eleifend quam ipsum, bibendum luctus arcu varius nec. Aliquam erat volutpat. Mauris quis tincidunt libero. Cras nec nibh et orci blandit interdum vel eu nunc.
+
+Etiam tincidunt bibendum justo a tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ut diam metus. Nunc quis velit et ante interdum feugiat eget vel ipsum. Aenean arcu mi, porttitor vel turpis a, gravida mattis enim. Vivamus lacinia erat sapien, pulvinar feugiat nisi elementum in. Etiam iaculis aliquam erat fermentum ultricies. Donec non ligula eu quam ornare mollis non eget quam. Aenean vitae elit et justo suscipit viverra. Nunc gravida massa orci, ullamcorper tincidunt eros congue eget. Nullam vel interdum magna, et pharetra mauris. Nulla pellentesque nisl ut ante pulvinar rhoncus. Donec quam eros, ullamcorper quis ornare porttitor, vestibulum id nulla. Maecenas at eleifend ligula. Vestibulum bibendum ante ac massa vestibulum scelerisque vitae nec est.`;
+
+console.log(bonus1.split(" ").length); // number of words in this string
+
+console.log(bonus1.split("et").length - 1); // number of times that "et" exists
+
+//Bonus 2 --> not finished
+
+let phraseToCheck = "A man, a plan, a canal, Panama!";
+/*"Amor, Roma"
+"race car"
+"stack cats"
+"step on no pets"
+"taco cat"
+"put it up"
+"Was it a car or a cat I saw?" and "No 'x' in Nixon".*/
+
+console.log(phraseToCheck.replace(" ", "").replace(",", "").replace("!", ""));
+// replace just remove the first special character of each one, so nthat's not the answer
