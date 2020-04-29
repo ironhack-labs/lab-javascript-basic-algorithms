748fcf1fff5bb51d8b9e1bd812c95507f977f283
Daniel <izquiano@hotmail.com>
2020-04-29 21:02:20 +0200

    Primer paso hecho

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
+console.log("I'm ready!")
 // Iteration 1: Names and Input
+const hacker1 = "Daniel"
+console.log(`The driver's name is ${hacker1}`)
 
+const hacker2 = "Roman"
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
+
 
 
 // Iteration 3: Loops
