6a038b74e2ee8772e5c71c3c6851a5991885d7ea
Miguel Valle <miguelbachi5a@gmail.com>
2020-04-30 16:37:47 +0200

    Add Iteration 3: Loops

--- js/index.js
+++ js/index.js
@@ -17,3 +17,34 @@ if (hacker1.length > hacker2.length) {
 
 
 // Iteration 3: Loops
+let nameSeparated = '';
+for (let i = 0; i < hacker1.length; i++){
+    nameSeparated+= hacker1[i];
+    if (i !== hacker1.length - 1){
+        nameSeparated+= ' ';
+    }
+}
+console.log(nameSeparated.toUpperCase());
+
+let nameReversed = '';
+for (let i = hacker1.length - 1; i >= 0; i--){
+    nameReversed += hacker1[i];
+}
+console.log(nameReversed);
+/*Shorter way:
+let splittedName = hacker1.split(""); 
+
+let reversedArrayName = splittedName.reverse();
+
+console.log(reversedArrayName.join(""));
+*/
+
+
+
+if (hacker1.localeCompare(hacker2) === 0){
+    console.log('What?! You both have the same name?');
+} else if (hacker1.localeCompare(hacker2) === 1){
+    console.log('Yo, the navigator goes first definitely.');
+} else {
+    console.log("The driver's name goes first.");
+}
