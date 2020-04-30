e5afa0aecc02e6640f5ba99f1b912e54c755961b
MiguelValle94 <60062986+MiguelValle94@users.noreply.github.com>
2020-04-30 16:39:58 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,10 +11,7 @@ if (hacker1.length > hacker2.length) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
 } else {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
-}
-
-
-
+};
 
 // Iteration 3: Loops
 let nameSeparated = '';
@@ -23,13 +20,13 @@ for (let i = 0; i < hacker1.length; i++){
     if (i !== hacker1.length - 1){
         nameSeparated+= ' ';
     }
-}
+};
 console.log(nameSeparated.toUpperCase());
 
 let nameReversed = '';
 for (let i = hacker1.length - 1; i >= 0; i--){
     nameReversed += hacker1[i];
-}
+};
 console.log(nameReversed);
 /*Shorter way:
 let splittedName = hacker1.split(""); 
@@ -39,12 +36,10 @@ let reversedArrayName = splittedName.reverse();
 console.log(reversedArrayName.join(""));
 */
 
-
-
 if (hacker1.localeCompare(hacker2) === 0){
     console.log('What?! You both have the same name?');
 } else if (hacker1.localeCompare(hacker2) === 1){
     console.log('Yo, the navigator goes first definitely.');
 } else {
     console.log("The driver's name goes first.");
-}
+};
