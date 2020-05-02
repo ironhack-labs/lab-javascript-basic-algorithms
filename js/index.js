db61d799d0c61c11af385987d6fd54d2cd3c3260
Irma <iblanc12@corp.vodafone.es>
2020-05-02 15:00:01 +0200

    feat: bonus 1

--- js/index.js
+++ js/index.js
@@ -52,3 +52,31 @@ getAllCharacters(hacker1)
 ​
 getCharacterReverse(hacker1);
 
+// Bonus Time
+// Bonus 1
+const paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget elit eu dolor pharetra elementum. Suspendisse magna erat, ultricies non urna eu, sodales molestie enim. Nulla id est venenatis, sollicitudin lorem quis, hendrerit nisi. Sed vestibulum nibh rhoncus est pharetra, sit amet semper magna lobortis. Fusce felis leo, luctus nec neque nec, bibendum ultrices augue. Donec ullamcorper nibh ut luctus malesuada. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In dolor leo, euismod sed leo ut, semper vestibulum felis. Sed tempor, risus eu iaculis consectetur, est ex lacinia mi, id lobortis orci magna sed neque. Sed placerat iaculis scelerisque. Duis fringilla eros sit amet sollicitudin ultrices.\nQuisque eget arcu sed dui sollicitudin tempus. Sed eu leo eget nisi pulvinar scelerisque. Etiam sodales dolor non dolor finibus vehicula et nec ex. Nulla vestibulum metus pellentesque facilisis commodo. Duis maximus velit tincidunt lorem eleifend, et facilisis felis pretium. Integer quis vulputate eros, eget tempor purus. Sed dui magna, euismod sit amet blandit a, facilisis non augue. Etiam ullamcorper pharetra erat, eu efficitur sem viverra ut. Sed vestibulum maximus aliquet. Mauris semper ipsum sit amet nibh placerat consequat. Pellentesque sollicitudin lacinia felis vel volutpat. Proin facilisis, quam a venenatis blandit, elit nulla volutpat enim, vel rutrum est nibh sed quam. Morbi tempus ante ut eros venenatis finibus eget in tellus. Mauris ex arcu, tristique at convallis ut, tempus nec lectus. Sed vehicula est sed lorem consequat, in tincidunt dolor lobortis. Cras malesuada a felis quis dictum.\nFusce elementum porta nibh, quis feugiat est ullamcorper a. Phasellus vitae euismod elit. Sed sit amet lectus sollicitudin, ultrices odio et, fringilla nibh. Aliquam eu elit aliquam arcu mollis facilisis. Donec risus urna, tempor at finibus nec, laoreet vehicula augue. Vivamus ut odio tristique ex cursus mattis non eu magna. Cras commodo vitae risus at fermentum. Nullam imperdiet sit amet tortor ac tincidunt.' 
+​
+function getAllWords(string) {
+  let wordCount = 0;
+  const totalLineBreaks = paragraph.split('\n').length
+  for (let index = 0; index <= string.length; index++) {
+    const characterFounded = string.charAt(index)
+    if(characterFounded === ' ') {
+      wordCount++
+    }
+  }
+  const totalWords = totalLineBreaks+wordCount
+  console.log('totalWords', totalWords)
+}
+​
+getAllWords(paragraph)
+​
+const searchAllEnters = paragraph.match(/\n/igm).length
+const searchAllSpaces = paragraph.split(' ').length
+​
+const searchTotalWords = searchAllEnters+searchAllSpaces
+console.log(`Total words count is: ${searchTotalWords}`)
+​
+const searchWordCaseSenitive = paragraph.match(/et /g).length
+​
+console.log(`Total word 'et' founded is: ${searchWordCaseSenitive}`)
