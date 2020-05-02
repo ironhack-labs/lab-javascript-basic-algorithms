bcf53ae9ebb710edf0f21c840b244d7226e66002
Nidian Martin <nidianmartin@gmail.com>
2020-05-02 13:04:11 +0200

    Iteración 3

--- js/index.js
+++ js/index.js
@@ -15,3 +15,33 @@ if (hacker1Long > hacker2Long) {
 } else {
   console.log(`Wow, you both have equally long names, ${hacker2Long} characters!`)
 }
+
+// Iteration 3: Loops
+// Ejercicio 3.1
+let driver = ''
+for (let i = 0; i < hacker1.length; i++ ) {
+  driver += hacker1[i].toUpperCase() + ' '
+}
+console.log(driver);
+
+//Ejercicio 3.2
+let navigator = ''
+for (let i = hacker2.length -1; i >= 0 ; i--) {
+  navigator += hacker2[i]
+}
+console.log(navigator);
+
+const comparation = hacker1.localeCompare(hacker2)
+
+//Ejercicio 3.3
+switch (comparation) {
+  case -1:
+    console.log(`${hacker1} name goes first`)
+    break;
+  case 1:
+    console.log(`${hacker2} goes first definitely`)
+    break;
+  default:
+    console.log('What?! You both have the same name?')
+    break;
+}
