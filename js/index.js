82a9a13849d5da454ec01d51e35604a3fa4f9f3d
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-06-04 21:18:38 +0200

    iteration 1

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 
 console.log("I'm ready!");
 
+let hacher1 = "wafaa";
+console.log(`The driver's name is ${hacher1}`);
+
+let hacker2= "marwa";
+console.log(`The navigator's name is ${hacker2}`)
+
 
 // Iteration 2: Conditionals
 
