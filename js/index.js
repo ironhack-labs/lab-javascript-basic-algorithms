08f6689980c2cc0dc13e29b76e36e720d2dcfd5c
Nina Quaresma <quaresma.nina@gmail.com>
2020-06-04 21:58:35 +0200

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -12,13 +12,34 @@ console.log(`The navigator's name is ${hacker2}`);
 
 var longNina = hacker1.length; // 4
 var longEnine = hacker2.length; // 5
-var longestname =
-    if (longNina < longEnine) {
-        console.log(`The navigator has the longest name, it has ${longEnine}`);
-    } else if (longEnine < longNina) {
+
+if (longNina < longEnine) {
+    console.log(`The navigator has the longest name, it has ${longEnine}`);
+} else if (longEnine < longNina) {
     console.log(`It seems that the driver has the longest name, it has ${longNina} characters`)
 } else {
     console.log(`Wow, we both have equally long names, ${longNina} characters!`);
 }
 
 // Iteration 3: Loops$
+
+//3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+//3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+//3.3 Depending on the lexicographic order of the strings, print:
+//- The driver's name goes first.
+//- Yo, the navigator goes first definitely.
+//- What?! You both have the same name?
+
+var hacker1Upp = hacker1.toUpperCase();
+var hacker1Cut = hacker1Upp.split("");
+
+var hacker1Array = new Array();
+for (var i = 0; i < hacker1Cut.length; i++) {
+    hacker1Array.push(hacker1Cut[i]);
+    if (i != hacker1Cut.length - 1) {
+        hacker1Array.push(" ");
+    }
+}
+//for (i= 0; i < hacker1Upp; i++){
+//console.log(i);
+//}
