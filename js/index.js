e0f8887b566da2ef8b6782141136c25fd6d322bf
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-06-05 20:40:47 +0200

    Iteration 3: loops

--- js/index.js
+++ js/index.js
@@ -17,3 +17,28 @@ if (hacker1.length > hacker2.length){
   }
 
 // Iteration 3: Loops
+
+let result1 = "";
+  for(let i = 0; i<hacker1.length; i++){
+       result1 = result1 + hacker1[i] + " ";
+  }
+  console.log(result1.toUpperCase());
+  
+  
+  let result2 = "";
+  for(let i = hacker2.length-1; i>= 0; i--){
+      result2 = result2 + hacker2[i];
+  }
+   console.log(result2);
+
+   switch(hacker1.localeCompare(hacker2)){
+      case 1 :
+      console.log('The driver\'s name goes first.');
+      break;
+      case -1 :
+      console.log('Yo, the navigator goes first definitely.');
+      break;
+      case 0 :
+      console.log('What?! You both have the same name?');
+      break;
+   }
