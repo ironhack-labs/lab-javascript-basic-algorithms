c0a51dfa95aa89bf0c80bb8ebc5a3d39a220a285
ajhdrummer <thedrew88@gmail.com>
2020-06-08 17:47:45 -0400

    improved wordCounter function

--- js/index.js
+++ js/index.js
@@ -55,11 +55,7 @@ else{
 let paragraphs = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dignissim odio at odio efficitur luctus. Praesent nec tincidunt purus, ut ullamcorper erat. Mauris nec viverra tellus, sit amet ultrices dolor. Morbi sollicitudin sem tellus, nec euismod elit ultrices eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lacinia elit sed tortor sodales accumsan sed vitae neque. Fusce eget lacus convallis, rutrum risus nec, fringilla erat. Vestibulum ex dui, egestas eu imperdiet quis, dictum eu quam. Mauris tempus ullamcorper metus. Nunc eu sollicitudin ex.Morbi et urna tempus augue suscipit dapibus egestas vitae turpis. Nullam posuere vitae libero eget eleifend. Vivamus augue massa, molestie at euismod a, vulputate eget metus. Nulla elit odio, efficitur quis dignissim eu, sodales vel nulla. Proin et venenatis odio. Suspendisse dapibus rhoncus ante eu consectetur. Vivamus lectus est, sollicitudin ac pretium vel, porttitor feugiat erat. Integer non ligula nec lacus malesuada congue at ut magna.Praesent a blandit felis, ac tincidunt turpis. Sed facilisis gravida lacus, non suscipit massa viverra sit amet. Praesent sed enim quis ipsum placerat maximus at elementum nisi. Vivamus consectetur purus ac urna laoreet, sit amet tempus lectus tempor. Sed vel blandit nunc. Quisque orci lacus, bibendum et nisi quis, dignissim suscipit tellus. Proin et nisl a nisl hendrerit placerat id sed ligula. Maecenas malesuada ullamcorper auctor. Donec condimentum turpis vitae dui varius varius. Nam at nibh ac sapien dapibus ultricies. Proin ut metus ut magna imperdiet posuere. In eu dolor sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
 
 const wordCounter = paragraphs =>{
-	let counter = 0
-	for (const word in paragraphs.split(' ')){
-		counter+=1
-	}
-	console.log(counter)
+	console.log(paragraphs.split(' ').length)
 }
 
 wordCounter(paragraphs)
