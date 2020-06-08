04da281119fd8c56883c65a6d425d74319ecde30
Renzo Pederzoli <rdp73@miami.edu>
2020-06-08 14:22:14 -0400

    first iteration

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-var hacker1 = 'Joe';
-console.log(`The driver's name is ${hacker1}`);
-var hacker2 = 'Jane';
-console.log(`The navigator's name is ${hacker2}`);
+let hacker1 = 'Joe'
+console.log(`The driver's name is ${hacker1}`)
+let hacker2 = 'Jane'
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
