f53034e4f1931079679da7c5e1d3129ed59bb027
koladeg <tayokay@yahoo.com>
2020-06-08 20:22:33 +0100

    iteration 1 push

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 // Iteration 1: Names and Input
 let hacker1 = "kay"
 // driver
-console.log(hacker1)
+console.log("The driver's name is " + hacker1)
 
 let hacker2 = 'doja' 
 // navigator
 
-console.log(hacker2)
+console.log("The navigator's name is YYYY" + hacker2)
 
 // Iteration 2: Conditionals
 
