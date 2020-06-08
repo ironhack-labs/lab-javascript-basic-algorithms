cb6fb9ca1f3df66c22b5e2e82cadd8975f737a6d
koladeg <tayokay@yahoo.com>
2020-06-08 20:08:04 +0100

    initail commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
+let hacker1 = "kay"
+// driver
+console.log(hacker1)
 
+let hacker2 = 'doja' 
+// navigator
+
+console.log(hacker2)
 
 // Iteration 2: Conditionals
 
