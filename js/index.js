ee6111fe1f1df16b933d11edb1296665859de54a
vlee13 <vlee13@gmail.com>
2020-06-08 13:17:52 -0700

    iteration3 partial complete

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-let hacker1 = valerie
+let hacker1 = 'valerie';
 console.log(`The driver's name is ${hacker1}`)
 
-let hacker2 = peter 
+let hacker2 = 'peter';
 console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
@@ -10,9 +10,11 @@ console.log(`The navigator's name is ${hacker2}`)
 if(hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 } else if(hacker1.length < hacker2.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length}`.);
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length}.`);
 } else if(hacker1.length = hacker2.length) {
     console.log(`Wow, you both have equally long names, ${hacker1.length}!`)
 }
 
 // Iteration 3: Loops
+
+console.log(hacker1.toUpperCase().split('').join(' '));
