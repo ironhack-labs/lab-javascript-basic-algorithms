8c3834790d8fdf65e22e7f80669307f94da8ded1
Renzo Pederzoli <rdp73@miami.edu>
2020-06-08 15:58:28 -0400

    bonus one

--- js/index.js
+++ js/index.js
@@ -40,3 +40,21 @@ else if ( hacker2 < hacker1) {
 else {
   console.log('What?! You both have the same name?')
 }
+
+//Bonus 1
+
+let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie sed lorem eu mollis. Nullam eu metus erat. Vivamus rhoncus porta erat non porta. Sed consequat nunc elit, imperdiet molestie nulla dictum ac. Vestibulum in enim id nisl mollis hendrerit. Integer vel malesuada elit. Nam at luctus velit. Nulla quis sollicitudin tellus. Aliquam pellentesque, risus ac commodo bibendum, nibh nisi placerat sapien, eu maximus mauris arcu vitae est. Integer accumsan ex vitae erat varius, bibendum pretium orci ornare. Curabitur a massa nec nulla pulvinar aliquam id malesuada lorem. Suspendisse rhoncus nisi a quam sodales, id posuere odio rhoncus. Donec at ante vel felis vulputate egestas. Nunc id condimentum nibh. Donec efficitur enim lectus, sed feugiat nisi vulputate non. Integer vel pharetra odio. Pellentesque sit amet magna ante. Cras congue est eros, a eleifend massa tempus et. Praesent auctor elementum est ac molestie. Praesent fringilla volutpat lacus, a commodo velit volutpat vel. Aenean dictum ligula enim, non mollis odio semper nec. Nullam sem sapien, lacinia at porttitor maximus, ullamcorper sit amet mi. Integer blandit luctus tempor. Donec nec urna lacus. Duis eget bibendum odio. Nunc nisl nibh, hendrerit non libero at, ultricies viverra mauris. Aliquam semper et ipsum eget iaculis. Vivamus rutrum non tortor nec cursus. Praesent mattis urna ut eros vehicula, ac fermentum ligula hendrerit. Morbi interdum risus ac ipsum placerat, eu dictum augue fringilla. Curabitur ac dignissim magna, eget varius purus."
+let wordCount = 0;
+for (const word in text.split(' ')) {
+  wordCount++
+}
+
+let etCount = 0;
+for (const word of text.split(' ')) {
+  if (word == "et") {
+    etCount++
+  }
+}
+
+console.log(`Words: ${wordCount}`)
+console.log(`etCount: ${etCount}`)
