2b58a730a736279b807fecfa49614524fb1af22b
JuanGrajales <grajales.juanfe11@gmail.com>
2020-06-09 14:38:07 -0400

    iteration 1

--- js/index.js
+++ js/index.js
@@ -2,6 +2,21 @@
 
 console.log('This is me doing a lab!!!')
 
+// 1.1
+// let hacker1 = 'Lorel'
+// let hacker1 = "Lorel"
+let hacker1 = `Lorel`
+
+// 1.2
+console.log('The drivers name is ' + hacker1) // string concatenation
+console.log(`The driver's name is ${hacker1}`) // string interpolation
+
+// 1.3
+let hacker2 = 'Luis'
+
+// 1.4
+console.log(`The navigator's name ${hacker2}`)
+
 // Iteration 2: Conditionals
 
 
