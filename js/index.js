073cf45537de9b5a3ef7f83fdaf61dfe116d3dc7
vlee13 <vlee13@gmail.com>
2020-06-08 21:56:32 -0700

    completion

--- js/index.js
+++ js/index.js
@@ -18,3 +18,24 @@ if(hacker1.length > hacker2.length) {
 // Iteration 3: Loops
 
 console.log(hacker1.toUpperCase().split('').join(' '));
+
+function reverseString(str) {
+  var newString = '';
+  for(var i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+
+console.log(reverseString(hacker1))
+
+function strcmp(hacker1, hacker2) {
+    if (hacker1.localeCompare(hacker2) === 1) {
+      console.log(`Yo, the navigator goes first definitely.`);
+    } else if (hacker1.localeCompare(hacker2) === -1) {
+      console.log(`The driver's name goes first.`);
+      } else if (hacker1.localeCompare(hacker2) === 0) {
+        console.log(`What?! You both have the same name?`);
+    }
+  }
+  console.log(strcmp(hacker1, hacker2))
