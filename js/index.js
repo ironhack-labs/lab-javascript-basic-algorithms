8d22e49f4feebb6dc538ef6c00a805344edcdada
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-06-09 15:07:39 +0200

    Bonus done, stuck line 73

--- js/index.js
+++ js/index.js
@@ -58,35 +58,36 @@ if(hacker1.localeCompare(hacker2) === 1){
 }
 
 
-
-
-
-
 // //Bonus 1
+//Generate 3 paragraphs. Store the text in a variable type of string.
 
-// var str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet leo quis arcu ornare rhoncus at quis neque. Fusce nec porta neque. Nullam efficitur nunc nec eros lacinia, eu consectetur lectus ornare. Quisque magna lacus, pretium sed mauris ac, dapibus blandit nulla. Maecenas nec justo maximus, laoreet justo sed, eleifend est. Nam lorem tortor, accumsan pharetra mauris aliquam, sollicitudin pharetra lorem. Pellentesque faucibus pretium odio, eget viverra mauris mollis vitae. Aliquam quis lectus libero. Etiam ac gravida nunc. Quisque non placerat quam. Phasellus nec ultricies justo. Nunc rutrum est sit amet mi eleifend elementum. Nunc tempus, quam eu cursus eleifend, lacus turpis pharetra est, eu finibus ante augue ut risus. Sed condimentum id lorem quis tincidunt. Donec nisl neque, commodo vitae mollis eu, porttitor sed erat. Aliquam at ex sapien. Ut sed euismod erat. Curabitur massa nibh, commodo in semper at, rutrum faucibus sapien. Sed massa nibh, rhoncus commodo rutrum vitae, dignissim ac metus. Etiam faucibus sem vel magna imperdiet, at aliquet sapien lacinia. Nunc elementum dignissim malesuada. Nulla facilisi. Fusce arcu ipsum, egestas et enim eu, imperdiet mattis lacus. Praesent finibus in tortor a finibus. Vivamus sagittis elit in facilisis sollicitudin. In sed purus velit. Aliquam et sapien euismod, porttitor dolor eu, imperdiet nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce et sapien ut dui sodales mattis. Curabitur id porta neque, eget maximus sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus dui erat, pulvinar et ornare quis, volutpat ultricies augue. Vivamus rutrum tortor arcu, non volutpat sem cursus a."
+let lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet leo quis arcu ornare rhoncus at quis neque. Fusce nec porta neque. Nullam efficitur nunc nec eros lacinia, eu consectetur lectus ornare. Quisque magna lacus, pretium sed mauris ac, dapibus blandit nulla. Maecenas nec justo maximus, laoreet justo sed, eleifend est. Nam lorem tortor, accumsan pharetra mauris aliquam, sollicitudin pharetra lorem. Pellentesque faucibus pretium odio, eget viverra mauris mollis vitae. Aliquam quis lectus libero. Etiam ac gravida nunc. Quisque non placerat quam. Phasellus nec ultricies justo.Nunc rutrum est sit amet mi eleifend elementum. Nunc tempus, quam eu cursus eleifend, lacus turpis pharetra est, eu finibus ante augue ut risus. Sed condimentum id lorem quis tincidunt. Donec nisl neque, commodo vitae mollis eu, porttitor sed erat. Aliquam at ex sapien. Ut sed euismod erat. Curabitur massa nibh, commodo in semper at, rutrum faucibus sapien. Sed massa nibh, rhoncus commodo rutrum vitae, dignissim ac metus. Etiam faucibus sem vel magna imperdiet, at aliquet sapien lacinia. Nunc elementum dignissim malesuada. Nulla facilisi. Fusce arcu ipsum, egestas et enim eu, imperdiet mattis lacus. Praesent finibus in tortor a finibus. Vivamus sagittis elit in facilisis sollicitudin. In sed purus velit. Aliquam et sapien euismod, porttitor dolor eu, imperdiet nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce et sapien ut dui sodales mattis. Curabitur id porta neque, eget maximus sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus dui erat, pulvinar et ornare quis, volutpat ultricies augue. Vivamus rutrum tortor arcu, non volutpat sem cursus a."
 
-// var strArr = str.split(' ');
-// console.log(strArr);
+// Make your program count the number of words in the string.
+// Make your program count the number of times the Latin word et appears.
 
-// /*for( i = 0 ; i<strArr.length; i++){
-//   let counterWords = 0;
-//   if(strArr[i] = true ){
-//     console.log(counterWords + 1);
-//   }
-// }*/
+for(let i = 0; i < lipsum.length; i++){
+  if (i === ' ' || '.') {
+    console.log(lipsum.length - 1);
+  }
+  // if (i === "et") {
+  //   var numberOfTimes += 1;
+  //   console.log(numberOfTimes);    // Bloqué
+  // }
+}
 
-// console.log(strArr.indexOf('a.')); // ==> 277 words
+//Bonus 2
+// Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome.
 
-// console.log(strArr.includes('et')); // true, il y a des 'et' dans mon lorem ipsum
+var phraseToCheck = "Mon prénom est un palindrome, il peut se lire dans les deux sens, je m'appelle enine"
 
-// for( i = 0 ; i<strArr.length; i++){
-//   let counter = 0
-//   if(strArr[i] === "et") {
-//     console.log(counter+1);
-//   }
-// }
+var phraseToCheckArr = phraseToCheck.split(' ');
 
+for (let i = 0; i <phraseToCheckArr.length; i++){
+  if (phraseToCheckArr[i].split("").reverse().join("") === phraseToCheckArr[i]) {
+    console.log('Hey there is a palindrome in your phrase !');
+    }
+}
 
 
 
