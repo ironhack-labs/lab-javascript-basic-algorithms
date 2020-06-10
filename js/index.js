987df3106dfce2c3abf69dc2767faed7702fd1d3
abWasHere <audrey.belson@gmail.com>
2020-06-10 17:30:23 +0200

    starts the bonus

--- js/index.js
+++ js/index.js
@@ -46,3 +46,47 @@ if (hacker1.localeCompare(hacker2) === -1) {
 } else {
   console.log("What?! You both have the same name?");
 }
+
+// Bonus 1
+
+let loremParagraph =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed sapien dui. Integer quis nunc a eros aliquet finibus eu in enim. Nunc ullamcorper lorem tincidunt, convallis felis nec, efficitur elit. Maecenas sagittis, dolor ac vestibulum finibus, ipsum dui sodales eros, vel commodo sem risus et felis. Integer lacinia velit id vestibulum luctus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis vel sapien non turpis scelerisque rutrum ac a neque. Donec eu dictum risus. Donec dignissim hendrerit enim, in sodales felis convallis eget. Ut laoreet efficitur nibh nec consequat. Donec porta vitae elit a accumsan. Nullam dapibus tincidunt turpis id viverra. Phasellus euismod placerat mauris sodales mollis. Praesent tincidunt orci at dolor tincidunt, in tristique neque elementum. Nulla placerat augue at tristique commodo. Ut metus est, iaculis eu mollis quis, convallis a quam. Nunc suscipit elit et massa mollis elementum. Sed tempor, diam ut scelerisque vulputate, nibh libero molestie dui, in interdum odio turpis sit amet augue. Maecenas in elementum odio. Nulla convallis lacus in erat ultrices varius. Duis ut sodales erat, a ornare dolor. Duis nec eros nec tellus vehicula convallis. Morbi commodo venenatis nibh sit amet interdum. Pellentesque aliquet euismod bibendum. Sed quam libero, semper pellentesque luctus sit amet, pretium nec quam. Aliquam ac accumsan nulla.Nulla facilisis ante lorem, eu porta justo varius eu. Vivamus auctor dignissim felis, quis cursus augue sollicitudin non. Nulla et egestas nisi, quis bibendum nisl. Pellentesque sodales, mauris id imperdiet luctus, velit dui bibendum turpis, et sagittis nulla neque ut neque. Aliquam sit amet tortor id quam varius interdum. Quisque nulla turpis, sagittis tristique mollis sit amet, suscipit eget odio. Nunc vestibulum diam at est dapibus venenatis. Maecenas et lacus eu enim tempus facilisis vel sit amet nulla. Vestibulum sodales tellus non nisl suscipit sodales. Quisque eget volutpat nisl, in dignissim libero. In hac habitasse platea dictumst. Nullam eget tempor augue, vitae tristique nibh.";
+
+// let oneString = "Audrey et John et Chakib";
+
+function wordCount(str) {
+  let myTotal = 0;
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      myTotal++;
+    }
+  }
+  myTotal++;
+  console.log(myTotal);
+}
+
+// wordCount(oneString);
+
+function countWords(str) {
+  var count = 0;
+  var words = str.split(" ");
+  for (i = 0; i < words.length; i++) {
+    // inner loop -- do the count
+    if (words[i] != "") count += 1;
+  }
+  console.log(count);
+  console.log(Array.isArray(words))
+};
+
+countWords(loremParagraph);
+
+/*
+Bonus 1:
+Go to lorem ipsum generator and:
+
+Generate 3 paragraphs. Store the text in a variable type of string.
+Make your program count the number of words in the string.
+Make your program count the number of times the Latin word et appears.
+
+l
+*/
