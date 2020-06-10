5feaaa9fd134795aae29db1d4de2181c2a3430c3
cchakibb <chakibbachir88@gmail.com>
2020-06-10 16:01:35 +0200

    resolve the first iteration

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 // Iteration 1: Names and Input
 
+var hacker1 = "Chakib";
+
+console.log(`the driver's name is ${hacker1}`);
+
+var hacker2 = "Audrey";
+
+console.log(`the navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
