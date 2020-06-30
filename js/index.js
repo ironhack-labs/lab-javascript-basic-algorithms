cdec3c366fb0685ca9dc156b63e076e29541284a
Filipe Freire <filipefreire@protonmail.com>
2020-06-30 15:48:14 +0100

    Added Bonus 1.

--- js/index.js
+++ js/index.js
@@ -42,18 +42,6 @@ for (let counter = hacker2.length - 1; counter >= 0; counter--) {
 console.log(revName);
 // 3.3
 
-/*
-for (let i = 0; i < hacker1.length && hacker2.length; i++) {
-  if (hacker1[i] < hacker2[i]) {
-    console.log("The driver's name goes first.");
-  } else if (hacker1[i] < hacker2[i]) {
-    console.log("Yo, the navigator goes first definitely.");
-  } else {
-    console.log("What?! You both have the same name?");
-  }
-}
-*/
-
 if (hacker1 < hacker2) {
   console.log("The driver's name goes first.");
 } else if (hacker1 > hacker2) {
@@ -61,3 +49,30 @@ if (hacker1 < hacker2) {
 } else {
   console.log('What?! You both have the same name?');
 }
+
+// BONUS 1
+
+let lorem =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at odio placerat, tristique libero vel, condimentum mi. Suspendisse fringilla augue leo, suscipit auctor ante sodales quis. Suspendisse pretium porttitor lorem, in volutpat urna. Maecenas suscipit rhoncus efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam id pharetra tellus. Donec sagittis sit amet ligula sit amet pretium. Fusce porta, nulla ac rutrum aliquet, tortor lorem semper dolor, ac euismod eros dolor at enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed placerat imperdiet mollis. Vestibulum erat diam, facilisis a arcu id, vehicula porttitor velit. In et imperdiet diam, id consequat tortor. Phasellus urna mauris, fringilla id viverra bibendum, fermentum sed nibh. Mauris a sapien nulla. Nulla urna sem, commodo et risus a, iaculis lobortis augue. Sed sit amet tincidunt odio, vitae gravida tortor. Morbi pretium tellus sed eros commodo, vitae elementum nibh pharetra. Sed mattis, urna sed pulvinar interdum, eros tellus gravida lacus, viverra volutpat ipsum nibh in mauris. Aenean congue ornare elit vitae pharetra. Nullam id aliquet odio, vitae faucibus ligula. Maecenas a augue at felis maximus commodo vitae a tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus eget dolor justo. Sed efficitur ex eget lobortis viverra. Sed at tempus enim, et euismod nibh. Ut malesuada volutpat diam nec imperdiet. Aliquam at consectetur nisi. Cras ac leo tortor.';
+
+let wordCounter = 1;
+
+for (let i = 0; i < lorem.length; i++) {
+  if (lorem[i] === ' ') {
+    wordCounter += 1;
+  }
+}
+
+console.log(wordCounter);
+
+// number of time "et" appears
+
+let etCounter = 0;
+
+for (let i = 0; i < lorem.length; i++) {
+  if (lorem[i] === 'e' && lorem[i + 1] === 't') {
+    etCounter += 1;
+  }
+}
+
+console.log(etCounter);
