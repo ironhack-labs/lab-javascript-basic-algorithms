b45aa5d54797e0d7b2cc6f01b169632e9918ecc9
Eloisa Seabra <seabra.eloisa@gmail.com>
2020-06-30 18:19:53 +0100

    finished 3.2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
 // Iteration 1: Names and Input
 
+let hacker1 = 'Steve';
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = 'Paul';
+console.log(`The navigator's is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length}`);
+} else {
+  console.log(`The navigator has the longest name, it has ${hacker2.length}`);
+}
+
+// Iteration 3: Loops
+
+const strCopy = hacker1.toUpperCase();
+const splitted = strCopy.split('');
+console.log(splitted);
+splitted.reverse().join(' ');
+
+// Iteration 2: Conditionals
 
 // Iteration 3: Loops
