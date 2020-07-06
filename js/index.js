b1fcfcfa1fb21d917705e789cc6dc4f5f0d47450
Alejandro Hernández <alejandro_hj.11@outlook.com>
2020-07-06 22:24:18 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-// Iteration 1: Names and Input
+//Itineration 1
+let hacker1 = 'Alejandro';
+let hacker2 = 'Paco';
+console.log(`The drivers name is ${hacker1}`);
+console.log(`The navigator´s name is ${hacker2}`);
+//Itineration 2
+if(hacker1.length > hacker2.length){
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}
+else if(hacker1.length < hacker2.length){
+  console.log(`The navigator has the longest name, it has ${hacker2.length} characters.`)
+}
+else{console.log(`Wow, you both have equally long names, ${hacker2.length} characters`);}
 
+//Itineration 3
+//3.1
+let spacesDriverName =""
+for(let i=0; i<hacker1.length; i++){
+  spacesDriverName += hacker1[i].toUpperCase() + " ";
+}
+console.log(spacesDriverName);
 
-// Iteration 2: Conditionals
+//3.2
+let reverseNavName  = "";
+for(let i=hacker2.length -1; i>=0; i--){
+  reverseNavName += hacker2[i];
+}
+console.log(reverseNavName);
 
-
-// Iteration 3: Loops
+//3.3
+//
