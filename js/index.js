c529e8bb27f6d953641c2f7f4a3e12506360e611
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-08-03 18:00:22 -0500

    YA

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
-// Iteration 1: Names and Input
-
 let hacker1 = 'Hugo';
 console.log (`The driver's name is ${hacker1}`);
 let hacker2 = 'Jero';
 console.log (`The navigator's name is ${hacker2}`)
-
-// Iteration 2: Conditionals
-
 function longestName() {
     if (hacker1.length > hacker2.length) {
       console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
@@ -35,26 +30,34 @@ function longestName() {
   }
   console.log(whoGoesFirst(hacker1, hacker2));
 
-
-// Iteration 3: Loops
-
 let parrafo1 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
-
-console.log(parrafo1);
 
 let palabras = parrafo1.split(" ");
 
 console.log(palabras.length);
 
-let contador = 1;
-
-palabras.forEach(element => contadorEt(element));
-console.log(contador);
-
-function contadorEt(element) {
-  if (element === "Lorem"){
-    contador + 1;
+let contador = 0
+for (const value of palabras) {
+  if (value === 'et') {
+    contador += 1;
   }
 }
+console.log(contador);
+
+let phraseCheck = "Ana Be";
+
+function palindromo (phrase){
+  let invertida = phraseCheck.split(" ").join("").split("").reverse().join("").toLowerCase();
+  console.log(invertida);
+  let normal = phraseCheck.split(" ").join("").toLowerCase();
+  console.log(normal);
+  if (invertida === normal){
+    console.log(`${phraseCheck} es un palíndromo`)
+  } else {
+    console.log(`${phraseCheck} no es un palíndromo`)
+  }
+}
+
+palindromo(phraseCheck);
