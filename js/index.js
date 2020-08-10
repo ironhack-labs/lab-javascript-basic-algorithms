aa8fe07573d588af79c81ba96ff636ae4eda140a
Katya Panova <katrin.panova@gmail.com>
2020-08-10 18:20:49 +0200

    bonus 2 unfinished

--- js/index.js
+++ js/index.js
@@ -34,3 +34,23 @@ if(hacker1 === hacker2) {
     } else {
       console.log("The driver's name goes first.")
     }
+
+//  Bonus 1
+
+let paragraphLorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque, nunc nec molestie malesuada, elit nisi faucibus leo, id aliquam odio elit vitae lorem. Sed tristique ullamcorper tempus. Nunc dignissim enim in lorem malesuada, sed tincidunt velit hendrerit. Etiam vitae est dictum, mollis diam a, luctus mi. Cras accumsan nec mi vitae placerat. Aliquam at nisi sit amet nisi auctor elementum ac et magna. Curabitur commodo ex ut tortor bibendum rhoncus. Suspendisse aliquam, nunc vitae volutpat aliquam, ipsum nisi viverra dui, quis laoreet arcu quam nec leo. Ut maximus augue lacus, et varius arcu elementum sed. Fusce dignissim consectetur massa vel dignissim. Maecenas egestas eget ipsum eu dictum. Ut elit massa, imperdiet nec quam commodo, rhoncus consectetur metus. Quisque euismod sapien vel neque aliquam molestie. Fusce at felis turpis. Duis dolor metus, euismod quis sagittis nec, vulputate a nunc. Curabitur porta vitae erat nec sagittis. Praesent quis elementum justo. Nulla arcu neque, posuere in ligula et, commodo ultricies mi. Nam ligula diam, convallis sit amet lorem quis, vehicula suscipit lacus. Sed consequat nibh dui, sed finibus massa fermentum at. Curabitur luctus mauris facilisis quam lacinia tristique. Duis consequat lacus vel erat fermentum, at molestie urna euismod. Mauris at placerat enim. Nunc massa quam, tempor a sapien ut, sodales interdum odio. Proin scelerisque ipsum et augue sagittis iaculis. Pellentesque lobortis varius nunc a hendrerit. Etiam vel metus sed nisi suscipit aliquet eu et leo. Curabitur dictum eros at erat lacinia lobortis. Nullam maximus, dui suscipit aliquam scelerisque, purus diam condimentum quam, vitae tempus augue odio sed diam. Integer at aliquet turpis. Cras blandit lorem nec auctor euismod. Cras faucibus sapien mi, ac sodales nisi pretium vel. Pellentesque sagittis bibendum elit at hendrerit. Proin id fringilla sapien, et vestibulum augue. Vestibulum varius tempus neque, ornare viverra sem."
+
+let spaceCount = (paragraphLorem.split(" ").length);
+console.log(spaceCount);
+
+// Bonus 2
+
+let phraseToCheck = "race car";
+let spaceFree = "";
+for(let i = 0; i < phraseToCheck.length; i++) {
+  if(phraseToCheck[i] !== " " && phraseToCheck[i] !== ",") { 
+    spaceFree += phraseToCheck[i] 
+    console.log(spaceFree)
+  }
+}
+
+console.log(spaceFree)
