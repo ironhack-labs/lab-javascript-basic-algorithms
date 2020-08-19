d4c4cb0854e9f8fd945d14b8eb0ce7275a4ed76f
Sebastian Grana <sebastiangrana1@gmail.com>
2020-08-19 17:28:58 -0400

    linked index.js to index.html

--- js/index.js
+++ js/index.js
@@ -1,40 +1,66 @@
 // Iteration 1: Names and Input
-let hacker1 = "Niko"
-console.log("The driver's name is " + hacker1)
-let hacker2 = "Juan"
-console.log("The navigator's name is " + hacker1)
+let hacker1 = "Niko";
+console.log("The driver's name is " + hacker1);
+let hacker2 = "Juan";
+console.log("The navigator's name is " + hacker1);
 
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
-    console.log('The driver has the longer name, it has ' + hacker1.length +' characters.')
+  console.log(
+    "The driver has the longer name, it has " + hacker1.length + " characters."
+  );
 } else if (hacker1.length < hacker2.length) {
-    console.log('It seems that the navigator has the longest name, it has ' + hacker2.length +' characters.')
+  console.log(
+    "It seems that the navigator has the longest name, it has " +
+      hacker2.length +
+      " characters."
+  );
 } else {
-    console.log('Wow, you both have equally long names, ' + hacker1.length + ' characters!')
+  console.log(
+    "Wow, you both have equally long names, " + hacker1.length + " characters!"
+  );
 }
 
 // Iteration 3: Loops
-let result1 = ""
+let result1 = "";
 for (let i = 0; i < hacker1.length; i++) {
-    result1 += hacker1.charAt(i) + " "
+  result1 += hacker1.charAt(i) + " ";
 }
 
-console.log(result1.toUpperCase().trim())
+console.log(result1.toUpperCase().trim());
 
-let result2 = ""
-for (let i = hacker2.length-1; i >= 0; i--){
-    result2 += hacker2.charAt(i)
+let result2 = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  result2 += hacker2.charAt(i);
 }
 
-console.log(result2)
+console.log(result2);
 
-let sortedArray = [hacker1, hacker2]
-sortedArray.sort()
+let sortedArray = [hacker1, hacker2];
+sortedArray.sort();
 
 if (hacker1 == hacker2) {
-    console.log("What?! You both have the same name?")
+  console.log("What?! You both have the same name?");
 } else if (sortedArray[0] == hacker1) {
-    console.log("The driver's name goes first.")
+  console.log("The driver's name goes first.");
 } else {
-    console.log("Yo, the navigator goes first definitely.")
+  console.log("Yo, the navigator goes first definitely.");
 }
+
+/*
+
+Anthony's Method
+
+for(i=0; i<hacker1.length;i++){
+  if(hacker1 == hacker2){
+    console.log('What?! You both have the same name?')
+    break;
+  } else if(hacker1[i]<hacker2[i]){
+    console.log("The driver's name goes first.")
+    break;
+  } else if(hacker2[i]<hacker1[i]){
+    console.log("Yo, the navigator goes first definitely.")
+    break;
+  }
+}
+*/
