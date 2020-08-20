bd89a312e4fc22efb80e2cdebf63658766f08735
juansemprun <33623712+juansemprun@users.noreply.github.com>
2020-08-20 16:38:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,3 +15,29 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+// LOOPS 3.1
+let str = ''
+
+for (let i = 0; i < hacker1.length; i++) {
+  str += hacker1[i] + ' ';
+}
+console.log(str.toUpperCase());
+
+
+// LOOPS 3.2
+let str2 = ''
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  str2 += hacker2[i] + ' '
+}
+console.log(str2.toUpperCase());
+
+
+// LOOPS 3.3
+
+if (hacker1.localeCompare(hacker2) < 0) {
+	console.log('The driver\'s name goes first.')
+} else if(hacker1.localeCompare(hacker2) > 0){
+  console.log('Yo, the navigator goes first definitely.')
+} else { 
+	console.log('What?! You both have the same name?');
+}
