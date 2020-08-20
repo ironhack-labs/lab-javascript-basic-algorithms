26b2624320d670f0cc3aa7837f3f8fe435388fc4
juansemprun <33623712+juansemprun@users.noreply.github.com>
2020-08-20 15:34:59 +0200

    Add iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 = "Juan Semprun"
+const hacker2 = "Carlos Garcia"
 
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
