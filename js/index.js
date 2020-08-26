bec2946307e7be8dd2ff87fc7eba6dc1e8bc8ebd
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-08-26 17:56:43 +0200

    bonus 1 done

--- js/index.js
+++ js/index.js
@@ -62,3 +62,53 @@ if (hacker1[0] < hacker2[0]) {
 if (hacker1[0] === hacker2[0]) {
     console.log("What?! You both have the same name?")
 }
+
+// Iteration bonus 1.2: Loops
+
+var lorem1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fringilla vehicula consectetur. Fusce id dignissim odio, sodales imperdiet enim. Integer in semper arcu, eu vulputate lectus. Quisque sed velit vitae eros hendrerit porta vitae in dolor. Aliquam sollicitudin velit nulla, eu auctor massa efficitur semper. Donec eget tempor risus, ac mollis ex. Donec sit amet lobortis lectus. Fusce vehicula ac neque nec accumsan.";
+var lorem2 = "Mauris varius a nunc eu vehicula. Fusce vitae velit quis justo viverra tempus vitae at nibh. Curabitur varius finibus sapien, et vehicula est elementum vel. Suspendisse id odio finibus, molestie leo vitae, dictum lectus. Nulla mollis turpis ante, scelerisque faucibus magna ultricies eu. Aenean malesuada velit ac risus posuere, et ultrices nunc feugiat. Cras imperdiet malesuada lectus sit amet consequat. Vestibulum mollis ac nibh tempor sollicitudin.";
+var lorem3 = "Donec bibendum metus a velit aliquam, in tempus felis dictum. Quisque vulputate semper dui eget euismod. Nam sit amet felis convallis lacus blandit aliquet. Proin id bibendum ligula. Sed eget elit vitae nibh finibus sagittis. Nullam dictum quam at sem posuere, quis lobortis ligula laoreet. Suspendisse nec augue aliquet, auctor tellus eu, rhoncus arcu. Vestibulum dign";
+
+var nmbwords = 1;
+
+for (let i = 0; i < lorem1.length; i++) {
+    if (lorem1[i] === " ") {
+        nmbwords++;
+    }
+}
+
+console.log("There are " + nmbwords);
+
+nmbwords = 1;
+
+for (let i = 0; i < lorem2.length; i++) {
+    if (lorem2[i] === " ") {
+        nmbwords++;
+    }
+}
+
+console.log("There are " + nmbwords);
+
+nmbwords = 1;
+
+for (let i = 0; i < lorem3.length; i++) {
+    if (lorem3[i] === " ") {
+        nmbwords++;
+    }
+}
+
+console.log("There are " + nmbwords);
+
+// Iteration bonus 1.3: Loops
+
+var compare = "";
+nmbwords = 0;
+
+for (let i = 0; i < lorem1.length; i++) {
+    compare = lorem1[i] + lorem1[i + 1];
+    if (compare === "et") {
+        nmbwords++;
+    }
+}
+
+console.log("There are " + nmbwords + " et");
