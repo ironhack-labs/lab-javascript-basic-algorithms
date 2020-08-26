1c958a1548ac4b1236b4ea24134d64f246fa5d21
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-08-26 16:02:14 +0200

    exercice 1 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 // Iteration 1: Names and Input
 
+const hacker1 = "raphaelle";
+const hacker2 = "vincent";
+
+console.log("The driver's name is " + hacker1);
+console.log("The navigator's name is " + hacker2);
+
 
 // Iteration 2: Conditionals
+
 
 
 // Iteration 3: Loops
