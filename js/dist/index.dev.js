795d32c37f925b4e7376c1c5d969bb22debd9811
Rico <rasim.rico@gmail.com>
2020-08-26 18:02:04 +0200

    js lab fourth commit with bonus

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -69,8 +69,48 @@ var lexicography = function lexicography(name1, name2) {
   }
 };
 
-console.log(lexicography(hacker1, hacker2)); // Bonus
+console.log(lexicography(hacker1, hacker2));
+console.log("--------------------------------"); // Bonus
 
-var phraseToCheck = "madam";
+var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non ante egestas, facilisis ex sit amet, porta sem. Suspendisse vulputate et enim ac eleifend. Cras eu hendrerit felis. Donec hendrerit, nibh vitae fringilla feugiat, odio quam lobortis arcu, at condimentum sapien tellus ut ligula. Praesent auctor tortor at ipsum vulputate convallis. Pellentesque maximus erat leo, a eleifend justo venenatis vel. Phasellus rhoncus ex felis, eu finibus lorem venenatis ac. Duis vitae nibh volutpat, congue ligula a, rhoncus erat.\n\nDonec interdum pretium vulputate. Aenean malesuada, erat sed facilisis facilisis, turpis diam sagittis sapien, eu sodales tortor metus ac magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi vitae felis elementum, sollicitudin mauris id, cursus ipsum. Vivamus at cursus ipsum. Suspendisse mollis nisl nec lectus tincidunt, in pulvinar ligula pharetra. Suspendisse ante neque, iaculis quis placerat volutpat, tempor ut nisi. Vestibulum euismod ante nec ipsum aliquam, vel facilisis dolor sagittis. Suspendisse id mi neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed eleifend varius porta. Fusce ligula sem, vestibulum commodo ullamcorper in, ornare vel nibh.\n\nVestibulum blandit eu tellus at pellentesque. Nulla eleifend, orci id varius aliquam, libero nibh finibus elit, id pulvinar tellus felis a eros. Pellentesque eu ipsum sit amet dui volutpat luctus in quis orci. Vivamus maximus hendrerit lacus ac aliquam. Donec vel turpis viverra, pretium libero egestas, rutrum lorem. Suspendisse luctus quam eu magna aliquet blandit. Donec dapibus imperdiet dolor, blandit interdum neque vestibulum non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec et fermentum ex. Nam fringilla nisi nisi, eget vulputate sapien porta nec. Nullam convallis mauris ultrices tincidunt sodales. Pellentesque sit amet mi et ante cursus sagittis. Mauris lacus odio, suscipit nec diam vel, gravida facilisis neque. Nam nec lorem risus.";
 
-var palindromCheck = function palindromCheck(string) {};
+var countLoremWords = function countLoremWords(text) {
+  return text.split(/\n+|\s+/).length;
+};
+
+var etCount = function etCount(text) {
+  var count = 0;
+  var loremWords = text.split(/\n+|\s+/);
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = loremWords[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      word = _step.value;
+      word === "et" && count++;
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return count;
+};
+
+console.log(countLoremWords(lorem));
+console.log(etCount(lorem));
+console.log("--------------------------------");
+var phraseToCheck = "step on no pets";
+
+var palindromCheck = function palindromCheck(string) {}; // console.log(palindromCheck(phraseToCheck))
--- js/index.js
+++ js/index.js
@@ -78,10 +78,37 @@ const lexicography = (name1, name2) => {
 
 console.log(lexicography(hacker1, hacker2));
 
+console.log("--------------------------------")
+
 // Bonus
 
-let phraseToCheck = "madam";
+let lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non ante egestas, facilisis ex sit amet, porta sem. Suspendisse vulputate et enim ac eleifend. Cras eu hendrerit felis. Donec hendrerit, nibh vitae fringilla feugiat, odio quam lobortis arcu, at condimentum sapien tellus ut ligula. Praesent auctor tortor at ipsum vulputate convallis. Pellentesque maximus erat leo, a eleifend justo venenatis vel. Phasellus rhoncus ex felis, eu finibus lorem venenatis ac. Duis vitae nibh volutpat, congue ligula a, rhoncus erat.
+
+Donec interdum pretium vulputate. Aenean malesuada, erat sed facilisis facilisis, turpis diam sagittis sapien, eu sodales tortor metus ac magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi vitae felis elementum, sollicitudin mauris id, cursus ipsum. Vivamus at cursus ipsum. Suspendisse mollis nisl nec lectus tincidunt, in pulvinar ligula pharetra. Suspendisse ante neque, iaculis quis placerat volutpat, tempor ut nisi. Vestibulum euismod ante nec ipsum aliquam, vel facilisis dolor sagittis. Suspendisse id mi neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed eleifend varius porta. Fusce ligula sem, vestibulum commodo ullamcorper in, ornare vel nibh.
+
+Vestibulum blandit eu tellus at pellentesque. Nulla eleifend, orci id varius aliquam, libero nibh finibus elit, id pulvinar tellus felis a eros. Pellentesque eu ipsum sit amet dui volutpat luctus in quis orci. Vivamus maximus hendrerit lacus ac aliquam. Donec vel turpis viverra, pretium libero egestas, rutrum lorem. Suspendisse luctus quam eu magna aliquet blandit. Donec dapibus imperdiet dolor, blandit interdum neque vestibulum non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec et fermentum ex. Nam fringilla nisi nisi, eget vulputate sapien porta nec. Nullam convallis mauris ultrices tincidunt sodales. Pellentesque sit amet mi et ante cursus sagittis. Mauris lacus odio, suscipit nec diam vel, gravida facilisis neque. Nam nec lorem risus.`
+
+const countLoremWords = function (text){
+    return text.split(/\n+|\s+/).length;
+} 
+
+const etCount = text => {
+    let count = 0
+    let loremWords = text.split(/\n+|\s+/) 
+    for (word of loremWords) {
+        (word === "et") && count++
+    }
+    return count
+}
+console.log(countLoremWords(lorem))
+console.log(etCount(lorem));
+
+console.log("--------------------------------")
+
+let phraseToCheck = "step on no pets";
 
 let palindromCheck = (string) => {
-
+    
 };
+
+// console.log(palindromCheck(phraseToCheck))
