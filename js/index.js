f1eab70af21b4293a7f8ee4c683c3d15170b0ff2
Marcha <marchaf@gmail.com>
2020-08-26 17:46:39 +0200

    push exercice 3

--- js/index.js
+++ js/index.js
@@ -45,3 +45,63 @@ function reverseString(str) {
 newNavigatorsName = reverseString(hacker2);
 
 console.log(`3.2 : ${newNavigatorsName}`);
+
+// ------
+// "traitement" pour récuper le plus court et le stocker 
+/*
+function compareName(driver, navigator) {
+  let indice = 0;
+  console.log("Print du localeCompare: "  + driver.localeCompare(navigator));
+  if( driver.localeCompare(navigator) < 0 ) {
+    console.log("The driver's name goes first.");
+    indice = 1;
+    return indice;
+  } else if(driver.localeCompare(navigator) > 0 ){
+    console.log("Yo, the navigator goes first definitely.");
+    indice = 1;
+    return indice;
+  } else if(driver.localeCompare(navigator) == 0   ){
+    console.log("What?! You both have the same name?");
+  }
+};
+
+var shortestString = 0;
+
+if(hacker1.length <  hacker2.length){
+  shortestString = hacker2.length ;
+} else if(hacker1.length > hacker2.length) {
+  shortestString = hacker1.length ;
+} else {
+  shortestString = hacker1.length;
+}
+
+for (let i = 0; i < shortestString; i++) {
+  console.log("permier hacker " + hacker1[i] + " deuxieme " + hacker2[i]);
+  if( compareName(hacker1[i], hacker2[i]) == 1) {
+    break;
+  } else {
+    // 
+    if( driver.localeCompare(navigator) < 0 ) {
+      console.log("The driver's name goes first.");
+      
+    } else {
+      console.log("Yo, the navigator goes first definitely.");
+    
+    }
+  } 
+   
+}
+
+
+//hacker1[i].localeCompare(hacker2[i]);
+*/
+
+
+if (hacker1 < hacker2){
+  console.log("The driver's name goes first.");
+} else if(hacker1 > hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
+
