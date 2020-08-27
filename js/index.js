7f306ea3b25520add1ecc3ab9e3bff100711930f
Alexsr91 <helloalexsr@gmail.com>
2020-08-27 21:54:33 +0200

    Done by Alex and Shyam

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
 // Iteration 1: Names and Input
+let hacker1 = "Alex";
+console.log(`The driver's name is ${hacker1}`)
 
+let hacker2 = "Shyam"
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
 
+
+if (hacker1.length >= hacker2.length){
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker2.length >= hacker1.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else{
+  console.log(`Wow, you both have equally long names, it has ${hacker2.length} characters.`)
+}  
+
+
 // Iteration 3: Loops
+
+for (let i = 0; i <= hacker1.length-1; i++){
+    console.log(hacker1[i].toUpperCase());
+}
+
+var result  = '';
+
+/*for (let i = 0; i <= hacker1.length-1; i++){
+  result = result + hacker1[i].toUpperCase();
+}
+console.log(result);*/
+
+for (let i = hacker1.length-1; i >=0; i--){
+  result = result + hacker1[i].toUpperCase();
+}
+
+console.log(result)
