86f4ea4c96f36878da796c004c9d80479f1a8659
Alicja Slupska <ala.slupska@gmail.com>
2020-08-28 00:21:23 +0200

    lab lab-javascript-basic-algorithms in progress

--- js/index.js
+++ js/index.js
@@ -1,7 +1,41 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Alicja"
+console.log(`The driver's name is ${hacker1}.`);
+let hacker2 = "Philipp"
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
 
+console.log(hacker1.length);
+console.log(hacker2.length);
+
+if (hacker1.length>hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+} 
+else if (hacker2.length>hacker1.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} 
+else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters`);
+}
+
 
 // Iteration 3: Loops
+
+let emptyString = ""
+
+for (let i = 0; i < hacker1.length; i++) {
+  emptyString += hacker1[i].toUpperCase() + " ";
+}
+
+console.log(emptyString);
+
+
+/* First approach was: 
+
+for (let i = 0; i < hacker1.length; i++) {
+  hackerName += hacker1[i];
+}
+console.log(hackerName.split('').join(' ').toUpperCase())
+*/
