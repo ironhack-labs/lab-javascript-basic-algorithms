73f9c4437f94008668f5c4248791958ce48dac1d
Lcdevi <caillauxlaura@gmail.com>
2020-08-27 12:33:25 +0200

    add the first part of lab

--- js/index.js
+++ js/index.js
@@ -2,6 +2,40 @@
 // Iteration 2: Conditionals
 // Iteration 3: Loops
 
+
+//Ailie and Laura
+// Iteration 1: Names and Input
+var hacker1 = "Ailie";
+console.log(`The driver's name is ` + hacker1);
+var hacker2 = "Laura";
+console.log(`The driver's name is ` + hacker2);
+// Iteration 2: Conditionals
+if(hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ` + hacker1.length + " characters.");
+} else if (hacker1.length < hacker2.length) {
+    console.log(`The navigator has the longest name, it has ` + hacker2.length + " characters.");
+} else {
+    console.log(`Wow, you both have equally long names, ` + hacker1.length + " characters!");
+}
+// Iteration 3: Loops
+let driver = "";
+for(let i = 0; i < hacker1.length; i++) {
+    driver += hacker1[i].toUpperCase() + ' ';
+}
+console.log(driver);
+let navigator = "";
+for(let i = (hacker2.length - 1); i >= 0; i--) {
+    navigator += hacker2[i];
+}
+console.log(navigator);
+if(hacker1 < hacker2) {
+    console.log(`The driver's name goes first`);
+} else if (hacker1 > hacker2) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
+
 // Bonus 1
 console.log("------------bonus 1 : how many words-------------------------------------------------------------")
 
