97a39905bf50c5a86eb8380e0db9e63c7183a52d
katrin panova <katrin.panova@gmail.com>
2020-09-14 23:04:59 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,56 +1,142 @@
-// Iteration 1: Names and Input
-
-let hacker1 = 'Boris';
-console.log(`The driver's name is ${hacker1}.`);
-let hacker2 = 'Olga';
-console.log(`The navigator's name is ${hacker2}.`);
-
-// Iteration 2: Conditionals
-
-if (hacker1.length > hacker2.length) {
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`) 
-  } else if (hacker1.length < hacker2.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`) 
-  } else {
-    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
+// Iteration #1: Find the maximum
+let num1;
+let num2;
+function maxOfTwoNumbers(num1, num2) {
+  let max = num1;
+  if (num1 < num2) {
+    max = num2;
   }
-
-// Iteration 3: Loops
-
-let capitalName = '';
-for(let i = 0; i < hacker1.length; i++) {
-  capitalName += hacker1[i].toUpperCase() + ' ';
+  return max;
 }
 
-let reverseName = '';
-for(let i = hacker2.length -1; i >= 0; i--) {
-  reverseName += hacker2[i];
-}
+// Iteration #2: Find longest word
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-if(hacker1 === hacker2) {
-    console.log("What?! You both have the same name?")
-    } else if(hacker1 > hacker2) {
-    console.log("Yo, the navigator goes first definitely.")
-    } else {
-      console.log("The driver's name goes first.")
+function findLongestWord(words) {
+  if (words.length == 0) {
+    return null;
+  }
+  let longestWord = words[0];
+  for (let i = 1; i < words.length; i++) {
+    if (longestWord.length < words[i].length) {
+      longestWord = words[i];
     }
+  }
+  return longestWord;
+}
 
-//  Bonus 1
+// Iteration #3: Calculate the sum
 
-let paragraphLorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque, nunc nec molestie malesuada, elit nisi faucibus leo, id aliquam odio elit vitae lorem. Sed tristique ullamcorper tempus. Nunc dignissim enim in lorem malesuada, sed tincidunt velit hendrerit. Etiam vitae est dictum, mollis diam a, luctus mi. Cras accumsan nec mi vitae placerat. Aliquam at nisi sit amet nisi auctor elementum ac et magna. Curabitur commodo ex ut tortor bibendum rhoncus. Suspendisse aliquam, nunc vitae volutpat aliquam, ipsum nisi viverra dui, quis laoreet arcu quam nec leo. Ut maximus augue lacus, et varius arcu elementum sed. Fusce dignissim consectetur massa vel dignissim. Maecenas egestas eget ipsum eu dictum. Ut elit massa, imperdiet nec quam commodo, rhoncus consectetur metus. Quisque euismod sapien vel neque aliquam molestie. Fusce at felis turpis. Duis dolor metus, euismod quis sagittis nec, vulputate a nunc. Curabitur porta vitae erat nec sagittis. Praesent quis elementum justo. Nulla arcu neque, posuere in ligula et, commodo ultricies mi. Nam ligula diam, convallis sit amet lorem quis, vehicula suscipit lacus. Sed consequat nibh dui, sed finibus massa fermentum at. Curabitur luctus mauris facilisis quam lacinia tristique. Duis consequat lacus vel erat fermentum, at molestie urna euismod. Mauris at placerat enim. Nunc massa quam, tempor a sapien ut, sodales interdum odio. Proin scelerisque ipsum et augue sagittis iaculis. Pellentesque lobortis varius nunc a hendrerit. Etiam vel metus sed nisi suscipit aliquet eu et leo. Curabitur dictum eros at erat lacinia lobortis. Nullam maximus, dui suscipit aliquam scelerisque, purus diam condimentum quam, vitae tempus augue odio sed diam. Integer at aliquet turpis. Cras blandit lorem nec auctor euismod. Cras faucibus sapien mi, ac sodales nisi pretium vel. Pellentesque sagittis bibendum elit at hendrerit. Proin id fringilla sapien, et vestibulum augue. Vestibulum varius tempus neque, ornare viverra sem."
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumNumbers(numbers) {
+  let sum = 0;
+  if (numbers.length == 0) {
+    return numbers.length;
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
 
-let spaceCount = (paragraphLorem.split(" ").length);
-console.log(spaceCount);
+// Iteration #4: Calculate the average
+// Level 1: Array of numbers
+const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+function averageNumbers(numbersAvg) {
+  if (numbersAvg.length == 0) {
+    return null;
+  }
+  let average = sumNumbers(numbersAvg) / numbersAvg.length;
+  return average;
+}
 
-// Bonus 2
+// Level 2: Array of strings
+const wordsArr = [
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace",
+];
+function averageWordLength(wordsArr) {
+  let totalLength = 0;
+  if (wordsArr.length == 0) {
+    return null;
+  }
+  for (let i = 0; i < wordsArr.length; i++) {
+    totalLength += wordsArr[i].length;
+  }
+  let averageLength = totalLength / wordsArr.length;
+  return averageLength;
+}
 
-let phraseToCheck = "race car";
-let spaceFree = "";
-for(let i = 0; i < phraseToCheck.length; i++) {
-  if(phraseToCheck[i] !== " " && phraseToCheck[i] !== ",") { 
-    spaceFree += phraseToCheck[i] 
-    console.log(spaceFree)
+// Iteration #5: Unique arrays
+const wordsUnique = [
+  "crab",
+  "poison",
+  "contagious",
+  "simple",
+  "bring",
+  "sharp",
+  "playground",
+  "poison",
+  "communion",
+  "simple",
+  "bring",
+];
+
+function uniquifyArray(wordsUnique) {
+  let uniqResult = [];
+  if (wordsUnique.length == 0) {
+    return null;
+  }
+  for (let word of wordsUnique) {
+    if (uniqResult.indexOf(uniqResult) < 0) {
+      uniqResult.push(word);
+    }
+  }
+  return uniqResult;
+}
+
+// Iteration #6: Find elements
+const wordsFind = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
+
+function doesWordExist(wordsFind, word) {
+  if (wordsFind.length == 0) {
+    return null;
+  }
+  if (wordsFind.indexOf(word) >= 0) {
+    return true;
+  } else if (wordsFind.indexOf(word) < 0) {
+    return false;
   }
 }
 
-console.log(spaceFree)
+// Iteration #7: Count repetition
+const wordsCount = [];
+
+// Iteration #8: Bonus
+
+const matrix = [];
