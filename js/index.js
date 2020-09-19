968d3169c1bfdfc8e3bcec237fab9b4b75c663f5
Gonzalo Loza <cg.lozarojas@gmail.com>
2020-09-18 22:58:53 -0500

    First update of the lab

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
 // Iteration 1: Names and Input
-
+const hacker1 = prompt(`Driver's name is:`);
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = prompt(`Provide navigator's name:`);
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+  } else if (hacker1.length<hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+  } else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+  };
 
 // Iteration 3: Loops
+
+for (value of hacker1.toUpperCase()) {
+    console.log(value);
+};
+  
+let navigator = "";
+for (i=0; i<hacker2.length; i++) {
+    navigator += hacker2[hacker2.length -i-1];
+};
+console.log(navigator);
+  
+if (hacker1<hacker2) {
+    console.log(`The drive's name goes first`);
+} else if(hacker1>hacker2) {
+    console.log(`Yo, the navigator goes first definitely.`)
+} else {
+    console.log(`What?! You both have the same name?`);
+};
