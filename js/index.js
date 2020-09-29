319fd81dd4219ced888c49c8567b40d0f0e66f7c
Alberto <a.p.r.13287@gmail.com>
2020-09-29 19:05:14 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//Ejercicio 1.1
+//Ejercicio 1
 
 let hacker1 = "Marc";
 console.log("The driver  name is " + hacker1);
@@ -36,4 +36,44 @@ if (hacker1lexic === -1) {
     console.log(`Yo, the navigator goes first definitely.`);
 } else {
     console.log(`What?! You both have the same name?`);
+}
+
+
+
+
+// Bonus 1
+
+let texto1 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium neque enim, ut aliquam elit sollicitudin eu. Sed rutrum at erat id ultricies. Donec iaculis felis pretium sollicitudin bibendum. Morbi nisi velit, sagittis in velit ac, finibus vulputate diam. Donec consectetur venenatis eros at elementum. Sed at eros sapien. Pellentesque molestie lorem pulvinar fringilla efficitur. Suspendisse congue euismod nunc, vehicula tincidunt mauris. Donec lorem enim, vestibulum vel euismod ut, mollis nec mauris. Pellentesque venenatis ac tellus ac scelerisque. Pellentesque tristique eleifend est, vitae facilisis nisl porta sit amet. Nullam lorem nisi, pulvinar commodo nisi at, ornare dictum eros. Phasellus a sodales turpis. Maecenas eu malesuada ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam erat volutpat.`
+let texto2 = `Etiam gravida volutpat dolor, quis hendrerit risus gravida eget. Maecenas fringilla orci magna, sed sagittis lacus vestibulum sed. Aenean imperdiet purus eu velit bibendum, non cursus magna tempor. Nam vehicula eget enim vel tristique. Cras convallis purus volutpat, congue neque non, feugiat sem. Aenean pretium fermentum viverra. Curabitur molestie odio ac purus tincidunt, eu rutrum elit rutrum. Quisque in nunc libero. Donec id est a diam fringilla mollis sed et ipsum. Donec maximus tortor augue. Pellentesque sit amet faucibus turpis, in pulvinar libero. Donec volutpat at odio eu suscipit.`
+let texto3 = `Fusce congue rutrum tellus, vel tincidunt urna sodales ut. Phasellus quis mi ac neque accumsan lacinia eget sed odio. Proin luctus at sem non mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi aliquam mollis vestibulum. Sed at placerat diam. Maecenas tempus, ex non pretium rhoncus, velit leo commodo odio, non mollis lorem purus condimentum nisl. Nulla aliquam sit amet diam quis molestie. Cras accumsan vestibulum quam ut molestie. Cras tempor eros in enim lobortis euismod.`;
+
+let contador1= texto1.split(" ")
+console.log("El primer parrafo contiene " + contador1.length + " palabras")
+let contador2= texto2.split(" ")
+console.log("El segundo parrafo contiene " + contador2.length + " palabras")
+let contador3= texto3.split(" ")
+console.log("El tercer parrafo contiene " + contador3.length + " palabras")
+let suma= contador1.length+contador2.length+contador3.length;
+console.log("La suma de los tres parrafos contiene "+ suma + " palabras")
+
+
+let phraseToCheck1 = texto1.indexOf("et")
+let phraseToCheck2 = texto2.indexOf("et")
+let phraseToCheck3 = texto3.indexOf("et")
+let sumaPhrase= phraseToCheck1+phraseToCheck2+phraseToCheck3;
+//console.log(sumaPhrase)
+console.log(phraseToCheck1)
+
+
+
+//Ejercicio Bonus 2
+
+let phraseToCheck = "amor , roma"
+let palindromo = phraseToCheck.toLowerCase().split("").join("")
+let palindromoReverse = phraseToCheck.toLowerCase().split("").reverse().join("")
+if (palindromo == palindromoReverse){
+  console.log("La frase '" + phraseToCheck + "' es un palíndromo")
+}
+else{
+  console.log("No es un palíndromo")
 }
