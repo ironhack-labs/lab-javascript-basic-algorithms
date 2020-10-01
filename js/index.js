44f98c86e8c328e5d8ff84cd29e6f5c3564d0ce7
Aditya <63795831+Aditya231@users.noreply.github.com>
2020-10-01 10:36:22 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,27 @@
 
 
 // Iteration 3: Loops
+
+//Basic JavaScript Knowledge for beginners !
+//alert function - gives a pop-up message on reloading website !
+<script>
+  alert("This is a alert message");
+// Writing Something in console of the website !
+  console.log("This is a console message")
+// function in js ! - these runs on specific times when you call it !
+  function function_name (parameters){
+       // code here
+    }
+  function func1(){
+       alert("Hello World");
+    }
+//Calling A function
+<html>
+  <body onreload = func1()>// this will run on reload of the page
+    
+    <button onclick = func1()>// this runs on clicking on button !
+    </button>
+  </body>
+</html>
+</script>
+
