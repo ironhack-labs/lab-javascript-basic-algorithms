c6dc4e5e2dd83205c0ef422be55ca135a4e344f6
Pablo Fernandez <pablo_fz@hotmail.com>
2020-10-01 21:31:54 +0200

    primera iteración

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Pablo";
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "Monica";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
