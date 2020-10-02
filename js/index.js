47d9f9ab72cf444e79254fc04dac90573dc2dde0
LucasFeli <felipe298@gmail.com>
2020-10-02 22:46:49 +0200

    almost

--- js/index.js
+++ js/index.js
@@ -1,34 +1,48 @@
 // Iteration 1: Names and Input
-let hacker1 = "felipe"
+   hacker1 = "felipe"
 console.log(" The driver`s name is " + (hacker1))
 let hacker2 = "carla"
 console.log("  The navigator`s name is "  +  (hacker2))
 
 // Iteration 2: Conditionals
-console.log (hacker1.length); //6
-console.log (hacker2.length); //5
-let driver  = hacker1.length
-let navigator = hacker2.length
-if(driver > navigator){
-console.log("The driver has the longest name, it has" + (driver) + "characters");
-} else if (navigator > driver){
-   console.log ("It seems that the navigator has the longest name, it has" + (navigator) + "characters"); 
-} else if (navigator == driver) {
-  console.log("Wow, you both have equally long names, characters!");
-} else{
-  console.log("empty")
+let hacker1 = "Elisenda"
+console.log(`The drivers name is ${hacker1}`)
+let hacker2 = "Felipe"
+console.log(`The navigator name is ${hacker2}`)
+
+
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker1.length < hacker2.length) {
+hacker2.length} 
+else if (hacker1.length == hacker2.length) {
+  console.log("Wow, you both have equally long names, characters!")
+} else {
+  console.log("Woow, you're both a hackers")
 }
+
 
 
 // Iteration 3: Loops
 
 //3.1
-let driver2 = "felipe"
-console.log(driver2.toUpperCase().split("").join(" "))
+let text = "";
+let person = hacker2.toUpperCase().split("").join(" ")
+let dirver;
+for (dirver in person) {
+  text += person[dirver] + " ";
+}
+
+console.log(text)
 //3.2
-let driver1 = "felipe"
-let driverStr = driver1.split("").reverse().join("")
-console.log(driverStr)
+let text2 = "";
+let person2 = hacker2.split("").reverse().join("")
+let driver2;
+for (driver2 in person2) {
+  text2 += person2[driver2] ;
+}
+
+console.log(text2)
 
 //3.3
 //Bonus 1
