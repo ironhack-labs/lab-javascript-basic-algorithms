e475e25a126a9eeec0640ca32b8eba31ccced6d4
carmen valencia <carmenpilar1990@gmail.com>
2020-10-02 17:03:17 +0200

    Names and input

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let firstHacker = "Carmen";
+console.log("The driver's name is", firstHacker);
+let secondHacker = "Andrea";
+console.log ("The navigator's name is", secondHacker);
 
 // Iteration 2: Conditionals
 
