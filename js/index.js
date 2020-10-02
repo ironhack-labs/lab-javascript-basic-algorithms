c31508e84e7e7cf95f51fa0330c1a0f634120032
LucasFeli <felipe298@gmail.com>
2020-10-02 23:00:34 +0200

    almost

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
 // Iteration 1: Names and Input
-   hacker1 = "felipe"
-console.log(" The driver`s name is " + (hacker1))
-let hacker2 = "carla"
-console.log("  The navigator`s name is "  +  (hacker2))
-
-// Iteration 2: Conditionals
 let hacker1 = "Elisenda"
 console.log(`The drivers name is ${hacker1}`)
 let hacker2 = "Felipe"
 console.log(`The navigator name is ${hacker2}`)
 
+// Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
   console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
