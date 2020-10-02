066f171486b8fc275f76d785b378b2fc4f7dab6f
Elisenda <elisenda.monserrat@gmail.com>
2020-10-02 20:25:14 +0200

    add 1st and second iteration to js lab

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
 // Iteration 1: Names and Input
+let hacker1 = "Elisenda"
+console.log(`The drivers name is ${hacker1}`)
 
+let hacker2 = "Felipe"
+console.log(`The navigator name is ${hacker2}`)
 
 // Iteration 2: Conditionals
+let driver = hacker1.length
+let navigator = hacker2.length
+
+if (driver > navigator) {
+  console.log(`The driver has the longest name, it has ${driver} characters`)
+} else if (driver < navigator) {
+  console.log(`It seems that the navigator has the longest name, it has ${navigator} characters`)
+} else if (driver == navigator) {
+  console.log("Wow, you both have equally long names, characters!")
+} else {
+  console.log("Woow, you're both a hackers")
+}
 
 
 // Iteration 3: Loops
