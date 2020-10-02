668a14d59bf1925c7846c8849ca45aff3b16c765
Pablo Fernandez <pablo_fz@hotmail.com>
2020-10-02 20:59:15 +0200

    tercera iteración

--- js/index.js
+++ js/index.js
@@ -18,3 +18,27 @@ console.log(`Wow, you both have equally long names, ${driverLength} characters!`
 }
 
 // Iteration 3: Loops
+let capitals ="";
+for(let i=0; i<driverLength; i++){
+  capitals += hacker1[i].toUpperCase() + " ";
+}
+console.log(capitals);
+
+let reverse ="";
+for(let i=navigatorLength-1; i>=0; i--){
+  reverse += hacker2[i];
+}
+console.log(reverse);
+
+let comparation = hacker1.localeCompare(hacker2);
+
+switch(comparation){
+  case -1: 
+  console.log("The driver's name goes first.");
+  break;
+  case 1: 
+  console.log("Yo, the navigator goes first definitely.");
+  break;
+  default: 
+  console.log("What?! You both have the same name?"); 
+}
