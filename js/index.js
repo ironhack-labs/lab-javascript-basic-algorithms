7a8f7a8f50685229904b4d75bb85cafd0417087a
Sebastian Montoya <seb246@gmail.com>
2020-10-09 17:41:34 -0400

    adding iteration 2

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ console.log(`The driver's name is ${driver}.`);
 console.log(`The navigator's name is ${navigator}.`);
 
 // Iteration 2: Conditionals
-
+if (driver.length > navigator.length){
+    console.log(`The driver has the longest name, it has ${driver.length} characters.`);
+  }else if (driver.length < navigator.length){
+    console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters.`);
+  } else {
+    console.log(`Wow, you both have equally long names, ${driver.length} characters!.`);
+  }
 
 // Iteration 3: Loops
