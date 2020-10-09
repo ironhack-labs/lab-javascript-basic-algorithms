1737ea996e6d5c5529b84e1e927e509a1a27192c
Sebastian Montoya <seb246@gmail.com>
2020-10-09 17:26:11 -0400

    adding iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+driver = `Sebastian`;
+navigator = `Mya`;
+console.log(`The driver's name is ${driver}.`);
+console.log(`The navigator's name is ${navigator}.`);
 
 // Iteration 2: Conditionals
 
