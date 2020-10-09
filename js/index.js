89910d1d9d1101a296a1f63f60759391f6769b40
Christian <christian.garba@gmail.com>
2020-10-08 19:02:07 -0700

    complete interation 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
 
+const hacker1 = `Driver`;
+console.log(`The driver's name is ${hacker1}.`);
+
+const hacker2 = `Navigator`;
+console.log(`The navigator's name is ${hacker2}.`)
 
 // Iteration 2: Conditionals
 
