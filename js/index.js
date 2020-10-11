852ed5c5130de6f0ad02755681fb0ab600629ec1
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2020-10-10 21:37:41 -0400

    i just finished the first twi iteneratiom

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 // Iteration 1: Names and Input
-
-const = 'felix';
+let hacker1 = "xxxx";
+console.log("The driver\'s name is " + hacker1);
+let hacker2 = "yyyy"
+console.log("The navigator\'s name is " + hacker2)
 // Iteration 2: Conditionals
-
-
+const name = 'XX';
+console.log(name.length);
+switch (name.length) {
+  case 2:
+    console.log('The driver has the longest name, it has XX characters');
+    break;
+  case 2:
+    console.log('It seems that the navigator has the longest name, it has YY characters.');
+    break;
+  default:
+    console.log('Wow, you both have equally long names, XX characters!');
+    break;}
 // Iteration 3: Loops
