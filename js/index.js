2f62cffad8ac9c9785ba47bb4c86f415eb8bf5be
mondono85 <mondono85@gmail.com>
2020-10-10 20:54:01 -0400

    i just finish the second one

--- js/index.js
+++ js/index.js
@@ -4,6 +4,17 @@ console.log("The driver\'s name is " + hacker1);
 let hacker2 = "Michael"
 console.log("The navigator\'s name is " + hacker2)
 // Iteration 2: Conditionals
+const name = 'XX';
+console.log(name.length);
 
-
+switch (name.length) {
+  case 2:
+    console.log('The driver has the longest name, it has XX characters');
+    break;
+  case 2:
+    console.log('It seems that the navigator has the longest name, it has YY characters.');
+    break;
+  default:
+    console.log('Wow, you both have equally long names, XX characters!');
+    break;}
 // Iteration 3: Loops
