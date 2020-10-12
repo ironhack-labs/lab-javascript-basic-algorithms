f315955afc9c6c9edba755f7655595fd9ff2dbcc
Rich Alossi <alossix@gmail.com>
2020-10-12 18:16:17 +0200

    Third iteration third commit

--- js/index.js
+++ js/index.js
@@ -23,3 +23,19 @@ console.log(hacker1.split("").join(" ").toUpperCase());
 
 let reverseHacker2 = hacker2.split("").reverse().join("");
 console.log(reverseHacker2);
+
+if (hacker1 === hacker2) {
+  console.log("What?! You both have the same name?");
+} else {
+  for (let i = 0; i < hacker1.length; i++) {
+    if (hacker1[i] < hacker2[i]) {
+      console.log("The driver's name goes first.");
+      break;
+    } else if (hacker1[i] > hacker2[i]) {
+      console.log(`Yo, the navigator goes first definitely.`);
+      break;
+    } else {
+      continue;
+    }
+  }
+}
