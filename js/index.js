b2f27cc34bc57ac6eee6036b94a6f50de3728604
Roy-Arne Brussel <royarnebrussel@gmail.com>
2020-10-12 20:16:38 +0200

    Iteration 3 completed

--- js/index.js
+++ js/index.js
@@ -18,3 +18,27 @@ if (hacker1.length == hacker2.length) {
 
 // Iteration 3: Loops
 
+// 3.1
+var nameOfDriver = "";
+for (let i = 0; i < hacker1.length; i++) {
+  nameOfDriver += hacker1[i].toUpperCase() + " ";
+}
+console.log(nameOfDriver);
+
+// 3.2
+var reversedNameOfNavigator = "";
+for (let i = hacker2.length-1; i >= 0; i--) {
+  reversedNameOfNavigator += hacker2[i];
+}
+console.log(reversedNameOfNavigator);
+
+// 3.3
+if (hacker2.localeCompare(hacker1) === 1) {
+    console.log("The driver's name goes first.");
+  } else if (hacker2.localeCompare(hacker1) === 0) {
+    console.log("What?! You both have the same name?");
+  } else if (hacker2.localeCompare(hacker1) === -1) {
+    console.log("Yo, the navigator goes first definitely.")
+  }
+
+
