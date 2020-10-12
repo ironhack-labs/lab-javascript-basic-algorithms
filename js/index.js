7afa9a8718362224f277b6f2ca18e1a58f457a37
Margarida <margarida97pt@gmail.com>
2020-10-12 18:18:15 +0100

    lab solved

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Margarida'
+console.log(hacker1)
+
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = 'Marta'
+console.log(hacker2)
+
+console.log(`The navigator's name is ${hacker2}`);
 
 
 // Iteration 2: Conditionals
 
+hacker1.length = 9;
+console.log(`The driver has the longest name, it has ${hacker1.length}`);
+
 
 // Iteration 3: Loops
+
+console.log('margarida'.toUpperCase().split("").join(" "));
+
+console.log('Margarida'.split("").reverse().join(""));
+
+let order = ["The driver's name goes first", "Yo, the navigator goes first definitely", "What?! You both have the same name?"];
+order.sort();
+console.log(order);
