800be9a8526e36f4d7984e9759a373703af7c4cf
Sabela <sabela.ons2@gmail.com>
2020-10-12 20:24:34 +0200

    interation 3a

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
+
+console.log("I'm ready!");
 // Iteration 1: Names and Input
 let hacker1 = "Sabela"
-console.log("The driver's name is ${hacker1}")
+console.log(`The driver's name is ${hacker1}`)
 
 let hacker2 = "Nina"
-console.log("The navigator's name is ${hacker2}")
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
-    console.log("The driver has the longest name, it has ${hacker1.length} characters.")
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
 
 }
 else if (hacker2.length > hacker1.length) {
-    console.log("It seems that the navigator has the longest name, it has ${hacker2.length}")
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length}`)
 }
 
 else (
-    console.log("Wow, you both have equally long names, ${hacker2.length} characters")
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters`)
 )
-
 // Iteration 3: Loops
-
+let sepName = "";
+for (i = 0; i < hacker1.length; i++) {
+    sepName = sepName + hacker1.charAt(i).toUpperCase()+" ";
+}
+console.log(sepName);
