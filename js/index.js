3ca6bebd479aabbd02f2337d1829268adb69c7be
djnokin <nokindj@gmail.com>
2020-10-12 18:34:25 +0100

    with bonus 1

--- js/index.js
+++ js/index.js
@@ -34,3 +34,15 @@ if (hacker1.charAt(0) > hacker2.charAt(0)) {
 } else {
   console.log("What?! You both have the same name?");
 }
+
+// Bonus 1 
+
+let giganticString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra, quam id tempus cursus, leo nunc facilisis dui, in sagittis libero risus et mauris. Suspendisse consequat sodales augue. In tincidunt vestibulum sem, at vehicula nisi consequat at. In iaculis lobortis quam, quis tincidunt justo volutpat in. Nam et felis sit amet risus rhoncus dignissim. Nam quis lacus et turpis feugiat vulputate at a mi. Donec mattis a lacus nec mattis. Curabitur posuere lectus ipsum, et rhoncus lectus sollicitudin nec. Aenean viverra quis sem a dapibus. Vivamus ipsum orci, interdum ac venenatis nec, ornare vitae erat. Phasellus pulvinar ullamcorper diam, eu auctor justo scelerisque sit amet. Ut sed auctor ante. Praesent varius aliquet egestas. Integer consectetur mauris lacus, vitae auctor leo porta consectetur. Aenean nibh diam, luctus a elit nec, semper egestas ligula. Praesent metus dui, vulputate bibendum ullamcorper eu, sodales at risus. In sed nunc interdum, dictum sapien in, rhoncus leo. Donec hendrerit massa vel faucibus porttitor. Morbi aliquet justo nulla, ac eleifend quam lobortis vitae. Suspendisse sit amet nisl eget elit commodo cursus quis id nunc. Donec in dolor non nunc bibendum faucibus id sit amet orci. Suspendisse viverra vehicula vestibulum. Nullam sit amet lobortis mauris, eu aliquam metus. Pellentesque a consectetur mi. Sed sagittis, velit blandit congue convallis, nisi felis pellentesque ex, ut dictum dolor ante nec quam. Sed non lacus mi. In scelerisque augue ac dapibus facilisis. Quisque ultrices, risus nec imperdiet ullamcorper, magna est rhoncus eros, ac pretium risus ex non nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus.";
+
+let wordCount = 1;
+for (let i = 0; i < giganticString.length; i++) {
+  if (giganticString[i] === " ") {
+    wordCount += 1;
+  }
+}
+console.log(wordCount);
