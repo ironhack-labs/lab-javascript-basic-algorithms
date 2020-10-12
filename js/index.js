dcee80f6c908699c4740964bde854b9e5e936d8a
Emma Northridge <emmanorthridge1@gmail.com>
2020-10-12 17:57:29 +0200

    First and Second Iteration complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
 // Iteration 1: Names and Input
+let hacker1 = "Emma";
+console.log(`The driver's name is ${hacker1}`);
 
+let hacker2 = "Delia";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+  }
+  else if (hacker1.length < hacker2.length){ 
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters!`);
+    }
+    else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+  }
 
+  // Iteration 3: Loops
+ //not sure how to only print the name only once with a loop?
+for (let i = 0; i < hacker1.length; i++)
+console.log(hacker1.toUpperCase().split("").join(" "));
 
-// Iteration 3: Loops
