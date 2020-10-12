9b515be9060760ac53d9eb02a4c491a8cc960d6e
Rich Alossi <alossix@gmail.com>
2020-10-12 16:01:56 +0200

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Rich";
+console.log(`"The driver's name is ${hacker1}".`);
+let hacker2 = "Jacob";
+console.log(`"The navigator's name is ${hacker2}".`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
