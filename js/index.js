5a53b8c65cd9a45044f643ae8ac9f5476b865154
Anna Mosley <annahmosley@gmail.com>
2020-10-12 20:14:37 -0400

    completed iteration 1 and added the variables

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 // Iteration 1: Names and Input
 
+let name1 = 'Anna';
+const hacker1= name1
+console.log(`The driver's name is, ${hacker1}!`);
+
+let name2 = 'Sandra';
+const hacker2= name2
+console.log(`The navigator's name, ${hacker2}!`);
+
+
 
 // Iteration 2: Conditionals
 
