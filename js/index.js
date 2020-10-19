3d8e7daaf5d46d68be9f56aa256281930a0c43fc
ararita <munjizavesna@gmail.com>
2020-10-19 17:16:37 +0200

    Third iteration

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@
 // 1.3 Create a variable `hacker2` with the navigator's name.
 // 1.4 Print `"The navigator's name is YYYY"`.
 
-const hacker1 = "Driver";
-const hacker2 = "Navigator";
+const hacker1 = "Leonie";
+const hacker2 = "Vesna";
 
 console.log(`driver's name is ${hacker1} and navigator's name is ${hacker2}`);
 
@@ -28,3 +28,12 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name,
+//separated by a space and in capitals i.e. "J O H N"
+
+// console.log(hacker1.split("").join(" ").toUpperCase());
+let newHacker1 = "";
+for (let i = 0; i < hacker1.length; i++) {
+  newHacker1 += hacker1[i].toUpperCase() + " ";
+  console.log(newHacker1.trim());
+}
