7b52ed3c83d2cc924944320649f45fd7fd2413e5
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2020-10-19 22:24:58 +0200

    redid 3 tasks of w1d1

--- js/index.js
+++ js/index.js
@@ -1,40 +1,85 @@
-// Iteration 1: Names and Input
+// 1 Names and Input
 
-const hacker1 = 'Barcus';
+const hacker1 = 'Da';
 console.log(`The driver's name is ${hacker1}.`);
-const hacker2 = 'Fiona';
+const hacker2 = 'Dama';
 console.log(`The navigator's name is ${hacker2}.`);
 
-// Iteration 2: Conditionals
+// 2 Conditionals
 
 if (hacker1.length > hacker2.length) {
-  console.log(`The driver has the longest name. It has ${hacker1.length} characters`);
+    console.log(`${hacker1} has the longest name, it has ${hacker1.length} characters.`);
 } else if (hacker1.length < hacker2.length) {
-  console.log(`It seems that the navigator has the longest name. It has ${hacker2.length} characters`);
+    console.log(`It seems that ${hacker2} has the longest name, it has ${hacker2.length} characters.`);
 } else {
-  console.log(`Wow, you both ahve equally long names; ${hacker1.length} characters!`);
-}
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+};
 
-// Iteration 3: Loops
-// Uppercase + Space
-let result = "";
-for (let i=0; i <hacker1.length; i++) {
-  result += hacker1[i].toUpperCase()+" ";
-}
-console.log(result);
 
-//reverse 
-let reverse = "";
-for (let i=hacker2.length-1; i >= 0; i--){
-  reverse += hacker2.charAt(i);
+// 3 Loops
+
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+// Note: Addition(+) for strings equals strings concatenation 
+
+let results = '';
+for (i = 0; i < hacker1.length; i++) {
+    results += hacker1.charAt(i).toUpperCase() + ' ';
+}
+console.log(results);
+
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+// Note: Addition(+) for strings equals strings concatenation 
+
+// backwards for loop
+let reverse = '';
+for (i = hacker2.length - 1; i >= 0; i--) {
+    reverse = reverse + hacker2.charAt(i);
+    // reverse = reverse.concat(hacker2.charAt(i));
 }
 console.log(reverse);
 
-// lexicographic order 
-if (hacker1.charAt(0) < hacker2.charAt(0)) {
-   console.log(`${hacker1} goes first.`);
-} else if (hacker1.charAt(0) > hacker2.charAt(0)) {
-   console.log(`${hacker2} goes first.`);
+// forwards for loop
+// let reverse = '';
+// for (i = 0; i <= hacker2.length - 1; i++) {
+//     reverse = reverse + hacker2.charAt(hacker2.length-(i+1));  
+// }
+// console.log(reverse);
+
+
+// 3.3 Depending on the lexicographic order of the strings, print: 
+
+if (hacker1.length < hacker2.length) {
+    for (i = 0; i < hacker1.length; i++) {
+        if (hacker1.charAt(i) < hacker2.charAt(i)) {
+            console.log(`${hacker1} goes first.`);
+            break;
+        } else if (hacker1.charAt(i) > hacker2.charAt(i)) {
+            console.log(`Yo, ${hacker2} goes first definitely.`);
+            break;
+        };
+    };
+    console.log(`${hacker1} goes first.`);
+} else if (hacker1.length > hacker2.length) {
+    for (i = 0; i < hacker2.length; i++) {
+        if (hacker1.charAt(i) < hacker2.charAt(i)) {
+            console.log(`${hacker1} goes first.`);
+            break;
+        } else if (hacker1.charAt(i) > hacker2.charAt(i)) {
+            console.log(`Yo, ${hacker2} goes first definitely.`);
+            break;
+        };
+    };
+    console.log(`Yo, ${hacker2} goes first definitely.`)
 } else {
-  console.log('You both have the same name.');
+    for (i = 0; i < hacker1.length; i++) {
+        if (hacker1.charAt(i) < hacker2.charAt(i)) {
+            console.log(`${hacker1} goes first.`);
+            break;
+        } else if (hacker1.charAt(i) > hacker2.charAt(i)) {
+            console.log(`Yo, ${hacker2} goes first definitely.`);
+            break;
+        } else if (hacker1.charAt(i) === hacker2.charAt(i) && i === hacker1.length-1) {
+            console.log(`What?! You both have the same name?`);
+        };
+    };   
 }
