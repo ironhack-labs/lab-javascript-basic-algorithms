201578d1f1d7986a5988d07e24fdd2e815e0a399
JaimePintoP <jaimepinto.saxo@gmail.com>
2020-10-19 22:10:07 +0200

    Added Bonus 1

--- js/index.js
+++ js/index.js
@@ -49,15 +49,44 @@ if (hacker1.length < hacker2.length){
 
 for (let i = 0; i < longerName.length; i++){
   if (hacker1[i] === hacker2[i]){
-  continue;
+    continue;
   }else if(hacker1[i] < hacker2[i]){
-  console.log("The driver's name goes first")
-  break;
+    console.log("The driver's name goes first")
+    break;
   }else if (hacker1[i] > hacker2[i]){
   console.log("Yo, the navigator goes first definitely")
-  break;
+    break;
   }else{
-  console.log("What?! You both have the same name?")
-  }
-  
+    console.log("What?! You both have the same name?")
+  } 
 }
+
+// Bonus 1
+
+// bonus 1.1
+let paragraph = `"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+
+Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+
+Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`
+
+
+//bonus 1.2
+let howManyWords = paragraph.split(" ")
+
+console.log (howManyWords.length)
+
+// bonus 1.3
+
+console.log(howManyWords.includes("et"));
+
+let wordEt = 0;
+
+for (let i = 0; i < howManyWords.length; i++){ 
+  if (howManyWords[i] === "et"){
+    wordEt += 1;
+  }else{
+  continue;
+  }
+}
+console.log(wordEt);
