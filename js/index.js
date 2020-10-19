d2b6c3db149b29cdf8e3b163a9182317fadf3693
Juliana Caruso <juliana.caruso@gmail.com>
2020-10-19 19:59:49 +0200

    Add iteration 3.3

--- js/index.js
+++ js/index.js
@@ -28,3 +28,11 @@ for (let i = hacker2.length - 1; i >= 0; i--)  {
   res += hacker2[i];
 }
 console.log(res);
+
+if (checkOrder === -1) {
+    console.log('The driver\'s name goes first.');
+  } else if (checkOrder === 1) {
+    console.log('Yo, the navigator goes first definitely.');
+  } else {
+    console.log('What?! You both have the same name?');
+  }
