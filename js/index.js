ba9265bfb4d44145cca0ef1c43b96f77c215089d
franck-samy <72886529+franck-samy@users.noreply.github.com>
2020-10-19 17:14:11 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
 // Iteration 1: Names and Input
-
+console.log("I'm ready!");
+const hacker1 = "Milena";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = 'Franck';
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+if(hacker1.length > hacker2.length){
+  console.log(`hacker1 has the longest name with ${hacker1.length} characters`);
+} else if(hacker1.length < hacker2.length) {
+  console.log(`hacker2 has the longest name with ${hacker2.length} characters`);
+} else {
+  console.log (`They both have equality long names, ${hacker1.length} characters`);
+}
+console.log(hacker1.toUpperCase());
+let hackerUpperCase = hacker1.toUpperCase();
+let upperCase = hackerUpperCase.split("").join(" ");
+console.log(upperCase);
+
 
 
 // Iteration 3: Loops
+let finalString = "";
+for(let i =  0 ; i < hackerUpperCase.length; i++ ){
+  const letter = hackerUpperCase[i];
+  finalString += `${letter} `
+}
+console.log(finalString);
+
+let reverse = hacker2.split("").reverse("").join("");
+console.log(reverse);
