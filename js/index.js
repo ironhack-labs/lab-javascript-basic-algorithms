9cd4c37b45eba5e27a11bb1693487479d9f8ed25
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-10-19 22:25:10 +0200

    Second commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 // Iteration 1: Names and Input
+console.log("I'm ready!");
+// Iteration 1
+var hacker1 = "Martin";
+console.log(hacker1);
 
+var hacker2 = "Google Maps";
+console.log(hacker2);
 
 // Iteration 2: Conditionals
-
+ 
 
 // Iteration 3: Loops
