3c22a6165f5946f931a59f8ca5988f9f9ddf9848
Jan <jan.leon.voelker@gmail.com>
2020-10-19 16:28:27 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-
+console.log("I'm ready");
+const hacker1 = "Timmy";
+console.log(`The driver's name is ${hacker1}.`)
+const hacker2 = "Alice";
+console.log(`The navigator's name is ${hacker2}.`)
 
 // Iteration 2: Conditionals
 
