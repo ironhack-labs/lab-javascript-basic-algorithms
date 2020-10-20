484297d2c5163a7c49e0e3d072ab6cb7445008b2
Christian Ares <christianaresvillan@gmail.com>
2020-10-20 16:34:44 +0200

    ·rd iteration complete

--- js/index.js
+++ js/index.js
@@ -28,3 +28,9 @@ let reverseName = "";
    reverseName += hacker2[i];
  }
 console.log(reverseName);
+
+if(hacker1.localeCompare(hacker2) === -1){
+    console.log(`The driver's name goes first.`);
+  } else if(hacker1.localeCompare(hacker2) === 1){
+    console.log(`Yo, the navigator goes first definitely`);
+  } else(console.log(`What?! You both have the same name?`));
