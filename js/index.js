22b7245de41f0e7bb4ac6ed3f8b61d02ed3037f4
Raquel R <raquelrobles95@gmail.com>
2020-10-20 12:57:48 +0200

    Exercise with Bonus

--- js/index.js
+++ js/index.js
@@ -31,3 +31,32 @@ for (let i= hacker1Name.length-1; i>=0; i--){
   reverseName += hacker1Name[i];
 }
   console.log(reverseName)
+
+
+//BONUS EXERCISES
+
+//Bonus 1
+
+let loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate, velit et tincidunt faucibus, leo lorem ornare est, vel lobortis leo lectus at odio. Phasellus sit amet ultrices massa. Cras imperdiet augue purus, quis dictum dui scelerisque egestas. Quisque lorem orci, finibus vel lacus non, tristique lobortis leo. Vestibulum pellentesque nunc sed mollis efficitur. Phasellus vitae enim vitae leo faucibus consectetur ac quis odio. Etiam dictum a risus id hendrerit. Donec ut leo quis erat feugiat dictum a sed metus. Ut turpis urna, feugiat non lorem in, consectetur congue magna. Sed rutrum, est viverra posuere porta, magna diam feugiat felis, vitae volutpat nunc mi at mauris. Aliquam mauris mauris, tincidunt eu tellus sed, laoreet interdum magna. Nullam velit arcu, sagittis quis ex at, scelerisque sodales velit. Integer quis iaculis massa. Nullam euismod nisl a mauris blandit, et vulputate odio ullamcorper. Donec hendrerit congue est, ac elementum turpis molestie eu. Duis dignissim purus elit, quis congue justo vulputate a. Etiam euismod urna tincidunt, commodo diam in, aliquet sem. Nunc felis magna, venenatis a hendrerit nec, semper eu turpis. In pharetra tristique odio quis condimentum. Aliquam non mi quis erat lacinia bibendum in sed est. Quisque dapibus nulla nec erat rutrum faucibus. Fusce quis auctor orci. Fusce nec vehicula elit. Donec sagittis nunc sit amet congue accumsan. Nullam eget convallis tortor. Phasellus vehicula dui ac est hendrerit pharetra. Praesent hendrerit egestas faucibus. Ut ac lorem sodales mauris faucibus facilisis. Morbi placerat justo a risus tempor, id facilisis mi posuere.`
+
+console.log(loremIpsum);
+
+let counter = loremIpsum.split(" ");
+console.log (counter.length)
+
+let appears = loremIpsum.split("et");
+console.log (appears.length)
+
+
+//Bonus 2
+let phraseToCheck = "step on no pets"
+
+let reversePhrase= "";
+
+for (let i= phraseToCheck.length-1; i>=0;i--){
+  reversePhrase += phraseToCheck[i];
+} if(phraseToCheck === reversePhrase){
+  console.log ('They are equal')
+} else {
+  console.log(`They aren't equal`)
+}
