d039cfc0b044b6289ca15b7c3e263761e7b451a0
Mark <mark.sanjuan.sole@gmail.com>
2020-10-20 12:50:31 +0200

    added firt part created by Sara

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+// Iteration 1: Names and Input
+const hacker1 = 'Sara';
+const hacker2 = 'Mark';
+console.log(`The driver’s name is ${hacker1}`);
+console.log(`The navigator’s is ${hacker2}`);
+
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} character`);
+  } else if (hacker1.length < hacker2.length) {
+    console.log(`The navigator has the longest name, it has ${hacker2.length} character`);
+  } else {`Wow, you both have equally long names, ${hacker1.length} characters!`}
+
+  // Iteration 3: Loops
+let newHacker1 = '';
+for (let i=0; i<hacker1.length; i++) {
+ newHacker1 = newHacker1 + ' ' + hacker1[i]
+}
+console.log(newHacker1.toUpperCase())
+
+
 //BONUS 1
 let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus gravida, nibh vitae feugiat ultricies, leo tellus suscipit est, ut imperdiet urna massa id mi. Nam aliquam neque non interdum interdum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus mollis ligula mauris, ut ornare ante tincidunt vel. Nam egestas velit eget nulla pulvinar varius. Mauris sed mollis augue. Suspendisse imperdiet dictum nibh a aliquam. Interdum et malesuada fames ac ante ipsum primis in faucibus.\
 In pharetra venenatis erat, vitae varius ipsum fermentum et. Curabitur eu ultrices urna, et tincidunt justo. Proin dignissim ac diam vitae porttitor. Donec efficitur ligula quis dui tincidunt tincidunt. Fusce tellus turpis, faucibus ac molestie id, semper a mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet vitae neque vitae lobortis. Nulla commodo, diam ultricies pellentesque ultricies, orci mauris laoreet lacus, sed viverra est lacus ut dui. Duis dapibus, metus vitae faucibus vehicula, augue diam porta nulla, nec tristique ante nunc eu ante.\
