b51d6ddbf715c3a459cc3967c9f4fd4f152ddc9e
Paulo <paulobart@outlook.com>
2020-10-23 19:19:20 +0100

    nova versao

--- js/index.js
+++ js/index.js
@@ -4,34 +4,52 @@
 // print: Fabio
 // nome igual: Wow, you both have equally long names, XX characters!
 // nome 
-// Print PAULO
+// Print P A U L O
 // print oibaF
 // Lexo: Fabio, Paulo
 
-let hacker1 = `FabioH`;
+//1: names and input
+let hacker1 = `Fabio`;
 console.log(`The driver´s name is ${hacker1}`);
 
 let hacker2 = `Paulo`;
 console.log(`The navigator´s name is ${hacker2}`);
 
-console.log(hacker1.length);
-console.log(hacker2.length);
+// 2: conditionals
+//console.log(hacker1.length);
+//console.log(hacker2.length);
 
-if (hacker1.length >= hacker2.length){
-    console.log(`The driver has the longes name, it has ${hacker1.length} caracters`)
+if (hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} caracters`)
 }
-else if (hacker1.length <= hacker2.length){
+else if (hacker1.length < hacker2.length){
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
 }
-else {
+else if (hacker1.length = hacker2.length)
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
-}    
 
-let hacker1up = hacker1();
-let i = ``;
-for (let `hacker1up` in capsLock;) {
-    hacker1.push(hacker1up.indexOf(capsLock[i].toUpperCase()) + 1);
+    //3: loops
+console.log (hacker2.toUpperCase())
+let nomeC = ''
+for (letra of hacker2.toUpperCase()) { 
+     nomeC = (nomeC + letra + ' ')  
 }
-return hacker1up; 
+console.log(nomeC)
+
+let nomeAlcont = ''
+for (letra of hacker1) { 
+     nomeAlcont = (letra + nomeAlcont)  
+} 
+console.log(nomeAlcont);
 
 
+if (hacker1.length > hacker2.length){
+    console.log(`The driver's name goes first.`)
+}
+else if (hacker1.length < hacker2.length){
+    console.log(`Yo, the navigator goes first definitely.`)
+}
+else if (hacker1.length = hacker2.length)
+    console.log(`What?! You both have the same name?`)
+
+    // Para a ordem alfabética precisamos criar uma alfabeto como variável?     
