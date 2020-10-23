ecf1259cb21290cb61dfb75b22e4064107cf29fd
Paulo <paulobart@outlook.com>
2020-10-23 03:01:51 +0100

    almost done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,37 @@
-// Iteration 1: Names and Input
+// driver: Paulo
+// print: Paulo
+// navegador: Fabio
+// print: Fabio
+// nome igual: Wow, you both have equally long names, XX characters!
+// nome 
+// Print PAULO
+// print oibaF
+// Lexo: Fabio, Paulo
+
+let hacker1 = `FabioH`;
+console.log(`The driver´s name is ${hacker1}`);
+
+let hacker2 = `Paulo`;
+console.log(`The navigator´s name is ${hacker2}`);
+
+console.log(hacker1.length);
+console.log(hacker2.length);
+
+if (hacker1.length >= hacker2.length){
+    console.log(`The driver has the longes name, it has ${hacker1.length} caracters`)
+}
+else if (hacker1.length <= hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+}
+else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}    
+
+let hacker1up = hacker1();
+let i = ``;
+for (let `hacker1up` in capsLock;) {
+    hacker1.push(hacker1up.indexOf(capsLock[i].toUpperCase()) + 1);
+}
+return hacker1up; 
 
 
-// Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
