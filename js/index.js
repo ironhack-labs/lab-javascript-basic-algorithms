014601630cd4262e250f9ffefbdee89bbd566830
Victor Nastasa <victornast@gmail.com>
2020-10-24 16:35:37 +0200

    The three iterations

--- js/index.js
+++ js/index.js
@@ -2,8 +2,31 @@ console.log("I'm ready!");
 
 // Iteration 1: Names and Input
 
+let hacker1 = "Michael";
+let hacker2 = "Jean";
+
+console.log(`The Driver's name is ${hacker1}.`);
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log (`The driver has the longest name, it has ${hacker1.length} characters.`);
+  } else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+  } else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+  };
 
 // Iteration 3: Loops
+
+console.log(hacker1.split('').join(' ').toUpperCase());
+console.log(hacker2.split("").reverse().join(""));
+
+if (hacker1 < hacker2) {
+    console.log(`The driver's name goes first.`);
+  } else if (hacker1 > hacker2) {
+    console.log(`Yo, the navigator goes first definitely.`);
+  } else {
+    console.log(`What?! You both have the same name?`);
+  };
