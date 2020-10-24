cb8a3d98b64886b821c5981f19497d7bd4be6291
sarastackhouse <sarapalacios7@gmail.com>
2020-10-24 18:18:44 +0200

    3.3 Done and fix format

--- js/index.js
+++ js/index.js
@@ -1,35 +1,45 @@
 // Iteration 1: Names and Input
-
 const hacker1 = 'Sara';
 const hacker2 = 'Mark';
 console.log(`The driver's name is ${hacker1}`);
 console.log(`The navigator's is ${hacker2}`);
 
-// Iteration 2: Conditionals
 
+// Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} character`);
   } else if (hacker1.length < hacker2.length) {
     console.log(`The navigator has the longest name, it has ${hacker2.length} character`);
   } else {`Wow, you both have equally long names, ${hacker1.length} characters!`}
 
-// Iteration 3: Loops
-// 3.1:
 
+// Iteration 3: Loops
+
+// 3.1:
 let newHacker1 = '';
 for (let i=0; i<hacker1.length; i++) {
  newHacker1 = newHacker1 + ' ' + hacker1[i]
 }
-console.log(newHacker1.toUpperCase())
+console.log(newHacker1.toUpperCase());
 
 // 3.2:
-let hacher2NameReverse = "";
+let hacker2NameReverse = "";
 
 for(let i = hacker2.length-1; i>=0; i--){
-  hacher2NameReverse+= hacker2[i]
+  hacker2NameReverse+= hacker2[i]
 }
+console.log(hacker2NameReverse)
 
-console.log(hacher2NameReverse)
+// 3.3
+for(let i = 0; i<1;i++) {
+  if(hacker1.localeCompare(hacker2) === -1) {
+    console.log("The driver's name goes first")
+  } else if(hacker2.localeCompare(hacker1) === -1) {
+    console.log("Yo, the navigator goes first definitely.")
+  } else {
+    console.log("What?! You both have the same name?")
+  }
+}
 
 //BONUS 1
 let loremIpsumTest = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus gravida, nibh vitae feugiat ultricies, leo tellus suscipit est, ut imperdiet urna massa id mi. Nam aliquam neque non interdum interdum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus mollis ligula mauris, ut ornare ante tincidunt vel. Nam egestas velit eget nulla pulvinar varius. Mauris sed mollis augue. Suspendisse imperdiet dictum nibh a aliquam. Interdum et malesuada fames ac ante ipsum primis in faucibus.\
@@ -44,6 +54,7 @@ for(let i = 0; i < loremNoSpace.length; i++){
   }
 }
 console.log(wordCounter)
+
 //BONUS 2
 let phraseToCheck = "Put it up!";
 let phraseNoExp = ''
