a3c1c9edc05eabf9c3a36981204a6d2bb970ad2c
jaijur <jaimejuradoperez@gmail.com>
2020-10-28 19:30:22 +0100

    feat: js basic algorithms

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
-// Iteration 1: Names and Input
+let driver = 'Miguel';
+let navigator = 'Jaime';
 
+console.log(`the driver's name is ${driver}`);
+console.log(`the navigator's name is ${navigator}`);
 
-// Iteration 2: Conditionals
+if (driver.length > navigator.length){
+  console.log(`The driver has the longest name, it has ${driver.length} characters`);
+} else if (driver.length < navigator.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+  console.log(`Wow, you both have equally long names, ${driver.length} characters!`)
+}
 
+let newDr =  driver.toUpperCase().slice(' ').split('');
 
-// Iteration 3: Loops
+let drSpace = driver.toUpperCase().split('').slice(' ').join(' ');
+
+console.log(drSpace);
+
+let navReverse = navigator.split('').reverse().join('');
+
+console.log(navReverse);
+
+const compOrder = driver.localeCompare(navigator);
+  if (compOrder < 0) {
+    console.log(`The driver's name goes first`);
+      } else if (compOrder > 0) {
+           console.log('Yo, the navigator goes first definitely.');
+      } else {
+           console.log('What?! You both have the same name?')
+      }
