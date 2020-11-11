4a8ea745cd3dda3e08c267ea8df5c4ff828b9b05
Preacherush <jlrascon.job@gmail.com>
2020-11-11 23:56:12 +0100

    3.1 and 3.2 added again (eliminated by mistake). // 3.2 l41 added '-1' to don't get Undefined. //  New 3.3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Iteration 1: Names and Input
+
 // 1.1 Create a variable `hacker1` with the driver's name.
 let hacker1 = "Manuel Carrillo";
 
@@ -9,9 +10,15 @@ console.log(`The driver's name is ${hacker1}`);
 let hacker2 = "Jose Rascon"
 
 // 1.4 Print `"The navigator's name is YYYY"`.
-console.log(`The Navigator is ${hacker2}` )
+console.log(`The Navigator's name is ${hacker2}` )
 
 // Iteration 2: Conditionals
+
+//2.1. Depending on which name is longer, print:
+//- The driver has the longest name, it has XX characters. or
+//- It seems that the navigator has the longest name, it has XX characters. or
+//- Wow, you both have equally long names, XX characters!
+
 if (hacker1.length > hacker2.length){
     console.log(`${hacker1} has the longest name, it has ${hacker1.length} characters.`)
   } else if (hacker2.length > hacker1.length) {
@@ -19,11 +26,42 @@ if (hacker1.length > hacker2.length){
   } else {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
   }
-// Iteration 3: Loops
-if (hacker1.localeCompare(hacker2)){
+
+// ===Iteration 3: Loops===
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals
+// Option 1 - Loop
+for (i = 0; i < hacker1.length; i++){
+  console.log(hacker1[i].toUpperCase());
+}
+// Option 2 (no loop)
+console.log(hacker1.split('').join(' ').toUpperCase())
+
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e.
+// Option 1 - Loop
+for (i = hacker2.length -1; i >= 0; i--){
+  console.log(hacker2[i]);
+}
+// Option 2 (no loop)
+console.log(hacker2.split('').reverse().join(''));
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+if (hacker1.localeCompare(hacker2) === -1){
   console.log(`${hacker1} goes first.`);
-} else if (hacker2.localeCompare(hacker1){
-  console.log(`$hacker2`);
+} else if (hacker1.localeCompare(hacker2) === 1){
+  console.log(`Yo, ${hacker2} goes first definitely.`)
 } else {
   console.log(`What?! You both have the same name?`);
 }
+
+/*Manu, creo que esto es lo que se buscaba en el 3.3
+Tal como lo teniamos lo he dejado aqui debajo comentado.
+Pero haciendolo asi, no cambiaba el orden al cambiar los nombres.
+Echale un ojo y comentamos*/
+
+/*if (hacker1.localeCompare(hacker2)){
+  console.log(`${hacker1} goes first.`);
+} else if (hacker2.localeCompare(hacker1)){
+  console.log(`$hacker2`)
+} else {
+  console.log(`What?! You both have the same name?`);
+}*/
