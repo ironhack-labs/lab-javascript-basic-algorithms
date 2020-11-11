86123b76b56058b930bb1af3d7fc1d99fd3dd87f
azzarkani hanane <azzarkani.hanane@gmail.com>
2020-11-11 19:37:43 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,48 @@
 // Iteration 1: Names and Input
+var hacker1 ="XXXX";
+var hacker2 ="YYYY";
 
+console.log( "the driver's name is " + hacker1);
+
+var hacker1 ="XXXX";
+var hacker2 ="YYYY";
+
+console.log( "the navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
+const fact = 'Hacker1';
+console.log(`"${fact}" is a string and it is length is ${fact.length}.`);
 
+const nom = 'hacker2';
+console.log(`"${nom}" is a string and it is length is ${fact.length}.`);
+
+var nhack1 = hacker1.length;
+var nhack2 = hacker2.lenght;
+
+
+console.log(hacker1.lenght);
+
+if (nhack1 == nhack2) {
+console.log ('Wow, you have equally long names, ${nironhack1} characters !')
+
+
+} else {
+    console.log('It seems that the navigator has the longest name, it has {nironhack1}characters');
+
+}
 
 // Iteration 3: Loops
+
+console.log('hacker1'.toUpperCase());
+
+let s = 'nhoj';
+let rev = s.split('').reverse().join('');
+console.log(rev);
+
+
+var hacker1 ="Mario";
+var hacker2 ="John";
+
+
+console.log(hacker1.localeCompare(hacker2));
+
