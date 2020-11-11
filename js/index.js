900c326eb2645ea90ba04b7a7711553c66e7b2d7
Preacherush <jlrascon.job@gmail.com>
2020-11-11 20:46:36 +0100

    interation1 created

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 // Iteration 1: Names and Input
+// 1.1 Create a variable `hacker1` with the driver's name.
+let hacker1 = "Manuel Carrillo";
 
+// 1.2 Print `"The driver's name is XXXX"`.
+console.log(`The driver's name is ${hacker1}`);
+
+// 1.3 Create a variable `hacker2` with the navigator's name.
+let hacker2 = "Jose Rascon"
+
+// 1.4 Print `"The navigator's name is YYYY"`.
+console.log(`The Navigator is ${hacker2}` )
 
 // Iteration 2: Conditionals
 
