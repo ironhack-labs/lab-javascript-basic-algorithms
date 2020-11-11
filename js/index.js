5aa377c720c90fe321b10a18791d2c9d1a9cd99f
rgarsan <rgarsan84@gmail.com>
2020-11-11 23:04:38 +0100

    Primer ejercicio

--- js/index.js
+++ js/index.js
@@ -9,4 +9,24 @@ console.log(hacker2);
 // Iteration 2: Conditionals
 
 
+
+if(hacker1.length > hacker2.length){
+  console.log(` El conductor con nombre más largo es: ${hacker1} y tiene longitud ${hacker1.length}` )
+  
+}else if (hacker1.length < hacker2.length){
+  console.log(`El conductor con nombre más largo es: ${hacker2} y tiene longitud ${hacker2.length}` )
+  
+  
+}else{
+  console.log(`${hacker1} y ${hacker2} tienen la misma longitud`)
+}
+
+
 // Iteration 3: Loops
+
+let hacker1Mayus = hacker1.toUpperCase();
+
+for(let i = 0; i<hacker1Mayus.length; i++){
+    console.log(`hacker1Mayus[i] `)
+    
+}
