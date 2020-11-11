f5233d27f8c061e88fa9d3233c072bc39792e175
Preacherush <jlrascon.job@gmail.com>
2020-11-11 21:39:35 +0100

    interation3 created

--- js/index.js
+++ js/index.js
@@ -20,3 +20,10 @@ if (hacker1.length > hacker2.length){
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
   }
 // Iteration 3: Loops
+if (hacker1.localeCompare(hacker2)){
+  console.log(`${hacker1} goes first.`);
+} else if (hacker2.localeCompare(hacker1){
+  console.log(`$hacker2`);
+} else {
+  console.log(`What?! You both have the same name?`);
+}
