2c8757b365178b919201673b1a3cac32f4aca738
Diego Cerratos <diegocerratos@gmail.com>
2020-11-12 11:21:31 +0100

    Segundo ejercicio

--- js/index.js
+++ js/index.js
@@ -1,32 +1,21 @@
-// Iteration 1: Names and Input
-let hacker1 = "Eli"
+// ------ Iteration 1: Names and Input ------
+
+let hacker1 = "Elis";
 console.log(hacker1);
 
-let hacker2 = "Rafa"
+let hacker2 = "Rafa";
 console.log(hacker2);
 
+// ------ Iteration 2: Conditionals ------
 
-// Iteration 2: Conditionals
+if(hacker1.length>hacker2.length)
+    console.log(`${hacker1} tiene mayor longitud. longitud= ${hacker1.length}`);
 
+else if(hacker1.length<hacker2.length)
+    console.log(`${hacker2} tiene mayor longitud. longitud = ${hacker2.length}`);
 
-
-if(hacker1.length > hacker2.length){
-  console.log(` El conductor con nombre más largo es: ${hacker1} y tiene longitud ${hacker1.length}` )
-  
-}else if (hacker1.length < hacker2.length){
-  console.log(`El conductor con nombre más largo es: ${hacker2} y tiene longitud ${hacker2.length}` )
-  
-  
-}else{
-  console.log(`${hacker1} y ${hacker2} tienen la misma longitud`)
-}
+else
+    console.log(`${hacker1} y ${hacker2} tienen la misma longitud. longitud= ${hacker1.length}`);
 
 
-// Iteration 3: Loops
-
-let hacker1Mayus = hacker1.toUpperCase();
-
-for(let i = 0; i<hacker1Mayus.length; i++){
-    console.log(`hacker1Mayus[i] `)
-    
-}
+// ------ Iteration 3: Loops ------
