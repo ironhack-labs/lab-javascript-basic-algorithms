9282b9aecffb5426a4578d5ac07a8ae3dea150d3
Diego Cerratos <diegocerratos@gmail.com>
2020-11-12 23:52:32 +0100

    Bonus 2 with loops added

--- js/indexloops.js
+++ js/indexloops.js
@@ -10,3 +10,26 @@
 
 // ------ Bonus 2 ------ 
 
+let phrase = 'Was it a car or a cat I saw?';
+let phraseMod ='';
+let invPhrase ='';
+
+for(let i=0;i<phrase.length;i++){
+    if(phrase[i]===' '|| phrase[i]===','|| phrase[i]==='.'|| phrase[i]==='!'|| phrase[i]==='?')
+        phraseMod+='';
+    else
+        phraseMod+=phrase[i];
+}
+
+for(let i=phrase.length-1;i>=0;i--){
+    if(phrase[i]===' '|| phrase[i]===','|| phrase[i]==='.'|| phrase[i]==='!'|| phrase[i]==='?')
+        invPhrase+='';
+    else
+        invPhrase+=phrase[i];
+}
+
+if(phraseMod.toLowerCase===invPhrase.toLowerCase)
+    console.log(`${phrase} is a Palindrome`);
+
+else
+    console.log(`${phrase} is not a Palindrome`);
