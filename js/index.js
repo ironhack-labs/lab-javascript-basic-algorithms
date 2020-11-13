935ce46a8d8aff8ee8088ad31f54b3b9a4b9b9ba
Nicolas del Brio <nicodelbrio@telefonica.net>
2020-11-13 18:00:44 +0100

    itineration 3

--- js/index.js
+++ js/index.js
@@ -18,3 +18,36 @@ if (hacker1.length > hacker2.length) {
 
 
 // Iteration 3: Loops
+//3.1
+let hacker1Mayusculas = hacker1.toUpperCase()
+let hacker1Vacia = "";
+
+for (let i = 0;  i < hacker1Mayusculas.length; i++) {
+  if (i!=hacker1Mayusculas.lenght-1) {
+    hacker1Vacia = hacker1Vacia + hacker1Mayusculas[i]+" ";  }else {
+      hacker1Vacia = hacker1Vacia + hacker1Mayusculas[i];
+    }
+}
+
+console.log(hacker1Vacia)
+
+// 3.2
+
+let str = 'Luis'
+let newStr = str.split('');
+console.log(newStr.reverse().join(' '))
+
+
+// 3.3 
+
+if (hacker1[0] < hacker2[0]) {
+  console.log(`The driver's name goes first.`)
+}
+else if (hacker1[0] > hacker2[0])
+{
+  console.log(`Yo, the navigator goes first definitely.`)
+}
+else {
+  console.log(`What?! You both have the same name?`)
+}
+
