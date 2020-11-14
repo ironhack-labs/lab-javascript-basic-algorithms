310c142c1d025ab3e4937bb72b63b8367ab3a8e5
Pedro Frias <plfriasf@gmail.com>
2020-11-14 07:42:50 -0800

    Done

--- js/index.js
+++ js/index.js
@@ -55,3 +55,70 @@ if (hacker1.localeCompare(hacker2) === -1) {
 } else {
   console.log("Yo, the navigator goes first definitely.");
 }
+
+/*Generate 3 paragraphs. Store the text in a variable type of string.
+Make your program count the number of words in the string.
+Make your program count the number of times the Latin word et appears.*/
+
+let paragraph1 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+let paragraph2 =
+  "Integer vel faucibus enim, non volutpat libero. Quisque id diam dui. Sed sollicitudin porta ultrices. Aliquam et rhoncus arcu. Morbi eleifend odio tortor, at semper mauris tincidunt quis. Pellentesque quis consequat libero. Cras auctor, eros ut tristique ornare, neque mauris ornare nibh, nec tristique ipsum felis ut purus. Proin euismod vestibulum vulputate. Donec a lobortis ex. Donec eleifend viverra nibh, vehicula ornare tortor pretium nec. Morbi tempor sem enim, at faucibus eros pharetra sit amet. Integer quis malesuada lectus. Quisque at porta odio, vitae vulputate ligula. Ut quis felis porttitor nibh rhoncus bibendum congue non odio.";
+
+let paragraph3 =
+  "Praesent suscipit rutrum blandit. Nulla ultricies velit sit amet enim lacinia, et ullamcorper diam pulvinar. Nunc id pellentesque tellus. Nullam arcu felis, vestibulum eget convallis eget, posuere a odio. Etiam ac ex a ipsum pellentesque eleifend ac maximus tortor. Phasellus vitae rutrum magna, id mollis est. Morbi lorem nunc, tincidunt nec mi eu, tincidunt pretium nibh. Cras malesuada lectus est, vitae tempus ante lobortis ut. Nunc tempor sagittis egestas. Donec et dictum massa, in bibendum nisi. Mauris posuere lorem nec rhoncus interdum. Sed suscipit arcu quis justo placerat ornare. Aenean faucibus quam vitae erat gravida, ultrices ornare ligula fringilla. Praesent iaculis quam a nulla luctus feugiat. Integer cursus nunc vitae mauris luctus tincidunt.";
+
+function countWords(paragraph) {
+  console.log(paragraph.split(" ").length);
+}
+
+countWords(paragraph1);
+countWords(paragraph2);
+countWords(paragraph3);
+
+function findWord(paragraph, word) {
+  let paragraphArray = paragraph.split(" ");
+  let coincidence = [];
+  for (let i = 0; i < paragraphArray.length; i++) {
+    if (paragraphArray[i] === word) {
+      coincidence.push(paragraphArray[i]);
+    }
+  }
+  console.log(coincidence.length);
+}
+
+findWord(paragraph1, "et");
+findWord(paragraph2, "et");
+findWord(paragraph3, "et");
+
+/*Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
+
+"A man, a plan, a canal, Panama!"
+"Amor, Roma"
+"race car"
+"stack cats"
+"step on no pets"
+"taco cat"
+"put it up"
+"Was it a car or a cat I saw?" and "No 'x' in Nixon".*/
+
+let phraseToCheck = "A man, a plan, a canal, Panama!";
+
+function palindromeCheck(str) {
+  let strLowerCase = str.toLowerCase().replace(/\W/g, "");
+  let i = 0;
+  let j = strLowerCase.length - 1;
+
+  while (i <= j) {
+    if (strLowerCase[i] !== strLowerCase[j]) {
+      return "It is not a Palindrome";
+    } else {
+      i++;
+      j--;
+    }
+  }
+  return "Yes this is a Palindrome";
+}
+
+palindromeCheck(phraseToCheck);
