2e487a97cab67a7cf0dfe35553e33c375da9c167
orianemgn <mgnoriane@gmail.com>
2021-01-11 17:56:11 +0100

    Exercice Oezge and Oriane part 2

--- js/index.js
+++ js/index.js
@@ -5,9 +5,23 @@ let hacker1="Oriane";
 console.log(`The driver's name is ${hacker1}`);
 
 let hacker2="Oezge";
-console.log(`The ,avigator's name is ${hacker2}`);
+console.log(`The Navigator's name is ${hacker2}`);
+
+
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length}characters.`);
+}
+
+else if(hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+}
+
+else if(hacker1.length === hacker2.length){
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`);
+
+}
 
 // Iteration 3: Loops
