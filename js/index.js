710786868fa94eb6eb1b9658db5b35df4379de97
orianemgn <mgnoriane@gmail.com>
2021-01-11 17:34:14 +0100

    Exercice Oezge and Oriane first part

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
+console.log("I'm ready!")
 
+let hacker1="Oriane";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2="Oezge";
+console.log(`The ,avigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
