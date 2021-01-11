72ead6370918bbc647b6c62677cde12d3462ad34
orianemgn <mgnoriane@gmail.com>
2021-01-11 19:02:23 +0100

    Exercice Oezge and Oriane part3

--- js/index.js
+++ js/index.js
@@ -21,7 +21,38 @@ else if(hacker1.length < hacker2.length) {
 
 else if(hacker1.length === hacker2.length){
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`);
-
 }
 
 // Iteration 3: Loops
+
+let i=0;
+let stringName = ''
+for(i=0; i< hacker1.length; i++) {
+  stringName = stringName + ` ` + hacker1[i]
+  }
+
+console.log(stringName.toUpperCase())
+
+//3.2
+let y;
+let stringName2 = ''
+for(y=hacker2.length-1; y >= null; y--) {
+  stringName2 = stringName2 + hacker2[y]
+  }
+console.log(stringName2)
+
+//3.3
+
+ 
+const arrayNames = [hacker1, hacker2];
+arrayNames.sort();
+console.log(arrayNames);
+
+if(arrayNames[0] == "Oezge" && arrayNames[1] == "Oriane") {
+  console.log(`The driver's name goes first.`)
+} else if (arrayNames[0] == "Oriane" && arrayNames[1] == "Oezge") {
+  console.log(`Yo, the navigator goes first definitely.`)
+} else if (arrayNames[0] === arrayNames[1] ) {
+  console.log(`What?! You both have the same name?`)
+}
+
