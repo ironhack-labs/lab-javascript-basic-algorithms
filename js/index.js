29eb14c7908d40478852031039825db676bb89f9
unknown <ionut_vasile85@hotmail.com>
2021-01-13 12:47:23 -0500

    Iteration 1: Names and Input

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "John"
+let hacker2 = "Al"
+
+console.log(`The driver's name is ${hacker1}`)
+
+console.log(`"The navigator's name is ${hacker2}"`)
 
 // Iteration 2: Conditionals
 
