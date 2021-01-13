963be4e1155e0fb5ab37d56506649999ce052f72
unknown <ionut_vasile85@hotmail.com>
2021-01-13 13:24:17 -0500

    Iteration 3: Loops

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 let hacker1 = "John"
 let hacker2 = "Al"
 
-console.log(`The driver's name is ${hacker1}`)
+console.log(`"The driver's name is ${hacker1}"`)
 
 console.log(`"The navigator's name is ${hacker2}"`)
 
@@ -26,3 +26,20 @@ if(lenDriver > lenNavigator ){
 
 }
 // Iteration 3: Loops
+
+hackerCapitalLetters = hacker1.toUpperCase()
+
+let capitalString =""
+for(let i=0;i<lenDriver;i++){
+  capitalString+=hackerCapitalLetters[i]
+  if(i===lenDriver-1){break}
+  capitalString+=' '
+  }
+
+console.log(`"${capitalString}"`)
+
+reverseString=""
+for(let i=lenDriver-1;i>=0;i--){
+  reverseString +=hacker1[i]
+}
+console.log(`"${reverseString}"`)
