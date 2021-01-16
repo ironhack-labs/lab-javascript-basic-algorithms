2827a9e8b3b6ed461a67f3b58af61e438e0c07f8
mmanso <mayara.manso@mercadolivre.com>
2021-01-16 12:59:00 -0300

    update Interation 3

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-const hacker1 = 'mayara';
+let hacker1 = 'mayara';
 console.log(`"The driver's name is ${hacker1}"`);
 
-const hacker2 = 'chrome';
+let hacker2 = 'chrome';
 console.log(`"The driver's name is ${hacker2}"`);
 
 // Iteration 2: Conditionals
@@ -19,35 +19,22 @@ switch (true) {
 }
 
 // Iteration 3: Loops
-let newHacker1Name = '';
-let newHacker2Name = '';
+hacker1 = hacker1.toUpperCase().split('').join(' ');
+console.log(hacker1)
 
-for (let i = 0; i < hacker1.length; i++) {
-  newHacker1Name += `${hacker1[i].toUpperCase()} `;
-}
-console.log(newHacker1Name);
-
-for (let i = hacker2.length - 1; i >= 0; i--) {
-  switch (true) {
-    case i === 0:
-      const lastLetter = hacker2[i].toUpperCase();
-      newHacker2Name += lastLetter;
-      break;
-    default:
-      newHacker2Name += hacker2[i]
-      break;
-  }
-}
-console.log(newHacker2Name)
+hacker2 = hacker2.split('').reverse();
+hacker2[5] = 'C'
+hacker2 = hacker2.join('');
+console.log(hacker2)
 
 switch (true) {
-  case (newHacker1Name[0] > newHacker2Name[0]):
+  case (hacker1[0] > hacker2[0]):
     console.log(`The driver's name goes first.`)
     break;
-  case (newHacker1Name[0] < newHacker2Name[0]):
+  case (hacker1[0] < hacker2[0]):
     console.log(`Yo, the navigator goes first definitely.`)
     break;
-  case (newHacker1Name[0] === newHacker2Name[0]):
+  case (hacker1[0] === hacker2[0]):
     console.log(`What?! You both have the same name?`)
     break;
 }
