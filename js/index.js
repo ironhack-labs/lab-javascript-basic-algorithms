de6a5d63e02e5a624674ab8ece3693a75837e24c
unknown <ionut_vasile85@hotmail.com>
2021-01-18 17:44:49 -0500

    Bonus 1a - counts the number of words in the string

--- js/index.js
+++ js/index.js
@@ -12,19 +12,15 @@ console.log(`"The navigator's name is ${hacker2}"`)
 let lenDriver=hacker1.length
 let lenNavigator=hacker2.length
 
-if(lenDriver > lenNavigator ){
-
+if(lenDriver > lenNavigator){
   console.log(`The driver has the longest name, it has ${lenDriver} characters.`)
-
-
-}else if(lenNavigator > lenDriver){
-
+}else if(lenNavigator >lenDriver){
   console.log(`It seems that the navigator has the longest name, it has ${lenNavigator} characters.`)
-
 }else{
   console.log(`Wow, you both have equally long names, ${lenDriver} characters!`)
-
 }
+
+
 // Iteration 3: Loops
 
 hackerCapitalLetters = hacker1.toUpperCase()
@@ -43,3 +39,39 @@ for(let i=lenDriver-1;i>=0;i--){
   reverseString +=hacker1[i]
 }
 console.log(`"${reverseString}"`)
+
+
+// Bonus 1:
+
+let pragraphS =`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque purus vel molestie posuere. Vivamus cursus molestie turpis, sed aliquam enim accumsan ut. Duis ut tortor non metus aliquam egestas vel id enim. Sed vitae varius risus, faucibus luctus velit. Nulla facilisi. Quisque efficitur posuere dapibus. Praesent varius arcu id turpis congue, commodo viverra ante convallis. Suspendisse potenti. Donec sed dolor pulvinar arcu sollicitudin tincidunt. Vivamus lacus augue, sollicitudin lobortis tortor malesuada, bibendum eleifend augue. Donec vitae venenatis nunc. Morbi sem felis, feugiat ut massa et, euismod eleifend felis. Pellentesque lorem diam, dapibus nec leo finibus, dapibus tempor nulla.`
+let lenParagraph = pragraphS.length
+
+function wordCount(str){
+
+  // checks if the string is empty
+  if (str.length === 0){
+      return 0
+  }
+
+
+
+  let countofWords = 0
+
+
+  //exclude start and end spaces
+  if (str.substring(0,1) === ' '){
+       str = str.splice(0,1)
+  }else if(str.substring(-1) === ' '){
+      str = str.splice(-1,1)
+  }
+
+  //count the numnber of spaces
+  str.forEach(character => {
+      if (character === ' ')      {
+          countofWords++
+      }
+  });
+
+  //return the number of spaces + 1 to account for account for the last word
+  return countofWords
+}
