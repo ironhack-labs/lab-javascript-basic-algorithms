62baddf995cb8ded3adef759fb85f3e38420b09b
Yassine <yassineabouzaid@hotmail.com>
2021-02-03 15:55:52 +0100

    Adding first stuff

--- js/index.js
+++ js/index.js
@@ -2,10 +2,21 @@ console.log("I'm ready");
 
 // Iteration 1: Names and Input
 
+let hacker1 = "Yassine";
 
+console.log("The driver's name is"+ " "+ hacker1);
+
+let hacker2 = "Adrien";
+console.log("The driver's name is "+ " "+ hacker2);
 
 
 // Iteration 2: Conditionals
+
+if(hacker1.length>hacker2.length){
+    console.log(hacker1.length+" characters");
+} else {
+    console.log(hacker2.length+" characters");
+}
 
 
 // Iteration 3: Loops
