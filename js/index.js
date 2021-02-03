9ffbbcf11ca92a80ba8c3e19504ef4e2fa549411
Pauline Bellaud <pauline.bellaud@gmail.com>
2021-02-03 18:13:26 +0100

    almost done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
+console.log("I'm ready!");
 // Iteration 1: Names and Input
 
-
-// Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
-
-console.log("I'm ready!");
 let hacker1 = `Pauline`
 console.log (`The driver's name is ${hacker1}`)
 let hacker2 = `Victoria`
 console.log (`The navigator's name is ${hacker2}`)
+
+// Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
 console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
@@ -22,16 +18,28 @@ else if (hacker1.length === hacker2.length) {
 console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
 }
 
+// Iteration 3: Loops
+
 let tmp = "";
 for (let i=hacker1.length -1 ; i >= 0 ; i--) {
     tmp = hacker1[i].toUpperCase() + " " + tmp;
 }
-
 console.log(tmp);
 
 let tmp2 = "";
 for (let i=0 ; i < hacker2.length ; i++) {
-    tmp2 = hacker2[i].toUpperCase() + tmp2;
+    tmp2 = hacker2[i] + tmp2;
 }
-
 console.log(tmp2);
+
+let tmp3 = hacker1[0];
+let tmp4 = hacker2[0];
+if (tmp3 < tmp4) {
+console.log(`The driver's name goes first.`);
+}
+else if (tmp3 > tmp4) {
+console.log(`The navigator goes first definitely.`);
+}
+else if (tmp3 === tmp4) {
+console.log(`Wow, you have the same first letter in your names!`)
+}
