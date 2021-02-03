99c43923098719afc2780484495503f66731131d
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-02-03 16:29:42 +0100

    this is interation 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+console.log("I'm ready!");
+
+
 // Iteration 1: Names and Input
 
+let hacker1 = "Simo";
+console.log(`The driver's name is ${hacker1}.`);
+
+let hacker2 = "Matt";
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
 
