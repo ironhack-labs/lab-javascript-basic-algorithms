e9a86a8e760a36be2a4c1d1e7c76ccae4bf9cac0
Mackenzie <madameminch@gmail.com>
2021-02-03 18:53:43 +0100

    first draft complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,47 @@
 // Iteration 1: Names and Input
 
+console.log("I'm ready!");
+let hacker1 = "Mackenzie";
+console.log(`The driver's name is ${hacker1}.`)
+
+let hacker2 = "Anna";
+console.log(`The navigator's name is ${hacker2}.`)
+
+let hacker3 = "Anais";
+console.log(`The other navigator's name is ${hacker3}.`)
 
 // Iteration 2: Conditionals
 
+if (hacker1.length>hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+  } else if (hacker2.length>hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  } else if (hacker1.length===hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters.`)
+  } else {
+    console.log("Invalid statement")
+  }
 
 // Iteration 3: Loops
+
+// STEP 3.1
+let str =""; 
+for (let i=0; i<hacker1.length; i++){
+  str = str + hacker1[i] + ' ';
+
+};
+console.log(str.toUpperCase())
+
+// STEP 3.2
+for (var i = hacker1.length - 1; i >= 0; i--) {
+  console.log(hacker1[i]);
+}
+
+// STEP 3.3
+
+// console.log(hacker1.localeCompare(hacker2));
+// expected output: 1
+
+
+
+
