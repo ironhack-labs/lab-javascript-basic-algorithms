e7108bd00780769c41da469e3567850f4f8b0d91
Anderson Santiago <uusantiago@gmail.com>
2021-02-08 22:33:23 -0300

    Done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
 // Iteration 1: Names and Input
-
+const haker1 = 'Anderson'
+console.log(`The driver's name is ${haker1}`)
+const haker2 = 'Santiago'
+console.log(`The navigator's name is ${haker2}`)
 
 // Iteration 2: Conditionals
-
+if (haker1.length > haker2.length) {
+    console.log(`The driver has the longest name, it has ${haker1.length} characters.`);
+  } else if (haker1.length < haker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${haker2.length} characters`);
+  } else {
+    console.log(`Wow, you both have equally long names, ${haker1.length} characters!`)
+  }    
 
 // Iteration 3: Loops
+
+let toUpper = '';
+
+for (let i = 0; i < haker1.length; i++) {
+    toUpper += `${haker1[i].toUpperCase()} `}
+
+ let reverseString = haker2.split("").reverse().join("");   
+
+console.log(reverseString)
