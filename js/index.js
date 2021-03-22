29c599c65dbc00b3c7a379326f33ba7397e86244
Igor Figueiredo <igfigueiredo@outlook.com>
2021-03-22 15:57:40 +0100

    Iteration 1 completed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Andrea';
+console.log(`The driver's name is ${hacker1}`);
 
+const hacker2 = 'Igor';
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
