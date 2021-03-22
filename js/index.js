6b935129ad7d51dda3562c039b20d7bcae66de34
Pablo del Barrio <pablodbb@gmail.com>
2021-03-22 16:24:21 +0100

    Commit for Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 // Iteration 1: Names and Input
 
+var hacker1 = "Anthony";
+console.log(`The driver's name is ${hacker1}`);
+
+var hacker2 = "Explorer";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
