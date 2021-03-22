6697e426029832fe8000f343200f298287bf8276
jamal-nasser <nasserr.jamal@gmail.com>
2021-03-22 17:20:58 +0100

    Day 1 lab Jamal Nasser

--- README.md
+++ README.md
@@ -1,82 +1,4 @@
-![logo_ironhack_blue 7](https://user-images.githubusercontent.com/23629340/40541063-a07a0a8a-601a-11e8-91b5-2f13e4e6b441.png)
-
-# LAB | JS Basic Algorithms 
-
-Welcome to your first bootcamp exercise at Ironhack!
-
-The goal of this exercise is to get you familiarized with the primitive data structures in JavaScript, which we just covered in the class. Feel free to reference lesson materials and don't limit yourself but be curious and use Google to explore multiple solutions.
-
-Ready?
-
-## Introduction
-
-For this **pair-programming** activity we are going to use a [REPL](https://en.wikipedia.org/wiki/Read%E2%80%93eval%E2%80%93print_loop). You can find a REPL that runs in the browser for programming languages as JavaScript in [https://repl.it/languages/babel](https://repl.it/languages/babel).
-
-Ready to start?
-
-
-## Requirements
-
-- Fork this repo
-- Clone this repo
-- Go to [repl.it](https://repl.it/languages/nodejs) and create an account (or login if you have one)
-- Type this in the *Code Editor* (left panel)
-
-  ```javascript
-  console.log("I'm ready!");
-  ```
-- Press `run ►`
-- If you can see the message in the right side panel, you are really ready!
-
-  <!-- ![](https://i.imgur.com/4TQislb.png) -->
-  ![](https://s3-eu-west-1.amazonaws.com/ih-materials/uploads/upload_17f095b9bb4fa4bd1bee1c017c043327.png)
-
-- __When you are done completely or at any point after the first iteration, copy your code into the `js/index.js` file and follow the steps for submission.__
-
-## Submission
-
-Upon completion, run the following commands:
-```
-$ git add .
-$ git commit -m "done"
-$ git push origin master
-```
-Create Pull Request so your TAs can check up your work.
-
-_You should make a PR (stands for Pull Request) as soon as you make any significant change. You shouldn't have to wait until you're completely done with this or any other exercise to make the PR. After you do the first PR, any other time you push the changes (following the previous three steps), your change will appear automatically on the PR and your TAs will be able to check it._
-
-<!-- ## Submission -->
-
-<!-- When you are done and you have checked that everything works fine, click on the **Share** button and copy the link from the *Share Link* field. Send this link to your TAs so they can check up your work.
-![](https://s3-eu-west-1.amazonaws.com/ih-materials/uploads/upload_b2aa98f91affe5d4c5f12f216b069184.png) -->
-
-## Instructions
-
-### Iteration 1: Names and Input
-
-	1.1 Create a variable `hacker1` with the driver's name.
-
-	1.2 Print `"The driver's name is XXXX"`.
-
-  	1.3 Create a variable `hacker2` with the navigator's name.
-
-  	1.4 Print `"The navigator's name is YYYY"`.
-
-### Iteration 2: Conditionals
-  2.1. Depending on which name [is longer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length), print:
-  <br>
-	  - `The driver has the longest name, it has XX characters.` or <br>
-	  - `It seems that the navigator has the longest name, it has XX characters.` or <br>
-	  - `Wow, you both have equally long names, XX characters!`.
-
-### Iteration 3: Loops
-  3.1 Print all the characters of the driver's name, separated by a space and [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
-  i.e. `"J O H N"`
-  
-  3.2 Print all the characters of the navigator's name, in reverse order. 
-  i.e. `"nhoJ"`
-  
-  3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
+!Lexicographical_order) of the strings, print: <br>
     - `The driver's name goes first.` <br>
     - `Yo, the navigator goes first definitely.` <br>
     - `What?! You both have the same name?`
@@ -111,3 +33,24 @@ Create a new variable `phraseToCheck` and have it contain some string value. Wri
 
 
 __Happy coding!__ :heart:
+
+let hacker1="Jeroen van Bart";
+
+console.log(`The drivers name is ${hacker1}`);
+
+
+let hacker2 = "Jamal Nasser";
+
+console.log(`The navigator's name is ${hacker2}`);
+
+//Iteration 2: Conditionals
+//Jamal Nasser
+if (hacker1.length > hacker2.length){
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if(hacker1.length < hacker2.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else{
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters
+
+  console.log(hacker1.toUpperCase());
+  console.log("hacker1".split('').join(' '));
--- js/index.js
+++ js/index.js
@@ -1,7 +1,103 @@
-// Iteration 1: Names and Input
+console.log("I'm ready");
+//Iteration 1: Names and Input
+
+let hacker1="Jeroen van Bart";
+
+console.log(`The drivers name is ${hacker1}`);
 
 
-// Iteration 2: Conditionals
+let hacker2 = "Jamal Nasser";
+
+console.log(`The navigator's name is ${hacker2}`);
+
+//Iteration 2: Conditionals
+//Jamal Nasser
+if (hacker1.length > hacker2.length){
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if(hacker1.length < hacker2.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else{
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
+//jeroen van bart
+//Iteration 2: Conditionals
+// if (hacker1.length > hacker2.length){
+//     console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+// }else if(hacker2.length > hacker1.length){
+//     console.log(`The navigator has the longest name, it has ${hacker2.length} characters`);
+// } else{
+//     console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
+// }
+// }
 
 
-// Iteration 3: Loops
+//Iteration 3: Loops
+//3.1
+//Jamal Nasser
+console.log(hacker1.toUpperCase().split('').join(' '));
+
+//Jeroen van Bart
+// let Hacker1UpperCase= hacker1.toUpperCase();
+// let betweenLetters=" ";
+
+// console.log(Hacker1UpperCase.split("").join(betweenLetters));
+//3.2
+//Jamal Nasser
+let reversedName = hacker2.split('').reverse().join('');
+console.log(reversedName);
+
+//Jeroen van Bart
+//let stringReverse = hacker1.split("").reverse().join("");
+//console.log(stringReverse);
+
+//3.3
+//Jamal Nasser
+if (hacker1.localeCompare(hacker2) === 1) {
+  console.log(`The driver's name goes first.`);
+} else if (hacker1.localeCompare(hacker2) === -1) {
+  console.log(`Yo, the navigator goes first definitely.`);
+} else {
+  console.log(`What?! You both have the same name?`);
+}
+
+//jeroen van bart
+// if(hacker1.localeCompare(hacker2) == 1){
+//     console.log("Yo, the navigator goes first definitely.")
+// }else if(hacker1.localeCompare(hacker2) == -1){
+//     console.log("The driver's name goes first.")
+// }else{ console.log("What?! You both have the same name?")};
+
+//Bonus1
+//Jamal Nasser
+let paragraph = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \n It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like. \n There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`
+
+
+function WordCounter (paragraph) {
+	let words = paragraph.split(" ").length;
+	return words;
+}
+
+console.log(WordCounter(paragraph));
+
+console.log(paragraph.indexOf('et'));
+
+
+//jeroen van bart
+// let randomText= `"What is Lorem Ipsum?
+// Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+// Why do we use it?
+// It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+// Where does it come from?
+// Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32."`;
+
+//Jeroen van Bart
+// function WordCount(str){
+//     let words = str.split(" ").length;
+//     console.log(words);
+// }
+// WordCount(randomText);
+// console.log(randomText.indexOf('et'));
+
+
+
+
