c4f42dbc1420e9a121814f23924a650f1b9378e0
Philipp Dam <philippmac@Philipps-Air.fritz.box>
2021-03-22 18:08:57 +0100

    refactor bonus 1

--- js/index.js
+++ js/index.js
@@ -52,15 +52,13 @@ const para =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tristique purus a turpis placerat sagittis. Curabitur eu malesuada purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam eget nulla purus. Morbi et ex quis neque dignissim posuere. Nullam vulputate dapibus tellus ut lobortis. Cras dapibus tempus libero vel condimentum. Mauris consectetur ex mi, sed fringilla ipsum euismod vitae. Suspendisse euismod, risus ut tempor accumsan, lorem eros tristique neque, vitae sagittis elit lacus eu diam. Nullam vel lacus augue. Sed sollicitudin posuere nibh, id dignissim magna. Nam facilisis lorem in hendrerit viverra. Pellentesque vitae eros in mi efficitur interdum sit amet eu leo. Donec id felis a justo consequat commodo. Integer accumsan libero felis, ut ultrices ex aliquet ut. Quisque tortor turpis, posuere eget elit sodales, tristique ultrices ex. Phasellus at facilisis massa. Praesent interdum lobortis quam nec luctus. Aliquam sit amet est ultrices urna ultrices tincidunt. Vestibulum vel massa eros. Phasellus felis risus, consequat at elementum ut, pretium non ex. Sed vestibulum tellus metus, at dictum diam dictum eu. Integer tortor libero, pharetra vitae mauris quis, fermentum tempus elit. Sed id turpis sit amet metus semper suscipit. Fusce consequat sapien vitae lectus gravida, sit amet sodales tellus laoreet. Sed porttitor, mauris quis laoreet laoreet, velit dolor luctus risus, quis sodales sem dui in nibh. Donec lobortis aliquet molestie. Etiam lectus risus, finibus vitae mollis non, mattis eu nulla. Integer fermentum convallis urna vel efficitur. In lorem odio, pulvinar vel tristique ac, faucibus eu nisl. Vivamus et lectus venenatis, finibus tellus vitae, efficitur elit.";
 
 function countWords(paragraph) {
-  let count = paragraph.split(" ").length;
-  return count;
+  return paragraph.split(" ").length;
 }
 
 console.log(countWords(para));
 
 function countET(paragraph) {
-  let count = paragraph.match(/et/g).length;
-  return count;
+  return paragraph.match(/et/g).length;
 }
 
 console.log(countET(para));
