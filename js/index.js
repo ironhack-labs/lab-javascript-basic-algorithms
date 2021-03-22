d10d84285704b9335359d3b22b9a837c520f665f
Igor Figueiredo <igfigueiredo@outlook.com>
2021-03-22 20:09:09 +0100

    Bonus 1

--- js/index.js
+++ js/index.js
@@ -50,3 +50,40 @@ for(let i = 0; i < longestName.length; i++) {
         console.log(`What?! You both have the same name?`);
     }        
 }
+
+// Bonus 1
+let lorenIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis arcu dapibus, efficitur quam sed, aliquet felis. Fusce luctus massa et augue sollicitudin, vel semper ligula molestie. Nam tristique varius elit, tincidunt hendrerit eros. Sed luctus orci id quam vulputate venenatis. Sed sed accumsan diam. Pellentesque auctor sem nec ligula congue tempor. Nullam finibus velit cursus porttitor elementum. Nam luctus felis ante, ac volutpat lacus lacinia sed. Aliquam egestas mollis sem sit amet vulputate. Etiam in est sed arcu gravida tincidunt in et nibh.
+
+Nulla ut arcu in ante maximus mattis ac id felis. In hac habitasse platea dictumst. Nam mollis finibus mollis. Curabitur tincidunt gravida justo ac elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel nibh mauris. Mauris ut mi id nisl pulvinar vehicula. Quisque est eros, pharetra ut nibh viverra, gravida maximus arcu. Fusce sed pellentesque nunc. Vivamus est lacus, elementum non ultrices non, aliquam eu diam. Vivamus vehicula orci in ultrices commodo. Etiam pharetra vulputate gravida. Ut venenatis leo quis sagittis viverra. Aliquam sagittis maximus est id varius.
+
+Nullam vehicula a felis sit amet dictum. Pellentesque luctus vitae nunc ut tincidunt. Proin sit amet rutrum lorem. Pellentesque felis nulla, tempor ut dui non, hendrerit suscipit eros. Suspendisse potenti. Suspendisse ultrices felis vitae neque volutpat finibus. Donec eget neque eleifend, dapibus velit nec, eleifend velit. Proin sit amet semper nunc. Sed aliquam, turpis id sodales sodales, urna odio rutrum ante, at tristique lacus turpis at urna. Maecenas tellus lacus, rutrum et elit in, tempus sagittis massa. Phasellus mollis ipsum at feugiat venenatis.`
+
+let lorenIpsumWords = [];
+let word = '';
+for(let i = 0; i < lorenIpsum.length; i++) {
+    let c = lorenIpsum.charAt(i);
+    switch (c) {
+        case '.':
+        case '!':
+        case '?':
+        case ',':
+        case ' ':
+            if(word.length > 0)
+              lorenIpsumWords.push(word);
+            word = '';
+            break;
+        default:
+            word += c;
+            break;
+    }
+}
+
+console.log(`The lorenIpsum string has ${lorenIpsumWords.length} words`);
+
+let etCounter = 0;
+for (let i = 0; i < lorenIpsumWords.length; i++) {
+    if(lorenIpsumWords[i] == 'et')
+        etCounter++;
+}
+
+console.log(`The word 'et' appears ${etCounter} times in the lorenIpsum string`);
