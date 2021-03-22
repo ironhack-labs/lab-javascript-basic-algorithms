2d2d419c41b0e9503e602b79421cc9df1f64f893
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-22 14:58:45 +0100

    finished

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 // Iteration 1: Names and Input
+const hacker1 = "Malcolm";
 
+console.log(`The driver's name is ${hacker1}`);
 
 // Iteration 2: Conditionals
+const hacker1 = "Malcolm";
 
+console.log(`The driver's name is ${hacker1}`);
 
 // Iteration 3: Loops
+const hacker1 = "Malcolm";
+
+console.log(`The driver's name is ${hacker1}`);
