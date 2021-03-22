bf5777848b338f93c0424b31f817cd26c661fe48
jpsm83 <73789298+jpsm83@users.noreply.github.com>
2021-03-22 17:16:50 +0100

    last exercise

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
+console.log('I am ready!');
+
 // Iteration 1: Names and Input
-console.log('I am ready');
+let hacker1 = 'Joao';
+console.log(`The driver's name is ${hacker1}.`);
+let hacker2 = 'Maria';
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
+if (hacker1.length === hacker2.length){
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
+} else if (hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+}
 
+/* Iteration 3: Loops
+3.3 Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name?*/
+let upperCaseName = '';
+for (let i=0; i<hacker1.length; i++){
+    upperCaseName += `${hacker1[i]} `;
+};
+console.log(upperCaseName.toUpperCase());
 
-// Iteration 3: Loops
+let reversedName = hacker2.split('').reverse().join('');
+console.log(reversedName);
+
+let alphabet = 'abcdefghijklmnopqrstuvxwyz';
+
