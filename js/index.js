c2fbfbb964f62759a36cbd8cabdb7a8a7bc82cb7
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-03-23 00:07:12 +0100

    Starting Lab1 third iteration 3.3

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 // Iteration 1: Names and Input
 let hacker1 = 'Juan';
 console.log(`The driver name is ${hacker1}`);
-debugger
-let hacker2 = 'José';
+
+let hacker2 = 'Jose';
 console.log(`The navigator name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+//2.1
 if (hacker1.length > hacker2.length) {
   console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
 } else if (hacker1.length < hacker2.length) {
@@ -15,7 +16,7 @@ if (hacker1.length > hacker2.length) {
   }
 
 // Iteration 3: Loops
-
+//3.1
 let h1upperCased = hacker1.toUpperCase();
 let bigName = ""
 
@@ -23,3 +24,13 @@ for (let i=0; i<h1upperCased.length; i++) {
   bigName += h1upperCased[i] + " ";
 };
 console.log(bigName);
+
+//3.2
+let h2Inverse = "";
+  for (let i = hacker2.length-1; i>=0; i--) {
+  h2Inverse = h2Inverse + hacker2[i];
+}
+console.log(h2Inverse);
+
+//3.3
+for (let i=0; hacker)
