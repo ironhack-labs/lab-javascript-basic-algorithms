2947d5bb15ac0b309568d0a6608a7274cfe33571
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-03-24 17:52:12 -0400

    bonus 1 working

--- js/index.js
+++ js/index.js
@@ -53,3 +53,18 @@ if (hacker1[0] < hacker2[0]) {
   console.log("What?! You both have the same name?");
   }
 
+//Bonus 1
+
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit blandit lectus non venenatis. Cras id nunc quis diam mattis faucibus. Duis tempus leo vitae iaculis vulputate. Donec nunc orci, sagittis nec risus non, lacinia placerat nibh. Sed lacinia nunc eget est placerat malesuada. Sed venenatis eget eros ut fermentum. Etiam porta lorem quis fringilla pulvinar. Nunc eu turpis in turpis interdum posuere nec in sem. Proin luctus eget arcu ac imperdiet. Sed quis lacus egestas, sollicitudin lacus et, sagittis ante. Vivamus risus velit, ultricies ut nisi a, pharetra posuere libero. Maecenas lacus neque, ullamcorper eu sollicitudin vel, rhoncus eget ante. Vestibulum ac bibendum purus, nec laoreet felis. Morbi urna felis, finibus vel justo sit amet, porta mollis urna. Pellentesque fringilla dolor sit amet venenatis porttitor. Aliquam pretium justo id scelerisque dignissim. Vivamus hendrerit gravida eros, et fringilla augue rutrum ut. Sed semper convallis elit, ut cursus ipsum maximus non. Maecenas interdum pretium magna. Nam in est id arcu congue feugiat quis imperdiet erat. Nunc lacus neque, lobortis sit amet augue eget, euismod hendrerit nulla. Aenean quis fringilla nunc. Suspendisse eros purus, aliquam eu condimentum et, suscipit in lectus. Vivamus in venenatis velit. Sed eu tellus lacinia, tristique justo quis, faucibus quam. Vestibulum est urna, bibendum id dui quis, ultricies blandit nisi. Nunc condimentum metus eu malesuada cursus. Curabitur id tincidunt libero. Nam laoreet eleifend sem in scelerisque. Vivamus blandit malesuada sem, et interdum velit fringilla a. Quisque ullamcorper tellus vel nisi tempus elementum ac et sapien. Phasellus vehicula porttitor orci, nec ornare odio ornare sed. Phasellus ac dolor vel dolor molestie lacinia. Donec vitae feugiat est. Nulla interdum luctus luctus. Quisque suscipit sapien tortor, vitae venenatis ante egestas quis. Phasellus sit amet est a risus venenatis dapibus.";
+
+function words (finder){
+  var dirtyWord = ''
+  for(i = 0; i <= finder.length; i++)
+  if(i = 'et'){
+    console.log('lo encontre')
+  }else if (i = 'et') {
+    console.log('sigue')
+  }
+}
+
+words(lorem)
