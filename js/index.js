011b3f2bb817c59703088eeabe1ea65a69d6fafd
Luis Martires <luis.martires@outlook.pt>
2021-03-28 12:49:23 +0100

    bonus done

--- js/index.js
+++ js/index.js
@@ -9,31 +9,36 @@ console.log(`The navigator's name is ${hacker2}.`);
 
 //2.1. Depending on which name is longer, print:
 if (hacker1.length > hacker2.length) {
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
 } else if (hacker1.length < hacker2.length) {
-    console.log(`It seems that the navigator has the longest anem, it has ${hacker2.length} characters.`);
+  console.log(
+    `It seems that the navigator has the longest anem, it has ${hacker2.length} characters.`
+  );
 } else {
-        console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
-    }
-
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
 // Iteration 3: Loops
 
 // Print all the characters of the driver's name, separated by a space and // in capitals.
 let space = "";
 for (let i = 0; i < hacker1.length; i++) {
-   space += hacker1.charAt(i) + " ";     
+  space += hacker1.charAt(i) + " ";
 }
 
 console.log(space.toUpperCase());
 
 //3.2 Print all the characters of the navigator's name, in reverse order.
 let reverse = "";
-for (let i = hacker1.length-1; i >= 0; i--) {
-    reverse = reverse + hacker1.charAt(i);
-    }
+for (let i = hacker1.length - 1; i >= 0; i--) {
+  reverse = reverse + hacker1.charAt(i);
+}
 
-    console.log(reverse);
+console.log(reverse);
 
 //3.3 Depending on the lexicographic order of the strings, print:
 if (hacker1.localeCompare(hacker2) > 0) {
@@ -46,12 +51,27 @@ if (hacker1.localeCompare(hacker2) > 0) {
 
 //Bonus Time!
 //Bonus 1:
-
 //Generate 3 paragraphs. Store the text in a variable type of string.
-let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a rutrum ex. Morbi nec quam quam. Cras in mauris ut mi faucibus lacinia quis vitae augue. Aliquam non quam eget lacus pharetra blanditulla non ultrices nisl. Nulla facilisi. Vivamus malesuada ipsum quis felis posuere, ultricies pretium leo rutrum.";
+let loremIpsum =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a rutrum ex. Morbi nec quam quam. Cras in mauris ut mi faucibus lacinia quis vitae augue. Aliquam non quam eget lacus pharetra blanditulla non ultrices nisl. Nulla facilisi. Vivamus malesuada ipsum quis felis posuere, ultricies pretium leo rutrum.";
 
 //Make your program count the number of words in the string.
-console.log(`There are ${loremIpsum.split(' ').length} words`)
+console.log(`There are ${loremIpsum.split(" ").length} words`);
 
 //Make your program count the number of times the Latin word et appears.
-console.log(`Word: 'et' appears ${loremIpsum.split('et').length - 1} times`);
+console.log(`Word: 'et' appears ${loremIpsum.split("et").length - 1} times`);
+
+//Bonus 2:
+
+let phraseToCheck = "step on no pets";
+let palindrome = "";
+
+for (let i = phraseToCheck.length - 1; i >= 0; i--) {
+  palindrome += phraseToCheck[i];
+}
+
+if (palindrome == phraseToCheck) {
+  console.log(`Palindrome`);
+} else {
+  console.log(`Not Palindrome`);
+}
