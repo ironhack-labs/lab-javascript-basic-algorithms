8acb9b72fb990682c1a9e28628d69b3c65a429eb
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-04-01 18:32:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
 
+const hacker1 = "Daniel";
+
+console.log(`The dirver's name is ${hacker1}`);
+
+const hacker2 = "Joserra";
+
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if(hacker1.length === hacker2.length){
+    const length = hacker1.length;
+    console.log(`Wow, you both have equally long names, ${length}`);
+} else if (hacker1.length < hacker2.length){
+
+}
 
 // Iteration 3: Loops
+
