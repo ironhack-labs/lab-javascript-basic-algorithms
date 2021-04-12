a85b1e5ead2b52183cba56d285e8403cb23b87dd
Luis <luisalbertodavila@gmail.com>
2021-04-12 17:39:08 -0500

    Primera iteracion Rev 2

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ console.log("I'm ready!");
 // Iteration 1: Names and Input
 let hacker1 = "Driver Name"
 console.log(`"The driver name is ${hacker1}"`);
-// Iteration 2: Conditionals
 let hacker2 = "Navigators Name"
 console.log(`"The navigator name is ${hacker2}"`);
+
+// Iteration 2: Conditionals
+
 // Iteration 3: Loops
