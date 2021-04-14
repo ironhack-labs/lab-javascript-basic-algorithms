ab1b7dd503aa55e955001b28dea0b48019a0ab58
Alejandra Rubio <alejandrarubio@MacBook-Air-de-Alberto-2.local>
2021-04-14 20:45:26 +0200

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-// Iteration 1: Names and Input
+//Iteration 1
 
+//1.1 Create a variable `hacker1` with the driver's name.
+let hacker1 = "Grecia";
 
-// Iteration 2: Conditionals
+//1.2 Print `"The driver's name is XXXX"`.
+console.log(`The driver's name is ${hacker1}`);
 
+//1.3 Create a variable `hacker2` with the navigator's name.
+let hacker2 = "Alejandra";
 
-// Iteration 3: Loops
+//1.4 Print `"The navigator's name is YYYY"`.
+
+console.log(`The navigator's name is ${hacker2}`);
