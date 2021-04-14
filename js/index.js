918b791c6a2cefce1a58056aa11a9c381d79059c
Grecia Patricia <grecia.busquets@gmail.com>
2021-04-14 21:11:02 +0200

    Iteration 2 finished

--- js/index.js
+++ js/index.js
@@ -12,3 +12,17 @@ let hacker2 = "Alejandra";
 //1.4 Print `"The navigator's name is YYYY"`.
 
 console.log(`The navigator's name is ${hacker2}`);
+
+//Iteration 2
+
+/* console.log(hacker1.length);
+console.log(hacker2.length);*/
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} if (hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+}
+  else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`);
+  }
