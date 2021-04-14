eaa4c7597b6ff6a03d14a21262de46e454bc1302
Paula <paulanunocorte@gmail.com>
2021-04-14 22:00:39 +0200

    solved until 3

--- js/index.js
+++ js/index.js
@@ -14,5 +14,46 @@ console.log (`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+/*2.1. Depending on which name is longer, print:
+- The driver has the longest name, it has XX characters. or
+- It seems that the navigator has the longest name, it has XX characters. or
+- Wow, you both have equally long names, XX characters!.*/
+
+if (hacker1.length > hacker2.length){
+  console.log (`The driver has the longest name, it has ${hacker1.length} characters.`);
+}
+else if (hacker1.length < hacker2.length){
+  console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+}
+else{
+  console.log (`Wow, you both have equally long names, ${hacker1} characters!`);
+}
 
 // Iteration 3: Loops
+
+/*3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+
+3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+3.3 Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name?*/
+
+for (character of hacker1) {
+    console.log(character.toUpperCase())
+  }
+
+  var arr =  [1, 2, 3, 4, 5];
+ 
+  for (character = hacker2.length - 1; i >= 0; i--) {
+      console.log(hacker2[character]);
+  }
+
+if (hacker1 < hacker2){
+    console.log('hacker1 goes first');}
+else if (hacker1 > hacker2){ 
+    console.log('hacker2 goes first');}
+else{
+    console.log('You have same names');}
+
