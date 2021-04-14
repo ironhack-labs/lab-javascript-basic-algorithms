fce7c029da465fa48be51916e61d16f3fa07505c
Paloma Borro <palomabs93@gmail.com>
2021-04-14 21:33:55 +0200

    iterations 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-// Iteration 1: Names and Input
+// Iteración 1: nombres y entrada
+
+let hacker1 = 'Paloma'
+console.log(`"The driver's name is ${hacker1}"`);
+
+let hacker2 = 'Eléa'
+console.log(`"The driver's name is ${hacker2}"`);
 
 
-// Iteration 2: Conditionals
+//Iteración 2: condicionals
 
-
-// Iteration 3: Loops
+if (hacker1.length > hacker2.length) {
+  console.log('Es más largo');
+}
+ else if (hacker1.length < hacker2.length) {
+    console.log('No es más largo');
+  }
+else { console.log('Son iguales');
+}
