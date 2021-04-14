745ff0eab442c2f8f6b5312eb5f5b5f794e1268e
Paula <paulanunocorte@gmail.com>
2021-04-14 20:54:46 +0200

    Iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 // Iteration 1: Names and Input
 
+/* 1.1 Create a variable `hacker1` with the driver's name.
+1.2 Print `"The driver's name is XXXX"`.
+1.3 Create a variable `hacker2` with the navigator's name.
+1.4 Print `"The navigator's name is YYYY"`. */
+
+let hacker1 = "Gonzalo";
+let hacker2 = "Paula";
+
+console.log (`The driver's name is ${hacker1}`);
+console.log (`The navigator's name is ${hacker2}`); 
+
 
 // Iteration 2: Conditionals
 
