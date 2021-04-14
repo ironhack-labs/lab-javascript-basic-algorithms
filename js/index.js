777580cd78fa685738259e864b841f168cacdfb9
Israel Zambrana <zambrana90@gmail.com>
2021-04-14 21:32:21 +0200

    Termninado hasta iteración 3.2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,62 @@
+/*
 // Iteration 1: Names and Input
+let hacker1 = "Israel";
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "Marcos";
+console.log(`The navigator's name is ${hacker2}`);
+*/
 
-
+/*
 // Iteration 2: Conditionals
 
+let hacker1 = "Israel";
+let hacker2 = "Marcos";
+
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker2.length} characters!`
+  );
+}
+*/
 
 // Iteration 3: Loops
+
+/* 3.1
+
+let hacker1 = "Israel";
+let hacker2 = "Marcos";
+
+let hacker1Capitals = "";
+
+for (i = 0; i < hacker1.length; i++) {
+  hacker1Capitals += hacker1[i];
+  hacker1Capitals += " ";
+}
+
+console.log(hacker1Capitals.toUpperCase());
+
+*/
+
+/* 3.2
+
+let hacker1 = "Israel";
+let hacker2 = "Marcos";
+
+let hacker2Capitals = "";
+
+for (i = 5; i >= 0; i--) {
+  hacker2Capitals += hacker2[i];
+  hacker2Capitals += " ";
+}
+
+console.log(hacker2Capitals);
+
+*/
