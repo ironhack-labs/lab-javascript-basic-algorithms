f0527181328b8c91210fb8a29042852decebaa1c
Paloma Borro <palomabs93@gmail.com>
2021-04-16 17:56:52 +0200

    Lab 1 completo

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ let hacker2 = 'Eléa'
 console.log(`"The driver's name is ${hacker2}"`);
 
 
-//Iteración 2: condicionale
+//Iteración 2: condicionales
 
 if (hacker1.length > hacker2.length) {
   console.log('Es más largo');
@@ -18,53 +18,52 @@ if (hacker1.length > hacker2.length) {
 else { console.log('Son iguales');
 }
 
-
-// Iteración 3: Loops
-
 hacker1 = hacker1[0].toUpperCase() + ' ' + hacker1[1].toUpperCase() + ' ' + hacker1[2].toUpperCase() + ' ' + hacker1[3].toUpperCase() + ' ' + hacker1[4].toUpperCase() + ' ' + hacker1[5].toUpperCase();
 
 console.log(hacker1);
 
+hacker2 = hacker2[3] + hacker2[2] + hacker2[1] + hacker2[0];
+
+console.log(hacker2);
 
 
-for (let i = 0; i < hacker1.length && i < hacker2.length; i++) {
-if (hacker1[i] < hacker2[i]) {
-     console.log('hacker1 va primero');
-  break;
-}
-     else if (hacker2[i] < hacker1[i]) {
-       console.log('hacker2 va primero');
-  break;
- }
-     if (i + 1 === hacker1.length || i + 1 === hacker2.length) {
-       console.log('Tenemos el mismo nombre');
-     }
-}
- 
 
+//Bonus 1
 
-/*loremIpsum.split(" ");
-console.log(loremIpsum.split(" ").length);*/
+let loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et sollicitudin augue. Aenean hendrerit molestie ultrices. Fusce vitae ultricies eros, et posuere sapien. Nulla rhoncus sagittis faucibus. Pellentesque ultrices velit at quam interdum, id placerat ipsum luctus. Donec sit amet mi pellentesque tortor bibendum convallis at venenatis nunc. Proin pharetra fringilla dolor, a interdum nisl facilisis quis. Nam convallis libero tempus lobortis porta. Fusce sollicitudin ex vel justo mollis iaculis. Aenean libero ante, auctor a vulputate a, volutpat at dolor. Sed ac porta sem.
+
+Suspendisse potenti. Nulla facilisi. Nunc blandit nunc vel nunc cursus aliquet. Curabitur malesuada elit sapien, ac accumsan purus aliquet a. Nam eu tortor risus. Aenean ut dictum nulla. Curabitur viverra mi vitae massa ornare, ac blandit ante rutrum. Nunc fermentum lacinia tempor. Donec id metus auctor, semper nunc id, sollicitudin ipsum.
+
+Proin eget interdum quam. Nullam venenatis nibh quis orci blandit facilisis. Cras vehicula dui id eros convallis iaculis. Donec dolor massa, viverra eget porttitor nec, consequat in magna. In luctus dui nec volutpat pulvinar. Pellentesque luctus turpis quis justo accumsan congue. Phasellus nec velit ullamcorper velit sodales fermentum sit amet vitae elit. Quisque condimentum augue sed lectus pulvinar faucibus. In quis tortor id eros tincidunt pretium. Mauris scelerisque eu est non rhoncus. Nunc vel velit id augue commodo tempus in a elit. Integer imperdiet cursus ligula, ut iaculis augue cursus eu. Aliquam vel nisl a ex laoreet iaculis. Quisque dictum convallis massa, a malesuada lorem accumsan et. Proin consectetur ligula in mauris finibus sollicitudin. Nulla eleifend, dolor ut luctus laoreet, sem erat consectetur mi, in rutrum libero metus egestas nunc.`;
+
 
 let words = loremIpsum.split(' ');
 console.log(words);
 
 
-/*let posicion = words.indexOf('et');
-if (posicion !== -1)
-    console.log("La palabra está en la posición " + posicion);
-else
-    console.log("No encontré lo que estás buscando");*/
-
-
-let indices = [];
 let element = 'et';
-let idx = words.indexOf('et');
-while (idx != -1) {
-  indices.push(idx);
-  idx = words.indexOf('et', idx + 1);
+let count = 0;
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === element || words[i] === element +'.') {
+        count++;
+    }
 }
-console.log(indices);
+console.log(count);
 
-  
-  
+
+
+//Bonus 2
+
+let phrase = 'put it up'
+
+for (let i = 0, j = phrase.length-1; i < j; i++, j--) {
+    if (phrase[i] == phrase[j]) {
+      
+      console.log('si es un palindromo')
+      break;
+    } 
+    else {
+      console.log('no es un palindromo')
+    }
+}
+
