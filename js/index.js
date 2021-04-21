58713a73527c856fd41a74415b45e02bf611d678
Grecia Patricia <grecia.busquets@gmail.com>
2021-04-21 18:12:05 +0200

    Upload Bonus 1

--- js/index.js
+++ js/index.js
@@ -61,7 +61,29 @@ else if (hacker1name > hacker2name) {
 else {
   console.log("What?! You both have the same name?");
 }
+/* BONUS 1 */
 
+let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus lacus quis condimentum auctor. Integer in bibendum metus. Aenean rutrum, orci mollis finibus ultrices, enim tellus condimentum neque, nec vulputate tortor tortor et nisi. Mauris diam tellus, fermentum varius egestas ut, lobortis facilisis leo. Phasellus eget nisi ac nisl suscipit scelerisque vel eu quam. Nullam porttitor nisl ac nulla imperdiet, ut viverra risus aliquet. Duis pretium arcu quis pharetra interdum. Aenean posuere aliquam dapibus. Aenean placerat odio in tellus imperdiet dapibus. Cras tincidunt hendrerit magna, a semper mi varius sed. Integer at ex nec lacus mattis vehicula nec id ligula. Cras mattis quam fringilla nulla fringilla lobortis. Nullam felis lorem, congue sit amet felis non, tristique tristique ex. Suspendisse et est luctus, porttitor metus sed, fringilla ante. Donec dignissim lacus purus, vel bibendum diam volutpat quis. Suspendisse imperdiet condimentum nisl, a consequat massa lacinia eget. Proin quis blandit ante, eget convallis magna. Proin sodales sapien sagittis vestibulum hendrerit. Nunc eget ante ut justo consequat ullamcorper. Vivamus imperdiet, urna eget ullamcorper laoreet, justo felis accumsan enim, sed bibendum lacus nibh porttitor mi. Proin posuere nunc vitae fringilla tristique. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas aliquet laoreet commodo. Vestibulum varius, erat vitae finibus euismod, sapien libero volutpat augue, non sollicitudin urna odio a velit. Suspendisse posuere neque non ligula sodales, at consequat risus sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Duis mollis placerat lobortis.";
+
+let newLoremIpsum = loremIpsum.split(" ");
+
+function countWords(x){
+  word = 0;
+  for(let i = 0; i < x.length; i++){
+     word++;
+  }
+    return word;
+}
+countWords(newLoremIpsum);
+
+function countEt(x, e) {
+  let count = 0;
+  for(let i = 0; i < x.length; i++){
+    if (x[i] === e) count++;
+  }
+    return count;
+}
+countEt(newLoremIpsum, "et");
 
 /* BONUS 2 */
 
