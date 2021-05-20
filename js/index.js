d3dc01c2333c4a608ee7680ff635d078d8532cb0
rodrigofxxx <rodrigofelix.leite@gmail.com>
2021-05-20 20:03:57 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,39 +1,47 @@
 // Iteration 1: Names and Input
-let hacker1='Rodrigo';
-console.log(`The driver name is ${hacker1}`);
-let hacker2='Alison';
-console.log(`The navigator name is ${hacker2}`);
+let hacker1 = 'RODRIGOO'
+console.log (`The driver's name is ${hacker1}`)
+let hacker2 = 'ALUIZIO'
+console.log (`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
-if (hacker1.length == hacker2.length){
- console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
-} else {
-if (hacker1.length > hacker2.length){
- console.log(`The driver has the longest name, it has ${hacker1.length} characters`); 
- }
- else {
-   console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
- }
+if (hacker1.length > hacker2.length) {
+  console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker2.length > hacker1.length) {
+  console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else if (hacker1.length == hacker2.length) {
+  console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
-// Iteration 3.1: Loops
-// Comment: Passando uma string para UpperCase e depois inserindo espaços entre as letras.
-let novoNome = "";
-for(let i=0;i<hacker1.length;i += 1){
-  let space = " ";
- novoNome+=(`${hacker1[i]} ${space}`);
+
+// Iteration 3: Loops
+
+//3.1 with JOIN
+console.log(hacker1.split('').join(' '));
+
+//3.1 with FOR
+
+let separatedName = '';
+
+for (let i = 0; i<hacker2.length; i+=1) {
+
+    separatedName += hacker2[i].toUpperCase() + ' ';
 }
-console.log(novoNome.toUpperCase());
-// Iteration 3.2: Loops
-// Comment: Invertendo uma string
-console.log(hacker2.split("").reverse().join(" "));
-// Interaction 3.3
-// Comment: Neste código verificamos a ordem alfabética do Driver e Navigator
-if(hacker1===hacker2){
-  console.log(`What?! You both have the same name?`);
-} else{
-  var hackers = [hacker1, hacker2];
-  hackers.sort();
-  if(hackers[0]==hacker1){
-  console.log(`The driver's name goes first.`);} else{
-   console.log(`Yo, the navigator goes first definitely.`)
-  }
+console.log(separatedName);
+
+//3.2 with FOR
+
+let nameInverted ='';
+for (let i = hacker1.length - 1; i>=0; i-=1) {
+    nameInverted += hacker1[i];
+}
+console.log(nameInverted);
+
+//3.3 with IF ELSE
+
+if(hacker1.length > hacker2.length) {
+  console.log(`The ${hacker1} goes first.`);
+} else if(hacker1.length < hacker2.length) {
+  console.log(`Yo,the ${hacker2} goes first definitely.`);
+} else if(hacker1.length === hacker2.length) {
+  console.log(`What?! You both have the same name?`)
 }
