fb97e4b1f6fc687b832abde785865585c294387b
pasqualipedro <pasqualipedro@gmail.com>
2021-05-22 14:11:51 +0100

    Versão 02.1 - Bonus não finalizado e com algumas dúvidas na última etapa

--- js/index.js
+++ js/index.js
@@ -27,11 +27,12 @@ let index=0;
 let iterationN=0; //-not used
 let spacesNeeded=hacker1.length-1; //--- 4 ----not used
 let toAdd=(hacker1[index]+` `); //+hacker1[index+=1]+` `)
-let toAdd2=(hacker2.charAt((index-(hacker2.length))));
+let toAdd2=(hacker2.charAt(hacker2.length-1-index));
 let hacker1new=``;
 let hacker2new=``;
 
-/* --- UNCOMMENT TO TEST
+/* //---3.1--- UNCOMMENT TO TEST
+
 while (index<hacker1.length){
     hacker1new=toAdd;
     index+=1;
@@ -40,9 +41,40 @@ while (index<hacker1.length){
 console.log(hacker1new.toUpperCase());
 */
 
+
+//---3.2--- UNCOMMENT TO TEST
+
 while (index<hacker2.length){
-    hacker2new=toAdd2
-    index+=1
-    toAdd2+=(hacker2.charAt(index-hacker2.length))
+    hacker2new+=toAdd2;
+    index+=1;
+//    toAdd2+=(hacker2.charAt(index-`hacker2.length`))
 }
 console.log(hacker2new)
+
+
+
+//------ BONUS-----NOT FINISHED
+
+let paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non tortor in velit luctus faucibus eu aliquam est. Nunc sodales id dui vehicula lacinia. In vel consectetur nibh. Integer id sapien blandit arcu dignissim eleifend eu blandit elit. Nullam a est et justo vestibulum interdum vel eu arcu. Mauris et pulvinar justo. Vestibulum et est id elit porttitor dapibus. Nunc at pellentesque nisi. \n Donec maximus scelerisque mi, id convallis urna vulputate id. Aenean dignissim purus justo, nec placerat purus mollis non. Nam egestas faucibus lorem eget euismod. Vestibulum at mollis lectus. Maecenas congue luctus augue, iaculis convallis lectus egestas eu. Aenean vitae neque nec justo vulputate sagittis. Nullam venenatis vel justo id auctor. Sed in tortor nunc. Proin mattis eros ac ligula tempor ornare. Nunc elementum ornare ultrices. \n et et et "
+
+// ? arrumar paragrafos, pq o terceiro nao ta indo
+
+function WordCount(str) { 
+    return str.split(" ").length;
+  }
+  console.log("The paragraph has", WordCount(paragraph), "words."); // Sempre colocar vírgula , entre diferentes coisas que vão ser retornadas no console.log ("string", variable, "string") exemplo.
+
+
+  let count = (paragraph.match(/et/g) || []).length; // ? deixar em funcoes que aprendemos ate agora
+  console.log("The paragraph has", count, "'et' words");
+
+  // Bonus 2
+
+	@@ -79,8 +81,8 @@ for (let i = phraseToCheck.length - 1; i>= 0; i -= 1) { // reparar que o hacker.
+    palindromeCheck += phraseToCheck[i]
+}
+
+if (phraseToCheck === palindromeCheck) { // ? esta faltando tirar os espacos do meio da string, senao todos estao dando not palindrome
+    console.log('Palindrome phrase')
+}   else {
+    console.log('Phrase chosen is not palindrome.')
