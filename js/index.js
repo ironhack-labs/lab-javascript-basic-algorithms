7cd323349ade3f3849bbc5f48de5133c705bd700
Joao Pedro <joaopbbezerra@gmail.com>
2021-05-31 16:54:59 +0100

    Solution

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 // Iteration 1: Names and Input
+let hacker1 = "João" //Driver
+let hacker2 = ""     // Navigators
 
-
+console.log(`The drivers name is ${hacker1}`);
+console.log(`The navigators name is ${hacker2}`);
 // Iteration 2: Conditionals
+
+if (hacker1.length > hacker2.length){
+    console.log("")
+} else if (hacker1.length < hacker2.legnth){
+    console.log("")
+} else {
+    
+}
 
 
 // Iteration 3: Loops
