4b9f1ff1b6dfe9c10925423a0ac5d711a87aed3c
Tiago Seixas <tiagoseixasfonseca@gmail.com>
2021-05-31 17:31:38 +0100

    loop 3.1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
+console.log("I'm ready!");
+
+
 // Iteration 1: Names and Input
+const hacker1 = "Tiago";
+let driver = hacker1;
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Victor";
+console.log(`The Navigator's name is ${hacker2}`);
 
 
-// Iteration 2: Conditionals
+if (hacker1.length<hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker2.length} characters`);
+}   
+    else if (hacker1.length>hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker1.length} characters`);
+    }   
+    else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
 
 
 // Iteration 3: Loops
+let result = "";
+for (let i = 0; i < driver.length; i++){
+result = result + driver[i] + " ";
+}
+console.log(result.toUpperCase())
