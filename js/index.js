1176e5650633d6141ed1b012765156e2efe2f7ad
Joao Pedro <joaopbbezerra@gmail.com>
2021-05-31 17:23:49 +0100

    Done but bonus

--- js/index.js
+++ js/index.js
@@ -1,18 +1,41 @@
 // Iteration 1: Names and Input
-let hacker1 = "João" //Driver
-let hacker2 = ""     // Navigators
-
+let hacker1 = "Aoão" //Driver
+let hacker2 = "João"     // Navigators
+let hackerLen1 = hacker1.length;
+let hackerLen2 = hacker2.length;
 console.log(`The drivers name is ${hacker1}`);
 console.log(`The navigators name is ${hacker2}`);
 // Iteration 2: Conditionals
 
-if (hacker1.length > hacker2.length){
-    console.log("")
-} else if (hacker1.length < hacker2.legnth){
-    console.log("")
+if (hackerLen1 > hackerLen2){
+    console.log(`The driver has the longest name, it has ${hackerLen1} characters.`)
+} else if (hackerLen1 < hackerLen2){
+    console.log(`The navigator has the longest name, it has ${hackerLen2} characters.`)
 } else {
-    
+    console.log(`Wow, you both have equally long names, ${hackerLen1} characters`)
 }
 
 
 // Iteration 3: Loops
+let nameHacker1 = "";
+let nameHacker2 = "";
+for (let i = 0; i < hackerLen1; i++){
+    nameHacker1 = nameHacker1 + hacker1[i] + " "
+}
+
+for (let i = 0; i < hackerLen2; i++){
+    nameHacker2 = nameHacker2 + hacker2[i] + " "
+}
+console.log(nameHacker1)
+console.log(nameHacker2)
+
+if (hacker1 < hacker2){ //
+    console.log("The drivers name goes first")
+} else if (hacker1 > hacker2){
+    console.log("Yo, the navigator comes first")
+} else {
+    console.log("What?! You both have the same name?")
+}
+
+// Bonus
+const lorem = ""
