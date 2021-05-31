3dee9ad5848074fe3a0db401c837d1889bea786b
Irina <ifalko@protonmail.com>
2021-05-31 17:10:54 +0200

    First commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 // Iteration 1: Names and Input
-//Test =
+
+// 1.1 Create a variable `hacker1` with the driver's name.
+
+// 1.2 Print `"The driver's name is XXXX"`.
+
+// 1.3 Create a variable `hacker2` with the navigator's name.
+
+// 1.4 Print `"The navigator's name is YYYY"`.
+
+const hacker1 = "Nelly";
+console.log(`The driver's name is ${hacker1}`);
 
 // Iteration 2: Conditionals
 
