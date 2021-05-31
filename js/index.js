118c0cc6c1954a19777310b94df84d57ae1caab5
Tiago Seixas <tiagoseixasfonseca@gmail.com>
2021-05-31 18:20:21 +0100

    final essay

--- js/index.js
+++ js/index.js
@@ -3,10 +3,11 @@ console.log("I'm ready!");
 
 // Iteration 1: Names and Input
 const hacker1 = "Tiago";
-let driver = hacker1;
-console.log(`The driver's name is ${hacker1}`);
+const driver = hacker1;
 const hacker2 = "Victor";
-console.log(`The Navigator's name is ${hacker2}`);
+const Navigator = hacker2;
+
+//console.log(`The Navigator's name is ${hacker2}`);
 
 
 if (hacker1.length<hacker2.length) {
@@ -25,4 +26,21 @@ let result = "";
 for (let i = 0; i < driver.length; i++){
 result = result + driver[i] + " ";
 }
-console.log(result.toUpperCase())
+console.log(result.toUpperCase());
+
+let result2 = "";
+for (let i = Navigator.length - 1; i >= 0; i--){
+result2 = result2 + Navigator[i] + " ";
+}
+console.log(result2.toUpperCase())
+
+if (driver<Navigator) {
+    console.log(`The driver's name goes first`);
+}   
+    else if (driver>Navigator) {
+    console.log(`Yo, the navigator goes first definitely`);
+    }   
+    else {
+    console.log(`Yo, the navigator goes first definitely`);
+    
+}
