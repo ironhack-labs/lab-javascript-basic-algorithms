dbfbbf718022387d860ed6d313592a6f5d5e8078
Enrique VOZA <enriquevozmediano@yahoo.es>
2021-06-03 17:51:28 +0200

    90% solved

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
 // Iteration 1: Names and Input
+driver = 'Enrique'
+navegador = 'Kike'
 
+console.log(`hola ${driver} y ${navegador}`)
 
 // Iteration 2: Conditionals
 
+if (driver.length < navegador.length) {
+    console.log(`The driver has the longest name, it has ${driver.length} characters.`)
 
-// Iteration 3: Loops
+} else if (driver.length > navegador.length) {
+    console.log(`It seems that the navegador has the longest name, it has ${driver.length} characters`)
+} else {
+
+    console.log(`Wow, you both have equally long names, characters!`)
+}
+
+
+// Iteration 3: LoopsLoops
+for (let i = 0; i < driver.length; i++) {
+    console.log(`${driver.charAt(i)}`)
+}
+
+for (let i = driver.length - 1; i >= 0; i--) {
+    console.log(`${driver.charAt(i)}`)
+}
+
+
+
+
