1051fe780b1ae5633930c4f353942d9389e30cec
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-06-05 15:57:01 +0200

    Iteration 3 part 2

--- js/index.js
+++ js/index.js
@@ -23,3 +23,13 @@ if (hacker1.length > hacker2.length) {
 // Iteration 3: Loops
 
 console.log(hacker1.toUpperCase().split("").join(" "));
+
+function reverseString(str){  
+    let reversed = "";      
+     for (let i = str.length - 1; i >= 0; i--){         
+       reversed += str[i];  
+     }     
+    return reversed;
+  }
+  
+  reverseString(hacker1);
