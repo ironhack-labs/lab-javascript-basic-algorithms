96d5b407f7f3006c32439108755b6b08b478902c
Natasha Smith <pisceanchic@gmail.com>
2021-06-08 11:48:54 +0200

    done

--- js/index.js
+++ js/index.js
@@ -8,25 +8,23 @@ console.log("The navigator's name is ${hacker2}");
 
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
-  console.log("The driver has the longest name, it has ${hacker1.length} characters");
+  console.log ("The driver has the longest name, it has ${hacker1.length} characters");
 }
 else if (hacker1.length < hacker2.length) {
-  console.log("It seems that the navigator has the longest name, it has XX characters.");
+  console.log ("It seems that the navigator has the longest name, it has ${hacker2.length} characters.");
 }
-else if (hacker1.length = hacker2.length) {
+else if (hacker1.length == hacker2.length) {
   console.log("Wow, you both have equally long names, XX characters!");
 }
 
 // Iteration 3: Loops
 //3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
  
- let newDriverName = ""; //this is the original driver's name, but with the characters separated by spaces and in capital letters, so javascript will have to recognize it as a technically new name (but it's really just the old name with fancy commands, but we have to define the name in a new way because of this, hence "newDriverName")
- let driver = hacker1 //need to add this b/c 
+let newDriverName = ' '; //this is the original driver's name, but with the characters separated by spaces and in capital letters, so javascript will have to recognize it as a technically new name (but it's really just the old name with fancy commands, but we have to define the name in a new way because of this, hence "newDriverName")
+let driverUpperCase = hacker1.toUpperCase();
 
- for (let i=0; i < driver.length; i++) {
-   if ()
- }
+for (let i=0; i < hacker1.length; i++) {
+   console.log(newDriverName += driverUpperCase[i] + ' ');
+}
 
- //console.log(newDriversName)
-
-//I know I have to use splice and charAt here, but I am lost at this moment and have spent hours on this already as I was confused about the instructions for the LAB | JS Basic Algorithms
+console.log(newDriverName);
