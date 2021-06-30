99a792f75e225ed6e0525b763686bf05c47d04e7
JohannaMarie <johannamarie.jmiac@gmail.com>
2021-06-30 15:59:38 +0200

    exercice conditionals

--- js/index.js
+++ js/index.js
@@ -25,3 +25,31 @@ else {
 }
 
 // Iteration 3: Loops
+
+
+hacker1 = hacker1.split('').join(' ').toUpperCase();
+console.log(hacker1)
+
+
+let reverseHacker2 = ""
+for (let i = hacker2.length-1; i >= 0; i--) {
+    reverseHacker2 += hacker2[i]
+}
+console.log(reverseHacker2)
+
+
+function compare (hacker1,hacker2) {
+    for (let i = 0; i < hacker2.length; i++) {
+        if (hacker1[i] < hacker2[i]) {
+            return "The driver's name goes first.";
+        }
+        else if (hacker1[i] > hacker2[i]) {
+            return "Yo, the navigator goes first definitely.";
+        }
+        else if (i === hacker2.length-1) { 
+            return "What?! You both have the same name?";
+        }
+    }
+}
+
+console.log(compare(hacker1,hacker2))
