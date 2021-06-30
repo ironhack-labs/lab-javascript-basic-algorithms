9a08bd4e90521c16466bd6f70f1cf93a3d3275be
Flow-Fly <florian.aube@gmail.com>
2021-06-30 15:06:04 +0200

    Step 2 done

--- js/index.js
+++ js/index.js
@@ -11,5 +11,17 @@ console.log("The navigator's name is " + hacker2)
 
 // Iteration 2: Conditionals
 
+let driver = hacker1.length;
+let navigator2 = hacker2.length;
+
+if (driver > navigator2) {
+    console.log(`The driver has the longest name, it has ${driver} characters.`)
+}
+else if (navigator2 > driver) {
+    console.log(`It seems that the navigator has the longest name, it has ${navigator2} characters.`)
+}
+else {
+    console.log(`Wow, you both have equally long names, ${driver} characters!`)
+}
 
 // Iteration 3: Loops
