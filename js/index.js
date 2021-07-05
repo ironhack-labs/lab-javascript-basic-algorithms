99ea9d6b365b6f374713de3ce1948c088cef04df
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-05 22:12:17 +0300

    complete first iteration

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 // Iteration 1: Names and Input
 
+const hacker1 = "George";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Jonathan";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
