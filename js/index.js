e9678ee465e2f5185e9e9b6d98b78b4212bed382
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-05 23:50:25 +0300

    complete iteration three

--- js/index.js
+++ js/index.js
@@ -23,3 +23,51 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+
+// 3.1
+
+let driverNameInCapitals = hacker1.toUpperCase();
+// console.log(driverNameInCapitals.split('').join(' ')); // short way to do it
+
+let nameWithSpaces = "";
+for (let i = 0; i < driverNameInCapitals.length; i++) {
+  if (i === driverNameInCapitals.length - 1) {
+    nameWithSpaces += driverNameInCapitals[i];
+  } else {
+    nameWithSpaces += driverNameInCapitals[i] + " ";
+  }
+}
+console.log(nameWithSpaces);
+
+// 3.2
+
+let reverseName = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reverseName += hacker2[i];
+}
+console.log(reverseName);
+
+// 3.3.
+
+// a.
+
+// if (hacker1 < hacker2) {
+//     console.log("The driver's name goes first.");
+//   } else if (hacker1 > hacker2){
+//     console.log("Yo, the navigator goes first definitely.");
+//   } else {
+//     console.log("What?! You both have the same name?")
+//   }
+
+// b.
+
+if (hacker1.localeCompare(hacker2) < 0) {
+    console.log("Driver's name goes first")
+  }
+  else if (hacker1.localeCompare(hacker2) = 0){
+    console.log("What?! You both have the same name?")
+  }
+  else {
+    console.log("Yo, the navigator goes first definitely.")
+  }
+
