2483fde8998c39f14a66ca5911a7205f6670d666
Thiago Marques <morais.marques@hotmail.com>
2021-07-10 14:48:08 -0300

    ongoing lab

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 // Iteration 1: Names and Input
-
+console.log("I'm ready!");
+let hacker1 = "Thiago";
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = "Rafael";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if ("hacker1".localeCompare("hacker2") === 1) { console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+  } else if ("hacker1".localeCompare("hacker2") === -1) {console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+  } else {console.log(`Wow, you both have equally long names, ${hacker1.length || hacker2.length} characters!`);
+  }
 
 // Iteration 3: Loops
