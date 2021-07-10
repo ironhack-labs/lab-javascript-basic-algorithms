e4fc37a35161900d4a7b055627cb63f21f8f54f0
Carlos Daniel <caduccino@live.com>
2021-07-10 14:19:02 -0300

    done

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ console.log("The navigator's name is " + hacker2);
 if (hacker1.length > hacker2.length) {
   console.log("The driver has the longest name, it has" + " " + hacker1.length +  " " + "characters.");
 } else if (hacker1.length < hacker2.length) {
-  console.log("It seems that the navigator has the longest name, it has" + " " + hacker1.length +  " " + "characters.");
+  console.log("It seems that the navigator has the longest name, it has" + " " + hacker2.length +  " " + "characters.");
 } else if (hacker1.length === hacker2.length){
   console.log("Wow, you both have equally long names," + " " + hacker1.length +  " " + "characters.");
 }
