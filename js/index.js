b5e54eb1e71794258149c9d38f1c9d0366ec495f
Piero de Sá <desa.piero@gmail.com>
2021-07-15 15:09:04 -0300

    Lab done. Ready for Pull Request

--- js/index.js
+++ js/index.js
@@ -18,6 +18,17 @@ if (hacker1.length > hacker2.length) {
 
 
 // Iteration 3: Loops
+//Resposta para 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N":
+console.log(hacker1.toUpperCase().split("").join(""));
 
-let contadorDriver
-let contadorNavigator
+//Resposta para 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ":
+function reverterNome(nome) {
+    return nome.split("").reverse().join("");
+}
+console.log(reverterNome("Piero também"));
+
+//Resposta para 3.3 Depending on the lexicographic order of the strings, print...
+
+if(hacker1.localeCompare(hacker2) === -1) {console.log(`The driver's name goes first.`)}
+ else if(hacker1.localeCompare(hacker2) === 1){console.log(`Yo, the navigator goes first definitely.`)} 
+ else {console.log(`What?! You both have the same name?`)};
