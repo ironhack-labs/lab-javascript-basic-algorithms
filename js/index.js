9fc709c35e1199e508dc2b5379c91818babee9b1
thomaskno <thomas.kno@yahoo.com>
2021-08-09 17:54:41 -0400

    Add names and input

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 // Iteration 1: Names and Input
 
+let hacker1 = 'Alejandro'
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Vince"
+console.log(`The navigator's name is ${hacker2}`);
+
 
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
