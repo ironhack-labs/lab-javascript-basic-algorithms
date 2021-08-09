8cc107c7219decf14ab1f2df2f71790520afb450
ftvalex01 <ftvalex@hotmail.com>
2021-08-09 16:06:02 +0100

    Iteration1
    primera iteracion

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Paul"
+let hacker2 = "Scotty"
+console.log("The driver's name is " + hacker1);
+console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
 
