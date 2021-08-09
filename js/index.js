95badba63a026c5c1db42d476a04537764f93b81
Ninabaldo <nbaldorousseau@gmail.com>
2021-08-09 18:17:01 +0200

    WD082021-NinaBaldo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 // Iteration 1: Names and Input
+let hacker1 = "Nina"
+hacker1 = "Nina";
+
+console.log ( "The driver's name is " + hacker1);
+
+let hacker2 = "Vicenç"
+hacker1 = "Vicenç";
+
+console.log ( "The navigator's name is " + hacker2);
+
+
 
 
 // Iteration 2: Conditionals
+let long = 'Nina';
+console.log ('Nina is '   +   long.length   +  ' letters long');
+
+let Long = 'Vicenç';
+console.log ('Vicenç is '   +   Long.length   +  ' letters long');
+
+console.log ('It seems that the navigator has the longest name, it has '  +   Long.length  +  ' characters');
+
 
 
 // Iteration 3: Loops
+
+const str = 'nina';
+console.log(str.toUpperCase());
+
