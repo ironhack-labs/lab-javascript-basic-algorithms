c3c017dea5711d7a7b146bb25c186a7bd4c99236
lauraAlabau <arlaura3@gmail.com>
2021-08-09 16:54:39 +0200

    Segundo commit
    Iteración 1 completada

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 const hacker1 = 'Laura';
 console.log(`"The driver's name is ${hacker1}"`);
 
+const hacker2 = 'Damien';
+console.log( `"The navigator's name is ${hacker2}"`);
+
 // Iteration 2: Conditionals
 
 
