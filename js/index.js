956db37f8a7e243da794cda57bcee4f4943ec4dd
Carl Berthold <Carl@space-time.tv>
2021-08-10 08:15:46 +0200

    js loops name with spaces

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 // Iteration 1: Names and Input
-
+let hacker1 = 'Carl';
+console.log(`The driver´s name is ${hacker1}`);
+let hacker2 = 'Chai';
+console.log(`The Naviagtor´s name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length){
+    console.log(`It seems that the driver has the longest name, it has ${hacker1.length} characters.`);
+}
+else if (hacker2.length > hacker1.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else { 
+    console.log (`Wow, you both have equally long names, ${hacker2.length} characters!.`);
+}
 
 // Iteration 3: Loops
