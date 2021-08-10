14ba41d764bf081378559345a44b28f8c8e6d68a
Frank Batista <fb53901@gmail.com>
2021-08-09 22:31:03 -0400

    Conditionals

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 console.log("I'm ready!");
 // Iteration 1: Names and Input
-
-
+let hacker1 = 'Frank';
+console.log(`the driver's name is ${hacker1}`);
+let hacker2 = prompt ("What is your name");
+console.log(`the navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
-
-
+let driverLength = hacker1.length;
+let navigatorLength = hacker2.length;
+if (driverLength > navigatorLength){
+  console.log(`The driver has the longest name, it has ${driverLength} characters`);
+}
+else if (driverLength < navigatorLength){
+    console.log(`It seems that the navigator has the longest name, it has ${navigatorLength} characters`);
+}
+else{
+  console.log(`Wow, you both have equally long names, ${driverLength} characters!`)
+};
 // Iteration 3: Loops
