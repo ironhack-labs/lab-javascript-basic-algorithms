760c3f3d0d5292105faad1229ade496afd07be11
Cleiton1912 <cleiton.silvalima@outlook.fr>
2021-09-02 21:17:29 +0200

    first iterationdone

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Hassan";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Cleiton";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
