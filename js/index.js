2989021677a88fcd29e2676e9ae7c9b54fd55bde
unknown <martarrubio@telefonica.net>
2021-09-14 09:35:38 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Marta'
+console.log('The drivers name is', hacker1)
+
+let hacker2 = 'Pepe'
+console.log('The navigators name is', hacker2)
 
 
 // Iteration 2: Conditionals
-
+console.log(`The driver has the longest name, it has, 
+${hacker1.length} characters`)
 
 // Iteration 3: Loops
+
+let name = 'Marta'
+console.log(name.toUpperCase())
+
