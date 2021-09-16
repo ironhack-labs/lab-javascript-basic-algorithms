aee9879eaddf9b35fff575e97ceca88e23c1e4c9
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2021-09-16 21:03:54 +0200

    Iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
+const hacker1 = "tomas";
 
+console.log("The drive`s name is " + hacker1)   
+
+const hacker2 = "joana";
+
+console.log("The navigator's name is " + hacker2)
 
 // Iteration 2: Conditionals
 
