74f90b1a226d3377555ecbc8b8302c57e500e144
ignazka <picastle@posteo.de>
2021-09-16 21:32:40 +0200

    add second iteration

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 // Iteration 1: Names and Input
 
-
 // Iteration 2: Conditionals
+const driver = "Karl";
+const navigator = "Phil";
 
+if (driver.length > navigator.length) {
+  console.log(
+    `The driver has the longest name, it has ${driver.length} characters`
+  );
+} else if (driver.length < navigator.length) {
+  console.log(
+    `The navigator has the longest name, it has ${navigator.length} characters`
+  );
+} else {
+  console.log(`The both are equally long names, ${driver.length} characters`);
+}
 
 // Iteration 3: Loops
