97fc437943512306530fce315e71ea4a33b10d0e
karlajaramillo <karlav.jaramillo@gmail.com>
2021-09-16 21:03:45 +0200

    Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 = "tomas";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "joana";
+console.log(`The navigator's name is ${hacker2}`);
 
 
 // Iteration 2: Conditionals
