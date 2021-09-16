8e084515e79c292e65a734ee28742bd89afe2465
Lilli Reilly <lilli.reilly@gmail.com>
2021-09-16 21:35:02 +0200

    iteration 1

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@
 
 
 // Iteration 2: Conditionals
-
+let driver = "Lilli";
+let navigator ="TJ";
+if (driver.length > navigator.length) {
+  console.log(`The driver has the longest name, it has ${driver.length} characters.`)
+} else if (driver.length < navigator.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters`)
+} else if (driver.length === navigator.length) {
+  console.log(`Wow, you both have equally long names, ${driver.length} characters!`)
+}
 
 // Iteration 3: Loops
