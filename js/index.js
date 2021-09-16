b46d2d15eb990723d46df05364249729d0701d09
karlajaramillo <karlav.jaramillo@gmail.com>
2021-09-16 21:30:09 +0200

    Adds second iteration

--- js/index.js
+++ js/index.js
@@ -8,5 +8,17 @@ console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+const driver = "Karl";
+const navigator = "Phil";
+
+
+if(driver.length > navigator.length) {
+  console.log(`The driver has the longest name, it has ${driver.length} characters`);
+} else if (driver.length < navigator.length) {
+  console.log(`The navigator has the longest name, it has ${navigator.length} characters`);
+} else {
+  console.log(`The both are equally long names, ${driver.length} characters`);
+}
+
 
 // Iteration 3: Loops
