f8f85c3e27eefce1d7fba5a68dcb411eb2dc2012
Alice <alice.jost@outlook.de>
2021-09-20 19:46:08 +0200

    Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 // Iteration 1: Names and Input
 
+//hacker 1
+const hacker1 = "Aintzane";
+
+console.log(`The driver's name is ${hacker1}`);
+
+//hacker 2
+const hacker2 = "Alice";
+
+console.log(`The navigator's name is ${hacker2}`);
+
 
 // Iteration 2: Conditionals
 
