389eadd0e9cef9a7a7e59c3412e4ff525b6bd095
Kati Fe <k-felgitsch@hotmail.com>
2021-09-20 22:47:26 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
-// Iteration 1: Names and Input
+//Iteration 1
+let hacker1 = 'Kati';
+console.log(`The drivers name is ${hacker1}`) ;
 
+let hacker2 = 'Bob';
+console.log(`The navigators name is ${hacker2}`);
 
-// Iteration 2: Conditionals
+//Iteration 2
+console.log(`It seems that the navigator has the longest name, it has ${hacker1.length} characters`);
 
+//Iteration 3
+let uppercased = hacker1.toUpperCase();
+console.log(uppercased[0], uppercased[1], uppercased[2], uppercased[3]);
 
-// Iteration 3: Loops
+let str = 'Kati';
+let strReverse = str.split('').reverse().join('');  
+console.log(`${strReverse}`);
+
+console.log(hacker1);
+
+const hacker = ['Kati', 'Bob'];
+console.log(hacker.sort());
+
+if (hacker.sort[0] > hacker.sort[1]) {
+  console.log(`Yo, the navigator goes first definitely`);
+} else if (hacker.sort[0] < hacker.sort[1]) {
+  console.log(` The driver's name goes first.`);
+} else {
+  console.log(`What?! You both have the same name?`)
+}
