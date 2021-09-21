7c42eb95c10108e5c3c39ecd466ca7ed3eb9849f
Alice <alice.jost@outlook.de>
2021-09-21 08:04:57 +0200

    Bonus 2

--- js/index.js
+++ js/index.js
@@ -42,3 +42,55 @@ if(hacker1 === hacker2) {
   console.log("Yo, the navigator goes first definitely.")
 }
 
+
+//------------------------------------------------------------------//
+
+
+//Bonus 1
+
+
+//to store lorem ipsum
+const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+
+//count the amount of words
+function WordCount(str) {
+  return str.split(" ").length;
+};
+
+console.log(WordCount(loremIpsum));
+
+
+//count the amount of 'et'
+function count(str, find) {
+  return (str.split(find)).length - 1;
+}
+
+console.log(count(loremIpsum, "et"));
+
+
+//------------------------------------------------------------------//
+
+
+//Bonus 2
+
+//a random string
+let somePalindrome = "put it up";
+
+//make the string without space
+somePalindrome = somePalindrome.split(" ").join("");
+
+//console.log(somePalindrome);
+
+//check if string is a palindrome
+function checkPalindrome(str) {
+  let l = str.length;
+  for (let i = 0; i < l/2; i++) {
+   if (str[i] !== str[l - 1 - i]) {
+    return false;
+   };
+  };
+  return true;
+ };
+
+ console.log(checkPalindrome(somePalindrome));
