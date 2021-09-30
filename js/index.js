31bf15656c1781d417e62b01a5292e978123a129
Luis Bermudez <lapbermudez@gmail.com>
2021-09-29 23:24:07 -0500

    First commit -Add Iteration 1, Names and Input-

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
 
+//  1.1 Create a variable `hacker1` with the driver's name.
+let hacker1 = 'Mango';
+// 1.2 Print `"The driver's name is XXXX"`.
+console.log(`The driver's name is ${hacker1}`);
+// 1.3 Create a variable `hacker2` with the navigator's name.
+let hacker2 = 'Luis';
+// 1.4 Print `"The navigator's name is YYYY"`.
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
