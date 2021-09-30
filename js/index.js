55c223c4ac1bfd8b1add833456c192f8b2fa6de4
Luis Bermudez <lapbermudez@gmail.com>
2021-09-30 01:00:12 -0500

    Add code for Iteration 3, Loops

--- js/index.js
+++ js/index.js
@@ -24,3 +24,47 @@ if (driverLength > navigatorLength) {
 }
 
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+// Var with the name all in uppercase 
+let upperCasedDriver = hacker1.toUpperCase();
+// Variable that will contain the result name with spaces - for now it is an empty string
+let spacedDriver = '';
+// Iterate through the string, get each char added to the empty variable and get a spaced concatenated to it
+for (const value of upperCasedDriver) {
+  spacedDriver += value + ' ';
+}
+console.log(spacedDriver);
+
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+let reverseNavigator = '';
+
+for (let i = (navigatorLength - 1); i >= 0; i--) {
+  reverseNavigator += hacker2[i];
+}
+console.log(reverseNavigator);
+
+
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+let charDriver;
+let charNavigator;
+
+let charASCIIDriver;
+let charASCIINAvigator;
+for (let i = 0; i < smallName; i++) {
+  charDriver = hacker1[i].toUpperCase();
+  charNavigator = hacker2[i].toUpperCase();
+
+  charASCIIDriver = charDriver.charCodeAt(0);
+  charASCIINAvigator = charNavigator.charCodeAt(0);
+
+  if (charASCIIDriver < charASCIINAvigator) {
+    console.log(hacker1);
+    break;
+  } else if (charASCIIDriver > charASCIINAvigator) {
+    console.log(hacker2);
+    break;
+  } else {
+    continue;
+  }
+};
