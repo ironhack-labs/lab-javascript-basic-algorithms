52c22b599abaca7bffc51ecd447ac8a2d55e8e7a
Luis Bermudez <lapbermudez@gmail.com>
2021-09-29 23:40:52 -0500

    Add Iteration 2, Conditionals - Check who has the name with the most characters-

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // Iteration 1: Names and Input
-
 //  1.1 Create a variable `hacker1` with the driver's name.
 let hacker1 = 'Mango';
 // 1.2 Print `"The driver's name is XXXX"`.
@@ -9,7 +8,19 @@ let hacker2 = 'Luis';
 // 1.4 Print `"The navigator's name is YYYY"`.
 console.log(`The navigator's name is ${hacker2}`);
 
-// Iteration 2: Conditionals
 
+
+// Iteration 2: Conditionals
+// 2.1. Depending on which name is longer, print:
+let driverLength = hacker1.length;
+let navigatorLength = hacker2.length;
+
+if (driverLength > navigatorLength) {
+  console.log(`The driver has the longest name, it has ${driverLength} characters.`);
+} else if (driverLength < navigatorLength) {
+  console.log(`It seems that the navigator has the longest name, it has ${navigatorLength} characters.`)
+} else {
+  console.log(`Wow, you both have equally long names, ${driverLength} characters!`)
+}
 
 // Iteration 3: Loops
