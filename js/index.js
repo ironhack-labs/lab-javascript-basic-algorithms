88a5ead9ec03e101cd60355a1e4df3c90a97d79d
Gabriel Gutierrez <ggutie95@gmail.com>
2021-10-09 09:21:20 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Gabriel'
 
+console.log(`The driver's name is ${hacker1}`)
 
+let hacker2 = 'Samuel'
+
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+  }else if(hacker2.length > hacker1.length){
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters.`)
+  }else{
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 
 // Iteration 3: Loops
+let hcker1 = hacker1.split("").join(" ").toUpperCase();
+
+console.log(hcker1);
+
+let nvgtr = hacker2.split('');
+
+let revnvgtr = nvgtr.reverse();
+
+console.log(revnvgtr)
+
+const hackerz= [hacker1,hacker2];
+console.log(hackerz.sort());
+console.log(`The driver's name goes first.`)
