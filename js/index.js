9af8182f326e476f49f50330de6e38500a2af806
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-10-12 15:17:19 -0400

    done

--- js/index.js
+++ js/index.js
@@ -40,10 +40,15 @@ console.log(`What?! You both have the same name?`)
 
 //Bonus 
 
-let lorem = Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan scelerisque lectus, eu efficitur mi sollicitudin in. Vestibulum ipsum libero, semper a enim vitae, luctus consectetur eros. In sit amet est malesuada, tristique arcu id, faucibus orci. Nam tellus purus, tristique et urna ut, volutpat rutrum sapien. Nunc tincidunt massa at arcu ultricies volutpat. In eget pretium velit. Praesent lacus nunc, tempor at neque et, pharetra gravida libero. Mauris quis odio sed nibh posuere efficitur eget et nulla. Cras quis suscipit felis. Sed interdum turpis libero, quis bibendum leo pellentesque nec. Suspendisse semper at nulla quis gravida. Vivamus a elementum dui, eget tristique est. Sed vitae urna sed ipsum vulputate fermentum ut vitae massa. Proin at lectus sed metus viverra convallis.
-
-Nunc quis interdum orci, ut ornare nunc. Vestibulum id felis augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id porta ante. Donec quam dui, lobortis vitae pulvinar sit amet, blandit ut massa. Nunc sapien nibh, luctus nec dolor et, efficitur malesuada nisl. Fusce pulvinar urna ac elit faucibus, non rhoncus dolor gravida. Vivamus in varius justo, sit amet suscipit nunc.
-
-Ut dignissim facilisis urna a hendrerit. Fusce efficitur eget tortor vel vulputate. Donec eu scelerisque odio, id rutrum erat. Fusce semper elit a sem venenatis, vitae ultrices justo scelerisque. Suspendisse ac est sed massa venenatis consectetur. Donec aliquet accumsan venenatis. Phasellus quis cursus urna, ac gravida felis. Integer risus nulla, tincidunt in ullamcorper in, congue sit amet tellus.
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan scelerisque lectus, eu efficitur mi sollicitudin in. Vestibulum ipsum libero, semper a enim vitae, luctus consectetur eros. In sit amet est malesuada, tristique arcu id, faucibus orci. Nam tellus purus, tristique et urna ut, volutpat rutrum sapien. Nunc tincidunt massa at arcu ultricies volutpat. In eget pretium velit. Praesent lacus nunc, tempor at neque et, pharetra gravida libero. Mauris quis odio sed nibh posuere efficitur eget et nulla. Cras quis suscipit felis. Sed interdum turpis libero, quis bibendum leo pellentesque nec. Suspendisse semper at nulla quis gravida. Vivamus a elementum dui, eget tristique est. Sed vitae urna sed ipsum vulputate fermentum ut vitae massa. Proin at lectus sedmetus viverra convallis. Nunc quis interdum orci, ut ornare nunc. Vestibulum id felis augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id porta ante. Donec quam dui, lobortis vitae pulvinar sit amet, blandit ut massa. Nunc sapien nibh, luctus nec dolor et, efficitur malesuada nisl. Fusce pulvinar urna ac elit faucibus, non rhoncus dolor gravida. Vivamus in varius justo, sit amet suscipit nunc. Ut dignissim facilisis urna a hendrerit. Fusce efficitur eget tortor vel vulputate. Donec eu scelerisque odio, id rutrum erat. Fusce semper elit a sem venenatis, vitae ultrices justo scelerisque. Suspendisse ac est sed massa venenatis consectetur. Donec aliquet accumsan venenatis. Phasellus quis cursus urna, ac gravida felis. Integer risus nulla, tincidunt in ullamcorper in, congue sit amet tellus."
 
 
+let loremArray= lorem.split(" ")
+
+console.log(loremArray.length)
+let counter=0;
+for(i=0; i<loremArray.length; i++){
+  if(loremArray[i]=== "et")
+  counter++
+}
+console.log(counter)
