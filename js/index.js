f62a66faee71741c366041731558ed93329a4435
agrimald2 <agrimaldopci18@gmail.com>
2021-10-18 11:00:50 -0500

    Bonus 1 and 2 - Johannes and Alonso
    Bonus 1 and 2
    
    by:
    Johannes and Alonso

--- js/index.js
+++ js/index.js
@@ -45,3 +45,33 @@ if (hacker1 == hacker2) {
         console.log("Yo, the navigator goes first definitely.")
     }
 }
+
+// Bonus 1
+
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices elit nulla, sed convallis nisi dapibus sit amet. Quisque a ex ante. Phasellus sodales lorem purus, at mollis eros rutrum in. Proin scelerisque faucibus enim, et iaculis nulla condimentum ut. Nunc faucibus eros sit amet lorem sagittis rutrum eget in quam. Cras sem neque, vehicula sit amet lacus in, mollis facilisis velit. Integer feugiat vel ligula at aliquet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla vulputate, libero porttitor vulputate mollis, lectus dui tristique tellus, quis fermentum felis lacus et dui. Aliquam urna eros, congue sit amet eros quis, blandit fermentum velit. Etiam ac hendrerit tellus. Nunc posuere consequat elementum. " +
+
+    "Maecenas pharetra lacus quam, at finibus neque consectetur in. Nunc imperdiet tincidunt rutrum. Donec fermentum, purus molestie mattis vehicula, nibh diam lobortis augue, ut vestibulum diam ante a enim. Vivamus consequat mollis commodo. Vestibulum quis scelerisque purus. Sed pulvinar lorem est, lobortis sagittis ipsum semper nec. Praesent pellentesque turpis in massa venenatis, vel suscipit leo congue." +
+
+    " Donec auctor elit at sapien consequat facilisis. Nullam turpis sapien, pharetra sed commodo et, feugiat at orci. Nunc sem nisl, feugiat quis vestibulum ut, accumsan vitae purus. Curabitur odio leo, condimentum non egestas vitae, tempor eget tortor. Phasellus et rutrum nisl. Duis accumsan rutrum nunc in eleifend. Aenean et molestie libero. Mauris elementum vehicula augue. Duis eget nisl ut erat facilisis aliquam. Vestibulum id enim orci. Proin congue tortor vitae lectus vehicula, at posuere erat malesuada. Vivamus ut convallis elit. Morbi at finibus ipsum. In rhoncus, nulla vitae ullamcorper ullamcorper, nulla quam ultrices augue, quis sodales sapien quam ut nisi.";
+
+
+console.log(lorem.split(" ").length + "Words");
+
+console.log(lorem.split("et").length - 1 + " Times ET");
+
+//Bonus 2
+let phraseToCheck = "race g car";
+let normalWord = phraseToCheck.replace(/[\W_]/g, "").toLowerCase();
+
+let reversedWord = "";
+
+
+for (i = normalWord.length - 1; i >= 0; i--) {
+    reversedWord = reversedWord.concat(normalWord[i]);
+}
+
+if (normalWord == reversedWord) {
+    console.log("It is a Palindrome");
+} else {
+    console.log("No Palindrome");
+}
