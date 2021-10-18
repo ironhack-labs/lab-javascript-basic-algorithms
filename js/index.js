4358f078324c08331d3644177f609cf64e509993
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-10-18 19:20:15 +0200

    Completed the 3 exercice iterations

--- js/index.js
+++ js/index.js
@@ -1,7 +1,52 @@
 // Iteration 1: Names and Input
 // 
+let hacker1 = "Carolina";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Bob";
+console.log(`Ther navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+
+}else if (hacker1.length = hacker2.length){
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
+
 
 // Iteration 3: Loops
+let hackerName = "";
+
+for(let i=0; i<hacker1.length; i++) {
+    hackerName += hacker1[i].toUpperCase() + " ";
+}
+console.log(hackerName);
+
+let reverseHackerName = "";
+
+for(let i = hacker2.length; i >= 0; i--) {
+    reverseHackerName += [hacker2[i]];
+}
+console.log(reverseHackerName);
+
+
+
+for(let i = 0; i<hacker1.length; i++){
+    if (hacker1 == hacker2){
+        console.log("What?! You both have the same name?");
+        break
+    }else if(hacker1.charCodeAt(i) == hacker2.charCodeAt(i)) {
+        continue
+    }  
+    else if(hacker1.charCodeAt(i) < hacker2.charCodeAt(i)){
+        console.log(`The driver's name goes first.`);
+        break
+    }else if(hacker1.charCodeAt(i) > hacker2.charCodeAt(i)) {
+        console.log("Yo, the navigator goes first definitely.");
+        break
+    }
+}
