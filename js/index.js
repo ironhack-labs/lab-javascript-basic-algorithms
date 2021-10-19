739dacca943731cfe95c7516f09d56f6c347336d
Raquel <raqhidcor@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.name Raquelraqhidcor@gmail.com>
2021-10-19 23:42:06 +0200

    Bonus time, bonus 1 uncompleted

--- js/index.js
+++ js/index.js
@@ -53,5 +53,30 @@ function hackersOrder(name1, name2) {
       return `What?! You both have the same name?`;
     }
   }
+
+  //Bonus time
+
+  //Bonus 1
+
+  const loreIpsum3 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat congue sem, scelerisque vehicula augue ullamcorper ac. Nunc ex leo, tristique vel enim sit amet, mattis eleifend diam. Mauris consectetur felis mi, eget egestas est blandit et. Proin at leo augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus rutrum lacinia ipsum in finibus. Integer interdum lacus ut ipsum varius, quis tincidunt orci bibendum. Maecenas dignissim mi est, sit amet pellentesque purus aliquam id. Vestibulum convallis vel neque ut aliquam.Cras dapibus augue eget turpis commodo dapibus. Mauris nec nulla mattis nulla laoreet laoreet ut tempor orci. Pellentesque sed ante maximus turpis condimentum posuere et a velit. Etiam molestie, libero eu porttitor pretium, magna diam rhoncus sem, at porta diam lorem pretium ante. Vestibulum finibus varius neque, nec pulvinar sem. Nam aliquam blandit diam non viverra. Quisque gravida condimentum tempor. Nunc at bibendum dolor. Curabitur dignissim ex in erat semper tristique. Donec ut metus pellentesque, blandit mauris sed, cursus turpis. Phasellus accumsan ligula sit amet porta dapibus. Fusce commodo sapien et dui consectetur mattis.Duis iaculis mauris vel neque vulputate rutrum. Mauris at imperdiet quam. Fusce iaculis interdum lacinia. Proin sit amet venenatis arcu. Praesent at lacus eu enim lacinia egestas in at dolor. Integer nec risus ligula. Aenean ut dictum sapien, ac tempus nunc.";
+
+function wordCount(string) {
+  return string.split(" ").length;
+}
+
+function returnEt(string) {
+  let characters = string.split(" ");
+  let totalEt = 0;
+  for (let i = 0; i < characters.length; i++) {
+    if (characters[i] === "et") {
+      totalEt++;
+    }
+  }
+  return totalEt;
+}
+
+
+
   
 
