bea54a6cdecaf385f45f88a20fbd31a93a72575c
Raquel <raqhidcor@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.name Raquelraqhidcor@gmail.com>
2021-10-19 23:00:47 +0200

    Iteration 1 completed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 // Iteration 1: Names and Input
-// 
+
+const hacker1 = "José Luis";
+const hacker2 = "Raquel";
+
+console.log (`The driver's name is ${hacker1}`);
+console.log (`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+
+
 
 
 // Iteration 3: Loops
