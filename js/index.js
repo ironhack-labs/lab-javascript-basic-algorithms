9491b8047e0d8dbe6538ee2fac5f912e7803b9f9
JoseLValdes <jolvaldgo@gmail.com>
2021-10-19 20:35:04 +0200

    Generate 3 paragraphs. Store the text in a variable type of string.

--- js/index.js
+++ js/index.js
@@ -28,7 +28,6 @@ function changeName(name1) {
   return newName.toUpperCase().split("").join(" ");
 }
 
-
 //3.2
 
 const hacker2 = "Raquel";
@@ -40,8 +39,6 @@ function getReserve(name) {
   }
   return reverseName;
 }
-
-
 
 //3.3
 function hackersOrder(name1, name2) {
@@ -55,3 +52,8 @@ function hackersOrder(name1, name2) {
     return `What?! You both have the same name?`;
   }
 }
+
+//Bonus 1
+
+const loreIpsum3 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat congue sem, scelerisque vehicula augue ullamcorper ac. Nunc ex leo, tristique vel enim sit amet, mattis eleifend diam. Mauris consectetur felis mi, eget egestas est blandit et. Proin at leo augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus rutrum lacinia ipsum in finibus. Integer interdum lacus ut ipsum varius, quis tincidunt orci bibendum. Maecenas dignissim mi est, sit amet pellentesque purus aliquam id. Vestibulum convallis vel neque ut aliquam.Cras dapibus augue eget turpis commodo dapibus. Mauris nec nulla mattis nulla laoreet laoreet ut tempor orci. Pellentesque sed ante maximus turpis condimentum posuere et a velit. Etiam molestie, libero eu porttitor pretium, magna diam rhoncus sem, at porta diam lorem pretium ante. Vestibulum finibus varius neque, nec pulvinar sem. Nam aliquam blandit diam non viverra. Quisque gravida condimentum tempor. Nunc at bibendum dolor. Curabitur dignissim ex in erat semper tristique. Donec ut metus pellentesque, blandit mauris sed, cursus turpis. Phasellus accumsan ligula sit amet porta dapibus. Fusce commodo sapien et dui consectetur mattis.Duis iaculis mauris vel neque vulputate rutrum. Mauris at imperdiet quam. Fusce iaculis interdum lacinia. Proin sit amet venenatis arcu. Praesent at lacus eu enim lacinia egestas in at dolor. Integer nec risus ligula. Aenean ut dictum sapien, ac tempus nunc.";
