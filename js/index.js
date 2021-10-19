fd5e68b7db3d9869d6054ebd03bde4245b8e1705
JoseLValdes <jolvaldgo@gmail.com>
2021-10-19 20:28:17 +0200

    Iteration 1 Iteration 2 Iteration 3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,57 @@
 // Iteration 1: Names and Input
-// 
+const hacker1 = "José Luis";
+const hacker2 = "Raquel";
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+function getLongestName(name1, name2) {
+  if (name1.length > name2.length) {
+    return `The driver has the longest name, it has ${hacker1.length} characters.`;
+  } else if (name2.length > name1.length) {
+    return `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`;
+  } else {
+    return `Wow, you both have equally long names, ${hacker2.length} characters!`;
+  }
+}
 
 // Iteration 3: Loops
+
+//3.1
+function changeName(name1) {
+  let newName = "";
+  for (let i = 0; i < hacker1.length; i++) {
+    newName = newName + hacker1[i];
+  }
+  return newName.toUpperCase().split("").join(" ");
+}
+
+
+//3.2
+
+const hacker2 = "Raquel";
+
+function getReserve(name) {
+  let reverseName = "";
+  for (let i = 0; i < name.length; i++) {
+    reverseName = name[i] + reverseName;
+  }
+  return reverseName;
+}
+
+
+
+//3.3
+function hackersOrder(name1, name2) {
+  let number = name1.localeCompare(name2);
+
+  if (number === -1) {
+    return `The driver's name goes first.`;
+  } else if (number === 1) {
+    return `Yo, the navigator goes first definitely.`;
+  } else if (number === 0) {
+    return `What?! You both have the same name?`;
+  }
+}
