ec1233456177f10ff5728d7db8a357986b191044
Raquel <raqhidcor@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.name Raquelraqhidcor@gmail.com>
2021-10-19 23:18:20 +0200

    Iteration 2 completed

--- js/index.js
+++ js/index.js
@@ -8,7 +8,15 @@ console.log (`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
-
-
+function getLongestName(name1,name2){
+    if (name1.length > name2.length){
+        return `The driver has the longest name, it has ${hacker1.length} characters.`;
+    } else if (name2.length > name1.length){
+        return `It seems that the navigator has the longest name, it has ${hacker2.length} characters`;
+    } else {
+        return `Wow, you both have equally long names, ${hacker2.length} characters`;
+    }
+}
 
 // Iteration 3: Loops
+
