fd7be59924bb612784b1140382cd01b2df8b70a2
Fede <f.gomezlara@outlook.es>
2021-10-19 17:19:45 +0200

    iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
-// 
+let hacker1 = 'Federico';
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = 'Aaron';
+console.log(`The navigator's name is ${hacker2}`);
+
+
+
 
 // Iteration 2: Conditionals
 
