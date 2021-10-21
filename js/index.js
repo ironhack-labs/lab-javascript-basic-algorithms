892798fc2625176c1c286b8b3beb0874c7a861f6
Bárbara Monzú <monzu.barbara@gmail.com>
2021-10-21 16:42:07 +0200

    pair-programming-done+bonus

--- js/index.js
+++ js/index.js
@@ -39,3 +39,22 @@ var nameReverse = "";
        } else {
          console.log("What?! You both have the same name?")
        }
+
+      
+       //BONUS
+
+       let totalWords = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer risus dui, vulputate et nisi ut, semper sagittis ex. Aenean eget purus congue, convallis erat sed, egestas nibh. Quisque nec luctus nisl, et semper dolor. Donec fringilla enim at diam euismod, ac luctus libero lacinia. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla non mi commodo dolor dictum pretium id vel erat. Maecenas sit amet sem id ligula tincidunt tempus. Maecenas vitae convallis elit.Nullam vel est consectetur, ullamcorper nibh id, suscipit ligula. In condimentum leo id porta venenatis. Aenean eleifend aliquam libero, ac vehicula nulla tincidunt eget. Phasellus pellentesque ipsum ut dui euismod, ut faucibus lacus malesuada. Fusce sollicitudin dictum ornare. Vestibulum id tristique leo. Cras pharetra cursus turpis, quis blandit velit. Suspendisse potenti. Suspendisse potenti. Duis id sem laoreet, rhoncus elit sed, faucibus augue. Donec suscipit nisi ut felis vehicula gravida. Duis tortor ligula, vestibulum et augue nec, laoreet sollicitudin nibh. Quisque congue eu nunc ac ultrices. Sed interdum, nunc sed vestibulum sollicitudin, tortor nulla efficitur elit, non ultricies neque ex vel massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque rutrum placerat nisi a dignissim. Morbi efficitur sapien turpis, sit amet congue tellus egestas quis. In maximus ullamcorper dictum. Donec posuere orci id venenatis ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempor risus pellentesque elementum consectetur. Donec quis volutpat felis"
+
+ let array = totalWords.split(" ");
+ let countWords = 0
+ let countEt = 0
+
+ for (let i = 0; i < array.length; i++) {
+countWords++  
+if (array[i] === "et") {
+  countEt++  
+} 
+       }
+
+  console.log(countWords);
+  console.log(countEt);
