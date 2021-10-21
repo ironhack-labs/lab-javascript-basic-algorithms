f3e780eda8c8e7b341a9379b9055a88c7bad9088
guidocsm <guidoignaciocrespo@gmail.com>
2021-10-21 16:50:08 +0200

    Lab done

---
+++ js/index-bono-1.js
@@ -0,0 +1,10 @@
+
+
+const parr1 = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+
+const parr2 = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+
+const parr3 = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+
+console.log( parr1.split(' ').length, parr2.split(' ').length, parr3.split(' ').length );
+
--- js/index.js
+++ js/index.js
@@ -1,7 +1,44 @@
 // Iteration 1: Names and Input
-// 
+
+const hacker1 = 'Guido';
+const hacker2 = 'Alfredo';
+
+console.log( `The driver's name is ${hacker1}` );
+console.log( `The navigator's name is ${hacker2}` );
 
 // Iteration 2: Conditionals
 
 
+if ( hacker1.length > hacker2.length ) {
+    console.log( `The driver has the longest name, it has ${hacker1.lenght} characters`)
+  } else if ( hacker2.length > hacker1.length ) {
+      console.log( `It seems that the navigator has the longest name, it has ${hacker2.length} characters` );
+    } else {
+      console.log( `Wow, you both have equally long names, ${hacker2.length} characters! `)
+    }
+  
+
 // Iteration 3: Loops
+
+
+for ( i = 0; i < hacker1.length; i++) {
+    console.log( hacker1.toUpperCase()[i] );
+  };
+
+for ( let i = hacker1.length - 1; i >= 0; i-- ) {
+    console.log( hacker1[i] )
+  }
+
+
+for ( let i = 0; i < hacker1.length; i++ ) {
+    if( hacker1[0].localeCompare( hacker2[0] ) === - 1) {
+     console.log(`The driver's name goes first.`);
+     break
+    } else if ( hacker1[0].localeCompare( hacker2[0]) === 1 ) {
+      console.log( `Yo, the navigator goes first definitely.` );
+      break
+    } else if ( hacker1[0].localeCompare( hacker2[0]) === 0 ) {
+      console.log( `What?! You both have the same name?` );
+    }
+  }
+  
