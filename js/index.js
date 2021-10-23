9843d294b3afb59181f93832e9febfdf28016748
Karina Freitas <karinaglf@gmail.com>
2021-10-23 21:38:09 +0100

    solved lab

--- js/index.js
+++ js/index.js
@@ -16,10 +16,12 @@ if(hacker1.length > hacker2.length) {
 } else if (hacker1.length < hacker2.length) {
     console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
 } else if (hacker1.length = hacker2.length) {
-    console.log (`Wow, you both have equally long names, XX characters!`)
+    console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
 
 // Iteration 3: Loops
+
+// 3.1 - Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
 
 let withSpace = '';
 
@@ -29,6 +31,8 @@ for(let i=0; i<hacker1.length; i++){
 
 console.log(withSpace.toUpperCase());
 
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
 let reverseName = '';
 
 for (let i = hacker2.length - 1; i >= 0; i--) { 
@@ -37,11 +41,13 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 
 console.log(reverseName);
 
+// 3.3 Depending on the lexicographic order of the strings, print:
+
 if(hacker1 < hacker2) {
     console.log (`The driver's name goes first.`)
 } else if (hacker1 > hacker2) {
     console.log (`Yo, the navigator goes first definitely.`)
-} else if (hacker1 = hacker2) {
+} else if (hacker1 === hacker2) {
     console.log (`What?! You both have the same name?`)
 }
 
@@ -50,12 +56,12 @@ if(hacker1 < hacker2) {
 let loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed nulla pretium nunc porttitor interdum et ultrices velit. Sed venenatis rhoncus dolor, et auctor erat malesuada at. In eu massa semper, iaculis turpis ut, tempus dolor. Maecenas iaculis feugiat sollicitudin. Sed vulputate ornare neque, non semper ligula ultrices nec. Donec ac luctus nunc, id finibus nulla. Quisque in pulvinar ipsum, in luctus urna.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam vel aliquet metus, ac pharetra eros. Pellentesque tempus sem efficitur viverra sollicitudin. Praesent nec dolor nibh. Integer ac purus accumsan ipsum faucibus accumsan eu ac massa. Cras in arcu at urna tempus laoreet. Ut at vulputate libero. Nullam viverra libero nec lobortis dignissim. Pellentesque sem dui, hendrerit id sollicitudin eu, maximus non dolor. Nulla vel magna ante. Etiam malesuada justo id nunc egestas, quis rutrum turpis eleifend. Proin et enim ornare, rutrum lacus ac, fermentum nibh. Suspendisse sit amet ultricies lectus. Donec laoreet fermentum mauris eget ultricies. Sed eu consectetur urna.In semper risus feugiat ex suscipit porttitor. Proin ornare ultrices tincidunt. Sed vel metus eu mauris aliquam hendrerit. Nullam sodales finibus odio sed rhoncus. Donec ante quam, porttitor nec quam vehicula, egestas bibendum libero. Duis quis urna nisl. Nam fringilla magna vel est sodales faucibus. Aenean sit amet mauris vulputate, dignissim nisi id, porta turpis. Donec ut viverra odio, sit amet molestie magna.";
 
 let loremWords = loremText.split(" ")
-console.log(loremWords.length);
+console.log("Words: " + loremWords.length);
 
 function countOccurences(string, word) {
     return string.split(word).length - 1;
 }
-console.log(countOccurences(loremText, 'et'));
+console.log("Et: " + countOccurences(loremText, 'et'));
 
 // Bonus 2
 
