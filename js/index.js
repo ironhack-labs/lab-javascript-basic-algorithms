20d19a23dd39efc1afe77795d18ecbe917460a9b
Martin Giura <martin.giura@gmail.com>
2021-10-25 17:31:30 -0500

    includes iteration2 complete.

--- js/index.js
+++ js/index.js
@@ -8,5 +8,23 @@ console.log (`The drivers name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
+function longerName(driver,navigator) {
+
+    const hacker1 = driver;
+    const hacker2 = navigator;
+    
+    if (hacker1.length > hacker2.length)
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+    
+    if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker2.length}  characters!.`)
+    }
+    
+    if (hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+      }
+    }
+    
+    console.log(longerName("Miguel", "Martin"))
 
 // Iteration 3: Loops
