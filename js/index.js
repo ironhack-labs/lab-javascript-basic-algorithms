2f6f31f9ee060753cbdd8efda3314b2eb634ca6f
Martin Giura <martin.giura@gmail.com>
2021-10-25 17:08:38 -0500

    Iteration1, iteration2 pending with Miguel

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
-// 
+// Iteration 1
+
+let hacker1 = "Miguel"
+console.log (`The drivers name is ${hacker1}`)
+let hacker2 = "Martin Giura"
+console.log (`The drivers name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
