9547a546841c19592974adc32e2906b2d35dc77a
Edgar Uco <edgarin@gmail.com>
2021-10-25 17:28:17 -0500

    Lab-Javascript basic algorithms

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
 // Iteration 1: Names and Input
 // 
+const HACKER1 = 'Hector';
+console.log(`The driver name is ${HACKER1}`);
+
+const HACKER2 = 'Edgar';
+console.log(`The navigator name is ${HACKER2}`);
 
 // Iteration 2: Conditionals
-
+const HACKER1 = 'Hector';
+const HACKER2 = 'Edgar';
+if (HACKER1.length === HACKER2.length) {
+    console.log(
+        `Wow, you both have equally long names, ${HACKER1.length} characters!`,
+    );
+} else if (HACKER1.length > HACKER2.length) {
+    console.log(
+        `The driver has the longest name, it has ${HACKER1.length} characters.`,
+    );
+} else {
+    console.log(
+        `It seems that the navigator has the longest name, it has ${HACKER2.length} characters.`,
+    );
+}
 
 // Iteration 3: Loops
