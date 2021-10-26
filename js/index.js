e393f54c69389af3e122fa23c5c08faffc3b3fd1
Edgar Uco <edgarin@gmail.com>
2021-10-26 10:53:36 -0500

    Lab1 third exercise

--- js/index.js
+++ js/index.js
@@ -24,3 +24,41 @@ if (HACKER1.length === HACKER2.length) {
 }
 
 // Iteration 3: Loops
+
+/* 3.1 Print all the characters of the driver's name, separated by a space and [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
+i.e. `"J O H N"` */
+  
+const HACKER1 = 'Hector';
+const HACKER2 = 'Edgar';
+let newName = '';
+for (i = 0; i < HACKER1.length; i++) {
+    newName = newName + HACKER1[i] + ' ';
+}
+console.log(newName);
+
+
+ // 3.2 Print all the characters of the navigator's name, in reverse order. 
+//i.e. `"nhoJ"`
+  
+const HACKER1 = 'Hector';
+const HACKER2 = 'Edgar';
+let newName = ' ';
+
+for (i = HACKER2.length - 1; i >= 0; i--) {
+    newName = newName + HACKER2[i];
+}
+
+console.log(newName);
+
+
+//3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
+const HACKER1 = "Hector";
+const HACKER2 = "Edgar";
+let newName = "";
+if (HACKER1.toLocaleLowerCase() < HACKER2.toLocaleLowerCase()) {
+      console.log(`The driver's name goes first.`);
+} else if (HACKER1.toLocaleLowerCase() > HACKER2.toLocaleLowerCase()) {
+  console.log(`Yo, the navigator goes first definitely.`);
+} else {
+  console.log(`What?! You both have the same name?`);
+}
