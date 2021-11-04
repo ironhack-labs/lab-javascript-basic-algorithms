fb0211f7154690a2a6b8df62bb2464520588b052
Michael Foley <foley.p.michael@gmail.com>
2021-11-04 21:42:05 +0000

    finished second iteration, third iterations still WIP

--- js/index.js
+++ js/index.js
@@ -2,14 +2,37 @@
 // 
 let hacker1 = "Michael";
 
-console.log("The driver's name is" + hacker1);
+console.log("The driver's name is " + hacker1);
 
 let hacker2 = "Diogo";
 
-console.log("The navigator's name is" + hacker2);
+console.log("The navigator's name is " + hacker2);
 
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log("The driver has the longest name, it has " + hacker1.length + " characters");
+} else if (hacker2.length > hacker1.length) {
+ console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+} else {
+    console.log("Wow,  you both have equally long names, " + hacker1.length + " characters!" );
+}
 
 // Iteration 3: Loops
+let driverNameUppercase = ""; 
+for (let i = 0; i < hacker1.length; i++) {
+  driverNameUppercase += hacker1[i].toUpperCase()+ " "; 
+}
+console.log(driverNameUppercase);
+
+let navNameReverse = ""; 
+for (let i = hacker2.length-1; i >= 0; i--) {
+  navNameReverse += hacker2[i]; 
+}
+console.log(navNameReverse);
+
+if ("a" > "b"){
+  console.log("True")
+}
+
+
