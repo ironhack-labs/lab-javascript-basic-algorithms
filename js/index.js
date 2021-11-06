fbf811b8c77784d771e5ff22d03f066089cdea29
Yaiza Rubio <yaizarubio@Yaizas-MacBook-Pro.local>
2021-11-06 12:33:31 +0100

    names and input

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 // Iteration 1: Names and Input
-// 
+
+let hacker1;
+let hacker2;
+
+hacker1 = "Pepe";
+hacker2 = "Maria";
+
+console.log(hacker1);
+console.log("The driver's name is", hacker1);
+
+console.log(hacker2);
+console.log("The driver's name is", hacker2);
+
+// Iteration 2: Conditionals
+
+
+// Iteration 3: Loops
 
 // Iteration 2: Conditionals
 
