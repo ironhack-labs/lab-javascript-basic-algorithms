74c908b401875eeeb2db4092914b2880fddfd386
Calvin T <58273749+Calvin087@users.noreply.github.com>
2021-11-06 14:25:04 +0100

    lexograph done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Iteration 1: Names and Input
-let hacker1 = "Ale";
+let hacker1 = "Calvin";
 console.log(`The driver's name is ${hacker1}`);
 let hacker2 = "Ale";
 console.log(`The navigator's name is ${hacker2}`);
@@ -21,19 +21,27 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
-let tempString=hacker1.split("");
-let tempArray=[];
+let tempString = hacker1.split("");
+let tempArray = [];
 
-for (let i=0; i<hacker1.length; i++){
-  tempArray.push(hacker1[i].toUpperCase())
+for (let i = 0; i < hacker1.length; i++) {
+  tempArray.push(hacker1[i].toUpperCase());
 }
 
 console.log(tempArray.join(" "));
 
-let tempString2=hacker2.split("");
-let tempArray2=[];
+let tempString2 = hacker2.split("");
+let tempArray2 = [];
 
-for (let j=hacker2.length -1; j>=0; j--){
-  tempArray2.push(tempString2[j])
+for (let j = hacker2.length - 1; j >= 0; j--) {
+  tempArray2.push(tempString2[j]);
 }
-console.log(tempArray2.join(""))
+console.log(tempArray2.join(""));
+
+if (hacker1 < hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker2 < hacker1) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
