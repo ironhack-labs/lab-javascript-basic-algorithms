57efe0382452a8d132174ffced3806165e83a73d
Isa Gutierrez <isagr_kio@hotmail.com>
2021-11-06 13:02:33 +0100

    Coded iterations 1-3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
-// Iteration 1: Names and Input
-// Test
+// Iteration 1: Names and Input - Initialize variables with our names
 
-// Iteration 2: Conditionals
+console.log(`I'm ready!`)
 
+let hacker1 = "Eduardo"
+let hacker2 = "Isa"
 
-// Iteration 3: Loops
+console.log(`The driver's name is ${hacker1}.`);
+console.log (`The navigator's name is ${hacker2}.`);
+
+// Iteration 2: Conditionals - Check which name is longer
+
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} letters.`)
+} else if (hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+};
+
+// Iteration 3: Loops 
+
+  // 1. Print all the characters of the driver's name, separated by a space and in capitals
+
+let lettersInName1 = hacker1.split("")
+
+for (let i = 0; i < lettersInName1.length; i++) {
+  lettersInName1[i] = lettersInName1[i].toUpperCase() + ' '
+  console.log(lettersInName1[i])
+}
+
+console.log(lettersInName1.join(""));
+
+// 2. Print all the characters of the navigator's name, in reverse order
+
+let lettersInName2 = hacker2.split("");
+console.log(lettersInName2.reverse().join(""));
+
