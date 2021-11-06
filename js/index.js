e9c1f65b3d28833a0e0f2d5a99b17469e0147b71
“Giovanni” <giovannitakeshisesta@gmail.com>
2021-11-06 15:40:34 +0100

    lorem para acabar

--- js/index.js
+++ js/index.js
@@ -49,3 +49,33 @@ if (lexicographic === -1){
 } else {
   console.log("What?! You both have the same name?");
 }
+
+
+
+
+/* para terminar en casa */ 
+let lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt, nisi ac suscipit dictum, ante urna accumsan libero, eu tincidunt ipsum neque nec mi. Nam at ante velit. Donec id tempus dui. Donec sit amet sodales massa. Suspendisse porttitor, nulla non tincidunt mattis, justo quam pharetra sapien, rhoncus ultricies nibh elit id ligula. Quisque pulvinar aliquam vulputate. Cras sed dui id neque euismod porttitor. Donec sit amet leo nec sem tempor efficitur et quis nibh. Donec in libero nec eros consectetur accumsan. Pellentesque luctus, felis non vulputate suscipit, metus erat molestie est, quis congue ligula nunc eu magna. Donec congue velit in congue commodo. Cras quis sollicitudin justo, varius semper tortor. Aenean ut tincidunt arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Pellentesque sed augue cursus, cursus nunc at, volutpat lorem. Donec vel mattis arcu, in semper magna. Suspendisse mattis mattis purus, a malesuada neque. Etiam feugiat dignissim nisl ac vulputate. Sed pharetra pharetra est non congue. Quisque turpis enim, ornare ut pharetra in, fermentum eget nisi. Suspendisse hendrerit enim vitae felis ultrices, id fringilla justo euismod. Quisque venenatis augue sit amet elit tincidunt, nec tristique quam pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus id lectus condimentum, accumsan mi non, fermentum ante. Vivamus volutpat lorem tellus, eget auctor arcu pretium at. Sed venenatis orci nec felis feugiat congue.
+
+Nunc tempor massa in velit laoreet gravida. Mauris interdum dolor ac enim sagittis, et fringilla lacus gravida. Aenean dapibus elit aliquet, consectetur risus ut, blandit nisl. Nunc interdum eu enim vel cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras elementum sapien rutrum, semper sem ut, pretium ante. Aenean rhoncus augue sit amet pretium blandit. Aenean massa lorem, auctor vitae lectus vel, malesuada pharetra augue. Nam nec velit ac ipsum porttitor scelerisque. Pellentesque vel mauris nisl. Nunc ut tempor metus. Sed commodo neque sed sapien posuere euismod.`;
+
+const textoArray = lorem.split(" ")
+//console.log(textoArray)
+let counter = 0;
+let countersalto = 0;
+
+for (let i = 0; i< textoArray.length; i++){
+  
+  //console.log(textoArray[i])
+  if (textoArray[i].includes("\n")){
+    counter +=1
+    countersalto++
+  }
+  counter = i
+}
+
+console.log(counter,countersalto) 
+/*if (textoArray[i].includes("\n")){
+    counter++
+  } */
