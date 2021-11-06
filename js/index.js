f37d74b8cbda49bf5e7fd8b316203f204859f075
Eduardo Francos Margareto <efmargareto@hotmail.com>
2021-11-06 15:33:05 +0100

    Bonus: clean code

--- js/index.js
+++ js/index.js
@@ -52,36 +52,37 @@
             console.log('What?! You both have the same name?')
         }
 
-// Bonus 1 - Lorem Ipsum
 
-const stringLoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id ultricies augue. Ut non laoreet turpis. Pellentesque et nisl orci. Vestibulum luctus quam a est finibus blandit. Praesent efficitur lacinia sapien sit amet vulputate. Aliquam ut lacinia magna, in elementum felis. Aliquam eleifend dui nisi, vitae finibus sapien aliquet a. Suspendisse scelerisque purus faucibus orci pulvinar, a egestas lacus scelerisque. Sed elit nulla, mollis id arcu id, egestas suscipit turpis. Pellentesque at blandit nulla. Praesent eget ultricies dui, non imperdiet lacus. Quisque velit purus, hendrerit in ante id, malesuada hendrerit nisi. Vestibulum nec sapien non ipsum vulputate imperdiet a ut arcu. Nam ut faucibus augue. Vivamus turpis leo, convallis in cursus et, pulvinar quis tellus. Fusce dignissim lacus non justo bibendum, vitae sollicitudin diam fringilla. Vestibulum volutpat orci quis libero interdum, ut gravida enim congue. Vestibulum hendrerit est eu dolor luctus, rhoncus tristique ipsum vulputate. Sed justo libero, porttitor in interdum ut, congue nec sem. Nullam ut tellus urna. Praesent vehicula, turpis a pharetra facilisis, turpis magna porta lorem, non laoreet nulla augue in sapien. Etiam congue eu metus eget ultricies. Quisque rhoncus consectetur magna posuere gravida. Etiam rhoncus dui eros, sed pharetra tellus finibus nec. Sed in tortor bibendum, blandit urna sed, maximus dui. Cras non ultrices libero, in volutpat tortor. Nam sed justo a turpis pulvinar porta. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec dignissim velit sed sem congue hendrerit. Nullam bibendum nibh erat, sed faucibus arcu euismod a. Quisque eget sollicitudin diam. Sed egestas interdum urna vitae mattis. Quisque eget lacus sit amet dolor dignissim iaculis a non ante. Aliquam convallis nec metus vel condimentum. Nulla facilisi. Vestibulum at posuere ante, a rhoncus mauris. Aenean nec arcu ultrices, lacinia nisi non, tincidunt libero. Vivamus maximus mauris et lobortis elementum. Duis ut velit ac dui facilisis finibus nec fringilla purus."
+// BONUS
 
-let numberOfWords = stringLoremIpsum.split(" ").length
+    // Bonus 1 - Lorem Ipsum
 
-console.log(numberOfWords);
+        const stringLoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id ultricies augue. Ut non laoreet turpis. Pellentesque et nisl orci. Vestibulum luctus quam a est finibus blandit. Praesent efficitur lacinia sapien sit amet vulputate. Aliquam ut lacinia magna, in elementum felis. Aliquam eleifend dui nisi, vitae finibus sapien aliquet a. Suspendisse scelerisque purus faucibus orci pulvinar, a egestas lacus scelerisque. Sed elit nulla, mollis id arcu id, egestas suscipit turpis. Pellentesque at blandit nulla. Praesent eget ultricies dui, non imperdiet lacus. Quisque velit purus, hendrerit in ante id, malesuada hendrerit nisi. Vestibulum nec sapien non ipsum vulputate imperdiet a ut arcu. Nam ut faucibus augue. Vivamus turpis leo, convallis in cursus et, pulvinar quis tellus. Fusce dignissim lacus non justo bibendum, vitae sollicitudin diam fringilla. Vestibulum volutpat orci quis libero interdum, ut gravida enim congue. Vestibulum hendrerit est eu dolor luctus, rhoncus tristique ipsum vulputate. Sed justo libero, porttitor in interdum ut, congue nec sem. Nullam ut tellus urna. Praesent vehicula, turpis a pharetra facilisis, turpis magna porta lorem, non laoreet nulla augue in sapien. Etiam congue eu metus eget ultricies. Quisque rhoncus consectetur magna posuere gravida. Etiam rhoncus dui eros, sed pharetra tellus finibus nec. Sed in tortor bibendum, blandit urna sed, maximus dui. Cras non ultrices libero, in volutpat tortor. Nam sed justo a turpis pulvinar porta. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec dignissim velit sed sem congue hendrerit. Nullam bibendum nibh erat, sed faucibus arcu euismod a. Quisque eget sollicitudin diam. Sed egestas interdum urna vitae mattis. Quisque eget lacus sit amet dolor dignissim iaculis a non ante. Aliquam convallis nec metus vel condimentum. Nulla facilisi. Vestibulum at posuere ante, a rhoncus mauris. Aenean nec arcu ultrices, lacinia nisi non, tincidunt libero. Vivamus maximus mauris et lobortis elementum. Duis ut velit ac dui facilisis finibus nec fringilla purus."
+        let numberOfWords = stringLoremIpsum.split(" ").length
+
+        console.log(numberOfWords);
+
+        const arrayLoremIpsum = stringLoremIpsum.split(" ")
+
+        let numberOfEt = 0
+        for (let i = 0; i < arrayLoremIpsum.length; i++) {
+            if (arrayLoremIpsum[i].includes("et")) {
+                numberOfEt++
+            }
+        }
+
+        console.log(numberOfEt)
 
 
-const arrayLoremIpsum = stringLoremIpsum.split(" ")
+    // Bonus 2 - Palindromes
 
-let numberOfEt = 0
-for (let i = 0; i < arrayLoremIpsum.length; i++) {
-  if (arrayLoremIpsum[i].includes("et")) {
-    numberOfEt++
-  }
-}
+        let palindrome1 = "Amor, Roma"
 
-console.log(numberOfEt)
-
-
-// Bonus 2 - Palindromes
-
-let palindrome1 = "Amor, Roma"
-
-let stringPalindrome = palindrome1.toLowerCase().replace(/\s/g, "").replace(/[^\w\s]|_/g, "");
-let stringPalindromeReverse = palindrome1.split("").reverse().join("").toLowerCase().replace(/\s/g, "").replace(/[^\w\s]|_/g, "");
-  
-  if (stringPalindrome === stringPalindromeReverse) {
-    console.log("It is a palindrome")
-  } else {
-    console.log("It is not a palindrome")
-  }
+        let stringPalindrome = palindrome1.toLowerCase().replace(/\s/g, "").replace(/[^\w\s]|_/g, "");
+        let stringPalindromeReverse = palindrome1.split("").reverse().join("").toLowerCase().replace(/\s/g, "").replace(/[^\w\s]|_/g, "");
+        
+        if (stringPalindrome === stringPalindromeReverse) {
+            console.log("It is a palindrome")
+        } else {
+            console.log("It is not a palindrome")
+        }
