d73474dfb548e7d37fff69bad5373ae2e8ffd063
Arturo Vivar <arthas1237@hotmail.com>
2021-11-06 14:30:39 +0100

    Bonus1

--- js/index.js
+++ js/index.js
@@ -70,3 +70,45 @@ else {
 console.log(`"What?! You both have the same name?"`)
 
 }
+
+
+// Bonus 1.
+
+
+const paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fringilla lectus id arcu accumsan mollis. Fusce vel facilisis nisi. Phasellus congue enim orci, eget dictum felis accumsan at. Nullam lectus est, vulputate ac suscipit at, fermentum id ipsum. Vestibulum mi nunc, hendrerit vel congue non, rutrum ac elit. Curabitur ut arcu placerat, venenatis sapien at, tincidunt nunc. Maecenas in tristique augue, vel viverra mi. Aenean a enim nunc. Vestibulum sit amet sollicitudin est, sit amet fringilla felis. Quisque eu velit tortor. Sed eu mi nec lacus dictum ultricies. Sed est nunc, ullamcorper quis iaculis at, imperdiet sed purus. Quisque commodo magna nunc, sit amet scelerisque tortor tristique sit amet.
+
+Cras ultrices ornare urna non iaculis. Morbi ac aliquet est. Ut neque elit, consequat vel aliquet eget, vestibulum mollis enim. Suspendisse tempor nibh in condimentum convallis. In eu nisi sit amet erat commodo pharetra. Cras consequat nibh sit amet ornare fermentum. Nulla pellentesque neque id nulla sollicitudin, a hendrerit augue dictum. Sed tempor sagittis enim, eget elementum velit vestibulum eu.
+
+Nulla a dolor pharetra, laoreet odio ut, ullamcorper massa. Nullam tristique ligula nulla. Aliquam luctus suscipit nisi in volutpat. Praesent a felis consequat, ornare magna id, dictum lorem. Donec facilisis turpis ut ligula dapibus, at lacinia libero euismod. Sed vitae nibh mi. Nunc in arcu metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada maximus diam, ultrices faucibus magna pellentesque vitae. Fusce fermentum blandit quam quis viverra. In nisl arcu, tempus nec sollicitudin vel, vulputate sed ante.` 
+
+
+
+
+
+let words = 0;
+let etCount = 0;
+
+
+let splittedParagraph = paragraph.split(' ');
+
+
+for (let i = 0; i< splittedParagraph.length; i++) {
+
+  if(splittedParagraph[i].includes('\n')) {
+    words++
+  }
+  
+  if(splittedParagraph[i] === "et") {
+etCount++;
+
+
+
+  }
+
+
+
+ words++; 
+}
+
+console.log('words counter =', words);
+console.log('et counter =', etCount);
