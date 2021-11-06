472ece5617d7f1bdf7b6c8540d7088e54e11458c
“Giovanni” <giovannitakeshisesta@gmail.com>
2021-11-06 14:39:05 +0100

    add iteration 3.3

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 // Iteration 1: Names and Input
 /*1.1 Create a variable `hacker1` with the driver's name.
 1.2 Print `"The driver's name is XXXX"`. */
-let hacker1 = "Giovanni";
+let hacker1 = "Oscar";
 console.log(`"The driver's name is ${hacker1}."`);
 
 /*
 1.3 Create a variable `hacker2` with the navigator's name.
 1.4 Print `"The navigator's name is YYYY"`. */
-let hacker2 = "Oscar";
+let hacker2 = "Giovanni";
 console.log(`"The navigator's name is ${hacker2}."`);
 
 // Iteration 2: Conditionals
@@ -36,3 +36,16 @@ for ( let i = hacker2.length -1; i >= 0; i--) {
 }
 
 
+/*3.3 Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name? */
+let lexicographic = hacker1.localeCompare(hacker2);
+console.log(lexicographic);
+if (lexicographic === -1){
+  console.log("The driver's name goes first.");
+} else if (lexicographic === 1){
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
