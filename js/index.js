0b9681611e6fa9a0f8a4a21bd5e34939fd125503
Pablo Mateos <pmateos805@gmail.com>
2021-11-06 12:19:25 +0100

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-// new code
+
+let hacker1 = 'Pablo';
+console.log(`"The driver's name is ${hacker1}"`);
+let hacker2 = 'Vicente';
+console.log(`"The navigator's name is ${hacker2}"`);
 
 // Iteration 2: Conditionals
 
