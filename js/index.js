8ebe27edeebc7c8d4b677adc05bbee55a2e6d2a0
Yaiza Rubio <yaizarubio@Yaizas-MacBook-Pro.local>
2021-11-06 13:07:21 +0100

    conditionals

--- js/index.js
+++ js/index.js
@@ -14,6 +14,22 @@ console.log("The driver's name is", hacker2);
 
 // Iteration 2: Conditionals
 
+let name1;
+let name2;
+
+name1 = hacker1.length;
+console.log(name1);
+
+name2 = hacker2.length;
+console.log(name2);
+
+if (name1 > name2){
+  console.log(`The driver has the longest name, it has ${name1} characteres`);
+} else if (name1 < name2){
+  console.log(`It seems that the navigator has the longest name, it has ${name2} characteres`);
+} else {
+  console.log(`Wow, you both have equally long name ${name1} characteres`);
+}
 
 // Iteration 3: Loops
 
