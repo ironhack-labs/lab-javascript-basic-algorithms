0c34d2d56538647d28e82e61be5d793740704df8
Pablo Mateos <pmateos805@gmail.com>
2021-11-06 14:32:35 +0100

    Bonus 1 correction

--- js/index.js
+++ js/index.js
@@ -45,6 +45,12 @@ if(hacker1.localeCompare(hacker2) < 0 ) {
 
 //BONUS 1
 
+let paragraphs = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac tempus turpis, eu consectetur odio. Curabitur molestie, sem et dictum facilisis, sapien ante aliquam diam, nec interdum dolor libero eget sapien. Integer viverra blandit lorem eu feugiat. Duis blandit ligula mi, ut molestie leo mattis ac. Quisque fringilla odio sed odio bibendum tempor sed ut turpis. Praesent eget turpis magna. Nam molestie rutrum mauris vel vulputate. Aenean lacinia auctor lectus sed mattis. Fusce eu velit faucibus, fermentum massa eget, dignissim libero. Donec ullamcorper leo arcu, tincidunt luctus sapien tristique at. Proin cursus fermentum finibus. Vivamus porttitor vitae velit et dignissim. Aenean hendrerit, nunc id lacinia tempus, augue elit suscipit lacus, vel sollicitudin nisi velit a diam. 
+
+Proin eget urna non lacus viverra congue. Ut eu est finibus turpis fermentum pretium. Integer vitae urna fermentum nisi tristique interdum. Morbi sodales lorem urna, quis porttitor purus sodales vel. Sed sit amet posuere lectus. Vivamus aliquam tristique consectetur. Quisque tempus suscipit tempus. Vivamus commodo nunc et massa elementum maximus. Aliquam orci nisl, consectetur ac interdum mattis, suscipit at orci. Aliquam erat volutpat. Integer diam neque, lobortis sed pulvinar ut, aliquam quis dolor. Nam pretium massa lobortis diam porttitor ultricies. Sed interdum velit nec mauris fringilla, at faucibus lorem ultricies. Duis tincidunt turpis nec consequat porta.
+
+Aliquam in nunc sit amet nibh congue iaculis. Donec ullamcorper nulla ac mollis tincidunt. Quisque auctor, odio nec facilisis suscipit, risus quam imperdiet justo, nec elementum ante justo ut justo. Pellentesque luctus nulla eu lectus lobortis, et feugiat velit scelerisque. Donec sollicitudin libero vel felis vulputate, vitae rutrum odio ornare. Proin blandit magna a arcu convallis aliquam. Donec mollis vehicula mi id iaculis. Cras augue tortor, porta nec dui et, varius porttitor tortor. Quisque imperdiet nisl non nisl interdum, a convallis orci venenatis. Nulla ornare massa lectus, eget feugiat felis gravida in. Donec sed faucibus tortor. Mauris consequat sapien sed neque gravida facilisis. Quisque ut tellus mattis tellus fermentum finibus non mattis neque.`;
+
 paragraphs = paragraphs.split(' ');
 
 function countWords(arr) {
