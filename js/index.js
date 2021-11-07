a13c4535f4b0afa75d1e17df659c4d83879965e9
Juan Carlos Saldaña Herrero <jcarlossaldae@gmail.com>
2021-11-08 00:26:31 +0100

    iteration 3

--- js/index.js
+++ js/index.js
@@ -21,3 +21,24 @@ if (driverLenght > navigatorLenght) {
 } else if (navigatorLenght === driverLenght) {
     console.log(`Wow, you both have equally long names, ${driverLenght} characters!`)
 }
+
+// Iteration 3: Loops
+
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+console.log(hacker1.split("").join(" ").toUpperCase())
+
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+console.log(hacker1.split("").reverse().join(""))
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+
+
+const hackerLengthComparison = hacker1.localeCompare(hacker2)
+
+if (hackerLengthComparison < 0) {
+    console.log(`The driver's name goes first`)
+} else if (hackerLengthComparison > 0) {
+    console.log(`Yo, the navigator goes first definitely`)
+} else if (hackerLengthComparison === 0) {
+    console.log(`What?! You both have the same name?`)
+}
