a4ecf8a9d9cad87ff90d33e05fc86a6caeacaf2b
Juan Carlos Saldaña Herrero <jcarlossaldae@gmail.com>
2021-11-08 00:27:09 +0100

    Bonus

--- js/index.js
+++ js/index.js
@@ -42,3 +42,48 @@ if (hackerLengthComparison < 0) {
 } else if (hackerLengthComparison === 0) {
     console.log(`What?! You both have the same name?`)
 }
+
+
+
+
+
+
+// Bonus 1
+
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dictum imperdiet felis, ut hendrerit turpis ullamcorper et. Quisque vel volutpat dolor. Pellentesque feugiat tellus sed nunc bibendum, euismod consectetur diam porta. Donec aliquam risus sed eros pretium, facilisis iaculis elit scelerisque. Aenean et porta odio, sit amet interdum ex. Suspendisse interdum lectus in sodales suscipit. Vivamus non leo rutrum, facilisis nulla sed, posuere urna. Nulla dapibus felis magna, ac pulvinar sem vehicula et. Proin ut condimentum orci.
+
+Nullam a consectetur nisi. Vestibulum nunc mi, gravida et vestibulum vitae, lacinia ut velit. Aliquam pharetra suscipit lacus quis ullamcorper. Phasellus sed felis gravida, feugiat metus sed, hendrerit ex. Duis faucibus faucibus diam, et ullamcorper purus faucibus a. Aliquam mauris sapien, luctus in diam sit amet, facilisis luctus sapien. Ut ut molestie sem, in sagittis urna. Donec quis interdum neque.
+
+Nam varius est lacus, at imperdiet nunc dictum vel. Integer ut augue consequat, feugiat sem vitae, congue ipsum. Fusce rutrum, arcu eget suscipit volutpat, nisi dolor molestie sapien, ac ornare ante risus a velit. Suspendisse bibendum lorem quis orci vehicula, non tempus augue semper. Donec justo sapien, aliquet a arcu nec, efficitur lacinia eros. Donec semper tortor at arcu elementum ultricies. Morbi porttitor, est eget pharetra imperdiet, sem nisl faucibus eros, eget dictum tellus urna nec tellus. Phasellus id consectetur tellus, vitae bibendum urna. Suspendisse potenti. Quisque imperdiet sed elit sit amet viverra. Duis a dui elit. Duis nec varius justo. Suspendisse consequat magna id ex hendrerit tempus.`
+
+const wordsLenght = text.split(" ").length
+console.log(wordsLenght)
+
+const etCount = text.split("et").length - 1
+console.log(etCount)
+
+// Bonus 2
+
+const phraseToCheck = "redivider"
+
+let forwardCheck = ""
+let backwardsCheck = ""
+
+
+for (let i = 0; i < phraseToCheck.length; i++) {
+    forwardCheck += phraseToCheck[i]
+}
+
+for (let i = phraseToCheck.length - 1; i >= 0; i--) {
+    backwardsCheck += phraseToCheck[i]
+}
+
+let result
+
+if (forwardCheck === backwardsCheck) {
+    result = "It's a palindrome!"
+} else {
+    result = "It's not palindrome..."
+}
+
+console.log(result)
