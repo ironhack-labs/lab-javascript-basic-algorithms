9474bab910ce30e794087656ad82939d2f14cd01
Kelly Morelatti <kelly.morelatti@gmail.com>
2021-11-12 00:21:36 -0300

    Iteration 3 done

--- js/index.js
+++ js/index.js
@@ -15,3 +15,22 @@ if (hacker1.length > hacker2.length){
 }
 
 // Iteration 3: Loops
+let result = '';
+for (let i=0; i < hacker1.length; i += 1){
+    result += hacker1[i].toUpperCase() + ' ';
+}
+console.log(result);
+
+let result2 = "";
+for (let i = hacker2.length - 1; i >= 0; i -= 1){
+    result2 += hacker2[i];
+}
+console.log(result2);
+
+if (hacker1 < hacker2){
+    console.log("The driver's name goes first.");
+} else if (hacker2 < hacker1){
+    console.log("Yo, the navigator goes first definetely.");
+}else{
+    console.log("What?!! You both have the same name?");
+}
