d6c23759fb6f0c4b30ff6dfff2015718a6323db4
Lucas Mendes <lucas.gabriel3.lg@gmail.com>
2021-11-11 22:03:11 -0300

    This is the commit of the first Interation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
-// 
+
+let hacker1 = 'John';
+
+console.log(`"The driver's name is ${hacker1}"`);
+
+let hacker2 = 'Lisa';
+
+console.log(`"The navigator's name is ${hacker2}"`)
+
 
 // Iteration 2: Conditionals
 
