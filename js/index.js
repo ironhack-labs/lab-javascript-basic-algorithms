e23b45ebe60566ef7a433a0eaaa51e269ca19965
Eduardo Brandiao <eduardo.brandiao@al.infnet.edu.br>
2021-11-12 20:25:13 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 // Iteration 1: Names and Input
+//hacker
+let hacker1 = "Eduardo Brandiao"
+let hacker2 = "Nome nome nome nome"
 
-console.log("I'm ready!");
-""
+const hacker1Sentence = "The driver´s name is" + ' ' + hacker1;
+const hacker2Sentence = "The driver´s name is" + ' ' + hacker2;
+
+console.log(hacker1Sentence);
+console.log(hacker2Sentence);
+
 // Iteration 2: Conditionals
+const hacker1length = hacker1.length;
+const hacker2length = hacker2.length;
 
+if (hacker1length > hacker2length) {
+    console.log(`The driver has the longest name, it has ${hacker1length} characters.`)
+} else if (hacker2length > hacker1length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2length} charecters.`)
+} else {
+    console.log(`Wow, you bath have equally long names, ${hacker1length} charecters!`)
+}
 
 // Iteration 3: Loops
