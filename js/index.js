82e3da0ec1b99fb4891846bb8c2549ea3816203a
Kelly Morelatti <kelly.morelatti@gmail.com>
2021-11-11 23:36:38 -0300

    Iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Iteration 1: Names and Input
-// 
+let hacker1 = 'Kelly';
+console.log(`The driver's name is ${hacker1}.`);
 
+let hacker2 = "Jucimeire";
+console.log(`The navigator's name is ${hacker2}.`);
 // Iteration 2: Conditionals
 
 
