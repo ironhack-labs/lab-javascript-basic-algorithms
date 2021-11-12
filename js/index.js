fe38011174e9a7a5768025c10ea78f9154b78cb4
Felipe Leite Mantovani <mantovani.devilz@gmail.com>
2021-11-11 21:56:33 -0300

    Feita primeira interação

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Iteration 1: Names and Input
 // 
-
+let hacker1 = felipe;
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = alguém;
+console.log(`The navigator name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
