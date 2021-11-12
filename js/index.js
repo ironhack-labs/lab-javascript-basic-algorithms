0e00a86cb5adac59ea0d4d3a125df0fe98156e4a
Jucimeire Silva <jucimeiresilva@hotmail.com>
2021-11-12 17:19:04 -0300

    O que foi feito

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
-console.log("teste")
+let hacker1 = 'Jucimeire'; 
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = 'Kelly';
+console.log(`The navigator's name is ${hacker2}`);
+
 
 // Iteration 2: Conditionals
 
