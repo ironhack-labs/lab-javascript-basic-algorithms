131109589b1e3af56acc80dbbac96b932435eb52
Lora Horst <lora.horst@t-online.de>
2021-11-13 09:29:36 +0100

    "tried to solve the exercises"

--- js/index.js
+++ js/index.js
@@ -1,7 +1,49 @@
 // Iteration 1: Names and Input
-// 
+
+
+let hacker1 = 'Thomas';
+console.log(`The driver's name is ${hacker1}.`)
+
+let hacker2 = 'Carlo';
+console.log(`The navigator's name is ${hacker2}.`)
+
+
 
 // Iteration 2: Conditionals
 
 
+let length1 = hacker1.length;
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${length1} characters.`);
+}
+
+let length2 = hacker2.length;
+if (hacker1.length < hacker2.length) {
+    console.log(`It seemes the navigator has the longest name, it has ${length2} characters.`);
+}
+
+if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equalle long names, ${length2} chracters!`);
+}
+
+
+
 // Iteration 3: Loops
+
+//a
+
+hacker1.toUpperCase();
+console.log(` {$hacker1} `)
+
+
+//b
+
+
+for (let i = hacker2.length; i< 0; i--){
+    console.log(hacker2[i]);
+}
+
+
+//c
+
+
