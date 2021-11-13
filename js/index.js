9d74d3f1fb16cdafac96fda5d33f09d61896e8a5
MurilloAvellaneda <murillo.avellaneda@gmail.com>
2021-11-13 08:50:20 -0300

    finalizando o primeiro lab de javascript

--- js/index.js
+++ js/index.js
@@ -63,3 +63,96 @@ if(hacker1 < hacker2){
   } else if (hacker1 == hacker2){
     console.log("What?! You both have the same name?");
   }
+
+/* 
+Bonus 1
+Generate 3 paragraphs. Store the text in a variable type of string.
+Make your program count the number of words in the string.
+Make your program count the number of times the Latin word et appears.
+*/
+
+// Word count
+const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Sed tristique placerat eros, eget vestibulum lectus viverra vel. 
+Praesent efficitur sapien ultricies fermentum lacinia. Phasellus odio felis, ultrices sed erat ultricies, 
+maximus feugiat massa. Cras tempus gravida laoreet. Interdum et malesuada fames ac ante ipsum primis in 
+faucibus. Vestibulum hendrerit venenatis porta. Vestibulum pretium metus non est aliquam, at convallis 
+erat sollicitudin. Nam dignissim justo in velit posuere pulvinar. Etiam dignissim interdum venenatis. 
+Donec neque tortor, posuere a aliquet a, imperdiet in elit. Ut ultrices purus rutrum ligula tincidunt mattis. 
+Pellentesque pellentesque eros nunc. Ut molestie faucibus turpis sit amet fermentum. Nam non placerat turpis. 
+Praesent id mauris purus. Ut augue velit, scelerisque sit amet pulvinar vitae, venenatis vitae odio.\n\nSed 
+id hendrerit enim. Proin id mollis nulla. Praesent a ipsum vitae nibh blandit hendrerit. Donec justo est, 
+vestibulum non hendrerit sit amet, vulputate et magna. Nulla sit amet massa diam. Pellentesque sodales sit 
+amet felis eu auctor. Phasellus semper lacus eget lectus pulvinar mollis. Maecenas laoreet, risus ut 
+pellentesque dictum, tellus nulla congue tellus, at hendrerit urna ligula et diam. Morbi vitae tincidunt 
+elit. Suspendisse sed libero mauris. Maecenas sem elit, tempor lacinia erat vitae, maximus blandit arcu. 
+Donec vitae fermentum dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
+cubilia curae;\n\nFusce ipsum nibh, rhoncus ut elementum eu, consectetur nec est. Ut vehicula et erat sed 
+ultrices. Donec ipsum tortor, condimentum at massa sit amet, laoreet convallis lorem. Mauris maximus 
+imperdiet magna non condimentum. Nunc ultricies neque at mollis cursus. Donec luctus sagittis pharetra. 
+Phasellus malesuada congue sem quis dignissim. Vivamus auctor, est sed malesuada maximus, dolor elit ultrices 
+sapien, eu auctor massa leo et sem. Pellentesque porttitor libero sapien, sed euismod justo bibendum sit amet. 
+Cras nec nisl at felis malesuada elementum. Sed et dolor iaculis, varius massa eu, fermentum est. Duis eget 
+leo at sapien iaculis molestie. Nulla maximus venenatis elit quis mollis. Donec ut auctor erat. Class aptent 
+taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam pretium ac lectus id 
+semper.`
+
+let spacesCount = 0;
+for(i = 0; i < loremIpsum.length; i += 1){
+  if(loremIpsum[i] === ' ' || loremIpsum[i] === '\n'){
+  spacesCount += 1;
+  }
+}
+let wordsCount = spacesCount + 1;
+console.log(wordsCount);
+
+// et count
+let etCount = 0;
+for(i = 0; i < loremIpsum.length; i += 1){
+  if(loremIpsum[i-1] === ' ' && loremIpsum[i] === 'e' && loremIpsum[i+1] === 't' && (loremIpsum[i+2] === ' ' || loremIpsum[i+2] === '.' || loremIpsum[i+2] === ',')){
+  etCount += 1;
+  }
+}
+console.log(etCount);
+
+
+/*
+Bonus 2:
+Create a new variable phraseToCheck and have it contain some string value. 
+Write a code that will check if the value we assigned to this variable is a Palindrome. 
+Here are some examples of palindromes:
+"A man, a plan, a canal, Panama!"
+"Amor, Roma"
+"race car"
+"stack cats"
+"step on no pets"
+"taco cat"
+"put it up"
+"Was it a car or a cat I saw?" 
+"No x in Nixon".
+*/
+
+let phraseToCheck = "Amor, coma";
+let newPhraseToCheck = "";
+let aux = 1;
+for(let i = 0; i < phraseToCheck.length; i+=1){
+    if(phraseToCheck[i] !== " " && phraseToCheck[i] !== "." && phraseToCheck[i] !== "?" && phraseToCheck[i] !== "," && phraseToCheck[i] !== "!"){
+        newPhraseToCheck += phraseToCheck[i].toLowerCase();
+    }
+}
+if(newPhraseToCheck.length % 2 == 0){ 
+    for(let i = 0, j = newPhraseToCheck.length - 1; i <= newPhraseToCheck.length/2; i+=1,j-=1){
+        if(newPhraseToCheck[i] !== newPhraseToCheck[j]){
+            aux = 0;
+            break;
+        }
+    }
+}else{
+    for(let i = 0, j = newPhraseToCheck.length - 1; i !== j; i+=1,j-=1){
+        if(newPhraseToCheck[i] !== newPhraseToCheck[j]){
+            aux = 0;
+            break;
+        }
+    }   
+}
+aux ? console.log("its a palindrome") : console.log("its not a palindrome");
