b7fe1e2e5aed80385d1797f4a794df5c49b6d606
Larissa Ferreira <larissadantasaf@gmail.com>
2021-11-16 14:58:07 +0100

    add trim method

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ let upperCaseName = '';
 for (let letter of hacker1){
   upperCaseName += letter.toUpperCase() + " ";
 }
-console.log(upperCasedName);
+console.log(upperCaseName.trim());
 
 
 let reverseName = ''; 
@@ -50,3 +50,42 @@ if (hacker1 === hacker2) {
       console.log("Yo, the navigator goes first definitely");
     } 
   }
+
+
+  //Bonus 1 
+
+count = 0;
+for (let y = 0; y < paragh.length; y++) {
+  if (paragh[y] !== ' ') {
+    count++;
+  }  
+}
+console.log(count);
+
+
+let paragh = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus tellus in maximus pretium. Etiam condimentum ac tortor eu porttitor. Sed in tortor est. Proin a ultricies sapien. Duis sit amet lectus augue. Nunc nec turpis in augue dictum ultrices. Etiam sollicitudin dictum est vitae vehicula. Sed in rutrum sapien. Sed viverra malesuada elementum. Praesent gravida arcu felis, non scelerisque dolor pretium et. Etiam at elit tincidunt, lacinia eros a, aliquet nibh. Nam rhoncus tincidunt porta. Quisque gravida gravida ex volutpat fringilla. Integer sit amet tellus sapien. Vestibulum pretium nibh a ante consequat, ut sodales lectus ornare. Praesent dui urna, facilisis et purus ut, eleifend hendrerit lorem. Nullam pulvinar enim a risus ultrices mattis. Praesent id arcu mi. Aenean tincidunt, nisi eget interdum vulputate, enim magna mollis ante, id fermentum mauris elit ut erat. Fusce condimentum orci eu metus tincidunt, eu pretium libero molestie. Nullam velit velit, rutrum eget iaculis et, volutpat ac enim. Vestibulum augue arcu, ultricies sit amet nunc eget, semper finibus nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis mattis sem, quis vestibulum felis condimentum a. Pellentesque ullamcorper eros non mollis feugiat. Fusce fringilla ex quis cursus vulputate. Integer ultricies arcu ut arcu blandit, vel ultricies lacus rutrum. Cras suscipit, dui ac ultricies volutpat, justo nibh pellentesque diam, ut ullamcorper nunc enim eget nulla. Duis vitae libero in quam condimentum consectetur vitae id ante. Duis tempor velit maximus elementum pretium. Maecenas vitae malesuada turpis, in sagittis arcu. In euismod non nunc quis bibendum. Donec hendrerit nibh non ligula efficitur, at porttitor diam varius. In nec luctus purus. Curabitur non risus at elit imperdiet consectetur non eget lacus. Nam fringilla massa eu eros congue semper at vel ipsum. Vivamus ut arcu vitae nulla varius pharetra. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.';
+
+
+count = 0;
+let palavra = '';
+for (let y = 0; y < paragh.length; y++) {
+  if (!isLetter(paragh[y])) {
+    if (palavra === "et") {
+      count++;
+    }
+    palavra = '';
+  } else {
+    palavra += paragh[y];
+  }
+
+  if (y === paragh.length - 1 && palavra === "et") {
+      count++;
+  }
+}
+console.log(count);
+
+function isLetter(ch) {
+  return ch.toUpperCase() != ch.toLowerCase();
+}
+
+
