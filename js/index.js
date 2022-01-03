0975f65df74bb578bfd6cbe9fe04e1f49124e023
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-01-03 20:45:09 +0100

    Finished excercises

--- js/index.js
+++ js/index.js
@@ -1,7 +1,55 @@
 // Iteration 1: Names and Input
-// 
+
+//
+
+
+const hacker1 = "Amanda";
+
+console.log(`The driver's name is ${hacker1}`);
+
+
+const hacker2 = "Adam";
+
+console.log(`The navigator's name is ${hacker2}`);
+
 
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length){
+
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+
+} else if (hacker2.length > hacker1.length) {
+
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+
+}else {
+
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+
+} 
 
 // Iteration 3: Loops
+//Print all the characters of the driver's name, separated by a space and in capitals
+let driver = "";
+for (var i = 0; i < hacker1.length; i++) {
+  driver += hacker1.charAt(i).toUpperCase() + " ";
+}
+console.log(driver);
+
+//Print all the characters of the navigator's name, in reverse order.
+let navReverse = "";
+for (var i = hacker2.length; i >= 0; i--) {
+  navReverse += hacker2.charAt(i).toUpperCase() + " ";
+}
+console.log(navReverse); 
+
+//sort strings alphabetically
+
+        if (hacker1.localeCompare(hacker2) === 1){
+        console.log("Yo, the navigator goes first definitely.");
+        } else if (hacker1.localeCompare(hacker2) === -1){
+        console.log("The driver's name goes first.");
+        } else {
+            console.log("What?! You both have the same name?");
+        }
