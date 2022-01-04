656dd213ce677b69aab7a8fb9dee3496b6bca1ca
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-01-04 08:29:24 +0100

    Iteration 3: Loops completed

--- js/index.js
+++ js/index.js
@@ -9,22 +9,41 @@ console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 const driverNameLength = hacker1.length
-//console.log(driverNameLength);
 const navigatorNameLength = hacker2.length
-//console.log(navigatorNameLength);
+
 if(driverNameLength > navigatorNameLength){
 console.log(`The driver has the longest name, it has ${driverNameLength} characters`)
 }
-if(driverNameLength < navigatorNameLength){
+else if(driverNameLength < navigatorNameLength){
     console.log(`It seems that the navigator has the longest name, it has ${navigatorNameLength} characters`)
     }
-if(driverNameLength === navigatorNameLength){
+else if(driverNameLength === navigatorNameLength){
         console.log(`Wow, you both have equally long names, ${driverNameLength} characters!`)
         }
 // Iteration 3: Loops
-let driverUpperCase = hacker1.toUpperCase()
-let output =""
+let driverUppercase = hacker1.toUpperCase()
+
+let outPutDriver =""
+
 for(let i = 0; i <= driverNameLength; i++ ) {
-    output = output + driverUpperCase.slice(i, i + 1) + " "
+    outPutDriver += driverUppercase.slice(i, i + 1) + " "
 }
-console.log(output);
+console.log(outPutDriver)
+
+let outPutNavigator =""
+for(let i = navigatorNameLength -1; i >= 0 ; i-- ) {
+    outPutNavigator += hacker2[i];
+}
+console.log(outPutNavigator);
+
+// Depending on the lexicographic order of the driver and navigator.
+switch (hacker1.localeCompare(hacker2)){
+    case 0:
+        console.log("What?! You both have the same name?")
+        break;
+    case 1:
+        console.log("Yo, the navigator goes first definitely.")
+        break;
+    default:
+        console.log("The driver's name goes first.")
+}
