b5e6ff901dc7b9a29bcd61085c7c81a67c4fa146
KimSteenBekke <Kim.Steen.Bekke@lego.com>
2022-01-04 08:42:20 +0100

    done

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ let hacker2 = "Bjorn Ironside";
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
   }
-  else if (hacker1.length > hacker2.length) {
+  else if (hacker2.length > hacker1.length) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
   }
   else {
