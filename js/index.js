01998e3d28036ac2892f46695e61aff79bc694d3
Stephan Geyer <stephangeyer@Stephans-MacBook-Pro.local>
2022-01-10 18:00:22 +0100

    FIrst Iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Hans";
+
+console.log(`The driver's name is ${hacker1}.`);
+
+let hacker2 = "Peter";
+
+console.log(`The driver's name is ${hacker2}.`);
+
 // Iteration 2: Conditionals
 
 
