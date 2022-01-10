a8869ede4d59ab85a5a67818a33e39b9afcaf670
Rospini <mulatsch@msn.com>
2022-01-10 20:04:24 +0100

    adding Bonus

--- js/index.js
+++ js/index.js
@@ -47,3 +47,14 @@ else if(driverFirstLetter < navigatorFirstLetter ){
 else if(driverFirstLetter > navigatorFirstLetter ){
   console.log("Yo, the navigator goes first definitely.")
 }
+
+let lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia pretium tellus, eget efficitur felis lacinia ac. Nullam in dapibus velit, ac suscipit urna. Proin ultricies, augue nec varius consectetur, ante arcu scelerisque risus, et commodo lorem nunc at est. Vivamus venenatis lobortis lectus sit amet gravida. Nullam fringilla auctor suscipit. Aliquam dapibus urna eu ligula faucibus commodo. Vivamus nibh tortor, auctor nec sapien eget, aliquam pharetra dui. Nunc metus ex, commodo et pretium et, cursus vel velit. Vestibulum dui massa, mattis vel orci ut, gravida cursus lorem. In hac habitasse platea dictumst. Pellentesque convallis ut turpis et commodo. Quisque varius leo id libero efficitur, et interdum nisl porttitor. Aenean malesuada imperdiet massa.
+
+Duis quis dolor lacus. Aliquam erat volutpat. Duis dapibus lacus diam, elementum facilisis libero aliquet eget. Morbi in felis vel sem varius pretium. Sed aliquam vitae mauris ut sodales. Curabitur maximus ex urna. Sed a hendrerit mi. Nunc feugiat justo placerat maximus molestie.
+
+Aenean magna nibh, vehicula non sodales quis, vestibulum lacinia velit. Donec id turpis nibh. Sed eget justo maximus ligula mollis auctor. Integer ligula diam, faucibus ac sapien sed, commodo euismod nulla. Duis quis commodo eros. Cras tempus, lacus in faucibus convallis, nunc erat eleifend risus, eu laoreet est diam sed turpis. Ut sagittis viverra ligula, a tempus nunc congue sed. Duis a ipsum sit amet ante accumsan rhoncus id at augue. Aenean eu ultrices felis, in fermentum nibh. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
+
+function WordCount(lorem) { 
+  return lorem.split(" ").length;
+}
+console.log(WordCount(lorem));
