ff6c5a5e7943b750fefb4d017bd0be2d45c06e8c
Marten Zand-Vakili <martenzv@hotmail.de>
2022-01-10 20:05:58 +0100

    Barts and Martens work

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 // Iteration 1: Names and Input
-const hacker1 = "Johnny";
-const hacker2 = "Dave";
+const hacker1 = "Marten";
+const hacker2 = "Bart";
 
 console.log("The driver's name is " +hacker1);
 console.log("The navigator's name is " +hacker2);
 
-// Iteration 2: Conditionals
+
+// Iteration 2: Conditionals and Iteration 3: Loops
+if (hacker1.length > hacker2.length){
+    console.log("The driver has the longest name, it has " + hacker1.length + " characters.");
+    console.log("the drivers name goes first")
+  } else if (hacker2.length > hacker1.length) {
+    console.log("It seems that the navigator has the longest name, it has " + hacker2.length + " characters.");
+    console.log("Yo, the navigator goes first definitely.");
+  } else {
+    console.log("Wow, you both have equally long names, " + hacker1.length + " characters!");
+    console.log("What?! You both have the same name?");
+  }
+   
+  var resultsDriver = hacker1
+  console.log(resultsDriver.split('').join(' ').toUpperCase());
+  
+  var resultsNavigator = hacker2
+   console.log(resultsNavigator.split("").reverse().join(""));
 
 
-// Iteration 3: Loops
