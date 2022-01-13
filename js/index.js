670ef436540b8b0d339acb6d3e92b1cff1249083
Laura Del valle <l.delvallebermejo@gmail.com>
2022-01-13 16:54:35 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,64 @@
-// Iteration 1: Names and Input
+//interacción 1
 
-// Iteration 2: Conditionals
+//1.1
+let hacker1 = "Laura" 
+//1.2
+console.log (`The driver name is ${hacker1}`)
+//1.3
+let driver = "Cristian"
+//1.4
+console.log (`the navigators name is ${driver}`)
+
+//Interacción 2
 
 
-// Iteration 3: Loops
+if  ( hacker1.length > driver.length) {
+
+console.log  (`The driver has the longest name, it has ${hacker1.length} characters`)
+
+} 
+else if ( hacker1.length < driver.length) {
+
+  console.log  (`It seems that the navigator has the longest name, it has ${driver.length} characters.`)
+
+}
+
+else {
+
+console.log (`Wow, you both have equally long names, ${driver.length} characters!`)
+
+}
+
+//Interaccion 3
+
+//3.1
+
+let nameWithSpace = ""
+
+for (let i=0; i<driver.length; i++){
+  nameWithSpace += driver[i].toUpperCase() + " "
+ 
+
+
+}
+console.log (nameWithSpace) 
+
+//3.2
+
+let backName = ""
+for (let i = driver.length-1; i >=0 ; i--){
+backName += driver[i]
+
+}
+
+console.log (backName)
+
+//3.3
+
+let orderName = [ "Shirley", "Cristian" , "Laura"]
+orderName.sort(function(a, b) {
+
+return a.localeCompare(b)
+})
+
+console.log (orderName)
