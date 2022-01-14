93249358e83df00b20f8f1dec8d0bade4a8104b0
Camila Solér <msoler.camila@gmail.com>
2022-01-14 19:12:52 -0300

    Iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Julia';
+console.log(`The driver\'s name is ${hacker1}`);
+
+let hacker2 = 'Beatriz';
+console.log(`The navigator\'s name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
