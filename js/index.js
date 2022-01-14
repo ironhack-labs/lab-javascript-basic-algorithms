763d80118daf95b793339251503207211a86f419
“Natalia <natalia.rudiger@gmail.com>
2022-01-14 19:18:08 -0200

    iteration 1 complete

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
 // Iteration 1: Names and Input
+// 1.1
+// Create a variable `hacker1` with the driver's name.
+
+let hacker1 = 'Natalia';
+
+// 1.2
+// Print `"The driver's name is XXXX"`.
+
+console.log(`The driver's name is ${hacker1}`);
+
+// 1.3
+// Create a variable `hacker2` with the navigator's name.
+
+let hacker2 = 'Camila';
+
+// 1.4
+//Print `"The navigator's name is YYYY"`.
+
+console.log(`The navigator's name is ${hacker2}`);
+
 
 // Iteration 2: Conditionals
+
+
+
 
 
 // Iteration 3: Loops
