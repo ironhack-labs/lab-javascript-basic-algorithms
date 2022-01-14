10b9bc29ddd225f54c792f796ed02e290df8d268
Bruno Mortella <bmortella@gmail.com>
2022-01-13 22:52:16 -0300

    Interation 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
 
+const hacker1 = "Zero Cool"
+
+console.log(`The drive's name is ${hacker1}`)
+
+const hacker2 = "Acid Burn"
+
+console.log(`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
 
