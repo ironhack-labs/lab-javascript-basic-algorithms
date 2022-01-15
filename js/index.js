c671b545f4aabaca918816b8e946eda73a62f1c6
Inger-Merete Bruhn Cliquet <you@example.com>
2022-01-15 17:23:30 +0100

    Completed iterations 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 // Iteration 1: Names and Input
+var hacker1 = "Jan"
+console.log(`"The driver's name is Jan"`)
+
+var hacker2 = "Benny"
+console.log(`"The navigator's name is Benny"`)
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log(`"The driver has the longest name it has ${hacker1.length} characters"`)
+  } else if (hacker2.length > hacker1.length) {
+    console.log(`"It seems that the navigator has the longest name, it has ${hacker2.length} characters"`)
+  } else if (hacker1.length === hacker2.length) {
+    consoel.log (`"Wow, you both have equally long names, ${hacker2.length} characters!"`)
+  }
 
 // Iteration 3: Loops
+
+/*
+git add --all
+git commit -m "Completed iterations 1 and 2"
+git push origin master
+*/
