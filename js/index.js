12304f8ac9528d1407c8111522c93bd4d1f8395f
Nathalia Maia <nathaliamaiape@gmail.com>
2022-01-14 21:54:01 -0300

    finished first interaction

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = "Nathalia Maia"
+console.log(`The driver's name is ${hacker1}.`);
+
+let hacker2 = "Flávia Maia"
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
 
