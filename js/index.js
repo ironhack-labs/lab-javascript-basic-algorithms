d87f5eb5687753650e438c5df1a30377e8704cc2
Dina Abdelbary <63638151+Dinaabdelbary@users.noreply.github.com>
2022-02-09 13:45:27 +0100

    Lab done

--- js/index.js
+++ js/index.js
@@ -38,5 +38,13 @@ let reversed = result.split("").reverse().join("");
 console.log(reversed);
 
 
+if ((hacker1.localeCompare(hacker2)) == -1) {
+    console.log("The driver's name goes first.")
+  } else if ((hacker1.localeCompare(hacker2)) == 1 ) {
+    console.log("Yo, the navigator goes first definitely.")
+  } else {
+    console.log("What?! You both have the same name?")
+  }
+
 
 
