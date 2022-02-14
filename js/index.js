9dd00b881abe8eaff2cd66cbbe7514aee1c29ad7
Renato Costa <renatoaccosta@gmail.com>
2022-02-14 20:12:23 +0000

    final lab

--- js/index.js
+++ js/index.js
@@ -82,3 +82,28 @@ for (let i = 0; i < longName; i++){
 }
 }
 console.log(output);
+
+// bonus 1
+
+const paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac erat eleifend, vehicula arcu eu, ullamcorper ex. Nam aliquam nisl ut justo auctor faucibus. Pellentesque sapien arcu, rutrum eget sagittis commodo, rhoncus eget enim. Aliquam nec vestibulum lectus, vel sollicitudin neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam condimentum ultrices est et consequat. Pellentesque dolor sem, auctor et mollis in, fermentum sed neque. In in sem nec erat vehicula lacinia. Pellentesque faucibus enim eget enim feugiat, a molestie felis accumsan.
+
+Nulla ultrices sagittis mi in blandit. Cras scelerisque, magna id tincidunt vestibulum, justo leo molestie sem, non imperdiet lacus justo efficitur mauris. Donec et mauris nec libero dapibus aliquam vitae at mauris. Proin mattis efficitur lectus quis dictum. Curabitur a est posuere, dapibus nibh imperdiet, cursus justo. Nulla venenatis nunc non ante maximus iaculis. Nam tristique maximus felis ut semper. Aenean sagittis libero nibh, sit amet porttitor ante volutpat eu. Donec consectetur molestie venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam sed eros odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam facilisis diam vulputate justo porttitor congue. Suspendisse iaculis nulla id leo dapibus convallis.
+
+Nunc in elementum risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin ligula purus, id vehicula sem elementum nec. Mauris at gravida odio. Morbi tincidunt leo vitae odio gravida, sed semper diam accumsan. Nulla dictum eros nunc, id vehicula turpis cursus et. Ut euismod neque quis fermentum tincidunt. Maecenas efficitur tortor nec nibh hendrerit, ut convallis ipsum consequat. Vivamus hendrerit sem eu fermentum efficitur. Nam rhoncus neque tortor, vitae efficitur diam vehicula ac. Mauris tristique urna sed venenatis placerat.`;
+
+const arr = paragraph.split(' ');
+const filterWords = arr.filter(element => element !== '');
+
+let totalWords;
+
+for(x = 0; x < filterWords.length; x++){
+
+}
+totalWords = x;
+console.log(`The number of words is : ${totalWords}.`);
+
+const filterEtWord = arr.filter(caracter => caracter === 'et');
+
+let totalEtWord = filterEtWord.length;
+
+console.log(`The number of "et" is ${totalEtWord}`);
