daf26d7267e5e3b96ecb7369c418ad231c75e95a
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-02-14 18:04:10 +0100

    Bonus 1 (maybe) done

--- js/index.js
+++ js/index.js
@@ -61,3 +61,27 @@ if (hacker1UpperCase === hacker2UpperCase) {
     }
 }
 console.log(message);
+
+
+//Bonus 1
+const paragraphs = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pulvinar nec neque ut fringilla. Phasellus sit amet justo ac orci suscipit mollis. Praesent sodales nisl sem, a sollicitudin orci pellentesque quis. Proin eu lacus a quam pretium tempus. Morbi eu sem ac neque condimentum bibendum sit amet scelerisque ligula. Ut eleifend lacus a odio pulvinar interdum. Duis iaculis enim ut tristique porta. Vivamus semper, mauris rutrum efficitur placerat, felis erat elementum lorem, non volutpat risus risus non quam. Curabitur risus nisi, volutpat laoreet magna id, bibendum blandit elit. Aenean malesuada suscipit dolor, tempor suscipit tellus tristique sed. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Aenean in iaculis velit, at ornare ex. Fusce volutpat nibh id cursus condimentum. Morbi faucibus sed justo at ultrices. Nullam turpis elit, varius sit amet sapien id, varius ullamcorper augue. Vestibulum eget ullamcorper eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc consequat sagittis tincidunt. Curabitur enim velit, mattis rhoncus erat a, tempor convallis odio. Proin fermentum felis id nisi congue vehicula. Sed vel arcu metus. Ut tempor diam accumsan massa tristique, sit amet maximus eros facilisis. Proin ipsum quam, dignissim quis massa eu, mollis faucibus neque. Nulla commodo ligula sit amet sagittis pharetra. Duis lobortis diam tincidunt lectus luctus semper. Vivamus nibh nisl, porttitor quis aliquet a, aliquet id eros.
+Duis luctus ipsum eros, a pulvinar lacus accumsan ut. Duis ultrices ligula vel faucibus tempus. Nam pellentesque quam ut ipsum fermentum, sit amet auctor elit consequat. Quisque volutpat dolor quis ex volutpat volutpat. Maecenas vehicula mi nec eros semper sagittis feugiat eu mauris. Proin eleifend sem eu lacus tempus eleifend. Integer tincidunt magna non neque fringilla lobortis. Sed lobortis ultricies eleifend. Aenean condimentum, tortor id feugiat blandit, est velit sollicitudin nunc, posuere gravida augue tellus at massa. Etiam eu erat ut diam porttitor rutrum. Donec interdum sapien eu risus dignissim, at aliquam justo pellentesque.`;
+let countWords = 0;
+let countEts = 0;
+
+for (i = 0; i < paragraphs.length; i++) {
+    if (paragraphs[i] === " ") {
+        countWords++;
+    }
+
+    if ((paragraphs[i-1] === " ") && 
+        (paragraphs[i] === "e") && 
+        (paragraphs[i+1] === "t") && 
+        ((paragraphs[i+2] === " ") || (paragraphs[i+2] === ",") || (paragraphs[i+2] === "."))) {
+        countEts++;
+    }
+}
+countWords++;
+console.log(`Number of Words: ${countWords}`);
+console.log(`Number of 'et': ${countEts}`);
