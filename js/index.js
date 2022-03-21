b4e40ac5480e0a21bb859aae00c7b045fe1b55f4
Alex Borrayo <Alexborrayo21@hotmail.com>
2022-03-21 17:14:12 -0600

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+let hacker1 = "Alfonso";
+console.log(`The drivers name is ${hacker1}`)
+let hacker2 = "Alex";
+console.log(`The navigators name is ${hacker2}`)
 
+// Iteration 2: Conditionals
+console.log(hacker1.length)
+console.log(hacker2.length)
 
 // Iteration 3: Loops
