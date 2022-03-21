9ff15a1d119a3d6b1a6cfa3a3cdf4cee718a19b7
Gian Lucca <giandacoll@gmail.com>
2022-03-21 16:56:49 -0300

    Interação 2

--- js/index.js
+++ js/index.js
@@ -2,12 +2,24 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
-const hacker1 = "João";
+const hacker1 = "João Dias";
 console.log(`"The driver's name is ${hacker1}"`);
-const hacker2 = "Pelé";
+const hacker2 = "Pelé Dos Santos";
 console.log(`"The navigator's name is ${hacker2}"`);
-//
+
 // Iteration 2: Conditionals
+function lengthNames(hacker1, hacker2) {
+    const numberHacker1 = hacker1.length;
+    const numberHacker2 = hacker2.length;
 
+    if(numberHacker1 > numberHacker2) {
+        return `The driver has the longest name, it has ${numberHacker1} characters.`
+    } else if(numberHacker2 > numberHacker1){
+        return `It seems that the navigator has the longest name, it has ${numberHacker2} characters.`
+    } else {
+        return `Wow, you both have equally long names, ${numberHacker1} characters!`
+    }
+}
 
+lengthNames(hacker1, hacker2);
 // Iteration 3: Loops
