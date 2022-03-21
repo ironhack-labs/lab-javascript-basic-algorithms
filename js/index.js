1869d6c4195f7594dd8be0228585c158244ff958
Nicholas Palomba <nicholaspalomba16@gmail.com>
2022-03-21 16:48:05 -0400

    completed bonus 1 and 2

--- js/index.js
+++ js/index.js
@@ -34,3 +34,32 @@ if (hacker1.localeCompare(hacker2) < 0) {
 else {
     console.log("please put your name")
 }
+
+
+//BONUS 1
+let paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin fringilla lorem eget ultrices. Nulla eu cursus lacus. Aliquam interdum egestas nulla, et mattis diam consectetur eget. Fusce pharetra nulla posuere congue lobortis. Curabitur ultricies mauris sed sodales efficitur. Aliquam euismod sodales velit in consequat. Nam at lacus id neque porta blandit id id lacus. Curabitur ut sollicitudin augue, eu dapibus tellus. Proin justo tortor, semper vitae interdum sit amet, ultricies vitae nisi. Fusce viverra neque sed pretium eleifend. Phasellus nec ipsum nec nisl iaculis scelerisque. Nam ultricies libero non elementum lacinia. Cras accumsan turpis eget libero fermentum pharetra. Duis quis neque a ipsum bibendum mollis at non neque. Fusce suscipit massa massa, in viverra lacus viverra quis. Phasellus viverra urna ut enim maximus, id ornare turpis tempus. Mauris varius, orci eget porta tempus, lorem orci scelerisque sem, sed accumsan nibh nibh eget magna. Etiam felis nisl, iaculis sed odio ut, laoreet vehicula magna. Aenean sit amet tincidunt erat, sed tristique metus. Duis pharetra erat nec placerat pharetra. Aenean convallis hendrerit lectus, at suscipit elit vehicula vel. Nulla facilisi. Praesent at interdum velit. Mauris justo lectus, aliquet non faucibus eget, cursus id massa. Morbi nec nisl quis ex consectetur suscipit. Nullam condimentum nibh vitae tellus ultricies, et interdum nisi gravida. Vestibulum fermentum lectus vitae orci egestas, at tincidunt neque tempus. Nulla facilisi. Suspendisse at lorem nec ligula porttitor auctor. Praesent a porta dui. Donec sollicitudin vulputate magna, nec ullamcorper nisl finibus et. Fusce aliquam elit egestas, fringilla metus ac, dapibus massa. Etiam augue est, laoreet non convallis nec, gravida volutpat ante. Vestibulum est risus, vehicula nec libero a, pharetra hendrerit velit. Morbi lectus massa, euismod vel elementum et, pharetra elementum eros. Quisque eu risus sit amet nunc ultrices efficitur. Aliquam feugiat, mauris ut eleifend luctus, ipsum erat consequat mi, ac aliquet urna metus vel velit."
+
+console.log(paragraph.split(" ").length);
+
+function etCounter() {
+    let newArr = [];
+    for (let i = 0; i < paragraph.length; i++) {
+        if (paragraph[i] + paragraph[i + 1] + paragraph[i + 2] === " et") {
+            newArr.push((paragraph[i] + paragraph[i + 1]))
+        }
+    }
+    return newArr.length;
+}
+
+console.log(etCounter(paragraph))
+
+//BONUS 2
+
+let string = "miami"
+
+if (string === [...string].reverse().join("")) {
+    console.log("this is a palindrome")
+}
+else {
+    console.log("this is not a palindrome")
+}
