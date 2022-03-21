5025dd7933b98ba4b9772256f22407a2168318ef
Alfonso Cano <alfonsoheartagram@gmail.com>
2022-03-21 17:30:18 -0600

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-// Iteration 1: Names and Input
-//
+let hacker1 = 'Alfonso';
+console.log (`The drivers name is ${hacker1}`)
+
+let hacker2 = 'Alex';
+console.log (`The navigators name is ${hacker2}`)
+
+console.log (hacker1.length)
+console.log (hacker2.length)
+
+
 // Iteration 2: Conditionals
 
 
