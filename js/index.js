de98fabc15aeea6d91c010989b3e9d7986038a0e
Esteban Munoz <esmunozdev@gmail.com>
2022-03-22 10:09:04 -0600

    Iteration 2 & Iteration 3.1, 3.2 completed

--- js/index.js
+++ js/index.js
@@ -7,8 +7,26 @@ let hacker3 = "Esteban";
 console.log(`"The second's navigator's name is ${hacker3}"`);
 
 // Iteration 2: Conditionals
-console.log(`"The longest name has: ${hacker3.length} letters"`);
+if (hacker1.length > hacker2.length && hacker1.length > hacker3.length) {
+  console.log(`"The driver ${hacker1} has the longest name, it has       
+      ${hacker1.length} characters."`);
+} else if (hacker2.length > hacker1.length && hacker2.length > hacker3.length) {
+  console.log(
+    `"It seems that the navigator ${hacker2} has the longest name, it has ${hacker2.length} characters."`
+  );
+} else if (hacker3.length > hacker1.length && hacker3.length > hacker2.length) {
+  console.log(
+    `"It seems that the navigator ${hacker3} has the longest name, it has ${hacker3.length} characters."`
+  );
+} else {
+  console.log(
+    `"Wow, you three have equally long names, ${hacker3.length} characters!"`
+  );
+}
 
 // Iteration 3: Loops
+for (i = 0; i < hacker1.length; i++) console.log(hacker1[i]);
+
+for (i = hacker2.length; i > 0; i--) console.log(hacker2[i]);
 
 //Comment
