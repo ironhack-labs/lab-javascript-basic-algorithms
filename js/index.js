a8ed25fb08c21cd70c3c5585034fcb8a71329c87
Gian Lucca <giandacoll@gmail.com>
2022-03-21 23:31:19 -0300

    3.3 imcomplete

--- js/index.js
+++ js/index.js
@@ -23,3 +23,32 @@ function lengthNames(hacker1, hacker2) {
 
 lengthNames(hacker1, hacker2);
 // Iteration 3: Loops
+const name = hacker1.toUpperCase();
+let arr = hacker2.split('');
+
+  /* 3.1 */ console.log(name.split("").join(" "));
+   /* 3.2 */ for (const nome of arr.reverse()) {
+                console.log(nome);
+                }
+
+/* 3.3 */   const arr1 = Array.from(hacker1); 
+            const arr2 = Array.from(hacker2);
+
+          function comparar(a, b) {
+            if (arr1 < arr2 ) {
+              return `The driver's name goes first.`;
+            }
+            if (arr1 > arr2 ) {
+              return `Yo, the navigator goes first definitely.`;
+            }
+            // a deve ser igual a b
+            return `What?! You both have the same name?`;
+          }
+                       
+comparar(arr1, arr2)
+// console.log( arr1, arr2);
+     
+
+
+
+
