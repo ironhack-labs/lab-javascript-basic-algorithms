05d4f1435195c832c64fb64bd46a355cde3900f6
Alfonso Cano <alfonsoheartagram@gmail.com>
2022-03-22 09:48:53 -0600

    done

--- js/index.js
+++ js/index.js
@@ -7,8 +7,18 @@ console.log (`The navigators name is ${hacker2}`)
 console.log (hacker1.length)
 console.log (hacker2.length)
 
+if(hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.legth} characters.`) 
+} else if (hacker1.length < hacker2.length) {
+  console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+  console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 
-// Iteration 2: Conditionals
+console.log ((`"${hacker1[0]} ${hacker1[1]} ${hacker1[2]} ${hacker1[3]} ${hacker1[4]} ${hacker1[5]} ${hacker1[6]}"`).toUpperCase())
 
+console.log ((`"${hacker2[3]} ${hacker2[2]} ${hacker2[1]} ${hacker2[0]}"`).toLowerCase())
 
-// Iteration 3: Loops
+console.log (hacker1.localeCompare(hacker2))
+
+console.log ("Yo, the navigator goes first definitely")
