28fd24f44805054695ab8beb1685f9897088ae65
Georgina Aceves* <aceves.georgina@hotmail.com>
2022-03-22 01:01:58 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
 
+let hacker1 = "David" ;
+console.log(`"The driver's name is ${hacker1}"`);
+let hacker2 = "Gina" ;
+console.log(`"The first navigator's name is ${hacker2}"`);
+let hacker3 = "Esteban";
+console.log(`"The second's navigator's name is ${hacker3}"`);
+
+// Iteration 2: Conditionals
+console.log(`"The longest name has: ${hacker3.length} letters"`)
 
 // Iteration 3: Loops
