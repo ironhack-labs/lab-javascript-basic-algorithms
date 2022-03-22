0ddab60d0184d748ea58b0b8cda597afd199f04b
nash0x <nachoest@gmail.com>
2022-03-22 08:50:22 -0700

    done2.0

--- js/index.js
+++ js/index.js
@@ -17,3 +17,15 @@ if (hacker1.length > hacker2.length){
   }
 
 // Iteration 3: Loops
+//3.1
+console.log(hacker1.toUpperCase().split("").join(" "));
+//3.2
+console.log(hacker2.split("").reverse().join(""));
+//3.3
+if (hacker1.localeCompare(hacker2) == 1){
+  console.log("The driver's name goes first.")
+} else if(hacker1.localeCompare(hacker2) == -1){
+  console.log("Yo, the navigator goes first definitely.")
+} else{
+  console.log("What?! You both have the same name?")
+}
