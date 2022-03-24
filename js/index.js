f8750321e85e8f18f64af42b5c7ddf63f400fa22
Gabriela <s.gabriela.bv96@gmail.com>
2022-03-24 17:01:51 +0100

    [MAD PP] Roberto Ezquerro + Gabriela Benalcazar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,34 @@
 // Iteration 1: Names and Input
+const hacker1 = "Roberto";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Gaby";
+console.log(`The driver's name is ${hacker2}`);
+
 //
 // Iteration 2: Conditionals
 
-
+if (hacker1.length > hacker2.length) {
+    console.log(
+        `the driver has the longest name, it has ${hacker1.length} characters.`
+    );
+} else if (hacker2.length < hacker1.length) {
+    console.log(
+        `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+    );
+} else {
+    console.log(
+        `Wow, you both have equally long names, ${hacker2.length} characters!.`
+    );
+}
 // Iteration 3: Loops
+let team = [hacker1, hacker2];
+let alf = team.sort();
+console.log(alf);
+
+if (alf[0] === hacker1) {
+    console.log("The driver's name goes first.");
+} else if (alf[0] === hacker2) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
