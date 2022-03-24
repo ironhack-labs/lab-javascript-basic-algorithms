d08f2ed9ec233780e6b7e807c6f0e20c3ca0e5dd
sherilin <sherilinm96@gmail.com>
2022-03-24 17:09:45 +0100

    done

--- js/index.js
+++ js/index.js
@@ -4,3 +4,25 @@
 
 
 // Iteration 3: Loops
+
+
+// ejercicio 1
+const driver="sherilin";
+console.log(`the drives name is ${driver}`)
+
+const hacker="Guillermo";
+console.log(`the hackers name is ${hacker}`)
+
+
+//ejercicio 2
+if (driver < hacker){
+  console.log ("It seems that the navigator has the longest name, it has XX characters.")
+}
+
+if (drive < hacker){
+  console.log ("The driver has the longest name, it has XX characters")
+}
+
+if (driver = hacker){
+  console.log ("Wow, you both have equally long names, XX characters")
+}
