61dd2950a3f242628cd6794a01cae0ac73a8afcb
Andre Sanches <andrecsanches2@gmail.com>
2022-03-27 13:02:23 -0300

    Subindo a primeira iteração pro git

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 // Iteration 1: Names and Input
-//
+let hacker1 = 'Andre'
+
+console.log(`The drivers name is ${hacker1}`)
+
+let hacker2 = 'Heber'
+
+console.log(`The navigators name is ${hacker2}`)
+
+
 // Iteration 2: Conditionals
 
+// Iteration 3: loops
 
-// Iteration 3: Loops
