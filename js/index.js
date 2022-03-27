9fc51728824c111498b3cb4eb6f5c9921a2274a1
Andre Sanches <andrecsanches2@gmail.com>
2022-03-27 13:49:31 -0300

    Subindo a terceira iteração

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Iteration 1: Names and Input
-let hacker1 = 'Andre'
+let hacker1 = 'andré'
 
 console.log(`The drivers name is ${hacker1}`)
 
-let hacker2 = 'Heber'
+let hacker2 = 'heber e bruno'
 
 console.log(`The navigators name is ${hacker2}`)
 
@@ -18,11 +18,26 @@ if (hacker1.length > hacker2.length) {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
 // Iteration 3: Loops
+
 let result = ''
 for (let i = 0; i < hacker1.length; i++) {
     result += hacker1[i] + ' '
 }
 console.log(result.toUpperCase())
 
-// Iteration 3: loops
+let result2 = ''
+let j = hacker2.length - 1
+for (; j >= 0; j--) {
+    result2 += hacker2[j] + ''
+}
+console.log(result2)
+
+if(hacker1[0] === hacker2[0]){
+    console.log("What?! You both have the same name?")
+}else if (hacker1[0] < hacker2[0]) {
+    console.log("The driver's name goes first.")
+}else if (hacker1[0] > hacker2[0]) {
+    console.log("Yo, the navigator goes first definitely.")
+}
+
 
