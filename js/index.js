88fe788f795d1afb5660510a687ebca360bebe3d
Andre Sanches <andrecsanches2@gmail.com>
2022-03-27 13:03:37 -0300

    Subindo a segunda iteração pro git

--- js/index.js
+++ js/index.js
@@ -10,5 +10,19 @@ console.log(`The navigators name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker2.length > hacker1.length) {
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
+// Iteration 3: Loops
+let result = ''
+for (let i = 0; i < hacker1.length; i++) {
+    result += hacker1[i] + ' '
+}
+console.log(result.toUpperCase())
+
 // Iteration 3: loops
 
