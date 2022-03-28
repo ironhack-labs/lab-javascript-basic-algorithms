bc5d38f13522f2615a43ac80fb899c1a78ce0a13
Mafalda Oliveira <mafalda.oliveira.21@icloud.com>
2022-03-28 18:49:52 +0100

    solve iteration 3

--- js/index.js
+++ js/index.js
@@ -18,3 +18,17 @@ if (hacker1.length > hacker2.length) {
 
 
 // Iteration 3: Loops
+
+const result = hacker1.split('' ).join(' ')
+console.log(result.toUpperCase());
+
+const result2 = hacker2.split("").reverse().join("")
+console.log(result2)
+
+if(hacker1.localeCompare(hacker2)<0) {
+    console.log("The driver's name goes first.")
+  } else if (hacker2.localeCompare(hacker1)<0) {
+    console.log("Yo, the navigator goes first definitely.")
+  } else if (hacker2.localeCompare(hacker1)===0){
+    console.log("What?! You both have the same name?")
+     }
