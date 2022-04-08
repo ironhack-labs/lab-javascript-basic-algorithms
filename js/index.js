c42ac837b3b88b8d2f2a3194f7ae80bc90ed1076
“Mapy <mapy.villalobos@gmail.com>
2022-04-07 22:34:04 -0500

    PR Example

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Iteration 1: Names and Input
 let hacker1; 
-  hacker1 = "David"
+  hacker1 = "David2"
 console.log("The driver's name is", hacker1)
   hacker2 = "Mapy"
 console.log("The navigator's name is", hacker2)
