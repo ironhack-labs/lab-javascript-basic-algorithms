ed691f745228219efc2b5a44c0251a3243a9e247
emmanuelsolis <emma.ruso@outlook.com>
2022-04-08 17:00:37 -0500

    names declared, compare their lengths and order them in an array by lexicographic order

--- js/index.js
+++ js/index.js
@@ -15,3 +15,30 @@ if (hacker1.localeCompare(hacker2) === 1){
     console.log(`Wow you both have equally long names, ${hacker1.length}!`);
 }
 // Iteration 3: Loops
+// Driver 
+let driverCap = hacker1.split(''); 
+driverCap = driverCap.join(" ");
+console.log(driverCap.toUpperCase());
+//Navigator 
+let navigatorRev = hacker2.split(''); 
+navigatorRev = navigatorRev.reverse();
+console.log(navigatorRev.join(""));
+
+//By lexicographic order
+
+let names = [hacker1, hacker2];
+// comparamos y ordenamos en el mismo array
+let order = names.sort(function(a, b) {
+    return a.localeCompare(b);
+})
+
+console.log(order[0]);
+console.log(order);
+if (order[0] === hacker1) {
+    console.log(`The driver's name goes first.`);
+} else if (order[0] === hacker2) {
+    console.log(`Yo, the navigator goes first definitely.`);
+}else{
+    console.log(`What!? You both have the same name?`);
+}
+        
