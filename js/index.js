83d386dc348c5fd19652f8fd2b36b69065259281
emmanuelsolis <emma.ruso@outlook.com>
2022-04-08 07:50:55 -0500

    compare the length of names, counts their characters and prints their number

--- js/index.js
+++ js/index.js
@@ -7,5 +7,11 @@ let hacker2 = 'Nicolas';
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.localeCompare(hacker2) === 1){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+}else if (hacker1.localeCompare(hacker2) === -1 ) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log(`Wow you both have equally long names, ${hacker1.length}!`);
+}
 // Iteration 3: Loops
