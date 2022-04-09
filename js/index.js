6418b5f0d9d4704e12bd8160139ccde6c5779fd4
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-04-09 03:24:58 -0500

    done

--- js/index.js
+++ js/index.js
@@ -67,3 +67,89 @@ console.log("The driver's name goes first.")
 console.log("What?! You both have the same name?")
     }
 
+// Bonus 1
+
+let paragraph1 = "Lorem ipsum dolor et amet, consectetur adipiscing et. Vestibulum euismod scelerisque metus, non consequat libero posuere non. Vestibulum quis elementum sem. Et erat volutpat. Pellentesque a elementum diam. In aliquet erat ut neque euismod et Mauris condimentum congue tellus a semper. Aliquam et ante vitae sem lacinia efficitur et id nisi.";
+let para1Words=paragraph1.split(" ");
+let words1Length = para1Words.length
+
+let paragraph2 = "Aliquam mollis magna augue, et ullamcorper magna maximus eget. Integer massa nisl, elementum eget mollis vel, tincidunt id orci. Vivamus at turpis ut magna pellentesque porta. Nunc lacinia, sem in vestibulum tincidunt, turpis arcu viverra enim, ac malesuada risus magna non ipsum. Aliquam sed condimentum ex. Aliquam blandit lacus a pulvinar accumsan. Proin ut justo interdum sem ullamcorper condimentum in ut diam. Donec interdum erat in magna luctus, sit amet dignissim nunc suscipit. Fusce ac diam pulvinar, congue neque eu, lobortis justo. Maecenas eget velit ut arcu iaculis dignissim. Aenean magna nisl, sagittis eu erat sit amet, tincidunt auctor erat. Maecenas ac interdum lacus, a pretium nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc tortor sem, luctus quis faucibus vel, dapibus ac enim.";
+let para2Words=paragraph2.split(" ");
+let words2Length =para2Words.length
+
+let paragraph3 = "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur tempor dolor quis dapibus viverra. Nullam eleifend et quam a tempor. In hac habitasse platea dictumst. Pellentesque consequat et sapien non egestas. Fusce cursus, odio et vestibulum sodales, eros ipsum lacinia magna, vitae viverra urna velit sed sapien. Nam sit amet justo sodales quam efficitur euismod et. In tristique massa tortor, nec efficitur leo condimentum sed.";
+let para3Words=paragraph3.split(" ") // its the array that contains each word in the paragraph
+let words3Length =para3Words.length
+
+//para3Words tiene 2 et 
+
+function etCounter (array){
+let counter = 0
+for (i=0;i<array.length;i++){
+    
+    if(array[i] === "et" || array[i] == "et."|| array[i] == "Et."|| array[i] == "Et"){
+counter += 1
+    }
+
+}
+return console.log(counter)
+}
+
+console.log(para3Words);
+let resultBonus1 = etCounter(para3Words);
+
+// Bonus 2
+
+/*
+
+  - "A man, a plan, a canal, Panama!"
+  - "Amor, Roma"
+  - "race car"
+  - "stack cats"
+  - "step on no pets"
+  - "taco cat"
+  - "put it up"
+  - "Was it a car or a cat I saw?"
+  - "No 'x' in Nixon".
+*/
+
+let phraseToCheck = "Amor, jamas"
+let phraseToArray=(phraseToCheck.toUpperCase()).split("")
+
+function filtro (array){
+    let phraseClean = []
+for(i=0;i<array.length;i++){
+    
+if(array[i]=== "," 
+|| array[i]=== "!" 
+|| array[i]=== " "
+|| array[i]=== "'"  
+|| array[i]=== "?"  ){
+    continue;
+} else {
+phraseClean.push(array[i]); // phraseClean is the array with only letters.
+}
+}
+return phraseClean}
+
+let phraseClean = filtro(phraseToArray);
+let phraseReverse = filtro(phraseToArray).reverse();
+
+console.log(phraseClean);
+console.log(phraseReverse);
+
+let counter = 0;
+
+for(i=0;i<phraseClean.length;i++){
+
+if(phraseReverse[i] === phraseClean[i] ){
+    counter +=1
+} else {
+    console.log("Que no es Palindrome, ya te dije.")
+    break;
+}
+}
+
+if (counter == phraseClean.length){
+    console.log("Es palindrome, we!")
+}
