4364d2b0fea472c34166ac0cc21b1ae1dbb1f596
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-04-09 08:18:05 +0200

    lexicographic order

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ console.log("I'm ready!");
 // Iteration 1: Names and Input
 let hacker1 = "Yash";
 console.log(`The driver's name is : ${hacker1}`);
+
 let hacker2 = 'Tapan';
 console.log(`The Navigator's name is : ${hacker2}`);
 
@@ -17,9 +18,23 @@ console.log(`The Navigator's name is : ${hacker2}`);
     }
 
 // Iteration 3: Loops
-let toUpperCaseSpace = hacker1.toUpperCase().split("").join(" ");
-console.log(`The Driver's Name separated by space and in capitals is : ${toUpperCaseSpace}`);
+
+let capsSpace = hacker1.toUpperCase().split("").join(" ");
+console.log(`The Driver's Name separated by space and in capitals is : ${capsSpace}`);
+
 let reversed = hacker2.split("").reverse().join("");
 console.log(`Navigators's Name in reverse order is: ${reversed}`);
+
+
+  if((hacker1.localeCompare(hacker2)) === -1) {
+    console.log(`The driver's name ${hacker1} goes first.`);
+} 
+  else if ((hacker1.localeCompare(hacker2)) === 1) {
+    console.log(`Yo, the navigator ${hacker2} goes first definitely.`);
+} 
+  else {
+     console.log("What?! You both have the same name?");
+}
+
 
 
