633959c45ed88cde92536450ea386a02ffffb223
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-04-09 09:03:23 +0200

    for loops added

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
+
 let hacker1 = "Yash";
 console.log(`The driver's name is : ${hacker1}`);
 
 let hacker2 = 'Tapan';
 console.log(`The Navigator's name is : ${hacker2}`);
 
-//
+
 // Iteration 2: Conditionals
+
     if( hacker1.length > hacker2.length) {
         console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
     } else if (hacker2.length > hacker1.length) {
@@ -18,6 +20,25 @@ console.log(`The Navigator's name is : ${hacker2}`);
     }
 
 // Iteration 3: Loops
+
+/*
+let driver = 'Yash';
+let navigator = 'Tapan';
+
+let result = '';
+for(let i = 0; i < driver.length; i++) {
+  driver[i];
+  result += driver[i].toUpperCase() + ' ';
+  console.log(result);
+  }
+
+let reversedName = '';  
+for(i = navigator.length - 1; i >= 0; i--) {
+  navigator[i];
+  reversedName += navigator[i];
+  console.log(reversedName);
+}
+*/
 
 let capsSpace = hacker1.toUpperCase().split("").join(" ");
 console.log(`The Driver's Name separated by space and in capitals is : ${capsSpace}`);
