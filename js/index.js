295d970bc6114953a1688e20bf8594789ca64642
João Antunes <joaocbantunes@gmail.com>
2022-04-11 19:44:00 +0100

    bonus 1 done

--- js/index.js
+++ js/index.js
@@ -53,18 +53,27 @@ if (lexicOrder = -1) {
     console.log("What?! You both have the same name?");
 }
 
-/* Bonus 1
+// Bonus 1
 
-let paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris convallis et est eu commodo. Vestibulum ut varius lorem. Aliquam accumsan sagittis lacus ac luctus. Phasellus sed nibh malesuada, faucibus sem eu, posuere dolor. Ut lacinia ipsum ut odio placerat pharetra. Sed lectus ex, pretium ac ipsum vel, hendrerit finibus augue. Nullam sed lectus quis nunc faucibus viverra. Nulla consectetur ex augue, nec lobortis nisl ultrices non. Praesent in elementum leo. Cras et cursus est. Pellentesque eget pulvinar ante. Sed vehicula tellus sed dolor consequat scelerisque. Donec id porta nisl. Donec in pellentesque lectus. Vivamus laoreet tempor tellus, at maximus augue tincidunt quis. Mauris finibus ex massa, vitae dictum dui blandit ut.
+// Count Words - 280
 
-Suspendisse varius eu dui a lobortis. Integer dignissim arcu vestibulum metus fermentum, at aliquet metus molestie. Etiam in dictum enim, at suscipit augue. Maecenas at faucibus urna. Nam cursus sed dui at rutrum. Ut pellentesque enim nec metus consectetur elementum. Aenean vel purus eros. Duis dapibus semper purus a condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras aliquam orci ut sapien laoreet malesuada. Donec volutpat augue ac nisl eleifend facilisis. Phasellus et nunc dui. Fusce eu tempor neque, et feugiat purus. Maecenas imperdiet, sem ac dapibus sodales, felis est laoreet justo, ut dignissim odio felis maximus augue. Sed mollis lectus vitae arcu maximus blandit.
-
-Nunc at consequat libero. Sed sem odio, dapibus nec justo non, molestie placerat ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam ultrices purus purus, a lacinia mi tempus a. Phasellus felis est, dapibus a suscipit non, scelerisque eu justo. Aenean at nisi nec lacus interdum ornare. Vivamus consequat neque quis felis congue sollicitudin. Integer aliquam vestibulum risus, eu placerat orci tincidunt vel. Aenean quis enim ut velit interdum tincidunt. Sed dui mi, posuere id orci nec, cursus dignissim nisi. Sed nec maximus enim. Aenean ut odio molestie, eleifend turpis ut, efficitur ex.'
-
-let count = 0
-
-for (let i = 0; i <= paragraph.length; i++){
-    console.log(i);
+function getWordCount(str) {
+    return str.split(' ')
+      .filter(function(n) { return n != '' })
+      .length;
 }
 
-*/
+const sent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel mattis augue, at dignissim lectus. Maecenas interdum, felis vitae eleifend bibendum, purus felis ultrices leo, rhoncus tristique lorem ex venenatis dolor. Aliquam ac faucibus orci. Praesent id ex gravida, rutrum felis eget, ultricies nunc. Morbi auctor lorem metus, eu tempor velit ornare fringilla. Nulla sit amet elit quis massa malesuada vestibulum sed sed dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent efficitur in mi vel vehicula. Aenean condimentum, diam sagittis vulputate convallis, nunc mi dignissim diam, sed maximus erat elit id metus. Donec et dignissim eros. Pellentesque hendrerit a arcu in pretium. Vivamus erat enim, cursus non lacus placerat, mollis porttitor urna. Nullam id porta odio, id laoreet mi. Aliquam malesuada nisl tortor, at aliquet elit euismod eu. Donec suscipit porttitor rhoncus. Nulla a tellus ante. Vivamus quis augue nisl. Vivamus vitae tincidunt magna, at pharetra purus. Pellentesque in quam gravida, tincidunt nisi eu, faucibus ex. Curabitur a ornare dolor, id pulvinar quam. Integer elementum porttitor purus, eget dapibus est. Nunc sapien tellus, mattis egestas lectus id, dictum congue lectus. Mauris hendrerit luctus tortor, sit amet dignissim sapien finibus nec. Morbi pulvinar hendrerit felis vitae sodales. Suspendisse potenti. Phasellus dapibus augue quis lorem laoreet, id gravida mi consectetur. Maecenas facilisis pulvinar sem quis ullamcorper. Ut varius, leo a placerat vehicula, ligula velit malesuada tortor, a maximus augue leo sed justo. Ut tincidunt eget eros eget auctor. Morbi maximus pretium urna quis maximus. In nec mauris dignissim, pretium nisl a, malesuada leo. Donec consectetur enim et venenatis imperdiet. Aenean magna lectus, dictum sed convallis sit amet, hendrerit eget metus. Sed porttitor id nunc non ultricies.';
+
+
+console.log(getWordCount(sent));
+
+// Count number of times 'et' appears
+
+function countOccurences(string, word) {
+    return string.split(word).length - 1;
+ }
+ var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel mattis augue, at dignissim lectus. Maecenas interdum, felis vitae eleifend bibendum, purus felis ultrices leo, rhoncus tristique lorem ex venenatis dolor. Aliquam ac faucibus orci. Praesent id ex gravida, rutrum felis eget, ultricies nunc. Morbi auctor lorem metus, eu tempor velit ornare fringilla. Nulla sit amet elit quis massa malesuada vestibulum sed sed dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent efficitur in mi vel vehicula. Aenean condimentum, diam sagittis vulputate convallis, nunc mi dignissim diam, sed maximus erat elit id metus. Donec et dignissim eros. Pellentesque hendrerit a arcu in pretium. Vivamus erat enim, cursus non lacus placerat, mollis porttitor urna. Nullam id porta odio, id laoreet mi. Aliquam malesuada nisl tortor, at aliquet elit euismod eu. Donec suscipit porttitor rhoncus. Nulla a tellus ante. Vivamus quis augue nisl. Vivamus vitae tincidunt magna, at pharetra purus. Pellentesque in quam gravida, tincidunt nisi eu, faucibus ex. Curabitur a ornare dolor, id pulvinar quam. Integer elementum porttitor purus, eget dapibus est. Nunc sapien tellus, mattis egestas lectus id, dictum congue lectus. Mauris hendrerit luctus tortor, sit amet dignissim sapien finibus nec. Morbi pulvinar hendrerit felis vitae sodales. Suspendisse potenti. Phasellus dapibus augue quis lorem laoreet, id gravida mi consectetur. Maecenas facilisis pulvinar sem quis ullamcorper. Ut varius, leo a placerat vehicula, ligula velit malesuada tortor, a maximus augue leo sed justo. Ut tincidunt eget eros eget auctor. Morbi maximus pretium urna quis maximus. In nec mauris dignissim, pretium nisl a, malesuada leo. Donec consectetur enim et venenatis imperdiet. Aenean magna lectus, dictum sed convallis sit amet, hendrerit eget metus. Sed porttitor id nunc non ultricies.';
+ var count=countOccurences(text,"et");
+
+ console.log(count);
