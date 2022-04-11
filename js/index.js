c39b70b9c717f86fcef30d759810ea1f7cdf23ff
Pipokinha <pipokinha@superpro.email>
2022-04-11 23:37:22 +0100

    include bonus solutions

--- js/index.js
+++ js/index.js
@@ -45,8 +45,69 @@ let reversedName = "";
 
     //Bonus 1
 
-    let paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et scelerisque nulla. Fusce lacinia interdum sem, id pharetra libero scelerisque eget. Vestibulum tempor ex in lacus commodo, eget elementum nulla efficitur. Nunc congue mauris nec nunc mattis, volutpat consequat turpis eleifend. Nunc tristique arcu vitae justo pharetra porta. Aenean ac pharetra quam. Integer in convallis turpis, ornare tincidunt nulla. Duis vulputate elit id laoreet porttitor. Vestibulum facilisis enim quis est tempor, ac dignissim dolor dignissim. Morbi aliquet ipsum a sem dapibus, in ornare diam tincidunt.
+    let paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec nisi at quam molestie hendrerit et quis tellus. Donec eu lorem ultricies, accumsan ipsum gravida, imperdiet ante. Quisque consectetur felis elit, quis fermentum erat accumsan nec. Nam at sapien sodales, finibus lacus in, accumsan odio. Etiam massa ex, vehicula egestas purus id, pharetra dictum elit. Nunc ut nisl at nisi elementum gravida. Pellentesque dictum quis enim ac consequat. Nam eget tortor nec nunc vestibulum fringilla. Etiam erat est, tincidunt eget viverra quis, ornare sit amet ex. Nam rutrum maximus ligula, viverra consectetur enim pellentesque id. Donec lobortis turpis vel magna dictum, a scelerisque nisi accumsan. Donec leo libero, ullamcorper vitae dolor sed, cursus luctus nisi. Duis bibendum turpis sapien, non ornare dolor porta in. Ut vitae pretium lectus. In varius lacus quis porta vestibulum. Aliquam eget est libero. Donec lobortis ultricies vestibulum. Duis eu lectus sapien. Vivamus iaculis felis ut nibh vehicula consectetur. Nam condimentum enim quis velit tincidunt iaculis. Curabitur tempus lacinia magna, at bibendum mauris sagittis quis. Maecenas a arcu nec ligula laoreet tincidunt. Nulla facilisi. Quisque bibendum gravida congue. Maecenas sollicitudin lacus diam, sit amet blandit ligula lobortis eget. Aenean malesuada, est quis rutrum aliquet, mi sem rutrum neque, ac rhoncus tellus odio sit amet arcu. Pellentesque a aliquet leo. Maecenas semper enim nec mauris pharetra, vel condimentum magna volutpat. Etiam est turpis, eleifend sed egestas ac, lacinia eu enim. Nullam commodo erat ac lorem rutrum tempus. Aliquam faucibus iaculis turpis, vel consectetur ipsum viverra sit amet. Nullam vulputate lacinia condimentum. Ut nec dignissim justo. Phasellus nec justo metus. Phasellus porta tortor eu turpis pharetra sagittis. Mauris consequat mattis nunc, vitae ultrices mauris varius ut. Curabitur viverra mi at ipsum gravida posuere ut non odio. Nam eget accumsan diam, sit amet dignissim mauris. Fusce viverra quam non ipsum maximus porta. Vivamus malesuada nisi ut porttitor pellentesque. Vestibulum cursus aliquam orci. Etiam metus magna, bibendum ornare lorem ut, tristique viverra felis. Sed sit amet metus scelerisque, facilisis dui ac, scelerisque ipsum."
 
-    Vestibulum a neque vitae lectus porta facilisis. Sed varius, orci eu sodales fringilla, diam odio venenatis felis, eget mattis leo magna nec lorem. Maecenas id faucibus quam. Praesent venenatis urna nec metus tempus feugiat. Aenean vitae leo vestibulum, rutrum purus eget, tincidunt mauris. In non dui dictum, sollicitudin massa vel, tincidunt tortor. Nunc cursus, leo non dignissim auctor, turpis orci euismod neque, vitae euismod metus magna in purus. Etiam sodales non quam vel auctor. Morbi consectetur volutpat egestas. Nullam at interdum sapien. Curabitur ut nibh augue.
+    function countWords(str) {
+        const arr = str.split(' ');
+      
+        return arr.filter(word => word !== '').length;
+    }
+
+      console.log(countWords(paragraph))
+
+    function countEt(message, wordToCount) {
+        let splitMessage = message.split(' ');
+        
+        let sum = 0
+        for (let i = 0; i < splitMessage.length; i++) {
+            if (splitMessage[i] === wordToCount) {
+                sum = sum + 1
+            }
+        }
+        return sum
+    }
     
-    Maecenas in urna et massa aliquam rutrum. Praesent elementum maximus tellus, sed gravida metus interdum vel. Nunc ac magna diam. Donec rhoncus dolor lacus, et laoreet massa vestibulum quis. Cras tristique nibh dignissim eros rhoncus lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis dignissim mauris sit amet enim placerat dapibus. Integer commodo lacus diam, non porttitor mauris dapibus a. Nunc vitae luctus ex."
+    console.log(countEt(paragraph, "et"))
+
+// for each 
+
+/* function countEt(message, wordToCount) {
+    let splitMessage = message.split(' ');
+    
+    let sum = 0
+    splitMessage.forEach(word => {
+        if (word === wordToCount) {
+            sum = sum + 1
+        }
+    }
+    return sum
+}
+
+console.log(countEt(paragraph, "et")) */
+
+
+// Bonus 2
+
+let phraseToCheck = "step on no pets";
+
+function checkPalindrome(string) {
+
+    const len = string.length;
+
+    for (let i = 0; i < len / 2; i++) {
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+console.log(checkPalindrome(phraseToCheck));
+
+/* Reverse
+const checkPalindrome = (string) => string.slice().reverse === string; */
+
+
+
+
+
