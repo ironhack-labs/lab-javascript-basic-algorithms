c9ac5a01b60a19dd0358085dcd630307579c8ec6
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-04-30 11:34:39 +0200

    first iteration done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
-//
+const hacker1 = 'Adrian'
+
+console.log(`"The driver's name is ${hacker1}"`)
+
+const hacker2 = 'Luis'
+
+console.log(`"The navigator's name is ${hacker2}"`)
+
+
 // Iteration 2: Conditionals
 
 
