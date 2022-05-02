019ff7b106a24a0dbea509b8ac62ba97c088a3e8
Eric Company <ericcovi@hotmail.com>
2022-05-02 22:13:02 +0200

    Adding extra 2 and correcting extra 1

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ console.log(`"The driver's name is ${hacker1}"`)
 
 // 	1.3 Create a variable `hacker2` with the navigator's name.
 
-let hacker2 ="Eric"
+let hacker2 ="Giacomo"
 
 // 	1.4 Print `"The navigator's name is YYYY"`.
 
@@ -73,15 +73,58 @@ if (hacker1===hacker2){
     }
     }
 
-
+// Extra 1
 // Creating the string
-let loremIpsum = `What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`
+let loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu facilisis metus. Nullam non ante vel lacus blandit tempus id quis enim. Vestibulum augue diam, convallis eu tincidunt quis, maximus at augue. Aliquam venenatis, enim nec porttitor suscipit, diam nisi luctus augue, et vestibulum turpis eros sit amet mauris. Donec justo purus, interdum ut nisl in, luctus fermentum urna. Praesent tristique felis non est pharetra, a fringilla neque consequat. Fusce tellus nulla, fermentum sit amet erat a, commodo pellentesque velit. Cras venenatis, ipsum et eleifend pharetra, risus ex vulputate ex, et dignissim nulla sapien vitae sapien. Sed efficitur quis risus vel sollicitudin. Sed vitae efficitur mauris, id pellentesque ante. Integer iaculis nulla sed fringilla convallis. Etiam mollis purus quam, non posuere dolor fringilla eu. Praesent euismod sem ut cursus iaculis. Cras non quam accumsan, aliquet nisi at, lacinia leo. Donec et iaculis odio. Nunc ut dictum tortor. Nullam eget efficitur sem, varius finibus tellus. Etiam tempus nibh nulla, eu condimentum tellus tincidunt tempus. Aliquam id leo vitae odio commodo dictum. Aliquam faucibus scelerisque metus eget tincidunt. Morbi vehicula, mi ut ullamcorper blandit, magna quam sodales nisi, id vulputate ante erat eget lacus. Aliquam arcu lacus, mollis sed ante in, pretium convallis felis. Donec ex metus, tincidunt in risus id, accumsan maximus nisi. Proin suscipit sollicitudin tortor nec dignissim. Aliquam pellentesque sed dolor ut suscipit. Mauris fringilla, quam tempus condimentum porta, urna quam lobortis nisl, sit amet malesuada ante lacus quis velit. Vestibulum non varius mauris. Maecenas a faucibus dui. Morbi quis tristique arcu. Mauris euismod, augue sodales lacinia mollis, diam augue dignissim orci, at tempor ante dui in nisl. Donec mattis tincidunt pretium. Fusce finibus facilisis risus vel mattis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin eu magna tincidunt, ultricies est non, tristique odio. Praesent nec dui vulputate, varius ligula sed, pulvinar lorem. Morbi scelerisque vitae purus sed cursus. Morbi pellentesque est a rhoncus ullamcorper.`
 
+// counting the number of words
 let countWords = 0
 for (let i=0; i < loremIpsum.length; i++) {
     if (loremIpsum[i] === " "){
         countWords += 1;
     }
-    countWords +=1 ;
+    
 }
+countWords +=1 ;
 console.log(countWords)
+
+//counting the number of et words
+let countEts=0
+let loremIpsumLowerCase = loremIpsum.toLowerCase()
+for (let index = 0; index<loremIpsumLowerCase.length; index += 1) {
+    if(loremIpsumLowerCase[index] === "e" && loremIpsumLowerCase[index+1] === "t" && loremIpsumLowerCase[index+2] === " " ) {
+        countEts += 1;
+    } 
+}
+
+console.log(countEts)
+
+//Extra 2
+// checking the palyndrome 
+// declaring the palyndrome
+let phraseToCheck= ""
+// transforming the string into lower case
+let phraseToLowerCase = phraseToCheck.toLowerCase()
+// extracting punctuation signs from the string
+let phraseWithoutPunctuation = phraseToLowerCase.replace(/[^\w\s\']|_/g, "")
+// extracting whitespaces
+let phraseWithoutWhitespaces = phraseWithoutPunctuation.replace(/\s/g,'') 
+let palyndrom = false
+// creating an index that points to the last position of the string in order to compare in the loop
+let oppositeIndex = phraseWithoutWhitespaces.length-1
+// checking pairs of letters to check if they're equal
+for (let index=0; index < (phraseWithoutWhitespaces.length)/2; index += 1) {
+    if (phraseWithoutWhitespaces[index] !== phraseWithoutWhitespaces[oppositeIndex-index]){
+        palyndrom = false
+        break
+    } else{
+        palyndrom = true
+    }
+}
+
+// Output depending on result
+if (palyndrom) {
+    console.log("It is a palyndrome")
+} else{
+    console.log("It is not a palyndrome")
+}
