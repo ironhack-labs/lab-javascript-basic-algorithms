282a790167699b359e93a0211efebc27fc47f8b4
Eric Company <ericcovi@hotmail.com>
2022-05-02 16:00:40 +0200

    Adding iteration 2

--- js/index.js
+++ js/index.js
@@ -10,12 +10,24 @@ console.log(`"The driver's name is ${hacker1}"`)
 
 // 	1.3 Create a variable `hacker2` with the navigator's name.
 
-let hacerk2 ="Giacomo"
+let hacker2 ="Giacomo"
 
 // 	1.4 Print `"The navigator's name is YYYY"`.
 
 console.log(`"The navigator's name is ${hacker2}"`)
+
 // Iteration 2: Conditionals
+// 2.1. Depending on which name [is longer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length), print:
+//  <br>
+//	  - `The driver has the longest name, it has XX characters.` or <br>
+//	  - `It seems that the navigator has the longest name, it has XX characters.` or <br>
+//	  - `Wow, you both have equally long names, XX characters!`.
 
-
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+    conseole.log(`Wow, you both have equally long names, ${hacker1.length} characters`)
+}
 // Iteration 3: Loops
