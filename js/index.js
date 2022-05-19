aa7f366484ffb4aee0c9de08ea334a59072f121a
Eduard Tudorache <edy_edytza2003@yahoo.com>
2022-05-19 23:48:52 +0200

    added solutions for bonus exercises, need help on both of them, got ideeas on how to solve both, couldn't manage to finish either for on reason or another

--- js/index.js
+++ js/index.js
@@ -47,3 +47,59 @@ if (hacker1Letter[0] > hacker2Letter[0]) {
 }else if (hacker1Letter === hacker2Letter) {
     console.log("What?! You both have the same name?")
 }
+
+// bonus 1
+
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+let text1 = text.split("")
+let wordCount = 0
+let etCount = 0
+
+for (i = 0; i < text1.length; i++) {
+    if (" ") {
+        wordCount += 1
+    }else {
+        continue
+    }
+}
+
+console.log (wordCount)
+
+
+for (j = 0; j < text1.length; j++) {
+    if ("et") {
+        etCount += 1
+    }else {
+        continue
+    }
+}
+
+console.log (etCount)
+
+
+// bonus 2
+
+let phraseToCheck = "A man, a plan, a canal, Panama!"
+let phraseToCheckPunctuationless = ""
+
+// solution 1
+for (i = 0; i < phraseToCheck.length; i++) {
+    if (",") {
+        continue
+    } else if ("!") {
+        continue
+    } else {
+        phraseToCheckPunctuationless += 
+    }
+    console.log(phraseToCheckPunctuationless)
+}
+
+// solution 2
+
+if (phraseToCheck.toLowerCase().split("").reverse().join("") === phraseToCheck) {
+    console.log("true")
+}
+
+console.log(phraseToCheck.toLowerCase().split("").reverse().join(""))
+
