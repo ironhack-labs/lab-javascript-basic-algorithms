bc1da27ce5623d2b3d0851c5523423821e358341
Maxime Erkens <mhmserkens@gmail.com>
2022-05-19 22:12:08 +0200

    Daiane and Maxime finish lab javascript basic algorithms

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+const hacker1 = "Daiane";
+console.log("The driver's name is " + hacker1 + ".");
+const hacker2 = "Maxime";
+console.log("The navigator's name is " + hacker2 + ".");
 
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
 // Iteration 3: Loops
+let nameInCapitals = "";
+for (let i = 0; i < hacker1.length; i++) {
+  nameInCapitals += hacker1[i].toUpperCase() + " ";
+}
+console.log(nameInCapitals);
+
+let nameInReverse = "";
+let length = hacker1.length - 1;
+for (let i = length; i >= 0; i--) {
+  nameInReverse += hacker1[i];
+}
+console.log(nameInReverse);
+
+if (hacker1 < hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker1 > hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
