8627e9740e9ddfc9dfc5a8804f2903ac883547b4
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-05-19 13:37:24 +0200

    First exercise done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+console.log ("I'm ready!!")
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Marta";
+console.log (`The driver's name is ${hacker1}`);
+let hacker2 = "Sara";
+console.log (`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
+
+
+// 
 // Iteration 3: Loops
