0c2b09651c9ef491a9315da79880ea8cd57d5a0c
Alyssa Sitto <alyssasitto1@gmail.com>
2022-06-08 19:23:22 -0700

    Add index.html file and add javascript logic

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
-
-
 // Iteration 3: Loops
+
+let driver = "jamie";
+let navigator = "samantha";
+
+// 1.
+let i = 0;
+while (i <= 30 - 1) {
+	i++;
+	console.log(i);
+	if (i === 10) {
+		console.log("ten");
+	} else if (i === 20) {
+		console.log("twenty");
+	}
+}
+
+// 2.
+driver = driver.toUpperCase();
+
+let str = "";
+for (const letter of driver) {
+	str += letter + " ";
+}
+console.log(str);
+
+let str2 = "";
+for (let i = navigator.length - 1; i >= 0; i--) {
+	str2 += navigator[i];
+}
+console.log(str2);
+
+// 3.
+const compare = driver.localeCompare(navigator);
+
+if (driver.localeCompare(navigator) > 0) {
+	console.log("The drivers name goes first.");
+} else if (driver.localeCompare(navigator) < 0) {
+	console.log("Yo, the navigator goes first definitely.");
+} else {
+	console.log("What!? You both have the same name?");
+}
