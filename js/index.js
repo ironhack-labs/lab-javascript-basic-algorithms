401c156ab5b32e248785bb0097e1fdaf08da653d
Nic Solitom <nicsolitom@gmail.com>
2022-06-15 12:19:07 +0200

    Iteration 1 - done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Nic";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Begum";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
