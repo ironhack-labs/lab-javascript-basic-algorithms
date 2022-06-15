c9ecebc2aaa598db68f90a869e890ea7b1a56792
Begum Eryildiz <begumeryildiz@gmail.com>
2022-06-15 15:26:46 +0200

    added Iteration 3.3

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 
+// Iteration 3.1
 
 let hacker1WithSpace = "";
 for (let i = 0; i < hacker1.length; i++) {
@@ -27,9 +28,48 @@ hacker1WithSpace = hacker1WithSpace.slice(0, -1);
 hacker1UpperCase = hacker1WithSpace.toUpperCase();
 console.log(hacker1UpperCase);
 
+// Iteration 3.2
 
 let hacker2Reversed = "";
 for (let j = hacker2.length-1; j >= 0; j--) {
     hacker2Reversed += hacker2[j];
 }
 console.log(hacker2Reversed);
+
+//Iteration 3.3
+
+let minNameLength = "";
+
+if (hacker1.length <= hacker2.length) {
+    minNameLength = hacker1.length;
+} else {
+    minNameLength = hacker2.length;
+}
+
+let situation = 0;
+for (let i = 0; i < minNameLength; i++) {
+    
+    if (hacker1[i] < hacker2[i]) {
+        situation = 1; 
+        break;
+    } else if ( hacker1[i] > hacker2[i]) {
+        situation = 2;
+        break;
+    } else {
+      
+    }
+}
+
+if (situation === 1 ) {
+    console.log("The driver's name goes first");
+} else if (situation === 2) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    if (hacker1.length < hacker2.length) {
+      console.log("The driver's name goes first");
+    } else if (hacker1.length > hacker2.length) {
+      console.log("Yo, the navigator goes first definitely.");
+    } else {
+      console.log("What?! You both have the same name?");
+    }
+}    
