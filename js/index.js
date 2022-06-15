4a878410554f8736567fc72b2618fa417cabdf18
Matthieu Duperrey <matth.duperrey@gmail.com>
2022-06-15 15:28:08 +0200

    Iteration 2 name.length

--- js/index.js
+++ js/index.js
@@ -4,8 +4,18 @@ console.log(`The driver's name is ${hacker1}`);
 
 const hacker2 = "Bob"
 console.log(`The navigator's name is ${hacker2}`)
+
+
 // Iteration 2: Conditionals
+let hacker1NameLength = hacker1.length
+console.log(hacker1NameLength);
+
+
+
+let hacker2NameLength = hacker2.length
+console.log(hacker2NameLength);
+
+
 
 
 // Iteration 3: Loops
-console.log("I'm ready!");
