79c7c9a2719a44de0aac2b273915b86ad13852a4
Ivona Pepic <ivona.pepic@gmail.com>
2022-06-15 11:55:09 +0200

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Iteration 1: Names and Input
 //
+const hacker1 = "Ivona";
+console.log(`The drivers name is ${hacker1}`);
+
+const hacker2 = "Alex";
+console.log(`The navigators name is ${hacker2}`);
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+console.log("I am reay");
