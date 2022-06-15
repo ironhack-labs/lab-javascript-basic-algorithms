23bdb36bbf7113810a0862dd744a5be59f6aaafe
Matthieu Duperrey <matth.duperrey@gmail.com>
2022-06-15 15:40:48 +0200

    Iteration 2 seem to be done…

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const hacker1 = "Matt"
 console.log(`The driver's name is ${hacker1}`);
 
-const hacker2 = "Bob"
+const hacker2 = "Boby"
 console.log(`The navigator's name is ${hacker2}`)
 
 
@@ -10,10 +10,16 @@ console.log(`The navigator's name is ${hacker2}`)
 let hacker1NameLength = hacker1.length
 console.log(hacker1NameLength);
 
-
-
 let hacker2NameLength = hacker2.length
 console.log(hacker2NameLength);
+
+if (hacker1NameLength > hacker2NameLength) {
+  console.log(`The driver has the longest name, it has ${hacker1NameLength} characters.`);
+} else if (hacker1NameLength < hacker2NameLength) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2NameLength}`)
+} else {
+  console.log(`Wow, you both have equally long names, ${hacker1NameLength} and ${hacker2NameLength}`)
+}
 
 
 
