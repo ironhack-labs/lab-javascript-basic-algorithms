79eab43f8b84b22a4ee41a3e1eaf265251c2ff05
heliodcf <heliodcf@gmail.com>
2022-06-17 18:06:03 -0400

    Segunda Inteiracao feita

--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ console.log(`"The driver's name is ${hacker1}"`);
 const hacker2 = "Helio";
 console.log(`"The navigator's name is ${hacker2}"`);
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+  } else if (hacker1.length < hacker2.length) {
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters.`);
+  } else {
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
+  }
 
 // Iteration 3: Loops
