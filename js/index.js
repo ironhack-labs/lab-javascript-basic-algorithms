fed3d7e163c24bf05a2ab68e1e2953abb2132acf
Hugo Lomba <hmlomba@gmail.com>
2022-06-17 00:10:04 -0300

    Finalizada iteração 2

--- js/index.js
+++ js/index.js
@@ -4,7 +4,17 @@ const hacker2 = "Caio"
 
 console.log(`The drivers name is ${hacker1}`)
 console.log(`The navigators name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
+if (hacker1.length === hacker2.length) {
+    console.log (`Wow, you both have equally long names, ${hacker1.length} characters`)
+  }  else if (hacker1.length > hacker2.length) {
+      console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+  } else if (hacker2.lenght > hacker1.length) {
+      console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} charecters.`);
+  } 
+  
+  
 
 // Iteration 3: Loops
