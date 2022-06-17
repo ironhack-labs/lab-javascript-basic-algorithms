7bd9f543cb0bf7739583ead6f847567a8fb10ad6
Hugo Lomba <hmlomba@gmail.com>
2022-06-17 00:08:29 -0300

    Finalizada iteração 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-//
+const hacker1 = "Hugo"
+const hacker2 = "Caio"
+
+console.log(`The drivers name is ${hacker1}`)
+console.log(`The navigators name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
