8f8fe4408acc0094805f05c6822add5a566604ba
Hugo Lomba <hmlomba@gmail.com>
2022-06-17 00:14:19 -0300

    Finalização iteração 3

--- js/index.js
+++ js/index.js
@@ -15,6 +15,32 @@ if (hacker1.length === hacker2.length) {
       console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} charecters.`);
   } 
   
-  
-
 // Iteration 3: Loops
+
+let nome = "";
+for (let i = 0; i < hacker1.length; i++) {
+nome += hacker1[i] + " ";
+}
+console.log(nome)
+
+
+let nome2 = [];
+for (let i = 0; i < hacker2.length; i++) {
+nome2.push(hacker2[i])
+}
+nome2.reverse();
+console.log(nome2)
+
+nome2.sort();
+
+console.log(nome2)
+
+
+
+if (hacker1[0] < hacker2[0]) {
+  console.log(`The ${hacker} name goes first`) 
+} else if (hacker1[0] > hacker2[0]) {
+  console.log(`Yo, the ${hacker2} goes first definitely.`)
+} else if (hacker1 === hacker2) {
+  console.log("What?! You both have the same name?")
+}
