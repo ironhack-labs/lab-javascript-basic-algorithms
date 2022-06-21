190fd69f7efa9483ad412ca4a9cba8ea2d2a30ab
Macia Estela <maciaestela97@gmail.com>
2022-06-21 21:14:23 +0200

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+console.log("I'm ready");
 // Iteration 1: Names and Input
-//
+const hacker1 = "Mark";
+console.log("The driver's name is " + hacker1);
+const hacker2 = "Oscar";
+console.log("The driver's name is " + hacker2);
 // Iteration 2: Conditionals
 
 
