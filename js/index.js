82891cdb99362f86ccd981ec00c0de5965987167
Samuel Taboas <droidsam@gmail.com>
2022-06-23 20:21:58 +0200

    solution to first iteration

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Iteration 1: Names and Input
+let hacker1 = "XXXX";
+let hacker2 = "YYYY";
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+
+
 //
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
