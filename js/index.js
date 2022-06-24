e0a9f1df8eb4d8783de9c1e6ea94078cff94250d
Samuel Taboas <droidsam@gmail.com>
2022-06-24 09:35:48 +0200

    add word et counter and style console logs

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 // Iteration 1: Names and Input
-let hacker1 = "driver";
-let hacker2 = "navigator";
+console.log("");
+console.log('Iteration 1️⃣ : Names and Input')
+console.log("");
+let hacker1 = "driversName";
+let hacker2 = "navigatorsName";
 
 console.log(`The driver's name is ${hacker1}`);
 console.log(`The navigator's name is ${hacker2}`);
 //
 // Iteration 2: Conditionals
+console.log("");
+console.log('Iteration 2️⃣ : Conditionals')
+console.log("");
+
 if (hacker2.length === hacker1.length) {
   console.log(
     `Wow, you both have equally long names, ${hacker1.length} characters!`
@@ -21,6 +28,10 @@ if (hacker2.length === hacker1.length) {
 }
 //
 // Iteration 3: Loops
+console.log("");
+console.log('Iteration 3️⃣ : Loops')
+console.log("");
+
 let letterSpacedName = "";
 for (let letter of hacker1) {
   letterSpacedName += `${letter.toUpperCase()} `;
@@ -42,19 +53,18 @@ if (hacker1.localeCompare(hacker2) === 0) {
 }
 
 //Bonus
+console.log("");
+console.log('Bonus 1️⃣')
+console.log("");
 
 const paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec interdum lacus. Suspendisse aliquam molestie enim ut molestie. Quisque eros erat, interdum eget pretium nec, placerat eget leo. Praesent volutpat sed ligula et mollis. Pellentesque sit amet neque odio. Duis volutpat nibh vel varius gravida. Etiam tincidunt, elit id feugiat cursus, odio lectus finibus purus, in sollicitudin sapien orci suscipit dolor. Nullam gravida dignissim varius. Maecenas malesuada vulputate justo, id varius risus luctus sit amet. In eget faucibus nulla. Nullam sed rutrum mi.
 
 Integer imperdiet lobortis iaculis. Aenean ultricies, justo in interdum pretium, ligula eros vulputate odio, vitae consectetur lacus urna ut enim. Nulla faucibus vel quam a facilisis. Etiam congue diam ex. Vestibulum mattis tincidunt erat pulvinar tincidunt. Aenean pellentesque nisi at porta volutpat. Maecenas pulvinar nec enim in porta. Nulla facilisis arcu mollis dolor molestie, vel auctor elit tempor. Mauris ultricies blandit arcu ac congue. Nam facilisis gravida risus non commodo. Nullam venenatis felis nec sem molestie, vitae facilisis massa facilisis. Quisque ut justo ultricies, bibendum nunc non, posuere odio. Praesent quis magna ac sapien eleifend malesuada. Sed nec leo et lacus fermentum pulvinar fermentum consectetur magna. Cras malesuada, ipsum a porttitor lobortis, dui ligula egestas mauris, et faucibus diam arcu eu neque. Vestibulum a hendrerit nulla, placerat ornare lectus.
 
 Ut in ex volutpat, pellentesque arcu id, tempus ex.      Duis ac dapibus metus. Suspendisse semper ligula sed turpis laoreet rhoncus. Nunc quis lectus dictum, maximus ex a, dapibus augue. Phasellus volutpat tincidunt leo, at imperdiet metus pulvinar eget. Nam ac ligula ac tellus maximus sollicitudin non sit amet ex. Phasellus venenatis ligula rhoncus diam placerat elementum.`;
-// If I could, I would use something like...
-// const MULTIPLE_NEW_LINE_REGEXP = /\n\s*\n/g;
-// let wordCounter = paragraph.replace(MULTIPLE_NEW_LINE_REGEXP, ' ').split(' ').filter(w => w!== '').length;
-// console.log(wordCounter);
 
 let wordCounter = 1;
-for (let i = 0; i < paragraph.length; i++) {
+for (let i = 1; i < paragraph.length - 1; i++) {
   let isSigleLineBreak = paragraph[i] === "\n";
   let isDoubleLineBreak = paragraph[i] === "\n" && paragraph[i - 1] === "\n";
   let isDoubleSpace = paragraph[i - 1] === " " && paragraph[i] === " ";
@@ -67,7 +77,25 @@ for (let i = 0; i < paragraph.length; i++) {
     wordCounter++;
   }
 }
-console.log(wordCounter);
+console.log(`Word counter: ${wordCounter}`);
+
+//Make your program count the number of times the Latin word et appears.
+let etCounter = 0;
+for (let i = 2; i < paragraph.length - 2; i++) {
+  if (
+    paragraph[i - 2] === " " &&
+    paragraph[i - 1] === "e" &&
+    paragraph[i] === "t" &&
+    paragraph[i + 1] === " "
+  ) {
+    etCounter++;
+  }
+}
+console.log(`Number of word 'et' appearances: ${etCounter}`);
+
+console.log("");
+console.log('Bonus 2️⃣')
+console.log("");
 
 const phraseToCheck = "A man, a plan, a canal, Panama!";
 // const phraseToCheck = "Amor, Roma";
@@ -98,4 +126,11 @@ for (let i = 0; i < normalizedString.length / 2; i++) {
     break;
   }
 }
-console.log(`The phrase: ${phraseToCheck} is palindrome?: ${isPalindrome}`);
+let sentenceToPrint = `The phrase: '${phraseToCheck}'`;
+if(isPalindrome){
+  sentenceToPrint+= ` is a palindrome ✅`
+}else{
+  sentenceToPrint+= ` is NOT a palindrome ❌`
+}
+console.log(sentenceToPrint);
+console.log("")
