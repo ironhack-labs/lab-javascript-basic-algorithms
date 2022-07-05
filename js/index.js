7d71eb508d06006a0cd52c4c65def2e52709f374
Leila Badra <leila.badra@hotmail.com>
2022-07-05 22:46:59 +0200

    Itineration 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-// Iteration 1: Names and Input
-//
+// Iteration 1: Names and Input//
+
+let hacker1;
+hacker1= 'Leila';
+console.log("the driver's name is",hacker1);
+let hacker2;
+hacker2= 'Mariana';
+console.log("The navegator's name is", hacker2)
+
 // Iteration 2: Conditionals
-
-
 // Iteration 3: Loops
