d9474edf9200a7c7fa9f0b846d35a2c26757874a
Mario Blazquez <mario.blazquez.m@gmail.com>
2022-07-05 21:35:56 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
 //
+let hacker1="Mario"
+console.log(`The driver's name is ${hacker1}`)
+let hacker2="Nacho"
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
