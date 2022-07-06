fc3fa8de13be03c3deb8d8db4020b2f4b283c48e
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-07-06 23:48:06 +0200

    Bonus 1 done

--- js/index.js
+++ js/index.js
@@ -53,4 +53,38 @@ else {
     console.log("What?! You both have the same name?");
 }
 
+//BONUS 1.1 WORDS in string
 
+let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper rhoncus finibus. Donec quis metus sed dui finibus sollicitudin. Phasellus vitae convallis sem, non sodales quam. Etiam tempor finibus velit in efficitur. Vestibulum ex erat, blandit nec facilisis eget, fringilla ut magna. Sed et tincidunt dolor. Proin non odio ut nunc egestas feugiat at et urna. Nullam neque quam, sagittis id metus nec, tincidunt iaculis metus. Praesent pharetra condimentum commodo. Proin mattis, arcu sed vulputate congue, nulla ligula iaculis mauris, ut dapibus enim eros in lectus. Mauris vitae lectus nec eros porta sagittis vitae sit amet eros. Vivamus non scelerisque orci. Nulla consequat enim nec lacus dignissim fringilla scelerisque nec tortor. Nullam dapibus purus quis efficitur finibus. Aliquam non lacus pellentesque, congue risus ac, molestie enim. Nunc et diam ultricies, ultrices arcu nec, scelerisque lacus. Sed placerat libero nec sodales consequat. Sed vehicula ac ipsum ut laoreet. Quisque a arcu est. Donec leo elit, semper ornare lobortis non, mollis a lacus. Sed eu turpis a felis hendrerit laoreet nec quis erat. Morbi eget auctor neque. Nunc sit amet efficitur odio. Ut vehicula lacus neque, nec elementum neque malesuada ac. Curabitur sagittis eu sapien ac gravida. Suspendisse finibus quam quis lacus imperdiet maximus. Vivamus tincidunt lacus purus. Mauris nulla ipsum, faucibus maximus ex ut, lobortis condimentum metus. Nulla eros est, molestie id neque eget, bibendum commodo magna. Fusce ut tincidunt felis. Integer elementum in odio sed auctor."
+
+let words = 0;
+//find & store the number of spaces, add 1 -> that is the number of words
+for (i in loremIpsum) {
+    if (loremIpsum[i] === ' ') {
+        words++;
+    }
+}
+console.log(words+1);
+
+// BONUS 1.1 "et" count
+let etCount = 0;
+
+/*find the first occurence of "et", 
+cut out the preceding part of the string + the occurence itself, 
+store the # of the occurence, 
+and keep going until you reach the end of the string */
+
+while (loremIpsum.length > 0) { 
+    if(loremIpsum.indexOf(" et ") > 0) {
+    loremIpsum = loremIpsum.slice(loremIpsum.indexOf(" et ") + 2);
+    etCount++;
+    }
+    else break;    
+}
+// in case of no occurences
+if (etCount === 0) {
+    console.log("'et' does not appear in the text.")
+}
+else {
+    console.log(etCount);
+}
