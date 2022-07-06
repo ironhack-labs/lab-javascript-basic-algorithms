8a11879d4d2edf5b0f0092dfc27c77bde894ccf6
aarestigue <108528013+aarestigue@users.noreply.github.com>
2022-07-06 21:43:36 +0100

    bonus 1

--- js/index.js
+++ js/index.js
@@ -44,3 +44,20 @@ else if (hacker1.localeCompare(hacker2)=== 1) {
 else {
   console.log("What?! You both have the same name?");
 }
+
+/* Bonus 1 */
+let countOfWords = 1
+
+let loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mollis lacus. Nulla viverra, ligula suscipit tincidunt vehicula, nisl augue sodales leo, vel consequat neque nisl sodales enim. Duis efficitur libero nisi, eu blandit erat tincidunt id. Curabitur vitae ante eget nisi sollicitudin gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi efficitur mauris nec odio porta placerat. Donec et sapien finibus, pellentesque massa eu, vehicula ante. Etiam lobortis mi nisl, et fringilla augue volutpat sit amet. Nullam sit amet efficitur felis, commodo rhoncus ligula.
+
+Nulla scelerisque lectus eget tortor lacinia, sit amet dignissim magna tincidunt. Cras id tempor libero. Donec mattis id nisl a fringilla. Fusce facilisis dolor vel erat ultrices volutpat. Mauris eget nunc eu massa fringilla porta. Duis elit urna, aliquet vestibulum est non, rhoncus eleifend sem. Donec cursus scelerisque scelerisque. Phasellus luctus accumsan mi id imperdiet. Pellentesque turpis nibh, rutrum sit amet efficitur et, viverra a justo. Nulla malesuada ornare orci, eget porttitor massa auctor vel. Vestibulum odio dolor, pulvinar at aliquam eu, ultrices id felis. Nunc ac magna quam.
+
+Ut suscipit nisi eu nunc ultricies efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin vitae dui porta, maximus quam et, mattis enim. Nullam ultrices enim in enim ornare lacinia hendrerit vitae risus. Fusce justo enim, pretium et ultricies sit amet, finibus eu orci. Praesent sit amet odio consectetur, rhoncus tortor quis, egestas nibh. Mauris varius, est nec laoreet tincidunt, felis neque sollicitudin risus, vel laoreet massa diam quis lorem. Aenean aliquam tincidunt ligula, sed volutpat ipsum pellentesque at. Ut venenatis molestie tristique. Fusce fermentum in leo in scelerisque. Donec risus mauris, elementum sed ultricies auctor, tincidunt ut magna. Curabitur nec metus massa. Phasellus aliquet libero eu tristique lacinia.`
+
+for (let i=0; i < loremIpsum.length; i++ ) {
+    if (loremIpsum[i] === ' ') {
+        countOfWords += 1;
+    }
+ }
+
+ console.log (countOfWords);
