2add0bcd003a3b189cfead1c07c84a03d9d74097
Lena Araújo <lenaaraujo92@gmail.com>
2022-07-13 16:16:48 -0300

    iterations 1-3 - done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,57 @@
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Lena";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Lena";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!.`
+  );
+}
 
 // Iteration 3: Loops
+
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+
+let spacedName = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+  if (i === hacker1.length - 1) {
+    spacedName += hacker1[i];
+  } else {
+    spacedName += hacker1[i] + " ";
+  }
+}
+console.log(spacedName.toUpperCase());
+
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+let reverseOrder = "";
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reverseOrder += hacker2[i];
+}
+console.log(reverseOrder);
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+
+if (hacker1 < hacker2) {
+  console.log(`The driver's name goes first.`);
+} else if (hacker1 > hacker2) {
+  console.log(`Yo, the navigator goes first definitely.`);
+} else {
+  console.log(`What?! You both have the same name?`);
+}
