c8be545dad9649e00c60af1d10e5223964ba5141
Romina Polo <rominalpolo@gmail.com>
2022-07-14 22:29:58 -0500

    completed first 2 iterations and first part of 3rd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 // Iteration 1: Names and Input
-//
+
+const hacker1 = 'Romina';
+const hacker2 = 'John';
+console.log(`The driver's name is ${hacker1}.`);
+console.log(`The navigator's name is ${hacker2}.`);
+
 // Iteration 2: Conditionals
 
+if (hacker1.length >= hacker2.length) {
+  console.log (`The driver has the longest name, it has ${hacker1.length} characters.`);
+}
+else if (hacker2.length >= hacker1.length) {
+  console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+}
 
 // Iteration 3: Loops
+
+const splittedString =hacker1.split('').join(' ').toUpperCase();
+console.log(splittedString);
+
