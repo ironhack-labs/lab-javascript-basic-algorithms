1ff934b689af306522bfc12fecd68ca5ec020825
Eurípedes <euripedes.naves@outlook.com>
2022-07-14 22:57:06 -0300

    Done ajusted lest word without backspace - Less Bonus

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 
 } else if (hacker1.length < hacker2.length) {
-        console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 
 } else {
     console.log(`Wow, you both have equally long names, it has ${hacker2.length} characters!`);
@@ -27,19 +27,25 @@ if (hacker1.length > hacker2.length) {
 // Adicionando espaço em todas as letras, ajustar para no final não ter espaço (ultima letra sem espaço)
 let UpperCaseDriverName = "";
 
-for(let i=0; i <hacker1.length; i++){
+for (let i = 0; i < hacker1.length; i++) {
 
-    UpperCaseDriverName += hacker1[i].toUpperCase() + " "; 
+    if (hacker1.length === i) {
+        UpperCaseDriverName += hacker1[i].toUpperCase();
+
+    } else {
+        UpperCaseDriverName += hacker1[i].toUpperCase() + " ";
+    }
 }
+
 console.log(UpperCaseDriverName);
 
 //Reverse
 let ReverseNavigator = "";
 
 
-for(let i=hacker2.length-1; i >= 0; i--){
+for (let i = hacker2.length - 1; i >= 0; i--) {
 
-    ReverseNavigator += hacker2[i]; 
+    ReverseNavigator += hacker2[i];
 }
 
 console.log(ReverseNavigator);
@@ -50,12 +56,12 @@ if (hacker1.localeCompare(hacker2) > 0) {
 
     console.log("Yo, the navigator goes first definitely.");
 
-} else if (hacker1.localeCompare(hacker2) < 0 ) {
+} else if (hacker1.localeCompare(hacker2) < 0) {
 
     console.log("The driver's name goes first.")
- 
+
 } else {
 
-    console.log("What?! You both have the same name?")    
+    console.log("What?! You both have the same name?")
 }
 
