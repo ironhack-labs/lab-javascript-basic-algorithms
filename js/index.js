63ea34b069c75796df32daf7b9c80df388e4ceaa
amandahp <amandahoffmann96@gmail.com>
2022-07-14 22:24:22 -0300

    feat: finish the exercise

--- js/index.js
+++ js/index.js
@@ -1,43 +1,61 @@
-
 function NamesAndInput(){
   // Iteration 1: Names and Input
+
   let hacker1 = prompt("Name, please:")
   let hacker2 = prompt("Navigators Name, please:")
 
   console.log(`The driver's name is ${hacker1}`)
-  console.log(`The navigator's name is ${hacker2}`)
+  console.log(`The pet name is ${hacker2}`)
 
   //
   // Iteration 2: Conditionals
 
-  if (hacker1.length > 10 && hacker1.length !== hacker2.length) {
+  if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
-  } else if (hacker2.length > 5 && hacker1.length !== hacker2.length) {
+  } else if (hacker2.length > hacker2.length) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
   } else if (hacker1.length === hacker2.length){
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
   }
 
-
   // Iteration 3: Loops
 
-  let i = 0
+  let lexicalValueHacker1 = [];
+  let lexicalValueHacker2 = [];
+  const hacker2Reverse = hacker2.split("").reverse().join("")
+  let totalLexicalValueHacker1 = 0;
+  let totalLexicalValueHacker2 = 0;
 
-  for (i; i < hacker1.length; i++) {
+  for (let i = 0; i < hacker1.length; i++) {
     console.log(hacker1[i].toLocaleUpperCase())
+    lexicalValueHacker1.push(hacker1.charCodeAt(i))
+
   }
 
-  let j = 0;
-
-  const hacker2Reverse = hacker2.split("").reverse().join("") 
-
-  for (j; j < hacker2.length; j++) {
+  for (let j = 0; j < hacker2.length; j++) {
     console.log(hacker2Reverse[j])
+    lexicalValueHacker2.push(hacker2.charCodeAt(j))
   }
+  
+  for (let k = 0; k < lexicalValueHacker1.length; k++) {
+    totalLexicalValueHacker1 += lexicalValueHacker1[k];
+  }
+
+  for (let l = 0; l < lexicalValueHacker2.length; l++) {
+    totalLexicalValueHacker2 += lexicalValueHacker2[l];
+  }
+
+  if (totalLexicalValueHacker1 > totalLexicalValueHacker2){
+    console.log("The driver's name goes first.")
+  }else if (totalLexicalValueHacker2 > totalLexicalValueHacker1){
+    console.log("Yo, the navigator goes first definitely.")
+  }else if (totalLexicalValueHacker2 === totalLexicalValueHacker1){
+    console.log("What?! You both have the same name?")
+  }
+  
 
 }
 
 NamesAndInput()
-
 
 
