00e9039f0ad86b19bc004924b79cf0bf56e27390
zacjjohnson <wecooked@gmail.com>
2022-07-16 10:54:04 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
 //
 
-const hacker1 = 'John';
-const hacker2 = 'Jacob';
+let hacker1 = 'John';
+let hacker2 = 'Jacob';
 console.log(`The driver's name is ${hacker1}`);
 console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
@@ -16,13 +16,29 @@ if(hacker1.length > hacker2.length){
 }
 
 // Iteration 3: Loops
+// 3.1
 
 for (let i = 0; i < hacker1.length; i++){
-    console.log(hacker1[i].toUpperCase() + ' ');
+   console.log(hacker1[i].toUpperCase() + ' ');
 }
+// 3.2
 
 for (let i = hacker1.length - 1; i >= 0; i--){
     console.log(hacker1[i]);
   }
 
-  
+ // 3.3
+
+ hacker1 = hacker1.toLowerCase();
+ hacker2 = hacker2.toLowerCase();
+ let nameArray = [];
+ nameArray.push(hacker1, hacker2);
+ 
+ 
+ if (nameArray[0] > nameArray[1]){
+   console.log(`The driver's name goes first.`);
+ } else if(nameArray[1] > nameArray[0]){
+   console.log(`Yo, the navigator goes first definitely.`)
+ } else if(nameArray[0] == nameArray[1]){
+   console.log(`What?! You both have the same name?`)
+ }
