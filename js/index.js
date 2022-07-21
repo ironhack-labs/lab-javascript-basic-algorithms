84add292ff4ea89e2843e25fe0c6086946897d9a
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2022-07-21 20:20:55 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 // Iteration 1: Names and Input
 //
+
+const hacker1 = "Josh"
+console.log(`The drivers name is ${hacker1}`)
+
+const hacker2 = "Bernd"
+console.log(`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
+console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
 
 // Iteration 3: Loops
