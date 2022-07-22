f26392b4a41ae2b82a7589e6bf836a22fff90614
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-07-22 17:56:06 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-//
+let hacker1 = "RZA";
+console.log(`The driver's name is $(hacker1)`);
+let hacker2 = "GZA";
+console.log(`The navigator's name is $(hacker2)`);
 // Iteration 2: Conditionals
 
 
@@ -7,3 +10,5 @@
 
 
 console.log("I'm ready!");
+
+let hacker1 = 
