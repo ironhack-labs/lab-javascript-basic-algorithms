ca4266c30f319480c9ebf4f7dbc167b22cf831ce
herrFernandez <105803598+herrFernandez@users.noreply.github.com>
2022-07-23 09:58:37 +0200

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -22,3 +22,26 @@ if (hacker1Char > hacker2Char) {
   else {
     console.log (`Wow, you both have equally long names, ${hacker1.length} characters.`);
   }
+
+// Iteration 3: Loops
+
+let hacker1Spaced = "";
+for (let i = 0; i < hacker1.length; i++) {
+	hacker1Spaced += hacker1[i] += " "
+}
+    console.log (hacker1Spaced.toUpperCase().trim())
+
+let hacker2Reversed = "";
+for (let i = hacker2.length -3; i >= 0; i--) {
+    hacker2Reversed += hacker1[i];
+}
+   console.log(hacker2Reversed);
+
+
+if (hacker2 > hacker1) {
+  console.log(`The driver's name goes first.`)
+} else if (hacker1 > hacker2) {
+  console.log(`Yo, the navigator goes first definitely.`)
+} else {
+  console.log(`What?! You both have the same name?`)
+}
