7c8e5124c4e7dcdfd82ec198bdf503f483ff54df
CescVila <vr.cesc@gmail.com>
2022-07-23 16:47:00 +0200

    Progres iteration 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Francesc"
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = prompt(`Wanna know who has it longer?`);
+console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
+if(hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${} characters`);
+}else if(hacker1.length > hacker2.length) {
+    for(hacker2.length )
+    console.log(`The driver has the longest name, it has ${} characters.`);
+}else{
+    console.log(`Wow, you both have equally long names, ${} characters!`);
+}
+console.log(hacker2");
+
 
 
 // Iteration 3: Loops
