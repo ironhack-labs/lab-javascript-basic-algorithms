a839875882f04e65ad8799bac3c1b8b7af554876
CescVila <vr.cesc@gmail.com>
2022-07-24 14:54:22 +0200

    Done but not working, why?

--- js/index.js
+++ js/index.js
@@ -1,20 +1,30 @@
-// Iteration 1: Names and Input
-
-let hacker1 = "Francesc"
+const hacker1 = "Francesc"
 console.log(`The driver's name is ${hacker1}`);
 let hacker2 = prompt(`Wanna know who has it longer?`);
 console.log(`The navigator's name is ${hacker2}`);
-// Iteration 2: Conditionals
+
+
 if(hacker2.length > hacker1.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${} characters`);
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
 }else if(hacker1.length > hacker2.length) {
-    for(hacker2.length )
-    console.log(`The driver has the longest name, it has ${} characters.`);
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 }else{
-    console.log(`Wow, you both have equally long names, ${} characters!`);
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
-console.log(hacker2");
 
+let hack1split = hacker1.split("");
+let hackspace = hack1split.join(" ");
+let hack1may= hackspace.toUpperCase();
 
+console.log(hack1may);
 
-// Iteration 3: Loops
+let hackrev = hacker2.reverse();
+console.log(hackrev);
+
+if(hacker2.length > hacker1.length) {
+    console.log(`Yo, the navigator goes first definitely.`);
+}else if(hacker1.length > hacker2.length) {
+    console.log(`The driver's name goes first.`);
+}else{
+    console.log(`What?! You both have the same name?`);
+}
