d5376e3e9150227b7c4cde59ac5951fdda827ee3
Kenneth Elumba <kennethelumba@gmail.com>
2022-08-08 17:22:29 +0200

    done with final update

--- js/index.js
+++ js/index.js
@@ -18,6 +18,21 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+let inCapitals = "";
+
 for (i = 0; i < hacker1.length; i++) {
-    console.log(hacker1[i] + " ")
+    inCapitals += hacker1[i].toUpperCase() + " ";
+}
+
+let revHacker2 = "";
+for (let i = hacker2.length - 1; i >= 0 ; i--) {
+   	revHacker2 += hacker2[i]
+}
+
+if (hacker1.localeCompare(hacker2) == -1) {
+  console.log("The driver's name goes first")
+} else if (hacker1.localeCompare(hacker2) == 1) {
+  console.log("Yo, the navigator goes first definitely")
+} else {
+  console.log("What?! You both have the same name?")
 }
