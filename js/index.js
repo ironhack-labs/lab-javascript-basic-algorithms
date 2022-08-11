e17208e824be3e1eab2395cc1cbef8dc1811f5e3
Lenis Cervantes <leniserik@gmail.com>
2022-08-11 17:03:11 +0200

    made js lab

--- js/index.js
+++ js/index.js
@@ -1,6 +1,60 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+
+// console.log('Iteration 1')
+
+let hacker1 = prompt("Enter the driver name")
+let hacker2 = prompt("Enter the navigator name")
+// console.log (hacker1)
+// console.log (hacker2)
 
 
-// Iteration 3: Loops
+
+// console.log('Iteration 2')
+
+// if (hacker1.length > hacker2.length) {
+//     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+// }
+// else if (hacker2.length > hacker1.length) {
+//     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+// }
+// else console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
+
+
+
+// console.log('Iteration 3.1')
+
+// let hacker1U = hacker1.toUpperCase()
+
+
+// let arrayName = []
+// for(let i = 0; i < hacker1U.length; i++) {
+// arrayName += hacker1U[i]
+// arrayName += ' '
+// }
+// console.log(arrayName)
+
+
+// console.log('Iteration 3.2')
+
+// let arrayName = []
+// for(let i = hacker1.length -1; i >= 0; i--) {
+// arrayName += hacker1[i]
+
+// }
+// console.log(arrayName)
+
+
+
+// console.log('Iteration 3.3')
+
+
+
+
+        // if( hacker1 < hacker2 ){
+        //     console.log('The drivers name goes first');
+           
+        // }
+        // else if ( hacker1 > hacker2 ){
+        //     console.log('Yo, the navigator goes first definitely.');
+           
+        // }
+        // else console.log('What?! You both have the same name?')
