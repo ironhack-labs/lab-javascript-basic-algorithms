03121b72baedce5cfeca027142aa826e60899ab8
Matthew Schröder <matthewschroeder94@gmail.com>
2022-09-07 17:50:52 +0200

    finished the iterations

--- js/index.js
+++ js/index.js
@@ -21,4 +21,33 @@ if (hacker1.length > hacker2.length){
 
 // Iteration 3: Loops
 
+// Iteration 3.1
+let driver = '';
 
+for (let i = 0; i <= hacker1.length; i++) {
+    driver += hacker1[i].toUpperCase() + ' ';
+    console.log(driver)
+}
+
+// Iteration 3.2
+
+let navigator = '';
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    navigator += hacker2[i];
+
+    console.log(navigator)
+}
+
+// Iteration 3.3
+
+if(hacker1.localeCompare(hacker2) === 1) {
+    console.log("Yo, the navigator goes first definitely.")
+  }
+  
+  else if (hacker1.localeCompare(hacker2) === -1) {
+    console.log("The driver's name goes first.")
+  }
+  
+  else {
+    console.log("What?! You both have the same name?")
+  }
