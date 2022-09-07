0cdd183568fadade8040e7cd4edb9bf9a50e325b
Matthew Schröder <matthewschroeder94@gmail.com>
2022-09-07 18:13:24 +0200

    started bonus1

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 if(hacker1.localeCompare(hacker2) === 1) {
     console.log("Yo, the navigator goes first definitely.")
   }
-  
+
   else if (hacker1.localeCompare(hacker2) === -1) {
     console.log("The driver's name goes first.")
   }
@@ -51,3 +51,20 @@ if(hacker1.localeCompare(hacker2) === 1) {
   else {
     console.log("What?! You both have the same name?")
   }
+
+  // Bonus1
+
+  let text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at justo at quam fringilla lacinia quis ullamcorper lacus. Vivamus vel dolor aliquet, sodales tortor nec, eleifend odio. Suspendisse volutpat risus ligula, sit amet pulvinar elit vulputate ac. Vestibulum porttitor ipsum vel fringilla dignissim. Pellentesque convallis mattis tellus. Phasellus condimentum tristique augue nec pulvinar. Donec posuere, augue non tempus dapibus, ipsum leo fermentum ex, non dictum magna sem nec lacus. Quisque at justo id quam malesuada sagittis. Curabitur in aliquet massa. Nunc at lorem quis elit dictum gravida. Donec vel ullamcorper tortor.
+
+  Aliquam cursus augue at massa pulvinar semper. Vestibulum placerat tristique euismod. Proin non elementum arcu. Fusce cursus sodales ante, sit amet pulvinar erat fringilla vitae. Vestibulum nec nisi congue libero maximus mollis. Aliquam facilisis in ligula eget ornare. Praesent laoreet enim at purus commodo, ut porta mi porta. Duis efficitur justo ipsum, nec hendrerit metus condimentum nec. Quisque pulvinar lacinia arcu id dignissim. Sed quis accumsan nisl, sed dapibus ex. Aliquam dignissim dui id risus molestie dictum.
+  
+  In ultricies consequat velit, viverra tempus leo porttitor id. Integer sagittis vel tortor facilisis cursus. Nulla vitae magna enim. Integer non urna enim. Praesent eget ligula iaculis, accumsan felis quis, dapibus mauris. Ut finibus lectus ut tempor pellentesque. Sed eleifend enim id mi aliquam tristique. Cras maximus ullamcorper ex et rutrum. Suspendisse gravida semper suscipit. Cras ultricies odio vel diam sagittis viverra. In ultrices id lacus id dapibus.'
+
+
+  function countWords(text) {
+    const arr = text.split(' ');
+
+    return arr.filter(word => word !== '')
+  }
+
+  console.log(countWords(text))
