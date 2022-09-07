76698e668391c5520f552f1c564f3c3d2ad80fcc
Saskia Peach <saskiapeach@gmail.com>
2022-09-07 17:01:36 +0100

    iterated through all 3 tasks, no bonuses done yet

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
 // Iteration 1: Names and Input
-//
+const hacker1 = "Saskia"
+console.log(`The driver's name is ${hacker1}`)
+const hacker2 = "Martin"
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
-
+if(hacker1.length > hacker2.length){
+    console.log('The driver has the longest name, it has XX characters.')
+} if (hacker2.length > hacker1.length){
+    console.log('It seems that the navigator has the longest name, it has XX characters.')
+} else if (hacker2.length === hacker1.length){
+    console.log('Wow, you both have equally long names, XX characters!')
+} 
 
 // Iteration 3: Loops
+let spacedName = "";
+for(const char of hacker1){
+    spacedName += char + '_'
+    
+}
+console.log(spacedName.toUpperCase())
+
+// 3.2 // 
+
+let reversed = "";      
+   for (var i = hacker1.length - 1; i >= 0; i--){         
+     reversed += hacker1[i];  
+   }     
+
+  console.log(reversed)
+
+// 3.3 //
+
+const result = hacker1.localeCompare(hacker2) 
+
+if (result === -1){
+    console.log("The driver's name goes first.")}
+    else if (result === 1){
+        console.log("Yo, the navigator goes first definitely.")}
+        else {console.log('What?! You both have the same name?')}
+        
