fd3b69dee6f1a19cad1eb6584c71461cb0d01741
VKhodyrev <vkhodyrev1@gmail.com>
2022-09-07 18:56:16 +0300

    Iteration 1 performed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
 //
+const hacker1 = "Volodymyr";
+const hacker2 = "Mog";
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
 
 
