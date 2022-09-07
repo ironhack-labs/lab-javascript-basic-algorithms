5d3c4b3b494fdef4530debd5554b23623045e995
Daniel Plesa <danielplesa@Daniels-MacBook-Pro.local>
2022-09-07 18:58:23 +0200

    solve iteration 1&2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+// Iteration 1: 
 
+const hacker1= "Daniel";
+console.log(`The driver's name is ${hacker1}`);
 
-// Iteration 3: Loops
+const hacker2= "Serhii";
+console.log(`The navigator's name is ${hacker2}`);
+
+// Iteration 2:
+if (hacker1.length === hacker2.length) {
+ console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`) }
+
+ // Iteration 3:
+ const driver= "Daniel"
+const result = driver.split('').join(' '); 
+ console.log(result.toUpperCase());
+
+ function navigator(str) {
+    let reverseNavigator = '';
+    for (let i= str.length -1; i >=0; i--) {
+      reverseNavigator += str[i];
+    }
+    return reverseNavigator;
+  }
+  
+  console.log(navigator ('Serhii'));
