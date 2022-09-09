b563bbbebc55a5f2755ef17120471620cd7f64ff
Pabl0Parra <pabsblue16@gmail.com>
2022-09-09 18:02:19 +0200

    finished Iteration 3 & fixed Iteration 2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 // Iteration 1: Names and Input
-const hacker1 = "El Vaquilla";
+const hacker1 = "against";
 console.log(`The driver's name is ${hacker1}`);
 
-const hacker2 = "El Gita";
-console.log(`The driver's name is ${hacker2}`);
+const hacker2 = "check";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-if (hacker1 > hacker2) {
+// - The driver has the longest name, it has XX characters. or
+// - It seems that the navigator has the longest name, it has XX characters. or
+// - Wow, you both have equally long names, XX characters!.
+
+if (hacker1.length > hacker2.length) {
   console.log(
     `The driver has the longest name, it has ${hacker1.length} characters`
   );
@@ -27,9 +31,7 @@ let newHacker1 = "";
 for (let i = 0; i < hacker1.length; i++) {
   newHacker1 += hacker1[i].toUpperCase() + " ";
 }
-console.log(
-  `The characters of the driver's name in capital are ${newHacker1.trim()}`
-);
+console.log(`The characters of the driver's name in capital are ${newHacker1}`);
 
 // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
 let newHacker2 = "";
@@ -39,3 +41,26 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 console.log(
   `The characters of the navigator's name in reverse order are ${newHacker2}`
 );
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+// - The driver's name goes first.
+// - Yo, the navigator goes first definitely.
+// - What?! You both have the same name?
+
+let driver = hacker1.toLowerCase();
+
+let passenger = hacker2.toLowerCase();
+
+let carRider = driver.localeCompare(passenger);
+
+console.log(`testing: ${carRider}`);
+// negative value --> driver before passenger
+if (carRider < 0) {
+  console.log(`The driver's name goes first.`);
+  // positive value --> driver after passenger
+} else if (carRider > 0) {
+  console.log(`The navigator's name goes second definitely.`);
+  // 0 value --> driver === passenger
+} else {
+  console.log(`What?! You both have the same name?`);
+}
