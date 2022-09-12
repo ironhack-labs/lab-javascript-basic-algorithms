d19e793bf0b8aa98591c477b009ab39c57cc07d8
Pabl0Parra <pabsblue16@gmail.com>
2022-09-12 09:42:43 +0200

    finished bonus 1&2

--- js/index.js
+++ js/index.js
@@ -64,3 +64,46 @@ if (carRider < 0) {
 } else {
   console.log(`What?! You both have the same name?`);
 }
+
+// Bonus 1:
+// Go to lorem ipsum generator and:
+
+// a. Generate 3 paragraphs. Store the text in a variable type of string.
+// b. Make your program count the number of words in the string.
+// c. Make your program count the number of times the Latin word et appears.
+
+const paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum rutrum nibh, ac efficitur augue consequat eu. Curabitur et luctus libero. Phasellus sit amet facilisis arcu. Sed auctor orci libero, in ultricies justo consequat vitae. Suspendisse vel lectus sit amet lacus aliquet laoreet. Vivamus justo elit, convallis quis accumsan facilisis, gravida vitae tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sed scelerisque ligula. Duis id ex sed diam convallis porttitor id sit amet magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ut massa aliquet, auctor sapien nec, semper eros.
+
+Etiam condimentum rutrum arcu a sagittis. Quisque auctor mi sit amet nunc ornare fermentum. Mauris consequat ultrices volutpat. Donec nec finibus eros. Sed vel accumsan nulla. Nullam vitae elit commodo, mattis arcu id, tincidunt mi. Sed volutpat nulla a bibendum pretium.
+
+Suspendisse eleifend, quam in lobortis vestibulum, odio augue facilisis lectus, ut semper elit neque ac sapien. Maecenas a est placerat augue consectetur tincidunt. Praesent auctor, tortor non varius ultrices, risus magna gravida nulla, vitae suscipit purus turpis et elit. Nam mauris est, hendrerit vitae euismod sit amet, egestas sit amet justo. Vivamus dapibus a leo ac sollicitudin. Donec fringilla ac arcu eu molestie. Aliquam bibendum venenatis gravida. Integer dapibus placerat sapien vulputate auctor. Nulla tincidunt vestibulum elit. Ut at malesuada leo, sit amet fermentum est. Duis sit amet libero quis mi fringilla condimentum. Maecenas fringilla tellus ac enim finibus viverra. Proin a posuere ex.`;
+
+let totalWords = 0;
+let totalEtWordsCount = 0;
+
+let splittedParagraph = paragraph.split(" ");
+
+for (let i = 0; i < splittedParagraph.length; i++) {
+  if (splittedParagraph[i].includes(" ")) {
+    totalWords++;
+  } else if (splittedParagraph[i] === "et" || splittedParagraph[i] === "et.") {
+    totalEtWordsCount++;
+  }
+  totalWords++;
+}
+console.log("total words -->", totalWords);
+console.log("total et words -->", totalEtWordsCount);
+
+// Bonus 2:
+// Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome.
+
+let stringToCheck = "step on no pets";
+stringToCheck = stringToCheck.replace(/[ ,!.]/g, "").toLowerCase();
+
+let reversedString = stringToCheck.split("").reverse().join("");
+
+if (stringToCheck === reversedString) {
+  console.log("String is a palindrome");
+} else {
+  console.log("String is not a palindrome");
+}
