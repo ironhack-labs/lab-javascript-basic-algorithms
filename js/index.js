993e81b7ab8ce6ad07f01d641fd686f7621774bc
humbertovacu <humbertovacu@gmail.com>
2022-09-17 09:36:35 +0200

    Adjustments to make code more readable

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 // Iteration 1: Names and Input
+
 let hacker1 = "Humberto";
 let hacker2 = "Maia";
 
 console.log(`The driver's name is ${hacker1}`);
 
 console.log(`The navigator's name is ${hacker2}`);
+
+
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
     console.log (`The driver has the longest name, it has ${hacker1.length} characters`);}
+    
     else if (hacker2.length > hacker1.length){
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters!`)
   }
@@ -38,7 +42,6 @@ console.log(reverseNav);
 
 for (i=0; i<hacker1.length; i++) {
   if (hacker1[i]<hacker2[i]) {
-    
     console.log("The driver's name goes first.")
     break;
   }
@@ -47,10 +50,12 @@ for (i=0; i<hacker1.length; i++) {
     console.log("Yo, the navigator goes first definitely.")
     break;
   }
+
   else if (hacker1===hacker2) {
     console.log("What?! You both have the same name?")
     break;
   }
+
   else {
     continue;
   }
