9e1a1d17d386f0fb114b139414fe2068f78e929f
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-09-23 15:17:19 +0200

    nuevos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "John"
+let hacker2 = "Albert"
+
+console.log(`The hacker1 is ${hacker1}`);
+console.log(`The hacker2 is ${hacker2}`);
+
+
+
 // Iteration 2: Conditionals
+
 
 
 // Iteration 3: Loops
