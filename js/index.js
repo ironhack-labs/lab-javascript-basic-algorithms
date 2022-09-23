3be335a5be0595c94df1d5202218ccd9b3f31361
anovau <novau_678@hotmail.com>
2022-09-23 15:34:01 +0200

    conditionals

--- js/index.js
+++ js/index.js
@@ -6,10 +6,16 @@ console.log(`El nombre del conductor es ${hacker1}`)
 let hacker2 = "omar"
 console.log(`El nombre del navegante es ${hacker2}`)
 
-
-
 //
 // Iteration 2: Conditionals
+
+if(hacker1.length > hacker2.length ){
+    console.log(`El conductor tiene el nombre más largo, tiene ${hacker1.length} caracteres`)
+}else if(hacker1.length < hacker2.length ){
+    console.log(`Parece que el navegante tiene el nombre más largo, tiene ${hacker2.length} caracteres.`)
+}else{
+    console.log(`¡Vaya, ambos tienen nombres igual de largos, ${hacker1.length} caracteres!`)
+}
 
 
 // Iteration 3: Loop
