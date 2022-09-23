8bdbb026073d1a1d08d59a3e42791fb631b8a332
anovau <novau_678@hotmail.com>
2022-09-23 15:23:52 +0200

    names and input

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 // Iteration 1: Names and Input
+
+let hacker1 = "albert"
+console.log(`El nombre del conductor es ${hacker1}`)
+
+let hacker2 = "omar"
+console.log(`El nombre del navegante es ${hacker2}`)
+
+
+
 //
 // Iteration 2: Conditionals
 
 
-// Iteration 3: Loops
-
-j
+// Iteration 3: Loop
 
 
 
