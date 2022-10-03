d7cff59b3e095e515cebbd2293989b4ecaf68283
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-10-03 21:36:26 +0100

    lab1 finish

--- js/index.js
+++ js/index.js
@@ -14,5 +14,21 @@ if(hacker1.length > hacker2.length){
 }
 
 
-let charactersName = hacker1.toUpperCase();
+let charactersName = hacker1.toUpperCase().split("").join(" ");
 console.log(charactersName);
+
+let reverseName = hacker2.split("").reverse().join("");
+console.log(reverseName);
+
+
+if (hacker1.localeCompare(hacker2) === 0){
+    console.log("What?! You both have the same name?");
+} else if (hacker1.localeCompare(hacker2) === 1) {
+    console.log("The driver's name goes first.");
+} else {
+    console.log("Yo, the navigator goes first definitely.");
+    
+}
+
+
+
