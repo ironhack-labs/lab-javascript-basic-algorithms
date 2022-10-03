1a395fd96676973456a4dcbc31fa4f5d7cfb2487
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-10-03 17:59:56 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+const hacker1 = "Gabriela";
+const hacker2 = "Pedro";
 
 
-// Iteration 3: Loops
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+
+if(hacker1.length > hacker2.length){
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+} else if (hacker1.length < hacker2.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+} else {
+ console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
+}
+
+
+let charactersName = hacker1.toUpperCase();
+console.log(charactersName);
