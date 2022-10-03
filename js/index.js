e153cd87edc2de245ea0d7f1c6b9da3983ac2111
MeerKat <98255504+MeerKatPT@users.noreply.github.com>
2022-10-03 19:46:36 +0100

    second commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 // Iteration 1: Names and Input
-let hacker1 = 'Miguel';
+let hacker1 = 'Filipe';
 let hacker2 = 'Filipe';
 console.log(`The driver´s name is ${hacker1}`);
-console.log(`The naviator´s name is ${hacker2}`);
+console.log(`The navigator´s name is ${hacker2}`);
 // Iteration 2: Conditionals
 
-if (hacker1.length > hacker2.length) {
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
-} else if (hacker2.length > hacker1.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
-} else {
-    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
-}
+// if (hacker1.length > hacker2.length) {
+//     console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+// } else if (hacker2.length > hacker1.length) {
+//     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+// } else {
+//     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+// }
 
 // Iteration 3: Loops
 // let str = "";
@@ -29,10 +29,26 @@ if (hacker1.length > hacker2.length) {
 // console.log(str1);
 
 
+// for (let i = 0; i < hacker1.length; i++) {
+//     for (let j = 0; j < hacker2.l {
+//         if (hacker1[i].localeCompare(hacker2[j])) {
+            
+//         }
+//     }
+// }
+// let equal = 0;
+
 for (let i = 0; i < hacker1.length; i++) {
-    for (let j = 0; j < hacker2.length; j++) {
-        if (hacker1[i].localeCompare(hacker2[j])) {
-            console.log()
-        }
+    if ((hacker1[i].localeCompare(hacker2[i])) === -1 ) {
+        console.log("The driver's name goes first") 
+        break;
+    } else if ((hacker1[i].localeCompare(hacker2[i])) === 1 ) { 
+        console.log("Yo, the navigator's name goes first definitely")
+        break;
+    } else if (i === (hacker1.length - 1)) {
+        console.log("What?! You both have the same name?")
+    } else if ((hacker1[i].localeCompare(hacker2[i])) === 0 ) {
+        continue;
     }
-}
+    }
+
