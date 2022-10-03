7af22d7224f47dd55212fc7c5b3d4cff045be38b
carlota-taveira <114014431+carlota-taveira@users.noreply.github.com>
2022-10-03 18:10:29 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -12,11 +12,17 @@ if (hacker1.length > hacker2.length) {
     console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
 
-for ( i = 0; i < hacker1.length; i++) {
+let separatedChar = hacker1.split("").join(" ");
+console.log(separatedChar);
+let reversedChar = hacker2.split("").reverse().join("");
+console.log(reversedChar);
+if (hacker1.localeCompare(hacker2)) {
+  console.log("The driver's name goes first.");
+} else if (hacker2.localeCompare(hacker1)) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
 }
-console.log(hacker1.length) 
-
-console.log('André'.localeCompare('Carlota')); 
 
 
 
