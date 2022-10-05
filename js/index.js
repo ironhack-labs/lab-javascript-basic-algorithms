f26eb940c38514a74b629290bdf6b13135a632a4
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-10-05 13:02:03 +0200

    iteration1 done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
+const hacker1 = "Baba";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Mimi";
+console.log(`The navigator's name is ${hacker2}`);
 //
 // Iteration 2: Conditionals
 
