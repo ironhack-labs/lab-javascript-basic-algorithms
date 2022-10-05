9b7da1da898fca2f7a10a4e1dbfc22db4cdb340c
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-10-05 15:00:17 +0200

    iteration2 done

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
-console.log("I'm ready!");
+/* console.log("I'm ready!");
 
 // Iteration 1: Names and Input
-const hacker1 = "Baba";
+const hacker1 = "Baba Nosha";
 console.log(`The driver's name is ${hacker1}`);
 
-const hacker2 = "Mimi";
-console.log(`The navigator's name is ${hacker2}`);
-//
-// Iteration 2: Conditionals
+const hacker2 = "Mimi Macadamia";
+console.log(`The navigator's name is ${hacker2}`); */
 
+// Iteration 2: Conditionals
+if (sum1 === sum2) {
+  console.log(`Wow, you both have equally long names, ${sum1} + ${sum2} characters!`);
+} else if (sum1 < sum2) {
+  console.log(`It seems that the navigator has the longest name, it has ${sum2} characters`);      
+} else if (sum2 > sum1) {
+  console.log(`The driver has the longest name, it has ${sum1} characters`);;
+} else {
+  console.log("Problem");
+}
 
 // Iteration 3: Loops
