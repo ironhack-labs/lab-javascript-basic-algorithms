29f93f3ec726ee6c45ea90d0731eb28125ad0751
Andres Izarra <andresizarra@MacBook-Air.local>
2022-10-19 15:57:48 +0200

    Exercise finished

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ console.log("The navigator's name is " + hacker2)
 console.log(hacker1.length)
 console.log(hacker2.length)
 
+// Iteration 2: Conditionals
+
 if (hacker1.length > hacker2.length) {
     console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
 }
@@ -17,12 +19,25 @@ else {
     console.log(`Wow, you both have equally long names, XX characters!`)
 }
 
-
-
-
-
-
-// Iteration 2: Conditionals
-
-
 // Iteration 3: Loops
+
+let hackerCap = hacker1.toUpperCase()
+  let horString = ""
+  
+for (i = 0; i < hackerCap.length;i++) {
+  horString += hackerCap[i] + " " 
+  }
+console.log(horString)
+
+for (i = hacker2.length-1; i >= 0; i--) {
+    console.log(hacker2[i])
+    }
+
+if (hacker1.localeCompare(hacker2)=== -1) {
+  console.log(`The driver's name goes first.`)
+} 
+else if (hacker1.localeCompare(hacker2) === 1 ) {
+  console.log(`Yo, the navigator goes first definitely.`)
+} else {
+  console.log (`What?! You both have the same name?`)
+}
