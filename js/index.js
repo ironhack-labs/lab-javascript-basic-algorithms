1b542789606ae833d4061bff947e24d3dad14b64
Andres Izarra <andresizarra@MacBook-Air.local>
2022-10-19 14:54:24 +0200

    2nd iteration done

--- js/index.js
+++ js/index.js
@@ -4,6 +4,20 @@ console.log("The driver's name is " + hacker1)
 const hacker2 = "Alexandra"
 console.log("The navigator's name is " + hacker2)
 
+console.log(hacker1.length)
+console.log(hacker2.length)
+
+if (hacker1.length > hacker2.length) {
+    console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
+}
+else if (hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
+else {
+    console.log(`Wow, you both have equally long names, XX characters!`)
+}
+
+
 
 
 
