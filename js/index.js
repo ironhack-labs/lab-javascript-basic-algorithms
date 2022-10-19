80750d0185cb3a09217b98e6dd0434df0611c339
Jakob <jheintze33@gmail.com>
2022-10-19 14:57:58 +0200

    Jakob Heintze done

--- js/index.js
+++ js/index.js
@@ -29,13 +29,12 @@ else{
 
 // Iteration 3: Loops
 /* 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
-
 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
-
 3.3 Depending on the lexicographic order of the strings, print:
 - The driver's name goes first.
 - Yo, the navigator goes first definitely.
 - What?! You both have the same name? */
+
 let empty = ' ';
  for (let i = 0; i < hacker1.length; i++) {
    
