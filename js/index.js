266541d3d271117dd0e2e4273de092300408898a
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2022-10-19 10:15:44 -0400

    add string ``

--- js/index.js
+++ js/index.js
@@ -23,24 +23,22 @@ else {
 }
 
 // Iteration 3: Loops
-let firtHackersName = '';
+let firtHackersName = "";
 for (const letter of hacker1.split(" ")[0].toUpperCase()) {
-    if (firtHackersName.length === 0){
-        firtHackersName += letter;
-    }else {
-        firtHackersName = ${firtHackersName} ${letter};
-    }
+  if (firtHackersName.length === 0) {
+    firtHackersName += letter;
+  } else {
+    firtHackersName = `${firtHackersName} ${letter}`;
+  }
 }
-console.log(firtHackersName)
+console.log(firtHackersName);
 
-console.log(hacker2.split("").reverse().join(""))
+console.log(hacker2.split("").reverse().join(""));
 
-
-if (hacker1 > hacker2){
-    console.log("The driver's name goes first.")
-} else if (hacker2 > hacker1){
-    
-    console.log('Yo, the navigator goes first definitely.')
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker2 > hacker1) {
+  console.log("Yo, the navigator goes first definitely.");
 } else {
-    console.log('What?! You both have the same name?')
+  console.log("What?! You both have the same name?");
 }
