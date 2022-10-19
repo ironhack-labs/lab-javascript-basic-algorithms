835c6204a98755e309df4000895bb462fe56676f
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2022-10-19 09:07:09 -0400

    add iteration 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+const hacker1 = "Joel Alves Pereira";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Juliano Hausen Oliveira da Costa";
+console.log(`The navigator's name is ${hacker2}`);
 
+// Iteration 2: Conditionals
+const lenName = (name) => name.length;
+
+const driverIsLonger = (len) =>
+  `The driver has the longest name, it has ${len} characters.`;
+const navigatorIsLonger = (len) =>
+  `It seems that the navigator has the longest name, it has ${len} characters.`;
+const sameLength = (len) =>
+  `Wow, you both have equally long names, ${len} characters!`;
+
+if (lenName(hacker1) > lenName(hacker2))
+  console.log(driverIsLonger(lenName(hacker1)));
+if (lenName(hacker1) < lenName(hacker2))
+  console.log(navigatorIsLonger(lenName(hacker2)));
+
+if (lenName(hacker1) === lenName(hacker2))
+  console.log(sameLength(lenName(hacker1)));
 
 // Iteration 3: Loops
