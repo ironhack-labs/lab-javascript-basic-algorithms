7887df7161ef86c29a8b1d77e5b5ec7a570738a8
Andres Izarra <andresizarra@MacBook-Air.local>
2022-10-19 17:32:37 +0200

    almost done

--- js/index.js
+++ js/index.js
@@ -41,3 +41,32 @@ else if (hacker1.localeCompare(hacker2) === 1 ) {
 } else {
   console.log (`What?! You both have the same name?`)
 }
+//Count Par
+
+const threePar = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare odio a convallis bibendum. Duis varius, neque quis suscipit euismod, orci mi ultricies nisl, sit amet tincidunt ex odio et tellus. Aliquam erat volutpat. Pellentesque et turpis commodo, consectetur nibh vel, placerat risus. Pellentesque sapien nisi, lacinia ac cursus ut, aliquet ut leo. Pellentesque fermentum auctor odio ac malesuada. Aliquam at ante eget justo scelerisque fermentum. Praesent iaculis quam nulla, ac porta elit vestibulum in. Aliquam erat volutpat. Maecenas bibendum ante urna, a posuere tellus molestie nec. Nunc consequat sem eu erat lacinia, vitae feugiat erat dictum. Duis faucibus id libero in lacinia. In quis luctus nisl, quis elementum nulla. Suspendisse iaculis blandit ipsum et pretium. Nullam eu lorem egestas, ultrices mauris at, lacinia nibh. Suspendisse non hendrerit lorem. Sed rutrum elementum nibh id tristique. Donec congue, enim sed efficitur eleifend, nulla ipsum facilisis magna, a tempus lorem nunc nec ante. Nunc nec magna et massa ullamcorper rhoncus quis a libero. In accumsan consequat mi, ac bibendum neque. Pellentesque porttitor id sapien sit amet hendrerit. Phasellus at fermentum lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras aliquet ligula non quam feugiat sodales. Mauris pulvinar in libero ac ornare. Cras ac porta tortor, non mollis dui. Fusce tristique turpis eu dolor suscipit sagittis. Mauris fermentum, nisi nec dapibus aliquet, augue risus elementum ante, vel tincidunt enim massa quis tellus. Cras accumsan sodales semper. Suspendisse euismod mauris eu cursus tincidunt. Etiam vel libero et tortor tempus bibendum. Donec vitae lobortis diam. Integer sit amet facilisis turpis. Fusce sodales nibh diam, vitae molestie neque pharetra vel."
+
+let count = threePar.length
+console.log(count)
+let totalEt = 0
+
+for (i = 0; i < count; i++) {
+  if (threePar[i] + threePar[i+1] === "et") {
+    totalEt++
+  }
+}
+console.log(totalEt)
+
+// palindrome
+
+const phraseToCheck = "No 'x' in Nixon"
+let phraseReversed = ""
+
+for (i = phraseToCheck.length - 1; i >= 0 ; i--) {
+  phraseComp = phraseReversed += phraseToCheck[i]
+  if (phraseComp === phraseReversed) {
+    console.log(`${phraseToCheck} is a palindrome`)
+    break;
+  } else {
+    console.log(`This is not a palindrome`)
+  }
+}
