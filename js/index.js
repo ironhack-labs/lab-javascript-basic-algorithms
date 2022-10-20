1ac2308f5ed2c14c68b43faada1feca0a63c7342
David Fdez <dav.fdez.lopez@gmail.com>
2022-10-20 16:47:07 +0200

    done

--- js/index.js
+++ js/index.js
@@ -4,3 +4,42 @@
 
 
 // Iteration 3: Loops
+
+//node js/index.js
+
+        // ITERATION 1
+
+const hacker1='jhon'
+
+console.log(`The driver's name is ${hacker1}`)
+
+const hacker2='YYYY'
+
+console.log(`The navigator's name is ${hacker2}`)
+
+
+        // ITERATION 2
+
+if (hacker1.length > hacker2.length){
+    console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+    console.log (`Wow, you both have equaly long names, ${hacker1.length} characters.`)
+}
+
+
+        // ITERATION 3
+
+console.log(hacker1.charAt(1))
+
+
+for (let i = 0; i < hacker1.length; i++) { 
+
+    console.log ()
+}
+
+
+ 
+
+// constante.toUpperCase(
