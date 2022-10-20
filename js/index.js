7b1efd26763887307f8ccf487d807d3e7001ce1e
Alariko0 <105845940+Alariko0@users.noreply.github.com>
2022-10-20 17:08:32 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
 // Iteration 1: Names and Input
-//
+
+const driver = prompt("What's your name?");
+const navigator = prompt("What's your name?");
+
+
+console.log(`The drivers name is ${driver}`);
+
+console.log(`The navigators name is ${navigator}`);
+
+
 // Iteration 2: Conditionals
 
-
+if (driver.length > navigator.length) {
+    console.log(`The driver has the longest name it has ${driver.length} characters`)
+} else if (driver.length < navigator.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters`)
+}
+else {
+    console.log(`Wow, you both have equally long names, ${driver.length} characters!`)
+}
 // Iteration 3: Loops
+console.log(driver.toUpperCase().split("").join(" "))
+console.log(driver.split("").reverse().join(""))
+
+// BONUS
+if (driver < navigator) {
+    console.log('The drivers name goes first')
+}
+if (driver > navigator) {
+    console.log('Yo, the navigator goes first definitely')
+}
+if (driver === navigator) {
+    console.log('What?! You both have the same name?')
+}
