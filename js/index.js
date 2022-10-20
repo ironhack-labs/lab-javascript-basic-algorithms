39e32a324ed247913d7681b99d860decd551530a
DaniVazquezG <danivaz95@gmail.com>
2022-10-20 11:34:35 +0200

    done

--- js/index.js
+++ js/index.js
@@ -4,3 +4,30 @@
 
 
 // Iteration 3: Loops
+
+
+
+const driver = prompt("What's your name?");
+const navigator = prompt("What's your name?");
+
+console.log(`The drivers name is ${driver}`);
+console.log(`The navigators name is ${navigator}`);
+
+
+
+if (driver.lenght> navigator.lenght) {
+    console.log (`The driver has the longest name, it has ${driver.lenght} characters `)
+} else if (driver.lenght<navigator.lenght){
+    console.log (`It seems that the navigator has the longest name, it has ${navigator.lenght} characters.`)}
+    else {
+    console.log (`Wow, you both have equally long names, ${driver.lenght} characters!`)
+    }
+
+ console.log (driver.toUpperCase().split("").join(" "));
+ console.log (navigator.split("").reverse().join(""));
+
+ 
+
+    
+
+
