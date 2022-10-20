9ace3f49848986b006c14354576b5345073504ae
Larisa Mancebo <larisa.mancebo@gmail.com>
2022-10-20 11:47:32 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
 // Iteration 1: Names and Input
 //
-// Iteration 2: Conditionals
+const hacker1 = prompt("What is the driver's name?", "");
+const hacker2 = prompt("What is the navigator's name?", "");
 
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
+}
 
 // Iteration 3: Loops
+let hacker1Upper = hacker1.split("").join(" ").toUpperCase();
+let hacker2Reverse = hacker2.split("").reverse().join("");
+
+console.log(hacker1Upper);
+console.log(hacker2Reverse);
+
+let hackerArray = [hacker1, hacker2]
+
+if (hacker1 < hacker2) {
+    console.log("The driver's name goes first.")
+} else if (hacker1 > hacker2) {
+    console.log("Yo, the navigator goes first definitely.")
+} else {
+    console.log("What?! You both have the same name?")
+}
+
+const lorem = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+console.log(`The paragraph has ${lorem.length} words`)
+
+const countWord = lorem.split(" et ").length - 1
+console.log(`The paragraph repeats et ${countWord} times `)
+
+
+
