60a507b3d1c8baaca846b7a871b6e95e7256c451
david de la calle <daviddlcalle@gmail.com>
2022-10-27 13:17:45 +0200

    iteraciones 1 y 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
-// Iteration 1: Names and Input
-//
+// Iteration 1:
+let hacker1 = "Fernandito"
+  console.log(`the drivers name is ${hacker1}`)
+let hacker2 = "Antoñito"
+  console.log(`the navigator name is ${hacker2}`)
+
 // Iteration 2: Conditionals
+if(hacker1.length > hacker2.length){
+console.log(`the divers has the longest name, it has ${hacker1.length} char`)
+}
+else if(hacker1.length < hacker2.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+}
+else{
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 
 
 // Iteration 3: Loops
+
+
+
+
+
+
+
+
+/*Iteration 1: Names and Input
+
+  1.1 Create a variable `hacker1` with the driver's name. <br>
+  1.2 Print `"The driver's name is XXXX"`.<br>
+  1.3 Create a variable `hacker2` with the navigator's name.<br>
+  1.4 Print `"The navigator's name is YYYY"`.
+
+### Iteration 2: Conditionals
+  2.1. Depending on which name [is longer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length), print:
+  <br>
+	  - `The driver has the longest name, it has XX characters.` or <br>
+	  - `It seems that the navigator has the longest name, it has XX characters.` or <br>
+	  - `Wow, you both have equally long names, XX characters!`.
+*/
