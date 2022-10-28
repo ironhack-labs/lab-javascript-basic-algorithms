def2f386cc70fca264e47eb50e925195e9667234
Barbara-rodcab <barbara.rodrigues@cabify.com>
2022-10-28 14:21:31 +0200

    IT1 IT2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+let hacker1 = "Barbara";
+console.log (`The driver's name is ${hacker1}`) ; 
 
+let hacker2 = "Alexandra" ; 
+console.log (`The navigator's name is ${hacker2}`) ;
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log ( `The driver has the longest name, it has ${hacker1.length} characters ` )
+  }
+    else if (hacker2.length >  hacker1.length) {
+      console.log ( `It seems that the navigator has the longest name, it has,${hacker2.length} characters`);
+    }
+      else {
+      console.log (`Wow, you both have equally long names,${hacker2.length} characters!`)
+    }
+    
 
 // Iteration 3: Loops
