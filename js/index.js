f1cf38a2a38269576e72f51bf6b2dd2fd9ddf960
MariaAriz <114582093+MariaAriz@users.noreply.github.com>
2022-10-29 14:01:27 +0200

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -15,3 +15,29 @@ console.log(`It seems that the navigator has the longest name, it has ${hacker2.
 else {
 console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
+
+// Iteration 3: Loops - Parte 1
+let resultP1 = ""
+for (let i = 0; i < hacker1.length; i++) {
+    resultP1 += hacker1[i].toLocaleUpperCase() + " ";
+    }
+
+console.log(resultP1.trim());
+
+// Iteration 3: Loops - Parte 2
+let resultP2 = ""
+for (let i = hacker2.length-1; i >= 0 ; i--) {
+    resultP2 += hacker2[i];
+}
+
+console.log (resultP2)
+
+// Iteration 3: Loops - Parte 3
+
+if (hacker1.localeCompare(hacker2) === 1) {
+console.log (`The driver's name goes first`);
+} else if (hacker1.localeCompare(hacker2) === -1) {
+console.log (`Yo, the navigator goes first definitely`);
+} else {
+console.log (`What?! You both have the same name?`);
+}
