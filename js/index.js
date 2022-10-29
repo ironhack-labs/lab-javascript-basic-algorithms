faf82ca4346f79e0ad273ddb6131c8b0bef9fbf8
pianattero <mariapianattero@gmail.com>
2022-10-29 13:21:40 +0200

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -10,10 +10,29 @@ console.log(`The navigator's name is ${hacker2}`)
 
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
-    } else if (hacker2.length > hacker1.length) {
+} else if (hacker2.length > hacker1.length) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
-    } else {
+} else {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
-    }
+}
 
 // Iteration 3: Loops
+
+//P A B L O (3.1)
+console.log(`${hacker1[0].toUpperCase()} ${hacker1[1].toUpperCase()} ${hacker1[2].toUpperCase()} ${hacker1[3].toUpperCase()} ${hacker1[4].toUpperCase()}`)
+
+//nauJ (3.2)
+console.log(`${hacker2[3]}${hacker2[2]}${hacker2[1]}${hacker2[0]}`)
+
+//(3.3)
+if (hacker1.localeCompare(hacker2) === 1) {
+    console.log(`The driver's name goes first`)
+} else if (hacker2.localeCompare(hacker1) === 1) {
+    console.log(`Yo, the navigator goes first definitely.`)
+} else {
+    console.log(`What?! You both have the same name?`)
+}
+
+
+
+
