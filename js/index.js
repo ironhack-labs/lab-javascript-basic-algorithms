6e11b29a541b51f7e3db59a2d765b5270e97d2ee
patri <patricia.mareg@gmail.com>
2022-10-29 13:15:13 +0200

    iteration 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Iteration 1: Names and Input
 
-const hacker1 = "taylor allison swift"
+const hacker1 = "taylor"
 console.log(`The driver's name is ${hacker1}`)
 
-const hacker2 = "slays"
+const hacker2 = "john"
 console.log(`The navigator's name is ${hacker2}`)
 
 
@@ -18,3 +18,40 @@ if (hacker1.length > hacker2.length) {
   }
 
 // Iteration 3: Loops
+
+// taylor -> T A Y L O R
+let hacker1_2 = ""
+for (i = 0; i < hacker1.length; i++) {
+  hacker1_2 += hacker1[i] + " "
+}
+hacker1_2 = hacker1_2.toUpperCase()
+
+// john -> J O H N
+let hacker2_2 = ""
+for (i = 0; i < hacker2.length; i++) {
+  hacker2_2 += hacker2[i] + " "
+}
+hacker2_2 = hacker2_2.toUpperCase()
+
+// taylor -> rolyat
+let hacker1_3 = ""
+for (i = hacker1.length - 1; i >= 0; i--) {
+  hacker1_3 += hacker1[i]
+}
+
+// john -> nhoj
+let hacker2_3 = ""
+for (i = hacker2.length - 1; i >= 0; i--) {
+  hacker2_3 += hacker2[i]
+}
+
+// The driver's name goes first.
+if (hacker1.localeCompare(hacker2) === 1) {
+    console.log("Yo, the navigator goes first definitely.")
+  } else if (hacker1.localeCompare(hacker2) === -1) {
+    console.log("The driver's name goes first.")
+  } else {
+    console.log("What?! You both have the same name?")
+  }
+
+  
