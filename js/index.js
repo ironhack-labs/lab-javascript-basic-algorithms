4ca96cc4ecbb67477ef41f5684a6de89e46abf78
Edson <dinhoyao@gmail.com>
2022-10-29 13:10:28 +0200

    added exercise

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 // Iteration 1: Names and Input
-//
+
+
+let hacker1 = "Carla"
+console.log(`The drive's name is ${hacker1}`);
+
+let hacker2 = "Pedro"
+console.log(`The navigator's name is ${hacker2}`);
+
+
+
 // Iteration 2: Conditionals
 
 
+console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+
+console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+
+console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+
+
+
 // Iteration 3: Loops
+
+
+console.log(hacker1.toUpperCase().split('').join(' '))
