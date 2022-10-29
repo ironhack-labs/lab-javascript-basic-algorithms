e3d712655f4aa896d352eca60dce1776f0ef1ba7
Barbara-rodcab <barbara.rodrigues@cabify.com>
2022-10-29 15:12:02 +0200

    it1 + it2 + it3

--- js/index.js
+++ js/index.js
@@ -17,3 +17,27 @@ if (hacker1.length > hacker2.length) {
     
 
 // Iteration 3: Loops
+// 3.1 UpperCase + espaciado //
+let upName = hacker1.toUpperCase()
+let result = ''
+for (let i = 0; i < upName.length; i ++) {
+  result += upName[i]+ ' '
+}
+console.log(result); 
+// 3.2 hacker 2 al reves //
+let reverseH2 =''
+for (let i = hacker2.length - 1; i >= 0; i--) {
+        reverseH2 += hacker2[i];
+    }
+  console.log (reverseH2);
+
+  // 3.3 lexigraphic order//
+  if (hacker1 > hacker2 ) {
+    console.log ( `The driver's name goes first`)
+  }
+  else if (hacker2> hacker1) {
+    console.log (`Yo, the navigator goes first definitely.`)
+  }
+  else {
+    console.log (`What?! You both have the same name?`)
+  }
