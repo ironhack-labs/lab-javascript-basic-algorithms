764b69111a2472067562e86102818a0eeeeaa7e0
silvi-hdez <silviahdez.b@gmail.com>
2022-10-30 22:44:07 +0100

    Se han añadido los bonus

--- js/index.js
+++ js/index.js
@@ -45,3 +45,56 @@ if (hacker1 > hacker2) {
   console.log ("Yo, the navigator goes first definitely")
 } else { console.log ("What?! You both have the same name?")
        }
+
+
+ //Bonus 1
+ //1.1 y 1.2
+let firstParagraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+let textDivided = firstParagraph.split(' ')
+let numWords = 0
+for (let i = 0; i < textDivided.length; i ++) {
+  numWords += 1
+}
+
+console.log(numWords)
+
+//1.3
+let etWord = 0
+
+for (i = 0; i < textDivided.length; i ++) {
+  if (textDivided[i] === 'et') {
+    etWord += 1
+  }
+}
+
+console.log (etWord)
+
+//Bonus 2
+
+let phraseToCheck = 'step on no pets'
+let allInOne = ''
+
+for (let i = 0; i < phraseToCheck.length; i ++) {
+  if (phraseToCheck[i] === ' ') {
+      continue    
+  } else {
+    allInOne += phraseToCheck[i]
+  }
+}
+
+//console.log (allInOne)
+
+let reversePhrase = ''
+
+for (let i = allInOne.length - 1; i >= 0; i --) {
+  reversePhrase += allInOne[i]
+}
+
+//console.log (reversePhrase)
+
+if (allInOne === reversePhrase) {
+  console.log ("It's a Palindrome!")
+} else {
+  console.log ("Sorry, it's not a Palindrome")
+}
