77397a159775363a9988f564c80ef49bb2b9c1aa
Amanda Cuervo <amandacuervo11@gmail.com>
2022-11-02 16:48:40 +0100

    Bonus done

--- js/index.js
+++ js/index.js
@@ -1,19 +1,39 @@
 // Iteration 1: Names and Input
 
 let hacker1 = "Marina"
-  console.log(`The driver's name is ${hacker1}`);
+console.log(`The driver's name is ${hacker1}`);
 
 let hacker2 = "Amanda"
-  console.log(`The navigator's name is ${hacker2}`);
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length){
- console.log(`The driver has the longest name, it has ${hacker1}`)
+  console.log(`The driver has the longest name, it has ${hacker1}`)
 } else if (hacker1.length < hacker2.length) {
- console.log(`It seems that the navigator has the longest name, it has ${hacker2} characters.`)
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2} characters.`)
 } else if (hacker1.length === hacker2.length) {
- console.log(`Wow, you both have equally long names, Marina = ${hacker1.length} and Amanda = ${hacker2.length} characters!`)
+  console.log(`Wow, you both have equally long names, Marina = ${hacker1.length} and Amanda = ${hacker2.length} characters!`)
 }
 
-// Iteration 3: Loops
+// Iteration 3: 
+let printUpperName = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+  printUpperName += `${hacker1[i].toUpperCase()} `
+}  
+console.log(printUpperName)
+
+let nameReversed = ""
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  nameReversed += hacker2[i]
+}
+console.log(nameReversed)
+
+if (hacker1.localeCompare(hacker2) === -1) {
+  console.log("The driver's name goes first")
+} else if (hacker1.localeCompare(hacker2) === 1) {
+  console.log("Yo, the navigator goes first definitely"
+} else {
+  console.log("What?! You both have the same name?")
+}
