ac74ad48d70c39b9fc49c880dc58cf952b30b1a2
Carlos Sosa <csosa@simpletg.com>
2022-11-01 22:52:16 -0400

    minor improvements

--- js/index.js
+++ js/index.js
@@ -25,11 +25,11 @@ if (hacker1.length > hacker2.length) {
 
 let printOut = '';
 
-for (let i of hacker1) {
-  printOut += i.toUpperCase() + ' ';
+for (let char of hacker1) {
+  printOut += char.toUpperCase() + ' ';
 }
 
-console.log(printOut);
+console.log(`Hacker's 1 name in CAPS and with Spaces is: ${printOut.trim()}`);
 
 // Iteration 3.2
 let reverseName = '';
@@ -38,7 +38,7 @@ for (let x = hacker2.length - 1; x >= 0; x--) {
   reverseName += hacker2[x];
 }
 
-console.log(reverseName);
+console.log(`Hacker's 2 Reversed Name is: ${reverseName} `);
 
 // Iteration 3.3
 
@@ -52,20 +52,27 @@ if (hacker2.localeCompare(hacker1) > 0) {
 
 // BONUS
 
-let myParagraphs = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vulputate lacus sit amet fermentum fermentum. Suspendisse non magna in nisl laoreet imperdiet a mattis leo. Quisque id sodales felis. Phasellus id lobortis dui. Vestibulum in ante vitae diam tempus pulvinar viverra eu ex. Pellentesque porttitor commodo egestas. Nam gravida non nisl at lobortis. Fusce tempor pulvinar metus, a ornare metus efficitur a. Mauris eget sem nibh. Maecenas augue tortor, elementum non quam et, ullamcorper consectetur diam.
+let myParagraphs =
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vulputate lacus sit amet fermentum fermentum. Suspendisse non magna in nisl laoreet imperdiet a mattis leo. Quisque id sodales felis. Phasellus id lobortis dui. Vestibulum in ante vitae diam tempus pulvinar viverra eu ex. Pellentesque porttitor commodo egestas. Nam gravida non nisl at lobortis. Fusce tempor pulvinar metus, a ornare metus efficitur a. Mauris eget sem nibh. Maecenas augue tortor, elementum non quam et, ullamcorper consectetur diam.
 
 Aliquam ullamcorper magna at tincidunt dapibus. Nam gravida orci non sapien pretium lacinia. Morbi maximus purus vitae lacus porttitor congue. Donec luctus consequat metus et posuere. Sed dictum est at nunc vestibulum, eu sodales nibh malesuada. Etiam faucibus imperdiet maximus. Sed tristique pharetra lacus et placerat. Vestibulum sodales lacinia lectus. Aenean viverra hendrerit odio, in pulvinar elit sagittis eu. Integer sed convallis nibh, vel pellentesque felis.
 
-Nunc non lectus eu felis accumsan eleifend quis vitae nisl. Phasellus felis purus, dapibus eget erat eget, feugiat venenatis massa. Etiam odio nunc, vulputate malesuada lectus eu, mollis tristique arcu. Cras luctus est at dui lacinia, nec fringilla urna imperdiet. Phasellus commodo mollis justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut molestie mauris leo, in rutrum tortor varius eu. Phasellus sed tortor vel lorem bibendum fermentum ac in turpis. Duis ullamcorper ipsum id ante aliquam, ut consectetur urna feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at libero ac est pretium vulputate. Nunc a metus id sapien lacinia cursus. Fusce sit amet ligula in sapien posuere aliquam et molestie purus.`;
+Nunc non lectus eu felis accumsan eleifend quis vitae nisl. Phasellus felis purus, dapibus eget erat eget, feugiat venenatis massa. Etiam odio nunc, vulputate malesuada lectus eu, mollis tristique arcu. Cras luctus est at dui lacinia, nec fringilla urna imperdiet. Phasellus commodo mollis justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut molestie mauris leo, in rutrum tortor varius eu. Phasellus sed tortor vel lorem bibendum fermentum ac in turpis. Duis ullamcorper ipsum id ante aliquam, ut consectetur urna feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at libero ac est pretium vulputate. Nunc a metus id sapien lacinia cursus. Fusce sit amet ligula in sapien posuere aliquam et molestie purus.`
+    .replace(/[,.!?]/g, '')
+    .replace(/(\n)+/g, ' ');
 
 let numOfWords = myParagraphs.split(' ').length;
 
 console.log(`Number of Words: ${numOfWords}`);
-console.log(myParagraphs.indexOf('et'));
+console.log(
+  `Found the latin word "et" ${
+    myParagraphs.split(' ').filter((e) => e === 'et').length
+  } times`
+);
 
 // Bonus 2
 
-let myString = 'Something else'.replace(' ', '').toLowerCase();
+let myString = 'put it up'.replace(/[ ]/g, '').toLowerCase();
 let myStringReversed = '';
 
 for (let i = myString.length - 1; i >= 0; i--) {
