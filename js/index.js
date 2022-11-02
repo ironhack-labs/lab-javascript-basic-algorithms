5a8571e3c7bd57c50da9de1f35425ca02e15077d
Edson <dinhoyao@gmail.com>
2022-11-02 08:41:55 +0100

    iterations

--- js/index.js
+++ js/index.js
@@ -4,24 +4,39 @@
 let hacker1 = "Carla"
 console.log(`The drive's name is ${hacker1}`);
 
-let hacker2 = "Pedro"
+let hacker2 = "Marco"
 console.log(`The navigator's name is ${hacker2}`);
-console.log(`The navigator's name is ${hacker2}`);
-
 
 
 // Iteration 2: Conditionals
 
-
+if (hacker1.lenght > hacker2.lenght){
 console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+}
 
+else if (hacker1 < hacker2.lenght){
 console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
-
+}
+else{
 console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
 
 
+//### Iteration 3: Loops
+  3.1 Print all the characters of the driver's name, separated by a space and [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
+  i.e. `"J O H N"`
 
-// Iteration 3: Loops
+
+  
+  3.2 Print all the characters of the navigator's name, in reverse order. 
+  i.e. `"nhoJ"`
+  
+  3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
+    - `The driver's name goes first.` <br>
+    - `Yo, the navigator goes first definitely.` <br>
+    - `What?! You both have the same name?`
 
 
 console.log(hacker1.toUpperCase().split('').join(' '))
+
+console.log(hacker1.reverse())
