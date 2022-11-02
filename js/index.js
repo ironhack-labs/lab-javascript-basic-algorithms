587a98b1c8afe982a72169b3148ef780e577135d
Edson <dinhoyao@gmail.com>
2022-11-02 09:02:53 +0100

    loop 3.1

--- js/index.js
+++ js/index.js
@@ -23,20 +23,25 @@ console.log(`Wow, you both have equally long names, ${hacker1.length} characters
 
 
 //### Iteration 3: Loops
-  3.1 Print all the characters of the driver's name, separated by a space and [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
-  i.e. `"J O H N"`
+//3.1
 
+let printSeparatedName = "";
 
+for (let i = 0; i < hacker1.length; i++) {
+    printSeparatedName += `${hacker1[i].toUpperCase()} `
+}
+console.log(printSeparatedName);
+
+// optional 
+  console.log(hacker1.toUpperCase().split('').join(' '))
   
-  3.2 Print all the characters of the navigator's name, in reverse order. 
-  i.e. `"nhoJ"`
+
+//  3.2 Print all the characters of the navigator's name, in reverse order. 
+ 
+
+
   
   3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
     - `The driver's name goes first.` <br>
     - `Yo, the navigator goes first definitely.` <br>
     - `What?! You both have the same name?`
-
-
-console.log(hacker1.toUpperCase().split('').join(' '))
-
-console.log(hacker1.reverse())
