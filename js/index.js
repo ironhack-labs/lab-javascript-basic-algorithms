e34f21acd5a143194e7cafed462ef4bdb50e0fc2
asm-dev <salamanca.m@outlook.com>
2022-11-11 14:36:25 +0100

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
-//
+let hacker1, hacker2;
+
+hacker1 = "Ana";
+hacker2 = "Pablo";
+
+console.log("The driver's name is "+ hacker1)
+console.log("The navigator's name is "+ hacker2)
+
 // Iteration 2: Conditionals
 
 
