b3c4075abf526598b9a568f97db05229a961a9a3
Blanca <blancabcbt@gmail.com>
2022-11-11 15:30:24 +0100

    add first iteration

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 // Iteration 1: Names and Input
-//
+
+
+console.log("I'm ready!");
+
+let hacker1 = "Blanca";
+let navigator = "Chus";
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${navigator}`);
+
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
+
+
