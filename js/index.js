01249db63c0f2387be3c31415c27300570514812
Blanca <blancabcbt@gmail.com>
2022-11-11 16:11:19 +0100

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 
 console.log("I'm ready!");
 
-let hacker1 = "Blanca";
-let navigator = "Jesusa";
+let hacker1 = "Zeta";
+let navigator = "Zeta";
 console.log(`The driver's name is ${hacker1}`);
 console.log(`The navigator's name is ${navigator}`);
 
@@ -21,5 +21,38 @@ if(hacker1.length > navigator.length){
 
 // Iteration 3: Loops
 
+//Exercise 1
+let name = "";
+for( char of hacker1){
+    name += char + " ";
+}
+
+console.log(name.toUpperCase());
+
+//Exercise 2
+let backwardsName = "";
+for(let i = (navigator.length - 1); i >= 0; i--){
+    backwardsName += navigator[i];
+    
+};
+
+console.log(backwardsName);
+
+//Exercise 3
 
 
+let arrNames = [ hacker1, navigator];
+console.log(arrNames);
+
+
+let sorted = arrNames.sort();
+console.log(sorted);
+
+if( sorted[0] === sorted[1]){
+    console.log("What?! You both have the same name?")
+}
+else if( sorted[0] === hacker1){
+    console.log("The driver's name goes first");
+}else if(sorted[0] === navigator){
+    console.log("Yo, the navigator goes first definitely.")
+}
