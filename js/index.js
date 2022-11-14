04783c35ae33301fdf1dc047227d1da765c619ba
Victor Lopez <victorlpz2301@gmail.com>
2022-11-14 20:22:45 +0100

    “const

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
 // Iteration 1: Names and Input
-//
+const hacker1 = "Juan";
+
+console.log(`The driver's name is ${hacker1}`)
+
+const hacker2 = "Luis";
+
+console.log(`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
+hacker1.length = '';
+hacker2.length = '';
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}
+
+else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
+
+else if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 
 // Iteration 3: Loops
