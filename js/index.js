7c2ad62c7b77c7a64b5821a2a15e1429ac212956
physiodevapp <ficapp2.0@gmail.com>
2022-11-14 23:15:22 +0100

    Iteration 1: Names and Input

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
-//
+const hacker1 = "XXXX";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "YYYY";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
-// Iteration 3: Loops
+// Itenoderation 3: Loops
