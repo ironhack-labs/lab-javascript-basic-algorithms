1c079e66d18b9915ce073c5647eb901ffe818786
physiodevapp <ficapp2.0@gmail.com>
2022-11-15 17:19:32 +0100

    Bonus 1

--- js/index.js
+++ js/index.js
@@ -36,3 +36,21 @@ for (let charIndex = driverName.length - 1; charIndex >= 0; charIndex--) {
 }
 
 console.log(driverNameReverse)
+
+// Bonus 1:
+let loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare augue et purus sollicitudin, non tempor ligula porttitor. Donec urna elit, pharetra id tellus sit amet, tincidunt tempus ante. Maecenas ac urna orci. Vestibulum ut velit et lacus dignissim fringilla. Suspendisse scelerisque mi eros, at luctus sapien pretium a. Cras euismod fringilla velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam gravida accumsan tincidunt. Praesent at molestie dolor. Suspendisse potenti. Nam iaculis sapien nec lectus aliquam fringilla. Aliquam quis tellus eget risus porttitor varius ac a mauris. Etiam placerat tellus non massa ultricies, et ornare dolor molestie. Etiam imperdiet lacus ac leo venenatis egestas. Nullam sed mollis lectus. Etiam rhoncus a lectus quis facilisis. Phasellus nunc velit, scelerisque ut tempor a, feugiat sed magna. Duis metus augue, ultricies sit amet ornare ut, luctus sed massa. Nam nunc augue, euismod vel commodo faucibus, facilisis et massa. Donec quis ornare orci. Curabitur pretium ante vitae pulvinar auctor. Pellentesque ac libero nec est pellentesque venenatis sed at lorem. Aliquam sit amet accumsan turpis. Duis accumsan neque enim, quis porttitor tortor consequat in. Donec condimentum cursus dapibus. Sed semper maximus nunc at malesuada. Donec sed ullamcorper tortor. Integer ut dolor eu mauris fermentum gravida.'
+
+let countWords = 0;
+let countWordsEt = 0;
+
+for (char of loremIpsum) {
+    if (char != " ") {
+        countWords++
+    }
+
+    if (char === "et") {
+        countWordsEt++
+    }
+}
+
+console.log(`Bonus 1 >> Palabras totales: ${countWords}. Palabras "et": ${countWordsEt}`)
