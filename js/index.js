af6bfe3c627e8b2e649e24aa72ac3b95d4091cf5
physiodevapp <ficapp2.0@gmail.com>
2022-11-15 17:12:15 +0100

    Iteration 2 & 3

--- js/index.js
+++ js/index.js
@@ -1,14 +1,38 @@
 
 console.log("I'm ready!");
 
-// Iteration 1: Names and Input
-const hacker1 = "XXXX";
-console.log(`The driver's name is ${hacker1}`);
+const hacker1 = "Edu";
+const hacker2 = "Google";
+let driverName = '';
+let driverNameReverse = '';
 
-const hacker2 = "YYYY";
+// Iteration 1: Names and Input
+console.log(`The driver's name is ${hacker1}`);
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+} else if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
 
 
 // Itenoderation 3: Loops
+for (char of hacker1) {
+    if (driverName.length > 0) {
+        driverName += " " + char.toUpperCase()
+        continue;
+    }
+    driverName += char
+}
+
+console.log(driverName);
+
+for (let charIndex = driverName.length - 1; charIndex >= 0; charIndex--) {
+    driverNameReverse += driverName[charIndex]
+}
+
+console.log(driverNameReverse)
