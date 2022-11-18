a9954ccf32c9863e2fef6f49cbcc475400a8edce
silvia Edreira <silviaedreiragg@gmail.com>
2022-11-18 10:51:49 +0100

    lab2 finished

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
+console.log('i am ready!');
+
 // Iteration 1: Names and Input
+const hacker1 ='silvia';
+console.log(`The driver name is ${hacker1}`);
+const hacker2 ='pedro';
+console.log(`The navigator's name is ${hacker2}`)
+
 //
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log(`the driver has the longest name, it has ${hacker1.length} characters`);
+    }
+else if (hacker2.length> hacker1.length) {
+    console.log(`it seems that the navigator has the longest name, it has ${hacker2.length} characters`);ç
+    }
+else {console.log(`wow! you both have equally long names, ${hacker1.length} characters`)
+    };
 
 // Iteration 3: Loops
-console.log('i am ready!')
+function capitalLetters (hacker1) {
+    const HACKER1= hacker1.toUpperCase();
+      let result='';
+      for (let i = 0; i < HACKER1.length; i++) {
+          result+=` ${HACKER1[i]}`; console.log(result)
+      }
+    return result;
+  }
+  capitalLetters(hacker1)
