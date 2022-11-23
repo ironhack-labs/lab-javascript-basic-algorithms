e7a28723fc6bdbe0a106088bbeac5a3c3146b07a
Laura Bährs <laura.baehrs@googlemail.com>
2022-11-23 21:18:20 +0100

    work on displaying name on one line

--- js/index.js
+++ js/index.js
@@ -21,20 +21,25 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
-
+let driverUpper = '';
 for (let i = 0; i < hacker1.length; i++) {
-    let characters = hacker1.charAt(i).toUpperCase();
-    console.log(`${characters} `);
+    let characters = '';
+    characters = hacker1.charAt(i).toUpperCase();
+    driverUpper = driverUpper.concat(characters, ' ');
 }
+console.log(driverUpper);
+let navRev = '';
 for (let i = hacker2.length; 0 <= i; i--) {
-    let characters = hacker2.charAt(i);
-    console.log(`${characters} `);
+    let characters = '';
+    characters = hacker2.charAt(i);
+    navRev = navRev.concat(characters);
 }
+console.log(navRev);
 
 if (hacker1.localeCompare(hacker2) < 0) {
-    console.log("The driver's name goes first");
+    console.log("The driver's name goes first.");
 } else if (hacker2.localeCompare(hacker1) > 0) {
-    console.log("Yo, the navigator goes first definitely");
+    console.log("Yo, the navigator goes first definitely.");
 } else {
     console.log("What?! You both have the same name??")
 }
