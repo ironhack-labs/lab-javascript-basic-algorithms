4e2da2aa61622237f59db9106f77f0b32c6f7e58
Rumeli Paul <rumelipaul@gmail.com>
2022-11-23 17:26:28 +0100

    First iteration done- Thalita and Rumeli

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 // Iteration 1: Names and Input
+const hacker1 = "Rumeli";
+const hacker2 = "Thalita";
+console.log(`The Driver's name is ${hacker1}`);
+console.log(`The Navigator's name is ${hacker2}`);
+
+
 //
 // Iteration 2: Conditionals
 
