63775c6bef168233474f55a4eb581f7ffb673825
Laura Bährs <laura.baehrs@googlemail.com>
2022-11-23 22:22:08 +0100

    finish bonus

--- js/index.js
+++ js/index.js
@@ -43,3 +43,59 @@ if (hacker1.localeCompare(hacker2) < 0) {
 } else {
     console.log("What?! You both have the same name??")
 }
+// Bonus 1
+let loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium erat interdum, elementum risus ac, lacinia neque. Aliquam a pellentesque augue. Sed ut lectus vulputate, varius dolor id, vulputate tellus. Phasellus a ligula quis sapien interdum dignissim. Cras accumsan ante quis tristique lacinia. Fusce consequat nisl quis aliquet mattis. Aliquam posuere aliquam metus, id molestie massa sodales sed. Aenean in pretium lectus. Aenean commodo semper scelerisque. Pellentesque venenatis libero quis fermentum consectetur. Pellentesque quis velit iaculis, commodo augue sit amet, commodo sapien. Pellentesque a sagittis turpis, ut varius nisi. Quisque aliquam dolor at felis auctor, pellentesque feugiat tortor suscipit. Suspendisse potenti. Phasellus pretium mattis quam, quis accumsan lorem pellentesque in. Aliquam rutrum venenatis velit id accumsan. Mauris a erat nibh. Donec a luctus felis. Maecenas non ultrices turpis. Suspendisse elementum a dolor non dictum. Donec sed mauris finibus, tincidunt dolor a, suscipit nisi. Etiam a faucibus erat, sed efficitur nunc. Pellentesque eget sagittis nibh, tincidunt mattis metus. Proin vulputate magna at lorem mollis convallis. Duis tincidunt felis nulla, tincidunt luctus ex ultricies nec. Etiam aliquam eros velit, ac convallis dolor gravida nec.';
+
+
+let wordCount = 0;
+for (let i = 0; i < loremIpsum.length; i++) {
+    // count all the spaces in string
+    if (loremIpsum.charAt(i) === ' ') {
+        wordCount += 1;
+    }
+}
+// wordCount +1 because there are no spaces in beginning or end of string
+console.log(`Words: ${wordCount + 1}`);
+
+//Bonus 2
+let phraseToCheck = "No 'x' in Nixon";
+let phraseReverse = '';
+
+// removing all extra characters and spaces
+// putting to lowerCase
+phraseToCheck = phraseToCheck.toLowerCase();
+
+while (phraseToCheck.includes(',')) {
+    phraseToCheck = phraseToCheck.replace(',', '');
+}
+while (phraseToCheck.includes('!')) {
+    phraseToCheck = phraseToCheck.replace('!', '');
+}
+while (phraseToCheck.includes(' ')) {
+    phraseToCheck = phraseToCheck.replace(' ', '');
+}
+while (phraseToCheck.includes('?')) {
+    phraseToCheck = phraseToCheck.replace('?', '');
+}
+while (phraseToCheck.includes("'")) {
+    phraseToCheck = phraseToCheck.replace("'", "");
+}
+
+//reversing phrase to check
+for (let i = phraseToCheck.length; 0 <= i; i--) {
+    let characters = '';
+    characters = phraseToCheck.charAt(i);
+    phraseReverse = phraseReverse.concat(characters);
+}
+
+let isPalindrome = true;
+for(var i=0; i < (phraseReverse.length)/2; i++){ 
+    if(phraseReverse[i] !== phraseReverse[phraseReverse.length-i-1]){ 
+      isPalindrome = false;
+    }
+  }
+  if (isPalindrome) {
+    console.log('It is palindrome! Oh yeah!')
+  } else {
+    console.log('Not a palindrome. ')
+  }
