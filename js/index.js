9ede9092990cd2d0cdd5c515d1b5f3235299e08d
Thalita dos Reis <dosreistha@gmail.com>
2022-11-23 17:34:52 +0100

    iterarion 2 done. Rumeli & Thalita

--- js/index.js
+++ js/index.js
@@ -4,9 +4,13 @@ const hacker2 = "Thalita";
 console.log(`The Driver's name is ${hacker1}`);
 console.log(`The Navigator's name is ${hacker2}`);
 
-
-//
 // Iteration 2: Conditionals
-
+if(hacker1.length === hacker2.length){
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+} else if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
 
 // Iteration 3: Loops
