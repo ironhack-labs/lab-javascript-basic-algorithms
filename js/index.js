35478ff428aa359b2db98021b72ae7e4c14fc3fe
SarahK-coder <117637388+SarahK-coder@users.noreply.github.com>
2022-11-24 08:43:41 +0100

    Completed Bonus 1

--- js/index.js
+++ js/index.js
@@ -48,3 +48,30 @@ if (result === -1) { // -1 Driver first
 } else { // 0 // Same
     console.log("What?! You both have the same name?");
 }
+
+//*** Bonus 1:
+// Generate 3 paragraphs. Store the text in a variable type of string.
+let loremIpsum = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec purus fermentum, dictum nisl auctor, porttitor eros. Phasellus sollicitudin nisi mauris, eu posuere turpis sollicitudin sit amet. Maecenas at dui varius erat dignissim condimentum. Donec pretium est et metus rhoncus, a convallis dui accumsan. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean tincidunt tellus nisi, et iaculis ligula blandit a. Curabitur eleifend tortor in efficitur blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas eleifend fringilla arcu sit amet congue. Aliquam aliquam sodales metus, fringilla pharetra augue fringilla et. In eu cursus orci, sed semper leo.
+
+Ut sollicitudin elit congue imperdiet euismod. Donec eget dui lectus. Aliquam placerat elit lacus, quis eleifend nibh mattis sit amet. Nulla facilisi. Mauris posuere lacinia mauris, non hendrerit sem dignissim a. Nullam ac eros suscipit, cursus magna quis, cursus risus. Vivamus vitae ante lobortis, blandit arcu ac, pulvinar massa. Morbi efficitur leo vel libero dignissim, pretium mattis lacus pulvinar. Mauris nec ex a orci blandit imperdiet.
+
+Sed vitae nibh sed dui laoreet aliquet. Praesent in aliquet leo. Fusce quis erat purus. Vestibulum scelerisque egestas dolor quis auctor. Fusce pellentesque gravida vehicula. Etiam cursus odio vitae erat efficitur dapibus. Morbi id dictum lorem, eget venenatis nibh.`;
+
+// Make your program count the number of words in the string.
+function countWords(str) { 
+    return str.split(" ").length;
+}
+  
+console.log(countWords(loremIpsum));
+
+// Make your program count the number of times the Latin word et appears.
+function countOccurence(str, theWord) {
+    return str.split(theWord).length -1;
+}
+console.log(countOccurence(loremIpsum, "et"));
+
+//*** Bonus 2:
+/*
+Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
+*/
