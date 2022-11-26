8ad95f626f9bca8672809ce82997ee23caa33356
Carlos Lopez <carlopex97@gmail.com>
2022-11-26 19:46:13 +0100

    solucionados algunos fallos

--- js/index.js
+++ js/index.js
@@ -1,51 +1,49 @@
-console.log("I'm ready!")
+console.log( "I'm ready!" )
 
-console.log("Empieza el 1")
+console.log( "Empieza el 1" )
 
-    const hacker1="antonio"
-    console.log("The driver's name is "+hacker1)
-    const hacker2="Nicolas"
-    console.log("The navigator's name is "+hacker2)
+    const hacker1 = "antonio"
+    console.log( `The driver's name is ${hacker1}.` )
+    const hacker2 = "Nicolas"
+    console.log( `The driver's name is ${hacker2}.` )
 
-console.log("Empieza el 2")
+console.log( "Empieza el 2" )
 
-    if(hacker1.length>hacker2.length){
-        console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
-        }else if(hacker1.length<hacker2.length){
-            console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
-            }else{
-                console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
+    if ( hacker1.length > hacker2.length ) {
+        console.log( `The driver has the longest name, it has ${hacker1.length} characters` )
+        } else if ( hacker1.length < hacker2.length ) {
+            console.log( `It seems that the navigator has the longest name, it has ${hacker2.length} characters` )
+            } else {
+                console.log( `Wow, you both have equally long names, ${hacker2.length} characters!` )
     }
 
-console.log("Empieza el 3")
+console.log( "Empieza el 3" )
 
-    let finalHacker1Name="";
+    let finalHacker1Name = "";
 
-    for(i=0;i<hacker1.length;i++){
-        finalHacker1Name+=hacker1[i].toUpperCase() + " ";
+    for ( i = 0 ; i < hacker1.length ; i++ ) {
+        finalHacker1Name += hacker1[i].toUpperCase() + " ";
     }
     console.log(finalHacker1Name)
 
-    let navigatorNameReverse=" "
+    let navigatorNameReverse = " "
     
-    for(i=hacker2.length-1;i>=0;i--){
-        navigatorNameReverse+=hacker2[i]
+    for ( i = hacker2.length-1 ; i >= 0 ; i-- ) {
+        navigatorNameReverse += hacker2[i]
     }
 
     console.log(navigatorNameReverse)
 
-    if(hacker1.toLowerCase()<hacker2.toLowerCase()){
-        console.log("The driver's name goes first.")
-        }else if(hacker2.toLowerCase()<hacker1.toLowerCase()){
-            console.log("Yo, the navigator goes first definitely.")
-            }else{
-               console.log("What?! You both have the same name?") 
+    if ( hacker1.toLowerCase() < hacker2.toLowerCase() ) {
+        console.log( "The driver's name goes first." )
+        } else if (hacker2.toLowerCase() < hacker1.toLowerCase()){
+            console.log( "Yo, the navigator goes first definitely." )
+            } else {
+               console.log( "What?! You both have the same name?" ) 
             }
+        
+    console.log( "comienza extra 1" )        
 
-console.log("Bonus time 1")            
-
-    let loremIpsum="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis auctor molestie. Mauris sagittis nisl iaculis, venenatis lacus at, molestie risus. Suspendisse egestas purus magna, sit amet tempor libero auctor sed. In nec dapibus sem. Nunc ut nunc et sapien rutrum volutpat. Curabitur nec auctor sapien. Integer fermentum condimentum erat. Duis semper viverra enim, vel venenatis arcu scelerisque at. Nullam faucibus quam ac laoreet tempor.
-
-    Nunc placerat velit quis erat egestas convallis. Nunc augue velit, vulputate ut ultricies non, iaculis id libero. Nam mollis rhoncus nisl. Cras maximus tortor ac suscipit congue. Nam ac sapien elit. Quisque ut tortor ut tellus porta accumsan. Morbi efficitur vulputate metus ut accumsan. Nulla porttitor gravida neque, dignissim pharetra ipsum. Praesent vel tortor quis erat mollis pellentesque. Aliquam semper sodales tellus, sed ornare metus ultrices vel. Nulla rhoncus hendrerit lacinia. Donec eget enim tortor. Maecenas lectus mauris, porta quis accumsan sed, dignissim ut erat. Vestibulum suscipit urna in bibendum mattis.
-    
-    Fusce ac porttitor eros. Sed sollicitudin felis a nunc aliquam, non aliquet velit condimentum. Morbi elementum at felis vitae vehicula. Morbi eros augue, congue ac venenatis a, faucibus non lacus. Duis efficitur quam ut nibh cursus tristique. Duis at leo purus. Nunc non vulputate leo. Phasellus sit amet ullamcorper libero."
+    const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id nulla dictum, posuere elit sed, lobortis nulla. Aenean mollis arcu diam. Phasellus bibendum, ante a porta molestie, metus ex auctor augue, ac placerat velit dui at enim. Vivamus tincidunt justo ut semper mollis. Sed sollicitudin velit ut dignissim sodales. Sed sit amet sollicitudin ex, at pretium orci. Sed finibus nibh interdum, ornare augue eget, auctor lorem. Vivamus sagittis ullamcorper nisi, in mattis sapien congue non. Etiam porttitor turpis risus, et commodo lacus suscipit id. Nulla a facilisis elit. Etiam non ante vulputate, efficitur ipsum quis, ullamcorper justo. Pellentesque rhoncus metus at bibendum vulputate.\n
+    Morbi felis lacus, fermentum vel erat in, porta hendrerit nulla. Curabitur arcu leo, scelerisque at massa non, blandit bibendum lectus. Etiam quis metus bibendum, accumsan ex at, interdum orci. Etiam a ante vitae augue convallis efficitur. Sed sit amet diam porttitor, tristique enim at, ultrices dui. Proin bibendum, lacus faucibus cursus bibendum, ligula quam cursus lorem, non ultrices dolor lacus ac urna. Donec rutrum tincidunt tortor. Ut ultrices, lectus non pulvinar maximus, libero nisl semper est, quis luctus lectus velit sit amet sem.\n
+    Proin aliquam sem vitae finibus ornare. Aliquam ornare sem elit, id tincidunt ipsum ullamcorper nec. Nullam sed luctus arcu. Donec sit amet ipsum magna. Vestibulum malesuada consequat varius. In hac habitasse platea dictumst. Vestibulum egestas porttitor nibh a viverra. Nulla dignissim non ex in tincidunt. Nunc pretium mi condimentum augue facilisis feugiat. Morbi luctus tempus est eu cursus. Maecenas malesuada leo sit amet tempus fringilla.`
