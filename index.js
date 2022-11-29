e3ff91b27ab11e7c651eda640ed64e3e2469df9d
Mevine Teelokee <mevine.teelokee@gmail.com>
2022-11-29 18:17:09 +0100

    Issue solved for iteration 2, All iterations completed

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ console.log(`The navigator's name is ${hacker2}.`)
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length ){
-    `The driver has the longest name, it has ${hacker1.length} characters.`
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
 }
 else if (hacker1.length < hacker2.length ){
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
 }
 else if (hacker1.length = hacker2.length ){
-    `Wow, you both have equally long names, ${hacker2.length} characters.`
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters.`)
 }
+
+// ternary operator version if needed
+//hacker1.length > hacker2.length ? console.log(`The driver has the longest name, it has ${hacker1.length} characters.`) :
+//hacker1.length < hacker2.length ? console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`) :
+//console.log(`Wow, you both have equally long names, ${hacker2.length} characters.`)
+
 
 //Iteration 3: Loops
 
