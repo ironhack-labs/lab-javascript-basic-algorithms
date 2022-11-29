6b96348a8d70d3807c3215d77ece3feaff494265
Mevine Teelokee <mevine.teelokee@gmail.com>
2022-11-29 16:43:25 +0100

    First iteration complete

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
 
+let hacker1 = 'Ronaldinho';
+console.log(`The driver's name is ${hacker1}`)
+let hacker2 = 'Zlatan';
+console.log(`The navigator's name is ${hacker2}`)
+
 
 // Iteration 2: Conditionals
 
