19026ee49fadd5f6c5515f3b2c07a53fb18feae6
Mevine Teelokee <mevine.teelokee@gmail.com>
2022-11-29 18:12:38 +0100

    ternary operations removed

--- index.js
+++ index.js
@@ -18,12 +18,6 @@ else if (hacker1.length = hacker2.length ){
     `Wow, you both have equally long names, ${hacker2.length} characters.`
 }
 
-// ternary operator version if needed
-hacker1.length > hacker2.length ? console.log(`The driver has the longest name, it has ${hacker1.length} characters.`) :
-hacker1.length < hacker2.length ? console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`) :
-console.log(`Wow, you both have equally long names, ${hacker2.length} characters.`)
-
-
 //Iteration 3: Loops
 
 // 3.1
