3b10dfb92ea81f0d7edcd1d5a3fbc644dc721329
Ipoddubnaya <65393860+Ipoddubnaya@users.noreply.github.com>
2022-12-09 01:39:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-// Iteration 1: Names and Input
+// Iteration 1
 
+let hacker1 = "Jack"
+console.log(`The driver's name is ${hacker1}`)
 
-// Iteration 2: Conditionals
+let hacker2 = "Fred" 
+console.log(`The navigator's name is ${hacker2}`)
 
+// Iteration 2
 
-// Iteration 3: Loops
+if (hacker1.length > hacker2.length) {
+     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+   } else if (hacker1.length < hacker2.length) {
+     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+   } else {
+     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+   }
+
+// Iteration 3
+
+console.log(hacker1.split("").join(" ").toUpperCase())
+
+console.log(hacker2.split("").reverse().join(""))
+
+let nameCompare = hacker1.localeCompare(hacker2)
+
+if(nameCompare > 0) {
+    console.log(`Yo, the navigator goes first definitely.`)
+} else if(nameCompare < 0) {
+    console.log(`The driver's name goes first.`)
+} else {
+    console.log(`What?! You both have the same name?`)
+}
