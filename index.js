b4bddfbbc7ca53cde1ccd1697ccbf0b9b0e2658b
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-08 21:49:25 +0100

    Add iteration 1

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Iteration 1: Names and Input
+let hacker1 = "Helmut"
+console.log(`The driver's name is ${hacker1}`)
+let hacker2 = "Frieda"
+console.log(`The driver's name is ${hacker1}`)
 
 
 // Iteration 2: Conditionals
