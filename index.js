417a787d81cf2eb07c806211f51d3fe87820ba4c
Nabil Aslam <118678544+NabilAslam@users.noreply.github.com>
2022-12-09 20:34:10 +0000

    Tasks done

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-// Iteration 1: Names and Input
+let driverName = `hacker1`("driverName")
+let navigatorName = `hacker2 ("navigatorName")
+
+if (driverName == `hacker1`) {
+console.log("The ${driverName} is XXXX")
+}
+
+else if (navigatorName = `hacker2`) {
+console.log("The ${navigatorName} is YYYY")
+}
+else {
+console.log("undefined")
+}
+
+let driverName = prompt("driver name:")
+  let navigatorName = prompt("navigator name:")
+
+if (driverName.lenght > navigatorName.lenght)
+{
+  console.log("The driver has the longest name, it has XX characters")
+}
+    else if (driverName.lenght < navigatorName.lenght)
+    {
+      console.log("It seems that the navigator has the longest name, it has XX characters")
+    }
+else {
+  console.log("Wow, you both have equally long names, XX characters!")
+}
+
+const str = 'driverName';
+
+const result = str.split('').join(' ');
+console.log(result)
+console.log(`${str} ${str.length}`);
 
 
-// Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
+// I tried what I could but I was not able to solve all tasks but I did my best
