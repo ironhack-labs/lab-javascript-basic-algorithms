66688ce3a7be74dc47e0d40a547e72baa86c6e05
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2022-12-11 13:53:08 +0100

    updated

--- index.js
+++ index.js
@@ -67,3 +67,18 @@ let nameArray = []
 let count = nameArray.push(hacker1, hacker2)
     console.log(nameArray.sort())
 
+
+// BONUS
+
+    let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                    mollit anim id est laborum.`
+
+     
+     let wordCount = longText.split(" ").length
+     console.log(wordCount)
+
+
+
