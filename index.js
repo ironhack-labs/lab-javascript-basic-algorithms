3696f9dfc90abbff26e7e0cad7d630d367a9c321
Paul Isoard <paulisoard2911@gmail.com>
2022-12-19 12:38:29 +0400

    done

--- index.js
+++ index.js
@@ -51,3 +51,48 @@ order < 0
   : order > 0
   ? console.log("Yo, the navigator goes first definetly")
   : console.log("What?! You both have the same name?"); */
+
+// Bonus 1 - Count the words in a string
+longText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+
+// Count the words of a string
+const countWords = (longText) => {
+  const newCount = longText.split(' '); // Return longText in an array
+  const myNewCount = newCount.filter(e => e !== ''); // Return the array without its spaces
+  return myNewCount.length // Return the array length
+}
+console.log(countWords(longText))
+
+// Count one word in particular in a string
+const countOneWord = (longText) => {
+  let howManyTimes = []
+  const arr = longText.split(' ')
+  for (let i = 0; i < longText.length; i++) {
+    if (arr[i] === 'Ipsum') {
+      howManyTimes.push(arr[i])
+    }
+  }
+  return howManyTimes.length
+}
+console.log(countOneWord(longText))
+
+
+// Bonus 2 - Check if the string is a Palindrome
+string = ''
+
+const palindrome = (string) => {
+  let newString = string.split(' ').join('')
+  let stringBackward = ''
+  
+  for (let i = newString.length - 1; i >= 0; i--) {
+    stringBackward += newString[i]
+  }
+
+  if (newString === stringBackward) {
+    console.log(`"${string}" is a palindrome`)
+  } else {
+    console.log(`"${string}" is not a palindrome`)
+  }
+}
+
+palindrome('race car')
