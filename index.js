fb10df6d16bfc3e9324491c89dc99a42857c918e
Lorexia <alexia.lorenza.martinel@protonmail.com>
2023-01-11 12:22:20 +0100

    iteration1 pushed

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
+/* Iteration 1: Names and Input
 
+1.1 Create a variable hacker1 with the driver's name.
+1.2 Print "The driver's name is XXXX".
+1.3 Create a variable hacker2 with the navigator's name.
+1.4 Print "The navigator's name is YYYY". */
+
+const hacker1 = "Silur";
+console.log(`The drivers name is ${hacker1}`);
 
 // Iteration 2: Conditionals
 
