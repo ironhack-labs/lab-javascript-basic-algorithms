7358cb7a2888c517677eb5dce6cde2340f6f7c7a
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-01-11 15:46:47 +0100

    Add answers of Bonus1

--- index.js
+++ index.js
@@ -54,3 +54,31 @@ else if (hacker1 > hacker2) {
 else if (hacker1 === hacker2) {
   console.log("What?! You both have the same name?")
 }
+
+//Bonus 1
+//Number of words in the string
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac sapien porta, varius dui id, hendrerit urna. Ut volutpat at tortor sit amet tempor. Pellentesque placerat eros a ligula rhoncus auctor. Maecenas ac elit in nisi elementum dignissim ut et ipsum. Donec quis dui in libero finibus mollis eget eu quam. Aliquam auctor, sapien feugiat tempor convallis, enim leo rutrum risus, id maximus orci lectus eget eros. Aliquam eget dui ante. Nullam arcu orci, congue ultrices mauris quis, ultricies malesuada ante. Mauris fermentum pretium lobortis. Vestibulum nec justo sodales, mattis purus vitae, porta eros. Vestibulum pharetra elit in nisl finibus volutpat. Mauris at accumsan urna, at condimentum diam. Nunc lacinia erat vel mollis molestie. Donec tincidunt consectetur mi id maximus. Sed auctor quam id sapien ornare, sit amet finibus augue porttitor. Nullam in metus quis ipsum vulputate imperdiet. Donec dignissim purus non libero rhoncus, nec tristique nisi tempor. Integer velit purus, accumsan in leo sed, facilisis tempus turpis. Sed tempor felis in dignissim varius. Nullam consectetur tellus nec tristique dapibus. Nullam vel dolor rutrum, lacinia risus ut, volutpat elit. Donec vehicula dui mauris, in semper erat posuere a. Sed a augue eget ex elementum viverra. Cras facilisis condimentum eros, ac interdum lorem faucibus ac. Donec tristique dui eu velit iaculis, mollis ullamcorper nulla mollis. Duis faucibus diam et aliquet vulputate. Quisque eget urna sed metus varius tempor non vel purus. Nulla in erat mattis sapien sagittis congue et ac nisl. Praesent pellentesque quis ex at tincidunt. In hac habitasse platea dictumst. Donec pellentesque quam vitae lacinia finibus. Ut et massa massa. Pellentesque accumsan, dui vitae dignissim suscipit, est diam placerat velit, at porta magna leo id dui."
+
+let count = 1;
+
+for (let i = 1; i <= longText.length -1; i++) {
+  if(longText[i] === " ") {
+    count++;
+  }
+}
+
+console.log(count)
+
+//Number of times the word et appears
+
+let count = 0
+
+for (let i = 0; i <= longText.length -1; i++) {
+  const fourChars = longText[i] + longText[i + 1] + longText[i + 2] + longText[i + 3];
+  
+  if ( fourChars === " et " ) {
+    count++;
+  }
+}
+
+console.log(count)
