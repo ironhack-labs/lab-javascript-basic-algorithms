e2415f3571d557438407fc426df55397557803d3
Keifer Shearer <keifearer@gmail.com>
2023-01-18 17:01:51 +0100

    iteration 3

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+console.log(`${hacker1.toUpperCase().split('').join(' ')}`)
+let str= ''
+for (let i=hacker2.length-1; i >= 0; i--) {
+  str = str + hacker2[i];
+}
+console.log(str);
+if (hacker1 > hacker2){
+    console.log(`The driver's name goes first`)
+}
+else if (hacker2 > hacker1){
+    console.log(`Yo, the navigator goes first definitely`)
+}
+else {
+    console.log(`What?! You both have the same name?`)
+}
