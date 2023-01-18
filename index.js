a6afedbed8dafcc65dc0ab98c1e3dbf9062ea0f8
Mehdi Eddira <m.eddira75@outlook.fr>
2023-01-18 15:57:51 +0100

    iteration2

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ let hacker2 = 'Mehdi'
 console.log(hacker2);
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+     console.log( `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log(`Wow, you both have equally long names,${hacker2.length} characters!`);
+}
+
 
 // Iteration 3: Loops
