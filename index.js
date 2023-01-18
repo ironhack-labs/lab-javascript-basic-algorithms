e3319215cf3d173b2c342515ef6fa62aa78b71f6
romi <romi.varnier@gmail.com>
2023-01-18 15:20:51 +0100

    3rd iteration

--- index.js
+++ index.js
@@ -15,4 +15,25 @@ if (hacker1.length > hacker2.length) {
 
 
 // Iteration 3: Loops
+let newName = "";
+for (let i = 0; i < hacker1.length; i++) {
+  newName += hacker1[i].toUpperCase();
+  if (i !== hacker1.length - 1) {
+    newName += " ";
+  }
+}
+console.log(newName);
 
+let newName2 = "";
+for (let i = hacker2.length -1; i >= 0; i--) {
+  newName2 += hacker2[i];
+}
+console.log(newName2);
+
+if (hacker1 < hacker2) {
+  console.log(`The driver's name goes first.`)
+} else if (hacker2 < hacker1) {
+  console.log(`Yo, the navigator goes first definitely.` )
+} else {
+  console.log(`What?! You both have the same name?`)
+}
