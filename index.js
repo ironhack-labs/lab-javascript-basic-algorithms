3e8d0b60bbcbc20c73835e8f4545804ff6c6187e
Keifer Shearer <keifearer@gmail.com>
2023-01-18 15:12:58 +0100

    first iteration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Keifer"
+let hacker2= "Christ"
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
