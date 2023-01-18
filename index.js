d45561dd54fd51a5952081894cf42294e243b4a1
romi <romi.varnier@gmail.com>
2023-01-18 14:59:22 +0100

    first itteration

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Iteration 1: Names and Input
-
-
+const hacker1 = "Romi";
+console.log(`"The driver's name is ${hacker1}"`);
+const hacker2 = "Sebastian";
+console.log(`"The navigator's name is ${hacker2}"`)
 // Iteration 2: Conditionals
 
-
 // Iteration 3: Loops
+
