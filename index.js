92e66ee5aef6f291d96e4b020fb364f1db9682d5
Loïc Messela <messelaloic@gmail.com>
2023-01-18 15:11:48 +0100

    1st iteration done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // Iteration 1: Names and Input
+let hacker1 = "Loic";
 
+console.log(`The driver's name is ${hacker1}.`)
 
+let hacker2 = "Marianne"
+
+console.log(`The navigator's name is ${hacker2}.`)
 // Iteration 2: Conditionals
+
 
 
 // Iteration 3: Loops
