01b07dd835e96042e2d8e324a90745d47982e296
Andrea <andreadepaul93@hotmail.com>
2023-01-19 16:30:30 +0100

    done

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // Iteration 1: Names and Input
 
+let hacker1= "Andrea"
+console.log(`THE DRIVERS NAME IS ${hacker1}`)
+
+let hacker2=" Adrian"
+console.log(`THE NAVEGATOR´S NAME IS ${hacker2}`)
 
 // Iteration 2: Conditionals
+let hacker1= "Andrea"
+console.log(`THE DRIVERS NAME IS ${hacker1}`)
 
+let hacker2="Adrian"
+console.log(`THE NAVEGATOR´S NAME IS ${hacker2}`)
+
+if(hacker1.length>hacker2.length){console.log(`the driver has the longest name,if has ${hacker1.length}`)}
+else if (hacker1.length<hacker2.length){console.log(`it seems that the navegator has the longest name,it has ${hacker2.length} Characters `)}
+else {console.log(`wow,you both have equally long names,${ hacker1.length} characters!`)}
 
 // Iteration 3: Loops
+let hacker1= "Andrea"
+console.log(`THE DRIVERS NAME IS ${hacker1}`)
+let hacker1InCaps=hacker1.toUpperCase()
+let space= " "
+console.log(hacker1InCaps)
+for ( let i = 0; i< hacker1InCaps.length; i++){ console.log(hacker1InCaps.length(i) + space(i))}
+
+let hacker2="Adrian"
+console.log(`THE NAVEGATOR´S NAME IS ${hacker2}`)
