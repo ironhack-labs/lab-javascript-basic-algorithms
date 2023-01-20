74d680f3c60cdec227e68ca436038cad1e7ba3e2
Joao Vitor <joao-vitor.fede@hpe.com>
2023-01-20 18:07:36 -0300

    bonus 1 e 2 done

--- index.js
+++ index.js
@@ -48,3 +48,54 @@ if(hackersCompare === 1){
 
   
 
+//BÔNUS1
+const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales nunc lectus, nec fringilla mi ultricies ac. Sed condimentum tempus magna blandit interdum. Vestibulum porttitor luctus dui at vehicula. Vestibulum commodo eros ut posuere mollis. Etiam gravida dapibus tellus, ullamcorper suscipit metus cursus nec. Morbi tristique scelerisque quam id viverra. Praesent mattis, nunc placerat vulputate mollis, metus nulla ultrices ligula, vitae elementum lacus turpis ut augue. Morbi aliquet quam et erat elementum, eget viverra ante hendrerit. Vestibulum vel nisi metus. In volutpat tortor efficitur interdum auctor. Vivamus accumsan suscipit elit nec maximus. Etiam et purus turpis. Vivamus at purus eget leo vestibulum dignissim quis et nibh. Integer leo sapien, pellentesque at viverra in, mattis ut ante. Integer aliquam vitae lorem condimentum aliquam. Phasellus vel felis ut ex finibus mollis. Vestibulum vulputate, tortor sed accumsan condimentum, risus nibh feugiat quam, quis sollicitudin ligula mi sit amet libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et ultrices libero. Phasellus gravida metus commodo metus sollicitudin, feugiat fermentum odio tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi gravida interdum nisl at suscipit. Pellentesque aliquet auctor tristique. Integer sed tortor a eros pellentesque luctus. Pellentesque urna eros, venenatis id porta eget, sodales a justo.Sed dictum malesuada odio, porttitor imperdiet nunc posuere sit amet. Proin eget ex eu nibh ultricies placerat. Aliquam at ullamcorper arcu. Quisque eget urna orci. Curabitur diam est, cursus quis volutpat eu, malesuada eu enim. Mauris ligula lacus, maximus nec felis vitae, convallis tristique quam. Etiam iaculis id orci at efficitur.";
+let wordCount;
+for (let i = 0; i < longText.length; i++) {
+  let separadorDePalavras = longText.split(" ");
+  wordCount = separadorDePalavras
+}
+console.log(wordCount.length + 1);
+let etCount = 0;
+for (let i = 0; i < longText.length; i++) {
+  let char1 = longText[i];
+  let char2 = longText[i + 1];
+  let char3 = longText[i + 2];
+  let char4 = longText[i +3];
+  if (char1 === " " && char2 === "e" && char3 === "t" && char4 === " "){
+    etCount++
+  }
+}
+console.log(etCount);
+
+
+//BONÚS2
+let phraseChecker = "Race car";
+let novaFrase = " "
+let fraseReversa = " "
+for (i = 0; i < phraseChecker.length; i++){
+  if(phraseChecker[i] === " " || phraseChecker[i] === ","){
+    continue;
+  }
+  novaFrase += phraseChecker[i];
+}
+let fraseMinuscula = novaFrase.toLowerCase();
+for (i = fraseMinuscula.length -1; i >= 0; i--){
+  fraseReversa += fraseMinuscula[i];
+}
+const comparaFrases = fraseMinuscula.localeCompare(fraseReversa);
+console.log (comparaFrases);
+if (comparaFrases === 0){
+  console.log("Yes! This is a Palindrome");
+} else {
+  console.log("Sorry, this is not a Palindrome");
+}
+
+//console.log(fraseReversa);
+//console.log(fraseMinuscula);
+//if (fraseMinuscula === fraseReversa) {
+//  console.log("Yes! This is a Palindrome");
+// else {
+//  console.log("Sorry, this is not a Palindrome");
+//};
+
