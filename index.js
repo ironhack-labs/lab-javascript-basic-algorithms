88463d30908c3d229ec99d51e6886fd91bf77c73
olgam <olgamartyniuk88@gmail.com>
2023-02-09 23:56:53 +0100

    3.3 completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Iteration 1: Names and Input
+// // Iteration 1: Names and Input
 const hacker1 = "Szczepan";
 console.log(`The driver's name is  ${hacker1}`);
 
@@ -18,7 +18,7 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 
-//1 
+//3.1 
 let hacker1UpperCassed = "";
 
 for (let i = 0; i < hacker1.length; i++) {
@@ -29,7 +29,7 @@ for (let i = 0; i < hacker1.length; i++) {
 
 console.log(...hacker1UpperCassed);
 
-//2
+//3.2
 let reversedString = "";
 let reversedString1 = "";
 
@@ -41,5 +41,44 @@ for (let i = hacker1.length - 1; i >= 0; i--) {
 }
 console.log(reversedString1);
 
-//3
+// 3.3 The instruction said that this part is about loops but for now I can't find a solution that would make it work with loop.
+
+// const hacker1 = "Szczepan"; > driver
+// const hacker2 = "Grzegorz"; > navigator
+
+
+    if (hacker1 > hacker2) {
+        console.log(`The driver's name goes first.`);
+
+    } else if (hacker1 < hacker2) {
+        console.log(`Yo, the navigator goes first definitely.`);
+
+    } else if (hacker1 === hacker2) {
+        console.log(`What?! You both have the same name?`);
+
+
+    
+}
+
+// I also tried to create a solution with a loop but it doesn't work properly and I don't think loop is needed here but this tasks is in the section which says it's loops.
+
+// for (let i = 0; i < hacker1.length; i++) {
+
+//     let compareStrings = hacker1.localeCompare(hacker2);
+
+//     if (compareStrings = -1) {
+//         console.log(`Yo, the navigator goes first definitely.`);
+
+//     } else if (compareStrings = 1) {
+//         console.log(`The driver's name goes first.`);
+
+//     } else if (compareStrings = 0) {
+//         console.log(`What?! You both have the same name?`);
+
+//     }
+
+// }
+
+
+
 
