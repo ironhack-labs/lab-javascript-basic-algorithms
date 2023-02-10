276518f3122dca419825ded6ee1b88703fb685ec
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-10 22:57:30 +0100

    Finished Iteration 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+const hacker1 = "Elisa";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Krisztian";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
