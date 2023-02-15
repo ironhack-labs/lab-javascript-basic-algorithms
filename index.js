162cbcf67dcb0e870e81dea1d20a346786cc182d
hjk200 <84971184+hjk200@users.noreply.github.com>
2023-02-15 21:32:53 +0100

    premier commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+var hacker1="Henri"
+console.log("The driver's name is " + hacker1)
+var hacker2="mohamed"
+console.log("The navigator's name is " + hacker2)
 
 // Iteration 2: Conditionals
 
