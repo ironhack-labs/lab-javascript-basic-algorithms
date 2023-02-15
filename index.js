2100acb0346737752d6e8df646ec9d1c53410328
Nathalie Bartel <ddlyss@gmail.com>
2023-02-15 21:47:01 +0100

    premier commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Iteration 1: Names and Input
 
-
+let hacker1 = "nathalie";
+console.log(`the driver's name is ${hacker1}`);
+let hacker2 = "singabe" ;
+console.log(`the navigator name is ${hacker2}`);
 // Iteration 2: Conditionals
 
 
