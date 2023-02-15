820255e08fcc4595eef04357c3057e1be4821174
Nicolas_M <nicolas.meot@live.fr>
2023-02-15 21:14:51 +0100

    1st iteration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Nicolas";
+console.log(`the driver's name is ${hacker1} ` )
+let hacker2= "Rayane";
+console.log("the navigator's name is " ,hacker2);
 
 // Iteration 2: Conditionals
 
