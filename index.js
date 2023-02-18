bcfa8e0c5402381127fb1bceb8cacc8d615489e6
hjk200 <84971184+hjk200@users.noreply.github.com>
2023-02-18 09:54:40 +0100

    deuxieme commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // Iteration 1: Names and Input
-var hacker1="Henri"
+/*var hacker1="Henri"
 console.log("The driver's name is " + hacker1)
 var hacker2="mohamed"
-console.log("The navigator's name is " + hacker2)
+console.log("The navigator's name is " + hacker2)*/
 
 // Iteration 2: Conditionals
+if ( hacker1.length > hacker2.length){
+    console.log("The driver has the longest name, it has characters")
+  
+} else if (hacker1.length < hacker2.length ){
+    console.log("The navigator has the longest name, it has character")
+  
+
+}else{
+    console.log("Wow, you both have equally long names, characters!")
+  
+
+}
 
 
 // Iteration 3: Loops
