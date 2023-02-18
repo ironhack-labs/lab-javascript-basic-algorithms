b29de823eda54bdfe1961ccbaa99638ea64d3b0b
rachelpytse <rachelpytse@gmail.com>
2023-02-18 10:23:59 +0100

    Finished iteration 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = prompt("What is the driver's name?")
+console.log(`The driver's name is ${hacker1}`)
+let hacker2 = prompt("What is the navigator's name?")
+console.log(`The navigator's name is ${hacker1}`)
 
 // Iteration 2: Conditionals
 
