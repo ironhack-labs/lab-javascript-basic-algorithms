67a4e48e0628a4368e26dfa09b1be30d6fc2a6c5
Cedric Aim <cedric.aim@gmail.com>
2023-02-18 10:50:31 +0100

    second iteration

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 // Iteration 1: Names and Input
-let hacker1 = prompt("What is the driver's name?")
-console.log(`The driver's name is ${hacker1}`)
-let hacker2 = prompt("What is the navigator's name?")
-console.log(`The navigator's name is ${hacker1}`)
+let hacker1 = prompt("What is the driver's name?");
+console.log(`The driver's name is ${hacker1}`);
+let hacker2 = prompt("What is the navigator's name?");
+console.log(`The navigator's name is ${hacker1}`);
 
 // Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker2.length < hacker1.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
+//loops
+//loops1
+//R A C H E L
+let i = 1;
+let result = hacker1[0];
 
-// Iteration 3: Loops
+while (i < hacker1.length) {
+  result += " " + hacker1[i].toUpperCase();
+  i++;
+}
+console.log(result);
