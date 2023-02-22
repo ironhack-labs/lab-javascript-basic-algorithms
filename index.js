7d93caf9ceaaa5210543c604950070534c2c1106
Canacepa <cassandre.lavallard@gmail.com>
2023-02-22 16:29:37 +0100

    Cassandre

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Iteration 1: Names and Input
 
-
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
 
-console.log(test)
+const lastIndex = word.length - 1;
+
+for (let i = lastIndex; i >= 0; i--) {
+  const char = word[i];
+  console.log(char);
+}
+
 //comment
 
-let test = 1
-console.log(test)
+let test = 1;
+console.log(test);
