d460be6de6c712481e8f5a3ba70b2a1d2564c0ea
Hugo Silva <dasilvapapis@protonmail.com>
2023-02-22 16:56:56 +0000

    finish iterations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Iteration 1: Names and Input
 const hacker1 = "valentin";
 console.log("The drivers name is " + hacker1);
-const hacker2 = "hugo"
+const hacker2 = "Xavier"
 console.log(`The navigators name is ${hacker2}`);
 
 // Iteration 2: Conditionals
@@ -20,3 +20,27 @@ for(char of hacker1) {
     hacker1wierd = hacker1wierd + char.toUpperCase() + " ";
 }
 console.log(hacker1wierd);
+
+let hacker2wierd = "";
+for (char of hacker2) {
+    hacker2wierd = char + hacker2wierd; 
+}
+console.log(hacker2wierd);
+
+for (i = hacker2.length - 1; i>=0; i--) {
+    console.log(hacker2[i]);
+}
+
+const compare = hacker1.localeCompare(hacker2);
+
+switch (compare) {
+    case -1:
+        console.log('The drivers name goes first');
+        break;
+    case 1:
+        console.log('Yo, the navigator goes first definitely');
+        break;    
+    case 0:
+        console.log('What? You have the same name?!');
+        break;
+}       
