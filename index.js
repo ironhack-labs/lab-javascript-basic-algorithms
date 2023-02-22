ccbfb1ad8fe5a820a15f1efcb7bac0e1cc532dfe
Ismael Freitas <ismael.freitas@sapo.pt>
2023-02-22 22:33:49 +0000

    Fix word counter for Bonus part 1

--- index.js
+++ index.js
@@ -52,22 +52,21 @@ switch (order){
 //BONUS FEATURES
 //PART 1
 
-const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt vel ipsum vitae vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed euismod mi eget leo efficitur placerat. Suspendisse accumsan nisl ac quam bibendum condimentum. In mattis dolor eu venenatis mollis. Nunc porttitor, enim nec viverra viverra, dui quam maximus lacus, a gravida nibh justo sed augue. Etiam consequat, tellus quis cursus faucibus, magna dui semper nibh, at sodales erat nisl bibendum tortor. Nullam ullamcorper aliquam leo, sit amet accumsan elit mattis eget.
+let textNoBreaks = longText.slice(0);
+textNoBreaks = textNoBreaks.replace(/(\r\n|\n|\r)/g, ' ');
 
-Suspendisse et elit venenatis, porttitor tortor nec, rhoncus velit. Ut id nisl interdum, tempus lacus eu, euismod tellus. Duis non congue quam, quis commodo ex. Praesent sollicitudin gravida tellus eget sagittis. Fusce tincidunt neque et dolor aliquet hendrerit. Aenean non mauris vitae est congue consectetur sit amet at lorem. Duis tempus justo eget viverra ultricies. Vestibulum et ligula ut nunc venenatis tincidunt. Proin nisi ex, eleifend quis aliquet pretium, pretium vel neque. Vivamus quis enim ullamcorper, rhoncus leo viverra, semper elit. Donec mauris leo, molestie quis elit ac, malesuada laoreet nulla. Fusce facilisis, turpis sit amet dignissim commodo, neque est vehicula nibh, vitae egestas neque elit condimentum dolor. Cras convallis nunc in lectus porttitor ultricies.
-
-Nullam ullamcorper condimentum mi id eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam at magna sodales lorem vestibulum tempus euismod in orci. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus nec auctor lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse ac tincidunt elit. Curabitur commodo ante a ligula auctor accumsan.`;
-
+console.log(longText);
+console.log(textNoBreaks);
 
 let counterSpace = 0;
 let counterEt = 0;
 
-let countText = longText.slice(0);
+let countText = textNoBreaks.slice(0);
 let position = 0;
 
 while (position !== -1) {
   position = countText.indexOf(' ');
-  countText = countText.slice(position + 1);
+  countText = countText.slice(position + 2);
   counterSpace++;
 }
 
@@ -82,8 +81,8 @@ while (position !== -1) {
   }
 }
 
-console.log(counterSpace);
-console.log(counterEt);
+console.log(`The text has ${counterSpace} words.`);
+console.log(`The word "et" appears ${counterEt} times.`);
 
 //PART 2
 
