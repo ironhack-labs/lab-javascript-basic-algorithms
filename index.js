8979055725fc4424be1528b4461b81a1354bdea8
valyy151 <marin.valenta1@gmail.com>
2023-02-22 16:37:15 +0100

    Initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // Iteration 1: Names and Input
+const hacker1 = 'Vali';
+const hacker2 = 'Senem';
 
+console.log('The drivers name is ', hacker1);
+console.log('The navigators name is ', hacker2);
 
 // Iteration 2: Conditionals
 
+const valiCharacters = hacker1.length;
+const senemCharacters = hacker2.length;
 
+console.log(valiCharacters, senemCharacters);
+
+if (valiCharacters > senemCharacters) {
+	console.log(
+		`The driver has the longest name, it has ${valiCharacters} characters.`
+	);
+} else {
+	console.log(
+		`It seems that the navigator has the longest name, it has ${senemCharacters} characters.`
+	);
+}
 // Iteration 3: Loops
