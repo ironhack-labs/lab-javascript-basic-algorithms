3f20f4d785dcba6af39f4049bd99517868293e96
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-02-22 16:25:35 +0100

    Initial commit + First iteration of the lab completed!

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 = 'Cristian';
+const hacker2 = 'Raúl';
 
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
