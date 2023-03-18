fbdc03e91a9eb8e6ea2de73a1710ed32d10005db
Amber Moody <aperkins0922@gmail.com>
2023-03-18 11:00:06 -0400

    Submit mandatory interations

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // Iteration 1: Names and Input
+let hacker1 = "Daniel Cornejo";
+ console.log(hacker1);
 
+let hacker2 = "Amber Moody";
+ console.log(hacker2);
 
 // Iteration 2: Conditionals
-
+ if (hacker1.length > hacker2.length) {
+   console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+ } else if (hacker1.length === hacker2.length) {
+   console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+ } else if (hacker1.length < hacker2.length) {
+   console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
 
 // Iteration 3: Loops
+let newDriver = '';
+let reverseDriver = '';
+
+for (let i = 0; i < hacker1.length; i++) {
+  newDriver += hacker1[i].toUpperCase() + " "
+}
+
+console.log(newDriver);
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reverseDriver += hacker2[i];
+}
+ 
+console.log(reverseDriver);
+
+if (hacker1 > hacker2) {
+console.log("The driver's name goes first.");
+} else if (hacker1 < hacker2) {
+console.log("Yo, the navigator goes first, definitely.");
+} else {
+console.log("What?! You both have the same name?");
+}
