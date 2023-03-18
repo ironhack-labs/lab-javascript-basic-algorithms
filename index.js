2878c4c1f24ed835916aab249276e01f41dd3004
Amber Moody <aperkins0922@gmail.com>
2023-03-18 11:25:06 -0400

    update lab

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let hacker2 = "Amber Moody";
    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
  } else if (hacker1.length < hacker2.length) {
    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
-
+ }
 // Iteration 3: Loops
 let newDriver = '';
 let reverseDriver = '';
