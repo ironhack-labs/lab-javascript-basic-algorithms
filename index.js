8e55e59fe378f05cc7416a3c0723e6fccf0551af
Patricia <marie.lou.trips@gmail.com>
2023-03-22 00:58:13 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+
+
 // Iteration 1: Names and Input
+
+let hacker1 = "Homer"
+console.log("The driver's name is Simpson")
+
+let hacker2 = "Marge"
+console.log("The navigator's name is Marge")
 
 
 // Iteration 2: Conditionals
+console.log(hacker1.length);
+console.log(hacker2.length);
+
+if(hacker1.length > hacker2.length) {
+  console.log("The driver has the longest name, it has " + hacker1.length +  " characters.");
+  } 
+else if(hacker1.length < hacker2.length) {
+  console.log("It seems that the navigator has the longest name, it has " + hacker2.length + " characters.");
+}
+else {
+  console.log("Wow, you both have equally long names, " + hacker1.length + " characters!");
+}
 
 
-// Iteration 3: Loops
+// Iteration 3.1
+
+let withSpaceMethod1 = hacker1.split("");
+console.log(withSpaceMethod1);
+
+let homerWithSpaces = withSpaceMethod1.join(" ");
+console.log(homerWithSpaces);
+
+let homerWithUpperCase = homerWithSpaces.toUpperCase();
+console.log(homerWithUpperCase);
+
+// Iteration 3.2
+
+let splitName = hacker2.split("");
+console.log(splitName);
+//Turn string into an array to use reverse method
+splitName = ['M', 'a', 'r', 'g', 'e' ];
+let reverseName = splitName.reverse();
+console.log(reverseName)
