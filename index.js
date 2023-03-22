61cdab301ba7fedf9fc909faa9d8589b45939716
Shawn Wolter <woltersh@outlook.de>
2023-03-22 22:36:57 +0100

    finished lab

--- index.js
+++ index.js
@@ -20,14 +20,26 @@ if(hacker1.length>hacker2.length){
 // Iteration 3: Loops
 let output = ""
 
-for (let i = 0; i <= hacker1.length; i++) {
-    output = hacker1.charAt(i).toLocaleUpperCase() + " ";
+for (let i = 0; i < hacker1.length; i++) {
+    output += hacker1.charAt(i).toUpperCase() + " ";
 }
 console.log(output.slice(0, -1));
 
+// Print navigator´s name in reverse order
 let reverseOutput = "";
 
 for (i = hacker2.length -1; i >= 0; i--) {
     reverseOutput += hacker2.charAt(i);
 }
 console.log(reverseOutput);
+
+// Print depending on lexicographic order
+if (hacker1.slice(0, hacker1.length) < hacker2.slice(0, hacker2.length)) {
+  console.log("The driver's name goes first.");
+  
+} else if (hacker1.slice(0, hacker1.length) > hacker2.slice(0, hacker2.length)) {
+  console.log("Yo, the navigator goes first, definitely.");
+  
+} else if (hacker2.slice(0, hacker2.length) === hacker1.slice(0, hacker1.length)) {
+  console.log("What?! You both have the same name?");
+}
