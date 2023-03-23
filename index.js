43a9b2260020a975f2fb4d47faef1baf784896ce
Luis Ramirez <rhluisenrique@gmail.com>
2023-03-22 23:50:50 -0400

    done

--- index.js
+++ index.js
@@ -51,3 +51,25 @@ else if(getValue === -1){
     console.log("The driver's name goes first.")
 }
 
+//#### Bonus 1:
+
+let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id mauris facilisis, eleifend nulla vitae, iaculis elit. Praesent gravida neque vitae dapibus accumsan. Duis ultrices auctor nulla ac laoreet. Ut condimentum bibendum dui, sed maximus nibh tristique nec. Duis ac lobortis diam, at dapibus odio. Pellentesque id turpis eget est interdum sodales. Sed quis nisl tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec sapien et massa convallis vulputate quis id leo. Nulla vel aliquet nulla, vitae sodales nulla.
+
+Morbi pharetra egestas egestas. Quisque ac dapibus felis, ac ullamcorper purus. Pellentesque facilisis lectus nec cursus porttitor. Suspendisse bibendum ipsum et mauris mollis feugiat. Morbi blandit quis lacus tempor placerat. Nam sollicitudin et ex et eleifend. Curabitur feugiat ullamcorper eros a tempor. Morbi luctus orci vel orci commodo imperdiet. Aenean rhoncus id tortor nec tincidunt. Suspendisse suscipit justo eu lacus egestas lacinia. Fusce sit amet tempus quam, sed feugiat orci. In posuere neque a neque auctor gravida. Vivamus scelerisque dui tellus, eu feugiat lectus congue et. Curabitur tellus orci, tempor non ornare eget, mattis ac est. Morbi egestas quis tellus et ultricies.
+
+Sed finibus tempor mattis. Vivamus scelerisque ac arcu quis gravida. Praesent dictum varius ornare. Praesent eu commodo purus. Vivamus consequat urna dui, in ultrices tellus porttitor vel. Duis quis eros vehicula, mollis sem vel, elementum nulla. In sodales convallis imperdiet. Curabitur id semper erat.`;
+
+console.log(longText.split(' ').length)
+
+let etCount = 0;
+
+for(let i = 0; i <= longText.length; i++){
+  
+  const chars = longText[i] + longText[i + 1];
+
+  if(chars === "et"){
+    etCount++
+  }
+  
+}
+console.log(etCount)
