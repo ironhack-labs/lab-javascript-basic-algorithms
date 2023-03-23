ce0e4600aab066a90fabd8fc41d5a0c111a38d62
Luke Chen <heylukechen@gmail.com>
2023-03-23 21:25:56 +0100

    second iteration

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ console.log(splitName.toUpperCase());
 
 //3.2
 let nameNavigatorNew = "";
-for (let i = hacker2.length-1; i >= 0; i--){
+for (let i = hacker2.length - 1; i >= 0; i--){
     nameNavigatorNew += hacker2[i];
 }
 console.log(nameNavigatorNew);
 
-//3.3
+//3.3 
 if (hacker1[0] < hacker2 [0]) {
     console.log("The driver's name goes first");
 } else if (hacker1[0] > hacker2 [0]){
@@ -47,15 +47,29 @@ if (hacker1[0] < hacker2 [0]) {
 }
 
 //BONUS 1
+//adding and creating long strings, the format is really long so we need break in between paragraphs
+let longText = `
+Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit. Integer tristique felis ac dapibus maximus. 
+Proin condimentum magna vitae libero blandit ornare. 
+Suspendisse cursus massa eu purus congue sollicitudin. 
+Pellentesque ac luctus ipsum, a dictum nulla. Nam mattis luctus libero vitae dapibus. 
+Morbi sit amet lacus eget lectus hendrerit volutpat et ac velit. 
+Curabitur cursus lacus vitae elit varius convallis. Sed nulla dolor, tincidunt eu nisi vel, vehicula varius odio. 
+Pellentesque diam leo, viverra at rutrum varius, vulputate vitae lacus. Nullam posuere odio in ex dictum convallis. 
+Maecenas vehicula dignissim mi. Aenean est purus, auctor nec sodales id, bibendum non erat. Vestibulum in mi turpis. 
+In rhoncus accumsan magna. Aenean non orci eget tellus ultrices luctus at nec dolor. Aliquam vitae felis a magna aliquet faucibus quis id odio. Nunc ac viverra eros.
+Fusce mollis massa vel placerat tempor.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis quis orci in leo hendrerit lacinia. 
+Curabitur dapibus tempus augue eu convallis. Ut sagittis est nec tempus eleifend. Quisque eget mollis libero. Praesent eget diam a odio porttitor tincidunt id ut neque. 
+Sed at justo vel est sodales ornare.`
+;
 
-let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique felis ac dapibus maximus. Proin condimentum magna vitae libero blandit ornare. Suspendisse cursus massa eu purus congue sollicitudin. Pellentesque ac luctus ipsum, a dictum nulla. Nam mattis luctus libero vitae dapibus. Morbi sit amet lacus eget lectus hendrerit volutpat et ac velit. Curabitur cursus lacus vitae elit varius convallis. Sed nulla dolor, tincidunt eu nisi vel, vehicula varius odio. Pellentesque diam leo, viverra at rutrum varius, vulputate vitae lacus. Nullam posuere odio in ex dictum convallis. Maecenas vehicula dignissim mi. Aenean est purus, auctor nec sodales id, bibendum non erat. Vestibulum in mi turpis. In rhoncus accumsan magna. Aenean non orci eget tellus ultrices luctus at nec dolor. Aliquam vitae felis a magna aliquet faucibus quis id odio. Nunc ac viverra eros.
-Fusce mollis massa vel placerat tempor.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis quis orci in leo hendrerit lacinia. Curabitur dapibus tempus augue eu convallis. Ut sagittis est nec tempus eleifend. Quisque eget mollis libero. Praesent eget diam a odio porttitor tincidunt id ut neque. Sed at justo vel est sodales ornare.
-`;
-
+//remove the white space and break in between paragraphs
 let words = longText.split(/\s+/);
 let wordsCount = words.length;
 console.log(wordsCount);
 
+//count the times ET showing up
 let countET = "";
 
 for (let i = 0; i <= longText.length; i++){
@@ -63,19 +77,34 @@ for (let i = 0; i <= longText.length; i++){
         countET ++;
     }
 }
-
 console.log(countET);
 
-
 ///BONUS 2
-let phraseToCheck = "Racer";
-let lowercasePhrase = phraseToCheck.toLowerCase;
-let isChecked = true;
-for (let i = 0; i < lowercasePhrase.length / 2; i++) {
-    let j = lowercasePhrase.length - i - 1;
-    if(lowercasePhrase[i] !== lowercasePhrase[j]){
-        isChecked = false;
-        break;
-    }
+
+///Method 2 - Looping -> find white space -> remove -> join -> new strings -> hcekc 
+
+
+////////////////////METHOD 1 - Reverse strings, and do checking.
+let phraseToCheck = "Rac e c ar";
+//set isChecked to false which will be later determine if it's pilandrome.
+let isChecked = false;
+
+//clean up the original strings, removing the whiteSpace. making lowercase
+let phraseNoWhiteSpace = phraseToCheck.replaceAll(" ","");
+let lowercasePhrase = phraseNoWhiteSpace.toLowerCase();
+
+// reverse the strings
+let reversePhrase = "";
+for (let i = lowercasePhrase.length - 1 ; i >= 0; i--){
+    reversePhrase += lowercasePhrase[i];
 }
+
+//checking if the reversed string is the same as the string -> if yes, set isChecked equals to true
+if (lowercasePhrase === reversePhrase){
+    isChecked = true;
+} else {
+    isChecked = false;
+}
+
+//print out the results from the check
 console.log(isChecked);
