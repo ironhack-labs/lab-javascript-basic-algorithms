ecfa1f5a6bb2367bff8564d3ef754ea6fd7d8cfe
Jelle Wallenburg <jellewallenburg@gmail.com>
2023-03-25 11:16:21 +0100

    added bonus exercises

--- index.js
+++ index.js
@@ -44,4 +44,33 @@ if(hacker1.localeCompare(hacker2)==1){
 }
 
 //Bonus 1
+longText= `Lorem et ipsum dolor sit  et et amet, consectetur adipiscing elit. Mauris quis est sapien. Vestibulum vestibulum euismod augue, sed scelerisque est semper non. Morbi ac urna pellentesque, blandit odio id, fringilla lectus. Nunc lobortis, nunc ut tincidunt elementum, nulla metus fermentum quam, id consequat arcu dui ac augue. Nullam quam ligula, pretium vel nibh quis, tincidunt tristique mi. Nam varius commodo ante, id blandit libero. Morbi in blandit urna, blandit posuere arcu.
+
+Ut facilisis imperdiet metus fermentum blandit. Cras vel felis vel lectus placerat suscipit. Vivamus nec tincidunt nisl. Fusce rhoncus aliquam ex, at imperdiet mauris efficitur a. Phasellus sagittis tellus eu nibh pellentesque lacinia. Duis faucibus, velit et aliquam hendrerit, sem felis maximus urna, ut facilisis eros urna et leo. Curabitur volutpat pharetra erat, et gravida diam tincidunt sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consectetur, odio sit amet commodo tristique, arcu purus hendrerit mauris, at imperdiet dolor dui non enim. Ut sollicitudin vehicula orci et vestibulum. Quisque auctor condimentum magna et blandit. Nullam leo urna, congue vel elementum a, porta eget lacus. Ut eget ligula enim. Fusce ornare imperdiet risus, et dictum sapien consequat blandit. Curabitur vitae viverra neque. Aenean nec ornare lacus.
+
+Duis pretium erat et libero sagittis, et dictum risus congue. Nunc maximus magna quam, ut tempor quam fringilla vitae. Praesent aliquam tellus odio. Nam tincidunt, mi ac euismod feugiat, ipsum nisl cursus justo, eu tempor metus nibh id ex. Phasellus lobortis turpis sit amet justo ornare mattis. Donec ut ipsum auctor, imperdiet lorem ut, ullamcorper odio. Maecenas vehicula risus eu lorem molestie sollicitudin. Suspendisse consequat felis est, in tristique sapien pharetra a. Integer vitae sodales orci, in venenatis ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed feugiat purus. Etiam id pretium dui, eget suscipit erat. Nunc vitae massa et quam hendrerit aliquet sed nec ex. In efficitur quam vitae turpis consectetur commodo."
+`
+longTextArray= longText.split(" ");
+let count=0;
+for(let i=0; i< longTextArray.length; i++){
+  if(longTextArray[i].search(/et/)==0){
+    count++;
+  };
+};
+
+console.log(`Aantal "et" ${count}`);
+
 //Bonus 2
+let phraseToCheck = "racecar";
+let isPalindrome;
+
+for(let i=0; i < phraseToCheck.length/2-1; i++){
+  if(phraseToCheck[i]=== phraseToCheck[phraseToCheck.length-1-i]){
+  isPalindrome= true;
+}  else{
+  isPalindrome= false;
+  break
+  }
+}
+
+console.log(isPalindrome)
