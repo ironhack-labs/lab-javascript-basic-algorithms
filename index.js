402e8238586dfdcd31cd5e4a97fb0bf2ec21da6c
MijyPark <parkmijy2021@gmail.com>
2023-03-29 14:18:55 +0200

    Day3

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-// Iteration 1: Names and Input
+console.log("I'm ready!");
 
+let hacker1 = 'Mijy'
+let hacker2 = 'Julia'
 
-// Iteration 2: Conditionals
+console.log(`The driver's name is ${hacker1}.`);
+console.log(`The navigator's name is ${hacker2}.`);
 
+if(hacker1.length > hacker2.length) {
+  console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if(hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length}characters.`)
+} else {
+    console.log('Wow, you both have equally long names, XX characters!.')
+  };
 
-// Iteration 3: Loops
+for (let i = 0; i<hacker1.length; i++) {
+   console.log(`${hacker1[i].toUpperCase([i]).split("[i]")}`)
+   
+ }   
+     
