3ef2cf0031859422fcbac8f7ca444573aae56adf
Cristian <Crissax2020@gmail.com>
2023-04-15 16:18:01 -0600

    Iteracion 2

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ let hacker2 = "Cristian"; //NAVIGATOR
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+let hacker2="Cristian";
+
+let hacker1="Aldo";
+
+if(hacker1.length>hacker2.length)
+{
+console.log("The driver has the longest name, it has "+hacker1.length+" characters.")
+} else if(hacker1.length<hacker2.length)
+{
+  console.log("It seems that the navigator has the longest name, it has "+hacker2.length+" characters.")
+} else
+{
+  console.log("Wow, you both have equally long names, "+hacker1.length+" characters!")
+}
 
 
 // Iteration 3: Loops
