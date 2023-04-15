4289c904341b685dfd4f5a4cfae9e9ae5bfc98cc
churrundo <churrundo@gmail.com>
2023-04-14 21:33:12 -0600

    Done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("I'm ready!")
 // Iteration 1: Names and Input
+let hacker1 = "Juan Pablo"
+console.log(`The driver's name is ${hacker1}`)
 
+let hacker2 = "Luis"
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
