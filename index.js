e122a63e8252f3c9140ccab7c5af53b6659979c8
Aldo Gonzalez <aldogonjuarez@.com>
2023-04-15 16:08:49 -0600

    1st iteration done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = "Aldo"; //DRIVER
+console.log(`The driver´s name is: ${hacker1}`);
 
+let hacker2 = "Cristian"; //NAVIGATOR
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
