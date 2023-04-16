60bebf890bc6c18e5fa14043e64aba941074bd02
KRcamara <113925794+KRcamara@users.noreply.github.com>
2023-04-15 18:01:27 -0600

    interaccion 1 y 2

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 // Iteration 1: Names and Input
 //
+const jaker1 = "Walter"
+const jaker2 = "Karla"
+
+console.log(`The driver´s name is ${jaker1}.`);
+console.log(`The navigator's name is ${jaker2}`);
+
 // Iteration 2: Conditionals
 
-
+if (jaker1.length > jaker2.length){
+   console.log(`The driver has the longest name, it has ${jaker1.length} characters`)
+}else if(jaker1.length < jaker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${jaker2.length} characters.`)
+}else{
+    console.log(`Wow, you both have equally long names, ${jaker1.length} characters!`)
+}
 // Iteration 3: Loops
 
 //
