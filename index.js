5c2f1a41b177fa4cfaa48ef1a2c5d0fa785561d2
Chiara888 <carolin@family-klose.de>
2023-05-10 17:26:06 +0200

    1st one done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
 
+const hacker1 = "Susi Schneller"
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Florian Finder"
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
