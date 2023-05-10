4decc484056bbebd46d4c3360f464cca5f780401
Ola Bonati <olabonati@gmail.com>
2023-05-10 20:20:16 +0200

    iteration 3

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
 /* Iteration1 
 1.1 Create a variable hacker1 with the driver's name.
 1.2 Print "The driver's name is XXXX".
 1.3 Create a variable hacker2 with the navigator's name.
 1.4 Print "The navigator's name is YYYY".*/
 
-let hacker1 = "Mandy";
+let hacker1 = 'Mandy';
 console.log(`The driver's name is ${hacker1}`);
 
-let hacker2 = "Ola";
+let hacker2 = 'Ola';
 console.log(`The navigator's name is ${hacker2}`);
 
 /*Iteration 2: Conditionals
@@ -17,13 +24,16 @@ console.log(`The navigator's name is ${hacker2}`);
 - Wow, you both have equally long names, XX characters!.*/
 
 if (hacker1.length > hacker2.length) {
-  console.log(`${hacker1} has the longest name, it has ${hacker1.length} characters`);
-} else if (hacker2.length > hacker1.length){
-  console.log(`It seems that ${hacker2} has the longest name, it has ${hacker2.length} characters`);
+  console.log(
+    `${hacker1} has the longest name, it has ${hacker1.length} characters`
+  );
+} else if (hacker2.length > hacker1.length) {
+  console.log(
+    `It seems that ${hacker2} has the longest name, it has ${hacker2.length} characters`
+  );
 } else {
-  console.log( `Wow, you both have equally long names, ${hacker1} characters!`)
+  console.log(`Wow, you both have equally long names, ${hacker1} characters!`);
 }
-
 
 /*Iteration 3: Loops
 3.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
@@ -36,10 +46,8 @@ The driver's name goes first.
 Yo, the navigator goes first, definitely.
 What?! You both have the same name?*/
 
-
-
 /*Version with an Array*/
-console.log("Hello");
+console.log('Hello');
 
 //let hacker1 = "Mandy";
 
@@ -51,7 +59,7 @@ newName.push(hacker1[i].toUpperCase()+' ');
 
 console.log(newName);*/
 
-let newName = ''
+/*let newName = ''
 
 for (let i = 0; i < hacker1.length; i++) {
   newName += (hacker1[i].toUpperCase()+' ');
@@ -60,3 +68,25 @@ for (let i = 0; i < hacker1.length; i++) {
 console.log(newName);
 
 console.log("hello")
+*/
+/*
+let newName = '';
+for (let i = hacker1.length - 1; i >= 0; i--) {
+  newName += hacker1[i];
+}
+console.log(newName);
+*/
+
+const result= hacker1.localeCompare(hacker2);
+if (result=== -1){
+  console.log ("Mandy's name goes first")
+
+}
+else if ( result=== 1){
+console.log ("Yo, the Ola's name goes first, definitely.")
+
+}
+else {
+console.log("What?! You both have the same name?")
+}
+
