6bfca87c6087f054a74867e453a3e5790fc639a2
Carolin Goerner <carolingoerner@yahoo.de>
2023-05-10 17:10:54 +0200

    1st iteration done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
- 
+
+const hacker1 = "carolin"
+
+console.log("The driver's name is " + hacker1);
+
+const hacker2 = "Linh"
+console.log("The navigator's name is " + hacker2);
+
+
 
 // Iteration 2: Conditionals
 
