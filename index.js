2683421a5b2e152ba109233d38f6598802ac031c
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-05-10 22:46:47 +0200

    JS-LAB Day 3

--- index.js
+++ index.js
@@ -18,6 +18,9 @@
     }
   }
 
+  
+// Iteration 3: Loops
+
   let capitalSpaceHacker1 = "";
 
   for (let i = 0; i < hacker1.length; i++) {
@@ -34,8 +37,6 @@
 
   console.log(reverseName);
 
-// Iteration 3: Loops
-
   const result = hacker1.localeCompare(hacker2);
   console.log(result);
 
@@ -44,3 +45,20 @@
   } else if (hacker2.localeCompare(hacker1) < 0) {
     console.log(`Yo, the ${hacker2} goes first, definitely.`)
   } else {console.log("What?! You both have the same name?")}
+
+  const paragraph1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  const paragraph2 = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  const paragraph3 = "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
+
+  const longText = `${paragraph1}\n\n ${paragraph2}\n\n${paragraph3}`;
+  console.log(longText);
+
+
+
+  const countString = longText.split(" ").length;
+  console.log(`The number of the words: ${countString}`);
+
+  const countEt = longText.split(" et ").length - 1;
+  console.log(`Number of Latin word "et" is: ${countEt}`);
+
+  
