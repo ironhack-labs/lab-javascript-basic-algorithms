cefd0d5f5c77dce3e0099a02278d13a324aa30a6
Philippe Miossec <pmiossec@gmail.com>
2023-05-10 18:25:48 +0200

    Bonus 1 (part2): count 'et'

--- index.js
+++ index.js
@@ -56,12 +56,12 @@ if (comparisonResult < 0) {
 // Bonus Time!
 // Bonus 1:
 
-const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae dui vitae felis ullamcorper imperdiet. Donec egestas ipsum at enim luctus rutrum. Nam sit amet fringilla mauris, a sodales enim. Pellentesque rhoncus mollis leo, eu iaculis turpis molestie quis. Quisque convallis accumsan nulla eget efficitur. Ut ornare libero tellus, at ornare purus finibus consequat. Curabitur eleifend vestibulum ante finibus lobortis. Quisque vitae sodales felis, et pellentesque tortor. Aenean vel condimentum magna. Nullam euismod eu nunc ornare pulvinar. Aenean cursus rutrum sem scelerisque facilisis. Nullam accumsan tellus sem, quis aliquam lacus sagittis tristique. Ut elementum vulputate massa, eu luctus tellus commodo vitae. Ut sodales, lorem non placerat feugiat, tortor magna vehicula felis, vel pharetra leo mauris eget felis. Donec quis purus vel est venenatis bibendum eu at nisl.
+const longText =
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae dui vitae felis ullamcorper imperdiet. Donec egestas ipsum at enim luctus rutrum. Nam sit amet fringilla mauris, a sodales enim. Pellentesque rhoncus mollis leo, eu iaculis turpis molestie quis. Quisque convallis accumsan nulla eget efficitur. Ut ornare libero tellus, at ornare purus finibus consequat. Curabitur eleifend vestibulum ante finibus lobortis. Quisque vitae sodales felis, et pellentesque tortor. Aenean vel condimentum magna. Nullam euismod eu nunc ornare pulvinar. Aenean cursus rutrum sem scelerisque facilisis. Nullam accumsan tellus sem, quis aliquam lacus sagittis tristique. Ut elementum vulputate massa, eu luctus tellus commodo vitae. Ut sodales, lorem non placerat feugiat, tortor magna vehicula felis, vel pharetra leo mauris eget felis. Donec quis purus vel est venenatis bibendum eu at nisl.
 
 Vivamus vel tristique augue. Aenean pulvinar dolor et cursus ullamcorper. Etiam sit amet risus tortor. Nulla dapibus, leo eget congue malesuada, augue tellus sagittis risus, at semper enim eros ac erat. Etiam ultricies quam nibh, quis consectetur sem lacinia vel. Fusce vitae convallis elit, non fermentum nulla. Donec sagittis, lorem eget aliquam condimentum, turpis lorem feugiat sem, eu fringilla lacus odio eu ipsum. Fusce sollicitudin arcu id lacus rhoncus laoreet. Vestibulum imperdiet dolor tortor, at dignissim tortor viverra at. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vestibulum nisi sit amet erat feugiat, non placerat mi bibendum. Praesent lacinia, nulla nec malesuada placerat, massa lectus porta metus, ac facilisis enim turpis non leo. Cras vitae urna vitae libero tristique varius sit amet iaculis tellus. Duis volutpat rutrum nibh in sagittis. Nam ac dui lacus.
 
-Mauris dignissim non ex quis vestibulum. Sed efficitur risus ultricies, tincidunt dui non, bibendum ipsum. Integer ornare odio orci, sed feugiat eros sodales vel. Morbi condimentum arcu erat, at pellentesque nibh commodo quis. Nullam et leo eget purus auctor eleifend. Fusce semper at mauris ut suscipit. Aenean lacinia laoreet enim sit amet tempus. Nunc pulvinar sit amet orci quis consectetur. Maecenas fermentum tristique condimentum. Donec neque lorem, elementum vel nibh ac, pulvinar euismod erat. Etiam pellentesque quam arcu, vitae dictum nisl pellentesque suscipit. Integer sodales, justo non malesuada lacinia, lectus mauris faucibus tortor, ac vestibulum tortor quam at ipsum. Phasellus vulputate tempor mollis. Nullam viverra in turpis id ultricies.`
-.toLowerCase();
+Mauris dignissim non ex quis vestibulum. Sed efficitur risus ultricies, tincidunt dui non, bibendum ipsum. Integer ornare odio orci, sed feugiat eros sodales vel. Morbi condimentum arcu erat, at pellentesque nibh commodo quis. Nullam et leo eget purus auctor eleifend. Fusce semper at mauris ut suscipit. Aenean lacinia laoreet enim sit amet tempus. Nunc pulvinar sit amet orci quis consectetur. Maecenas fermentum tristique condimentum. Donec neque lorem, elementum vel nibh ac, pulvinar euismod erat. Etiam pellentesque quam arcu, vitae dictum nisl pellentesque suscipit. Integer sodales, justo non malesuada lacinia, lectus mauris faucibus tortor, ac vestibulum tortor quam at ipsum. Phasellus vulputate tempor mollis. Nullam viverra in turpis id ultricies.`.toLowerCase();
 
 function isLetter(char) {
   return char >= 'a' && char <= 'z';
@@ -83,3 +83,19 @@ for (let i = 0; i < longText.length; i++) {
 }
 
 console.log('How many words found?', wordCounter);
+
+//     Make your program count the number of times the Latin word et appears.
+
+let etCount = 0;
+for (let i = 0; i < longText.length; i++) {
+  if (
+    !isLetter(longText[i - 1]) &&
+    longText[i] === 'e' &&
+    longText[i + 1] === 't' &&
+    !isLetter(longText[i + 2])
+  ) {
+    etCount++;
+  }
+}
+
+console.log("number of 'et':", etCount);
