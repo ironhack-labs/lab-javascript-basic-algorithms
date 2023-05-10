d723d85ef05d0aeeea41525420293d66d04b3635
EmilyStevenson <e.stevenson6@hotmail.co.uk>
2023-05-10 17:14:15 +0100

    add iteration 3 together and finished

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// Iteration 1: Names and Input
-
-let hacker1 = "Cédric";
+let hacker1 = "Emily";
 console.log("The driver's name is " + hacker1);
-let hacker2 = "Emily";
-console.log("The navigotor's name is " + hacker2);
+let hacker2 = "Cédric";
+console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
 
@@ -15,3 +13,35 @@ else
   console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
 
 // Iteration 3: Loops
+let driversName = ""
+let space = ""
+for (let i = 0; i < hacker2.length; i++) {
+  if (i !== (hacker2.length - 1)) {
+    space = " "
+  }
+  else {
+    space = ""
+  }
+  driversName = driversName + hacker2[i] + space
+}
+driversName = driversName.toUpperCase()
+console.log(driversName)
+
+let navigatorsName = ""
+for (let j = hacker1.length - 1; j >= 0; j--) {
+  navigatorsName = navigatorsName + hacker1[j]
+}
+navigatorsName = navigatorsName.toLowerCase()
+console.log(navigatorsName)
+
+
+let results = hacker2.localeCompare(hacker1)
+if (results === -1) {
+  console.log("Yo, the navigator goes first, definitely.")
+}
+else if (results === 1) {
+  console.log("The driver's name goes first.")
+}
+else {
+  console.log("What?! You both have the same name?")
+}
