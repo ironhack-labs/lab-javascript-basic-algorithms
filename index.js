553d419b369fa8cc4d1aa4cf31f879d62c05db6e
Luis Ramirez <rhluisenrique@gmail.com>
2023-05-28 18:49:42 -0400

    Bonus 1 Done

--- index.js
+++ index.js
@@ -14,13 +14,19 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
-const nuevoHacker1 = hacker1.split("").join(" ").toUpperCase();
-console.log(nuevoHacker1)
+let letterUp = hacker1.toUpperCase();
+let up = '';
+for(let i=0; i<letterUp.length; i++){
+   up += letterUp[i] + ' ';     
+}
+console.log(up)
+
 let reverso = "";
 for(let i = hacker1.length -1; i >= 0; i--) {
   reverso += hacker1[i];
 }
 console.log(reverso);
+
 const newHacker1 = hacker1.split("").sort().join("");
 const newHacker2 = hacker2.split("").sort().join("");
 if (newHacker1 > newHacker2) {
@@ -30,3 +36,41 @@ if (newHacker1 > newHacker2) {
 } else {
     console.log('What?! You both have the same name?')
 }
+
+// Iteration Bonus 1: Lorem ipsum paragraphs
+
+const longText = `
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend cursus mauris. Cras in elit vitae metus vestibulum viverra efficitur id
+ orci. Aliquam blandit interdum magna, vitae ornare libero scelerisque a. Vestibulum non aliquam quam, vehicula pulvinar risus. Aenean
+ ornare, risus eget pharetra sollicitudin, dui leo luctus elit, nec ultricies nisi lectus at enim. Vestibulum lobortis quam eget est cursus
+ bibendum. Proin eget mattis nisl. Mauris in lorem aliquam, eleifend ipsum sed, tempus arcu. Nunc in mauris est. Suspendisse dui lacus,
+ rhoncus sit amet rutrum dapibus, varius non massa. Phasellus suscipit mollis dui vel aliquam. Sed cursus sagittis arcu, sed gravida massa
+ tincidunt vitae. Nam et imperdiet massa. Vestibulum laoreet sollicitudin fermentum.
+
+ Duis tempus velit purus, eget interdum ex consectetur ac. Aliquam ullamcorper lectus tortor, nec rhoncus eros consectetur eu. Cras sed
+ metus et est consectetur gravida sit amet ac mauris. Donec enim neque, fermentum ut placerat vitae, posuere quis nisl. Suspendisse in ex
+ vitae odio scelerisque lobortis quis sit amet nisl. Suspendisse vel porttitor est. Mauris at nulla ac lectus imperdiet pretium cursus eu
+ massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin aliquet ex eu tristique
+ consectetur. Pellentesque quis lectus pretium, ornare enim eget, malesuada nulla. Integer rhoncus ante sit amet elit interdum, at viverra
+ ante vulputate. Mauris nec pellentesque turpis. Nam orci metus, efficitur a nisl et, fringilla imperdiet diam. Ut nec elit tempor, suscipit ante
+ id, semper orci. Nunc aliquam lorem non laoreet placerat.
+
+ Sed convallis, erat nec facilisis sodales, lectus purus dapibus magna, id auctor ligula libero nec augue. Nam pharetra nibh magna, a
+ accumsan odio lobortis vitae. Pellentesque finibus nulla non odio blandit efficitur. Vivamus sit amet eros ipsum. Phasellus sed euismod
+ orci. In hac habitasse platea dictumst. Donec molestie, massa id blandit ornare, metus magna placerat sapien, nec fringilla odio libero sed
+ sem.`
+
+ const splitLongText = longText.split(/\s+/);
+ let numeroDePalabras = splitLongText.length
+ console.log(numeroDePalabras)
+
+ let count = 0;
+ for(let i = 0; i < splitLongText.length; i++){
+
+    if(splitLongText[i] === 'et'){
+        count++
+    } 
+
+ }
+ console.log(count)
+
