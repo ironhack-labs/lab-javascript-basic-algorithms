67751689c34350e1e0368a0654c184493227d442
Neethu Kunjappan <neethu.kunjappan@tu_clausthal.de>
2023-05-31 21:17:17 +0530

    done

--- index.js
+++ index.js
@@ -1,75 +1,49 @@
-var hacker1="Thomas";
-var hacker2="Ben";
-let i=0;
-let longText=' ';
-var text='';
-var navre='';
-var rev = '';
-let result = hacker1.toUpperCase()
+console.log("I'm ready!");
+
+// Iteration 1&2
+
+var hacker1 = "Thomas";
+var hacker2 = "Ben";
+let i = 0;
+let longText = " ";
+var text = "";
+var navre = "";
+var rev = "";
+let result = hacker1.toUpperCase();
 let len1 = hacker1.length;
 let len2 = hacker2.length;
 let lenres = result.length;
-if(len1>len2)
-{
-console.log("The driver has longest name,it has " + len1 + "characters");
-}
-else if(len2>len1)
-{
-  console.log("The navigator has longest name,it has " +len2 + "characters");
-}
-else
-{
-console.log("wow,you both have equal long name with"+len1 + "characters");
+if (len1 > len2) {
+  console.log("The driver has longest name,it has " + len1 + "characters");
+} else if (len2 > len1) {
+  console.log("The navigator has longest name,it has " + len2 + "characters");
+} else {
+  console.log("wow,you both have equal long name with" + len1 + "characters");
 }
 
+// Iteration 3
 
-console.log(result);
-for(i=0;i<lenres;i++)
-  {
-     text+=result.charAt(i)+' ';
-    
-    
-  }
-console.log(text);
+let driverName = prompt("Please enter name");
+const capitalizedDriverName = driverName.toUpperCase();
+const separatedDriverName = capitalizedDriverName.split("").join(" ");
 
-function reverse(hacker2)
-  {
+console.log(separatedDriverName);
 
-for (var i = hacker2.length - 1; i >= 0; i--)
-  rev += hacker2[i];
-    console.log(rev);
+*****************************************************
 
-}
-reverse( hacker2);
-let res = hacker1.localeCompare(hacker2)
-if (res == -1) {
-  console.log ("The driver's name goes first.")
-}
+let NavigatorName = prompt("Please enter name");
 
-else if (res == 1) {
-  console.log ("Yo, the navigator goes first, definitely.")
-}
+const reversedNavigatorName = NavigatorName.split('').reverse().join('');
 
-else {
-  console.log ("What?! You both have the same name?")
-}
+console.log(reversedNavigatorName);
 
-console.log (res);
-function getCount(str) {
-return str.split(' ') .length
-}
-
-getCount(longText);
-console.log (getCount(longText)); 
-
-let longTextWords = longText.split(" ");
-
-let countWordEt = 0
-
-for (let i = 0; i <= longTextWords.length; i++) {
-  if (longTextWords[i] == "et") {
-    countWordEt++
-  }
-}
-
-console.log (countWordEt);
+***********************************************************
+let driverName = prompt("Please enter drivername");
+let navigatorName = prompt("Please enter navigatorname");
+ if (driverName < navigatorName) {
+  console.log("The driver's name goes first.");
+ } else if (driverName > navigatorName) {
+   console.log("Yo, the navigator goes first, definitely.");
+ } else {
+  console.log("What?! You both have the same name?");
+ }
