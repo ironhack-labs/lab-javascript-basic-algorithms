88142e1a971681b468b214deb0e161853cf084b9
UlviAzizli <133907952+UlviAzizli@users.noreply.github.com>
2023-06-14 17:40:32 +0200

    all iteration is done

--- index.js
+++ index.js
@@ -15,3 +15,33 @@ else {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 }
 // Iteration 3: Loops
+
+
+
+let changeUpperHacker1 = "";
+for (let i = 0; i < hacker1.length; i++) {
+    changeUpperHacker1 += `${hacker1.charAt(i)} `;
+}
+console.log(changeUpperHacker1.toUpperCase());
+
+
+
+let reverseHacker2 = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+reverseHacker2 += hacker2[i];
+}
+console.log(reverseHacker2);
+
+
+
+const hacker1Name = prompt("Enter the driver's name:");
+const hacker2Name = prompt("Enter the navigator's name:");
+
+if (hacker1 < hacker2) {
+    console.log(`The driver's name goes first.`);
+  } else if (hacker1 > hacker2) {
+    console.log(`Yo, the navigator goes first, definitely.`);
+  } else {
+    console.log(`What?! You both have the same name?`);
+  }
+
