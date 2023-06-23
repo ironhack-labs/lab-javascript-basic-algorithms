c1939be4927788d560b7a4e73a3bd3424ca01181
Anna Ivanivna <115419490+Ivanna121212@users.noreply.github.com>
2023-06-23 13:31:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-// Iteration 1: Names and Input
+let hacker1 = "Anna";
+console.log(`The driver's name is ${hacker1}.`);
+let hacker2 = "Lena";
+console.log(`The navigator's name is ${hacker2}.`);
 
 
-// Iteration 2: Conditionals
+if(hacker1.length > hacker2.length){
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+}
+else if(hacker2.length > hacker1.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+}
+else{
+  console.log(`Wow, you both have equally long names, ${hacker2.length} characters ?!`)
+}
 
 
-// Iteration 3: Loops
+let letters = "";
+for (let i=0; i<hacker1.length; i++){
+  letters += hacker1[i].toUpperCase() + " ";
+}
+console.log(letters);
+
+let reverseLetters = "";
+for (let i = hacker2.length-1; i >= 0; i--){
+  reverseLetters += hacker2[i];
+}
+console.log(reverseLetters);
