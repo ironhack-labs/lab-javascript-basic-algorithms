d73a3b8e32de513efef50f73fa2d6de9da2ec400
Ian IJmkers <ianymkers@gmail.com>
2023-08-24 23:26:55 +0200

    To be honest i had to use google for this because I had no idea where to begin. I realized some sort of comparison had to be made and a conditional within the loop. I started with the console.log as that was the easiest to do and then proceeded to use the web for the compare tools

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ let hacker2LastIndex = hacker2.length -1;
 }
 console.log (hacker2Reversed);
 
-/* 3.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N". - .toUpperCase, space " "
+let theChosenOne = ""
+let compare = hacker1 [0].localeCompare(hacker2[0])
 
-3.2 Print all the characters of the navigator's name in reverse order, i.e., "nhoJ".
-
-3.3 Depending on the lexicographic order of the strings, print:
-
-The driver's name goes first.
-Yo, the navigator goes first, definitely.
-What?! You both have the same name?*/
-
+    if (compare === 1) {
+        console.log (`${hacker1} goes first.`); 
+    }
+    else if (compare === -1) {
+        console.log (`Yo, ${hacker2} goes first, definitely.`);
+    }
+    else console.log ("What?! You both have the same name?");
