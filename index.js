e3c85be43d4e83271235a4af878739f90a869575
Lorena Barrera <l.barreranicolas@gmail.com>
2023-09-30 20:01:50 +0200

    Ejercicio terminado

--- index.js
+++ index.js
@@ -33,3 +33,20 @@ for (let i = 0; i < hacker1.length; i++) {
   }
   
   console.log (newName);
+
+  let reversedName = '';
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reversedName += hacker2[i];
+}
+
+console.log (reversedName)
+
+
+if (hacker1.localeCompare(hacker2) === 0) {
+  console.log("The driver's name goes first.")
+} else if (hacker1.localeCompare(hacker2) > 0) {
+  console.log("Yo, the navigator goes first, definitely.")
+} else {
+  console.log ("What?! You both have the same name?")
+}
