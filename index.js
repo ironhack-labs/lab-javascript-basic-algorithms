be996ace5d4a1d4f933c401e4f65f730fb035b45
TomG4L <tomepstein.te@gmail.com>
2023-10-06 21:15:09 +0200

    lab not done 100% I am not sure what to do point 3

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // Iteration 1: Names and Input
+let hacker1 = "Tom";
+console.log(`The driver´s name is ${hacker1}`);
+
+let hacker2 = "Todo";
+console.log("The navigator's name is " + (hacker2))
 
 
 // Iteration 2: Conditionals
+console.log(hacker1.length);
+console.log(hacker2.length);
+
+if (hacker1.length > hacker2.length){
+    console.log("The driver has the longest name, it has ${hacker1.length} characters.");
+}
+else if (hacker1.length < hacker2.length){
+    console.log("It seems that the navigator has the longest name, it has ${hacker2.length} characters.");
+} else {
+    console.log("Wow you both have equally ling names, ${hacker1.length} characters!.");
+}
+
+
 
 
 // Iteration 3: Loops
+
+
+
+
