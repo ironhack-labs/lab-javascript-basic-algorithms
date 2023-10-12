66835df034e3a1720e0629d34f2b1a1bbe84d05a
Anggie Castillo <anniecastillono@outlook.com>
2023-10-12 16:37:47 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // Iteration 1: Names and Input
+let Hacker = "Anggie"
+console.log(`the driver's name is "${Hacker}"`)
 
+let hacker2 = "Valentina"
+console.log(`the driver's is "${hacker2}"`)
 
 // Iteration 2: Conditionals
+let Hacker = "Angie"
+let Hacker2 = " Valentina"
 
+for (let i = 0; i < Hacker.length; i++) {
+    const char = Hacker[i];
+    console.log(`${char}`)
+}
+for (let i = 0; i < Hacker2.length; i++) {
+    const char2 = Hacker2[i];
+    console.log(`${char2}`)
+}
+
+if (char < char2) {
+    console.log(`${char} es mayor que ${char2}`)
+}
 
 // Iteration 3: Loops
