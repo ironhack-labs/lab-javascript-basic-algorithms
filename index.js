d4ace8ccd3ee8d616666c8df8481924cf1dc2e67
Mahmoud11111111 <147731732+Mahmoud11111111@users.noreply.github.com>
2023-10-18 18:03:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-Iteration 1:
+// Iteration 1:
 
 let hacker1= "Mahmoud";
 console.log(`The driver name is ${hacker1}`);
 let hacker2= "Marine";
 console.log(`The navigator name is ${hacker2}`);
 
-Iteration 2:
+// Iteration 2:
 
 if(hacker1.length > hacker2.length){
     console.log(`The driver has the longest name, it has ${hacker1.length}characters`);
@@ -17,7 +17,7 @@ if(hacker1.length > hacker2.length){
     console.log(`How, you both have equally long names,${hacker1.length}characters`);
   }
 
-  Iteration 3:
+  // Iteration 3:
 
   let newName ="";
 for(let i=0; i<= hacker1.length ; i++){
