1a135cf4ea76fc28d5960b87c83d0b027f2cc368
mayakilbertus <maya.kilbertus@yahoo.de>
2023-10-18 17:28:47 +0200

    Iteration 3 three

--- index.js
+++ index.js
@@ -30,12 +30,20 @@ for (let i = 0; i < hacker1.length; i++) {
 
 console.log(capitalName);
 
-
-
 let reversedHacker2 = "";
 
-for(let i = hacker2.length -1; i >= 0; i-- ){
-  reversedHacker2 += hacker2[i]
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reversedHacker2 += hacker2[i];
 }
 
-console.log(reversedHacker2)
+console.log(reversedHacker2);
+
+const lexiOrder = hacker1.localeCompare(hacker2);
+
+if (lexiOrder === -1) {
+  console.log("The driver's name goes first.");
+} else if (lexiOrder === 1) {
+  console.log("Yo, the navigator goes first, definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
