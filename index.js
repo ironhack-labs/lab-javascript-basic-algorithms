66687cf9b592cbd11d3d2782d368e37cc719b306
mayakilbertus <maya.kilbertus@yahoo.de>
2023-10-18 16:52:21 +0200

    Iteration 1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Iteration 1: Names and Input
+const hacker1 = "Maya";
+console.log(`The driver's name is ${hacker1}`);
 
+const hacker2 = "Marco";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
