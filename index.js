730d6a6cae136e5e1263654f2bad5f86d1834121
Banashree Aribenchi <banashree.aribenchi17@gmail.com>
2023-10-18 17:13:03 +0200

    Iteration 2: Conditionals

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Iteration 1: Names and Input
 
 const hacker1 = "Mauli";
-console.log(`The drivers's name is ${hacker1}`)
+console.log(`The drivers's name is ${hacker1}`);
 
-const hacker2 = "Banashree"
-console.log(`The navigator's name is ${hacker2}`)
-
+const hacker2 = "Banashree";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+  console.log(`The ${hacker1} has ${hacker1.length} characters`);
+} else if (hacker1.length < hacker2.length) {
+  console.log(`The ${hacker2} has ${hacker2.length} characters`);
+}
 
 // Iteration 3: Loops
