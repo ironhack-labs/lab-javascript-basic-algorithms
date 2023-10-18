abfc697c0faad83b4d258eb98d97d313d8e4451f
Sena Aydin <senaaydin484@gmail.com>
2023-10-18 15:56:12 +0200

    first iteration done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
 
+let hacker1= "Sena";
+console.log(`Driver's name is ${hacker1}.`)
+
+
+let hacker2= "Lisa"
+console.log(`Navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
