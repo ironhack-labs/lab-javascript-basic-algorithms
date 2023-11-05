047bfb4bfd82d6c1d614abf5f111e7c663b0ba10
RodriguezJoseMaria <143606586+RodriguezJoseMaria@users.noreply.github.com>
2023-11-05 18:15:58 +0100

    updated

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ console.log(`The navigatos's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
-  console.log("The driver has the longest name, it has " + hacker1.length + " characters");
+  console.log(`The driver has the longest name, it has  ${hacker1.length} characters`);
 }
 else if ( hacker1.length < hacker2.length) {
-console.log ("It seems that the navigator has the longest name, it has " + hacker1.length  + " characters.");
+console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
 }
 else {
-    console.log("Wow, you both have equally long names, " + hacker1.length + " characters!");
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
 }
 
 
@@ -22,6 +22,9 @@ else {
 
 for (let i = 0; i < hacker1.length; i++) {
   console.log(hacker1[i].toUpperCase());
+  if (i < hacker1.length - 1) {
+    container += " ";
+  };
 }
 
 let reversed = "";
@@ -30,13 +33,13 @@ for (let i = hacker1.length - 1; i >= 0; i--) {
 }
 console.log(reversed);
 
-console.log(hacker1.localeCompare(hacker2));
 
-if (hacker1.localeCompare(hacker2) >= 1) { 
-    console.log(`Yo, the navigator goes first, definitely.`)
-}
-else if (hacker1.localeCompare(hacker2) <= 1) {
-    console.log(`The driver'\s name goes first.`)
-}
-else { console.log (`What?! You both have the same name?`)
-}
+const lexicographicalOrder = hacker1.localeCompare(hacker2);
+
+if(lexicographicalOrder < 0) {
+    console.log("The driver's name goes first");
+    } else if (lexicographicalOrder > 0) {
+        console.log("Yo, the navigator goes first, definitely");
+    } else {
+        console.log("What?! You both have the same name?");
+    };
