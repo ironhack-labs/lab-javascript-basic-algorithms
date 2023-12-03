85409272518f1260995c6cb0a24ec528df15cdad
Jesús Sánchez <jsxexu38@gmail.com>
2023-12-03 17:13:04 +0100

    end 3.3

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
-const hacker1 = "Jesús"
+let hacker1 = "Jesús"
 console.log(`The driver's name is ${hacker1}`)
 
-const hacker2 = "Isabel"
+let hacker2 = "Isabel"
 console.log(`The navigator's name is ${hacker2}`)
   
 // Iteration 2: Conditionals
@@ -27,7 +27,6 @@ for (let i = 0; i < hacker1.length; i++) {
 
   result += char + " "
 }
-
 console.log(result)
 
 //3.2
@@ -39,5 +38,14 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 
   input += char1
 }
-
 console.log(input)
+
+//3.3
+
+if (hacker1 < hacker2) {
+  console.log("The driver's name goes first")
+} else if (hacker2 < hacker1) {
+  console.log("Yo, the navigator goes first, definitely")
+} else {
+  console.log("What?! You both have the same name?")
+}
