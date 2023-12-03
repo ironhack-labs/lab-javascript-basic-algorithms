f68dc29ef9727c3c934e56374a1bcea1b219981e
Jesús Sánchez <jsxexu38@gmail.com>
2023-12-03 10:55:52 +0100

    Loops 3.1 y 3.2

--- index.js
+++ index.js
@@ -18,3 +18,26 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+
+// 3.1
+let result = ""
+
+for (let i = 0; i < hacker1.length; i++) {
+  const char = hacker1[i].toUpperCase()
+
+  result += char + " "
+}
+
+console.log(result)
+
+//3.2
+
+let input = ""
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  const char1 = hacker2[i]
+
+  input += char1
+}
+
+console.log(input)
