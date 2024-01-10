25e8b46d4105a0de74498196e2346763c04ae269
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-10 17:12:06 +0100

    finish bonus 1

--- index.js
+++ index.js
@@ -56,3 +56,46 @@ if ('hacker1'.localeCompare('hacker2') === 1) {
 } else {
     console.log("Something went wrong")
 }
+
+// BONUS 1
+
+/*Generate 3 paragraphs. Store the text in a new string variable named longText.
+Make your program count the number of words in the string.
+Make your program count the number of times the Latin word et appears.*/
+
+let longText ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel tempus diam, ac vulputate nisi. Nulla consequat est quis ante cursus, quis efficitur nunc placerat. Duis sollicitudin vestibulum nulla non molestie. Integer aliquet maximus neque, eu vehicula neque. Suspendisse sapien tortor, faucibus ut tempor non, tempor eu quam. Quisque ut risus tortor. Proin id mattis arcu. Praesent sit amet turpis lacinia justo pharetra sollicitudin nec quis justo. Etiam venenatis risus vel rutrum cursus. In hac habitasse platea dictumst. Sed aliquet sagittis neque vitae porta. Proin porta semper purus quis tempus. Duis eu pulvinar diam. Maecenas egestas velit et sapien sodales, sit amet mollis tortor malesuada. Nulla facilisi.Fusce placerat feugiat tempor. Nulla quis turpis posuere, lobortis arcu a, lobortis augue. In mi nibh, ullamcorper finibus sagittis vitae, pellentesque ac lectus. Vivamus id sapien in velit gravida consectetur sit amet ac mi. Donec dapibus urna sit amet quam consequat, a vestibulum tellus viverra. Aliquam a odio massa. Ut lacinia vulputate facilisis. Praesent ac odio ut lorem accumsan sollicitudin ut non velit. Sed nec augue consequat, porttitor urna mollis, pellentesque dui. Integer at dui turpis. Donec varius eleifend rhoncus. Praesent id molestie nunc. Nullam feugiat dolor vel massa iaculis, eget accumsan massa elementum. Sed accumsan magna vitae justo tincidunt, sit amet volutpat mi suscipit. Vivamus eleifend neque vitae nulla tempor gravida. Aenean finibus nisl sed augue hendrerit, at convallis massa dignissim. Nulla a sem suscipit ipsum congue convallis. Vestibulum euismod dapibus malesuada. Integer venenatis dictum pretium. Ut id augue iaculis, iaculis nibh vehicula, eleifend ligula."
+
+function countWords(str) {
+    return str.trim().split(/\s+/).length;
+  } //found this in stackOverflow!
+
+  console.log(countWords(longText));
+
+
+//console.log(longText.indexOf(" et"));
+//console.log(longText.indexOf(" et", 658));
+// looks like it only appears once, not so happy with this method tho, if a very long text, then this is super manual work, should it be a loop??? //
+
+function countWords (text) {
+let etCount = 0;
+for (let i=0; i<text.length;) {
+    i = text.indexOf(" et", i);
+    if (i !== -1) {
+    etCount ++;
+    i += 3;
+    } else {
+        break;
+    }
+}; 
+
+if (etCount === 1) {
+    return (`The string "et" appears ${etCount} time.`);
+} else if (etCount > 1) {
+    return (`The string "et" appears ${etCount} times.`);
+}
+
+};
+
+countWords(longText);
+
+//got lost, asked chatgpt to correct my code and analyzed it step by step.
