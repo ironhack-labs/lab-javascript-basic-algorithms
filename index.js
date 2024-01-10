3bf06e25e2d91e3acb44ae69929f483a08163319
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-10 16:17:41 +0100

    iteration 3 complete

--- index.js
+++ index.js
@@ -20,4 +20,39 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 
+// 3.1
 
+let spacedAndCapitalized = "";
+
+for (let i=0; i < hacker1.length; i++) {
+    let upperChar = hacker1[i].toUpperCase() + " ";
+    spacedAndCapitalized += upperChar;
+}
+
+console.log (spacedAndCapitalized);
+
+
+// 3.2
+
+let reversedName = "";
+
+for (let i=hacker2.length-1; i >= 0; i--) {
+    reversedName += hacker2[i];
+}
+
+console.log (reversedName);
+
+// 3.3
+
+/*let hacker1 = "Natalia";
+let hacker2 = "Juanito";*/
+
+if ('hacker1'.localeCompare('hacker2') === 1) {
+    console.log("The driver's name goes first.")
+} else if ('hacker1'.localeCompare('hacker2') === -1) {
+    console.log("Yo, the navigator goes first, definitely.")
+} else if ('hacker1'.localeCompare('hacker2') === 0) {
+    console.log("What?! You both have the same name?")
+} else {
+    console.log("Something went wrong")
+}
