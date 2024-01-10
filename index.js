62d8514148809762b651b1da461a347a0ad7e8ac
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-10 15:23:46 +0100

    finish conditionals

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 let hacker1 = "Natalia";
 console.log(`The driver's name is ${hacker1}`);
 
-let hacker2 = "Juan";
+let hacker2 = "Juanito";
 console.log(`The navigator's name is ${hacker2}`);
-
-
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log (`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+    console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters! `);
+} else {
+    console.log("Something went wrong");
+};
 
 // Iteration 3: Loops
+
+
