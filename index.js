8ef456e6119aafa1536748602b306990a7c40114
Olumide <wenceslausajidahu@gmail.com>
2024-01-10 17:52:07 +0100

    algorithms lab

--- index.js
+++ index.js
@@ -2,6 +2,45 @@
 
 
 // Iteration 2: Conditionals
+console.log("I'm ready!");
+
+let hacker1 = "XXXX";
+
+console.log("The driver's name is " + hacker1);
+
+let hacker2 = "YYYY";
+
+console.log("The navigator's name is " + hacker2);
+
+
+let lengthHacker1 = hacker1.length;
+let lengthHacker2 = hacker2.length;
+
+if (lengthHacker1 > lengthHacker2) {
+    console.log("The driver has the longest name, it has " + lengthHacker1 + " characters.");
+} else if (lengthHacker1 < lengthHacker2) {
+    console.log("It seems that the navigator has the longest name, it has " + lengthHacker2 + " characters.");
+} else {
+    console.log("Wow, you both have equally long names, " + lengthHacker1 + " characters!");
+}
+
+let driver = "John";
+
+let navigator = "Jane";
+
+console.log("Driver's name: " + driver.toUpperCase().split('').join(' '));
+
+console.log("Navigator's name: " + navigator.split('').reverse().join(''));
+
+
+let comparison = driver.localeCompare(navigator);
+if (comparison < 0) {
+    console.log("The driver's name goes first.");
+} else if (comparison > 0) {
+    console.log("Yo, the navigator goes first, definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
 
 
 // Iteration 3: Loops
