c15687dc5e947359fed408f7672ebaa418ab7d65
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-01-10 16:49:53 +0100

    solve iteration 3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Iteration 1: Names and Input
+
 const hacker1 = "Joy";
 console.log(`The driver's name is ${hacker1}`);
-const hacker2 = "also Joy";
+
+const hacker2 = "alsoJoy";
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
@@ -28,3 +30,29 @@ compareNameLength("Banana", "Ana");
 compareNameLength("Omar", "Anna");
 
 // Iteration 3: Loops
+
+let capName1 = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+  capName1 += hacker1[i].toUpperCase() + " ";
+}
+
+let capName2 = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  capName2 += hacker2[i].toUpperCase() + " ";
+}
+
+console.log(capName1);
+console.log(capName2);
+
+function sortName(driver, navigator) {
+  if (driver.localeCompare(navigator) === -1) {
+    console.log("The driver's name goes first.");
+  } else if (driver.localeCompare(navigator) === 1) {
+    console.log("Yo, the navigator goes first, definitely.");
+  } else {
+    console.log("What?! You both have the same name?");
+  }
+}
+
+sortName(hacker1, hacker2);
