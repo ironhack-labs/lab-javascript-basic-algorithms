9be1205b37df5d1947ef40b47c9a607a36c5b528
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-10 15:13:19 +0100

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Natalia";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Juan";
+console.log(`The navigator's name is ${hacker2}`);
+
+
 
 // Iteration 2: Conditionals
 
