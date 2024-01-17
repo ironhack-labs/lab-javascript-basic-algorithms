45efb7f332ce190065a2c7b91452df9ca254939b
Rui Duram <ruiduran.jr@gmail.com>
2024-01-17 16:44:35 +0100

    done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-// Iteration 1: Names and Input
+const hacker1="Rui"
+console.log(`The driver´s name is ${hacker1}`)
+const hacker2="Betina"
+console.log(`The naivgator´s name is ${hacker2}`)
+
+console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
 
 
-// Iteration 2: Conditionals
+const hacker1NameUpperCased= hacker1.toUpperCase().split("").join(" ");
 
+const hacker2Reversed = hacker2.split("").reverse().join("");
 
-// Iteration 3: Loops
+console.log("The driver's name goes first.")
