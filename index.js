f4d82d94fade010352ebe784cdcd50f21ac9cb85
Gavin Alexander <gavalex7@aol.com>
2024-01-17 18:04:03 +0100

    drafted again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let hacker1 = "Gav";
 console.log("The driver's name is" + ' ' + hacker1);
 let hacker2 = "Barbie"
 console.log("The navigator's name is" + ' ' + hacker2)
- 
+
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
@@ -20,7 +20,7 @@ else if (hacker2.length === hacker1.length) {
 // Iteration 3: Loops
 
 const hacker1New = hacker1.toUpperCase();
-    console.log(hacker1New);
+console.log(hacker1New);
 
 for (i = 0; i < hacker1New.length; i++) {
     console.log(hacker1New[i])
@@ -28,7 +28,7 @@ for (i = 0; i < hacker1New.length; i++) {
 
 
 //for (var i = count - 1; i >= 0; i--)
-for (j = hacker2.length-1; j >= 0; j--) {
+for (j = hacker2.length - 1; j >= 0; j--) {
 
     console.log(hacker2[j])
 }
@@ -48,3 +48,23 @@ else if (hacker2.localeCompare(hacker1) === 0) {
 }
 
 
+let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec dui nec velit convallis rutrum. Donec eget ullamcorper massa, ac tempor tellus. Proin eget posuere sapien. Vivamus gravida finibus orci, in posuere ante consectetur mattis. Suspendisse potenti. Praesent justo lacus, vulputate non quam nec, gravida congue ligula. Maecenas tincidunt sollicitudin pellentesque.
+Morbi in sagittis ex. In non massa nec turpis maximus fringilla. Sed sit amet erat nisl. Praesent vestibulum a enim id pellentesque. Mauris gravida gravida semper. Curabitur tincidunt ipsum vitae nisl accumsan varius. Sed nulla augue, venenatis nec tortor sit amet, imperdiet euismod elit.
+Fusce fringilla neque leo, quis molestie libero commodo et. Pellentesque nunc est, tincidunt id urna ac, vehicula gravida risus. Suspendisse eget lorem id risus condimentum malesuada sit amet sit amet est. Suspendisse blandit volutpat consectetur. Morbi sollicitudin arcu in bibendum tincidunt. Maecenas enim ligula, porttitor eget rutrum sit amet, sodales nec nibh. Curabitur vulputate tellus posuere consectetur hendrerit. Donec a tincidunt velit, ac molestie lectus. Nam dignissim lorem dolor, vel tempor massa tincidunt id. Donec quis arcu vitae massa mollis tincidunt at eget sapien. Nullam lacinia sed nulla at tristique. Aliquam tincidunt ante purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris porttitor in metus at semper. Cras commodo ac turpis eget vehicula. Nullam eu arcu posuere, cursus neque vel, lobortis ligula.`
+
+
+let count = 1
+
+function howMany(wordCount) {
+
+    for (let i = 0; i < wordCount.length; i++) {
+        if (wordCount[i] === ' ') {
+            count++
+        }
+
+        
+    }
+    return count;
+}
+
+console.log(howMany(longText))
