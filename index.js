eb59f4c891eca11e50aaea46014f3c4b2fe12148
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-01-18 08:15:50 +0100

    changed mistakes - hopefully correct

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ const driver = hacker1.toUpperCase().split("").join(" ");
 
 console.log(driver);
 
+const reverseHacker1 = "";
+for (let i=hacker1.length - 1; i>=0; i--) {
+    reverseHacker1 += hacker1[i];
+ }
+ console.log(reverseHacker1);
+
+
+ if (hacker1.localeCompare(hacker2) <0) {
+    console.log("The driver's name goes first.");
+  } else if (hacker1.localeCompare(hacker2) >0) {
+    console.log(
+      "Yo, the navigator goes first definitely.")
+  } else {
+    console.log(
+      "What?! You both have the same name?");
+  }
+  
+
 // Bonus 1
 
 const longText = '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae ex massa. Curabitur non urna arcu. Nulla ac commodo sem. Proin leo sapien, mattis quis mauris tincidunt, dignissim bibendum lorem. Maecenas sem eros, volutpat in nulla sit amet, vehicula ullamcorper tortor. Duis imperdiet velit augue, non egestas sapien porttitor vitae. Nullam eros massa, sollicitudin sed pulvinar porta, faucibus fringilla ante. Integer nibh neque, vulputate nec cursus ut, congue eget dui. Donec mollis lectus varius, suscipit orci at, posuere metus. Vestibulum ut vestibulum purus. /nPhasellus sit amet nisi commodo, consequat tellus et, ultricies lacus. Nullam porta, velit et aliquet tempus, ante massa laoreet velit, in efficitur dolor purus non augue. Donec malesuada orci at commodo accumsan. Etiam tellus dui, blandit eget scelerisque id, pellentesque in mauris. Pellentesque tincidunt vitae mauris at blandit. Cras vel tristique dolor. Phasellus pellentesque massa eu ante blandit pellentesque vitae vel velit. /nCras fermentum, leo tristique laoreet accumsan, nulla metus tristique ex, eget malesuada ex est quis metus. Suspendisse consectetur leo non justo laoreet, non faucibus erat cursus. Quisque arcu turpis, vehicula sed neque eu, luctus dignissim libero. Aenean euismod facilisis vestibulum. Maecenas aliquam arcu at risus iaculis, congue varius ex facilisis. Donec nisl diam, rutrum consectetur quam et, condimentum tincidunt eros. Aliquam vel nisi nisi. Quisque lorem dolor, bibendum et aliquet sit amet, finibus vitae dui."'
@@ -37,7 +55,7 @@ console.log(longText.split(latinWord).length - 1);
 //Bonus 2
 
 const phraseToCheck = "step on no pets";
-const isPalindrome = true
+let isPalindrome = true
 
 for (let i = 0; i < phraseToCheck.length / 2; i++) {
   if (phraseToCheck[i] !== phraseToCheck[phraseToCheck.length - 1 - i]) {
