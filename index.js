7d3d4e73f47e427ff5cb68b07c9b041ead99e6f5
Eber Brown <eaba_89@hotmail.com>
2024-01-23 20:24:48 +0100

    Iteration 1: names and input JS code

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = "John";
+console.log("The driver's name is " + hacker1);
 
+let hacker2 = "Chris";
+console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
 
