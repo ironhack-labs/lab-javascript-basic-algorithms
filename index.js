74d7fba36b9180d9989ef902b69e3499e8f86530
pablosanchezcamara89@gmail.com <pablosanchez@MacBook-Air-de-Pablo.local>
2024-01-24 16:13:20 +0100

    Part1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Pablo";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Meri";
+console.log(`The navigator's name is ${hacker2}`);
+
 
 // Iteration 2: Conditionals
 
