dbf39c2dfec73a7984a92e99ae58a2a489e796e7
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-01-25 19:32:49 +0100

    2nd iteration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hacker1 = 'Stefano';
 
 console.log(`The driver's name is ${hacker1}`);
 
-const hacker2 = 'Nombre';
+const hacker2 = 'Stefano';
 
 console.log(`The navigator's name is ${hacker2}`);
 
@@ -15,6 +15,24 @@ console.log(`The navigator's name is ${hacker2}`);
 - It seems that the navigator has the longest name, it has XX characters. or
 - Wow, you both have equally long names, XX characters!. */
 
-switch 
+/* console.log(`${hacker1.length}`);
+console.log(`${hacker2.length}`); */
+
+
+const driverLength = hacker1.length;
+
+const navigatorLength = hacker2.length;
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+   console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else if (hacker1.length = hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`);
+}
+
+/* Preguntas:
+- Cuando está bien así o habría que convertirlo a string? */
+
 
 // Iteration 3: Loops
