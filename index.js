b57ea2e26a498c8a948471f333439e7a73db17a6
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-01-25 19:37:47 +0100

    shorter 2nd iteration

--- index.js
+++ index.js
@@ -10,29 +10,33 @@ console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
-/* 2.1. Depending on which name is longer, print:
-- The driver has the longest name, it has XX characters. or
-- It seems that the navigator has the longest name, it has XX characters. or
-- Wow, you both have equally long names, XX characters!. */
-
 /* console.log(`${hacker1.length}`);
 console.log(`${hacker2.length}`); */
-
 
 const driverLength = hacker1.length;
 
 const navigatorLength = hacker2.length;
 
-if (hacker1.length > hacker2.length) {
+/* if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 } else if (hacker1.length < hacker2.length) {
    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 } else if (hacker1.length = hacker2.length) {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`);
+} */
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length === hacker2.length) {
+   console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`);
+} else {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 }
 
-/* Preguntas:
-- Cuando está bien así o habría que convertirlo a string? */
 
+/* Preguntas:
+- Cuando está bien así o habría que convertirlo a string?
+- El ejemplo de str.length pone ${}
+- Tengo que usar el valor mayor o el valor menor? */
 
 // Iteration 3: Loops
