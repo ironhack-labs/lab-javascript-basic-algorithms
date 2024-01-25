6e3a0f0a9e8ac73e3a785305f27d8c0d615a8712
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-01-25 18:42:48 +0100

    1st iteration

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // Iteration 1: Names and Input
 
+const hacker1 = 'Stefano';
+
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = 'Nombre';
+
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+/* 2.1. Depending on which name is longer, print:
+- The driver has the longest name, it has XX characters. or
+- It seems that the navigator has the longest name, it has XX characters. or
+- Wow, you both have equally long names, XX characters!. */
+
+switch 
 
 // Iteration 3: Loops
