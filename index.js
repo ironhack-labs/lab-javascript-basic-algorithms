c28bc16d1b154968ea992d74e2dc5916f7690257
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-01-26 11:00:36 +0100

    spaced driver

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hacker1 = 'Stefano';
 
 console.log(`The driver's name is ${hacker1}`);
 
-const hacker2 = 'Stefano';
+const hacker2 = 'Laura';
 
 console.log(`The navigator's name is ${hacker2}`);
 
@@ -63,3 +63,50 @@ switch (comparison) {
 - Tengo que usar el valor mayor o el valor menor? */
 
 // Iteration 3: Loops
+
+/* 3.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
+
+3.2 Print all the characters of the navigator's name in reverse order, i.e., "nhoJ".
+
+3.3 Depending on the lexicographic order of the strings, print:
+
+The driver's name goes first.
+Yo, the navigator goes first, definitely.
+What?! You both have the same name? */
+
+
+/* for (const value of hacker1) {
+    console.log (value.toUpperCase());
+}
+
+for (const value of hacker2) {
+    console.log (value.toUpperCase());
+} */
+
+const mayuscDriver = hacker1.toUpperCase();
+
+let spacedMayuscDriver = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+    spacedMayuscDriver += mayuscDriver[i];
+    if (i !== hacker1.length - 1) {
+        spacedMayuscDriver += " ";
+    }
+}
+
+console.log(`${spacedMayuscDriver}`);
+
+/* for (let i = 0; i < mayuscDriver.length; i++) {
+    console.log(str[1]);
+}*/
+
+/* preguntas: is it more correct to use hacker1 or mayuscDriver here? */
+
+
+
+/* for (let i = hacker1.length; i = 1; hacker1[i]);
+for (let i = hacker2.length; i = 1; hacker2[i]); */
+
+/* if (hacker1[i] < hacker2[i]) {
+    console.log(`The driver's name goes first.`);
+}*/
