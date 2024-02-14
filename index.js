c05f35d74846a9f12f5727404b77ad92464e2f7c
shaza kashlan <eng.shazakashlan@gmail.com>
2024-02-14 16:50:42 +0100

    finish iteration 3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
 const hacker1 = "Shaza";
-const hacker2 = "Robxx";
-// console.log("The drivers name is " + hacker1)
-// console.log("The navs name is " + hacker2)
+const hacker2 = "Rob";
+console.log("The drivers name is " + hacker1);
+console.log("The navs name is " + hacker2);
 
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
@@ -18,5 +18,22 @@ if (hacker1.length > hacker2.length) {
     `Wow, you both have equally long names, ${hacker1.length} characters!`
   );
 }
-console.log(hacker1.toUpperCase);
+let newDriverName = "";
 // Iteration 3: Loops
+for (let i = 0; i < hacker1.length; i++) {
+  if (i < hacker1.length - 1) {
+    newDriverName += hacker1[i].toUpperCase() + " ";
+  } else {
+    newDriverName += hacker1[i].toUpperCase();
+  }
+}
+
+console.log(`newDriverName ${newDriverName}`);
+
+let newNav = "";
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  newNav += hacker2[i];
+}
+
+console.log(newNav);
