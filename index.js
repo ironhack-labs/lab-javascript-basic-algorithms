1d3b28eb0c6050f80140ee5bd8a9af2f1103f058
Memoryman2022 <rlacannon@gmail.com>
2024-02-14 16:03:57 +0100

    first iteration

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Iteration 1: Names and Input
+const hacker1 = "Shaza"
+const hacker2 = "Rob"
+// console.log("The drivers name is " + hacker1)
+// console.log("The navs name is " + hacker2)
 
 
 // Iteration 2: Conditionals
+
 
 
 // Iteration 3: Loops
