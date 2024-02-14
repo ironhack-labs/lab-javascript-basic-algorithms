a1fcaf14a8ae31332b920f01f70d9384bc50e640
Ben Friedman <benjamindfriedman@gmail.com>
2024-02-14 18:26:31 +0100

    almost complete

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
 // Iteration 1: Names and Input
 
-// 1.1 Create a variable hacker1 with the driver's name.
-// 1.2 Print "The driver's name is XXXX".
-// 1.3 Create a variable hacker2 with the navigator's name.
-// 1.4 Print "The navigator's name is YYYY".
-
-
 const hacker1 = "ben";
 const hacker2 = "mohamed";
 
 // console.log("The driver's name is " + hacker1);
 // console.log("The navigator's name is "+ hacker2)
 
+
 // Iteration 2: Conditionals
 
-// 2.1. Depending on which name is longer, print:
-// - The driver has the longest name, it has XX characters. or
-// - It seems that the navigator has the longest name, it has XX characters. or
-// - Wow, you both have equally long names, XX characters!.
-
-// if (hacker1.length === hacker2.length) {
-//     console.log("Wow, you both have equally long names, " + hacker1.length + " characters!")
-// } else if (hacker1.length > hacker2.length){
-//     console.log ("The driver has the longest name, it has " + hacker1.length+ " characters")
-// } else {
-//     console.log ( " It seems that the navigator has the longest name, it has " + hacker2.length + " characters")
-// }
+if (hacker1.length === hacker2.length) {
+    // console.log("Wow, you both have equally long names, " + hacker1.length + " characters!")
+} else if (hacker1.length > hacker2.length){
+    // console.log ("The driver has the longest name, it has " + hacker1.length+ " characters")
+} else {
+    // console.log ( " It seems that the navigator has the longest name, it has " + hacker2.length + " characters")
+}
 
 
 // Iteration 3: Loops
 
-// 3.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
-
-// 3.2 Print all the characters of the navigator's name in reverse order, i.e., "nhoJ  / demahom".
-
-// 3.3 Depending on the lexicographic order of the strings, print:
-
-// The driver's name goes first.
-// Yo, the navigator goes first, definitely.
-// What?! You both have the same name?
+// 3.1
 let h1 = ""
 for (let i = 0; i < hacker1.length ; i++) {
      h1 += hacker1[i].toUpperCase() + " "
@@ -46,8 +28,34 @@ for (let i = 0; i < hacker1.length ; i++) {
 }
 // console.log(h1)
 
+// 3.2
 let h2 =""
-for ( let i = hacker2.length -1 ; i >= 0 ; i--) {
-    h2 += hacker2[i]
+for ( let j = hacker2.length -1 ; j >= 0 ; j--) {
+    h2 += hacker2[j]
 }
-console.log (h2)
+// console.log (h2)
+
+// 3.3
+
+// I HAVE NO CLUE!!!!
+
+// Bonus 1:
+
+const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at felis varius, rutrum urna sit amet, placerat sapien. Etiam ornare ipsum a nisl gravida, ac euismod mi viverra. Cras id lacinia enim. Pellentesque sed et eget nisi mollis cursus. Ut nec sapien egestas, scelerisque odio eget, varius tellus. Maecenas bibendum semper lectus, ut consequat magna. Donec sagittis ex sit amet leo sollicitudin, efficitur tempor elit vulputate. Quisque nec velit at nulla pulvinar tempus. In neque lacus, varius vitae purus pellentesque, accumsan rhoncus dui. Vivamus quis justo tempus, eleifend ex non, ullamcorper erat. Aliquam lectus eros, vulputate vel aliquet feugiat, mattis at turpis. Nunc at ipsum blandit, dictum tellus quis, vulputate ante.Quisque non tristique neque. Donec ac lacinia elit. Aenean eget nibh ut mauris interdum tempus a sed metus. Donec sagittis elit sit amet felis vestibulum, a accumsan elit vehicula. Nunc vitae tempor lectus. Nulla sit amet lectus lectus. Mauris suscipit leo nec velit gravida, eu scelerisque est molestie. Proin hendrerit mattis consequat. Vivamus pellentesque ex at sem fermentum fermentum. Integer euismod neque imperdiet venenatis iaculis. Aliquam ac nulla tristique, sagittis ante vel, pellentesque ex.Fusce cursus interdum varius. Cras eget massa vel lorem congue vulputate. Pellentesque luctus sodales urna eu luctus. Sed porttitor sit amet dui ut tincidunt. Morbi a interdum urna, nec vulputate mi. Morbi a malesuada lectus. Sed dapibus ultrices scelerisque. Curabitur eget malesuada ligula. Nullam elementum, leo ac imperdiet malesuada, orci sem facilisis sapien, vel bibendum libero nulla in nulla. Nulla commodo tincidunt purus, vel placerat massa ullamcorper et. Sed vestibulum pharetra odio in tristique. Sed tincidunt dolor sem, id viverra quam porttitor eu. Vivamus ac lorem purus. Aenean quis ante ipsum."
+let wordCounter = 0
+for (let k = 0; k < longText.length ; k++) {
+    if (longText[k+1] === " ") {
+        wordCounter++
+    }
+}
+
+// console.log(wordCounter)
+
+let etCounter = 0
+for (let l = 0; l < longText.length ; l++) {
+    if (longText[l-1] === " " && longText[l] === "a" && longText[l+1] === "t" && longText[l+2] === " ") {
+        etCounter++
+    }
+}
+
+console.log(etCounter)
