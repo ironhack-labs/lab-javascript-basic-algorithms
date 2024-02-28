c5b89ba12d3f076cf997b48bb46e06651c994ec7
Feryal Doubali <feryalhalima@gmail.com>
2024-02-28 15:41:33 +0100

    a toi

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Iteration 1: Names and Input
-
-
+let hacker1 = "Feryal"
+let hacker2 = "Raphaël"
+console.log(`The driver's name is ${hacker1}` )
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
