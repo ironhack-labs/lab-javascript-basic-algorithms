d44885aed9f5b17fc3f6918b05e5d7dea7c2f35d
smnraphael <simonraphael@hotmail.fr>
2024-02-28 15:54:19 +0100

    à ton tour

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // Iteration 1: Names and Input
-let hacker1 = "Feryal"
-let hacker2 = "Raphaël"
-console.log(`The driver's name is ${hacker1}` )
-console.log(`The navigator's name is ${hacker2}`)
-// Iteration 2: Conditionals
+let hacker1 = "Feryal";
+let hacker2 = "Raphaël";
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
 
+// Iteration 2: Conditionals
+let lengthHacker1 = hacker1.length;
+let lengthHacker2 = hacker2.length;
+
+if (lengthHacker1 > lengthHacker2) {
+  console.log(
+    `The driver has the longest name, it has ${lengthHacker1} characters.`
+  );
+} else if (lengthHacker1 < lengthHacker2) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${lengthHacker2} characters.`
+  );
+} else {
+  console.log(`Wow, you bot h have equally long names, XX characters!`);
+}
 
 // Iteration 3: Loops
