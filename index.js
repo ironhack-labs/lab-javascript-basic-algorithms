e9af01825f7339e5eadf261467af475734ae7f20
Laura Pinto <laurampinto90@gmail.com>
2024-03-02 13:48:23 +0000

    Iteration 1

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
 
+const hacker1 = "Laura";
+
+const hacker2 = "Asma";
+
+console.log(`The driver's name is ${hacker1}`);
+
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
