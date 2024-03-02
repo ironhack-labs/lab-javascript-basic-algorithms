02be4574c88d671aaca3ce8e370909f350ce5166
Adrian Martin <martinolivaadrian@gmail.com>
2024-03-02 14:41:27 +0100

    Iteration1&2 done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // Iteration 1: Names and Input
+const driver = "Adrian";
+const navigator = "Michael";
 
+console.log(`The driver's name is ${driver}`);
+console.log(`The navigator's name is ${navigator}`);
 
 // Iteration 2: Conditionals
-
+if (driver.length > navigator.length){
+    console.log(`The driver has the longest name, it has ${driver.length} characters.`);
+} else if (driver.length < navigator.length){
+    console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters.`);
+} else if(driver.length === navigator.length){
+    console.log(`Wow, you both have equally long names, ${navigator} characters!.`);
+}
 
 // Iteration 3: Loops
