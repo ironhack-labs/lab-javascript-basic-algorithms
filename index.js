b42c318726243939928bd597445b07e298c57c25
“Mynorka <mynorkadaza@gmail.com>
2024-03-02 16:10:02 +0100

    3rd iteration

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Iteration 1: Names and Input
-let hacker1 = "mimmyy";
+let hacker1 = "mimmy";
 let hacker2 = "fatma";
 
 console.log(`The driver's name is ${hacker1}`)
@@ -15,3 +15,28 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+let pizza = ""
+for (let i=0; i < hacker1.length; i++){
+pizza += " " + hacker1[i].toUpperCase()
+}
+
+console.log(pizza)
+
+let flower = ""
+for (let i=hacker2.length-1 ; i>=0; i--){
+flower += hacker2[i]
+}
+console.log(flower)
+
+// console.log(`${hacker1.toUpperCase().split('').join(' ')}`);
+
+// let reversedNavigatorName = hacker2.split('').reverse().join('');
+// console.log(`${reversedNavigatorName}`);
+
+let lexicographicOrder = hacker1.localeCompare(hacker2);
+if (lexicographicOrder < 0) { console.log("The driver's name goes first.");
+} else if (lexicographicOrder > 0) {
+    console.log("Yo, the navigator goes first, definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
