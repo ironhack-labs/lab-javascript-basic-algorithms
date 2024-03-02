bacd0f904756b3b581a0862d3615d54cd62f6293
fatma2090 <boudayafatima@gmail.com>
2024-03-02 15:10:43 +0100

    second iteration

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Iteration 1: Names and Input
-let hacker1 = "mimmy";
+let hacker1 = "mimmyy";
 let hacker2 = "fatma";
 
 console.log(`The driver's name is ${hacker1}`)
 console.log(`The driver's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters`)
+}
 
 // Iteration 3: Loops
