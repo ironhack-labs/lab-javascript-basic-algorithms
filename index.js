116717bf5d1b084f09c01140a20364aba325fef1
“Mynorka <mynorkadaza@gmail.com>
2024-03-02 14:44:08 +0100

    1st iteration

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = "mimmy";
+let hacker2 = "fatma";
 
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The driver's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
