80dd193bc866c9ca890e91df81c2850e4aa88e46
Felipe Calderero <felipecalderero@gmail.com>
2024-03-13 11:53:34 +0100

    iteration 1

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 = "Felipe";
+console.log(`The driver's name is ${hacker1}`);
 
+const hacker2 = "Jinisha";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
