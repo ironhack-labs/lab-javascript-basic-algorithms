b8da0f82b39887539f1448c60f505168c3672d09
Vituin <124085647+Vituin@users.noreply.github.com>
2024-03-14 21:34:14 +0100

    done

--- index.js
+++ index.js
@@ -7,16 +7,18 @@
 // Iteration 3: Loops
 //Exercise 1
 
-const hacker1 = "Victor"
-console.log("The driver's name is ", hacker1)
 
-const hacker2 = "Oscar"
-console.log(`The navigator's name is ${hacker2}`)
+const hacker1 = "Victor"
+console.log(`The driver name is ${hacker1}`)
+
+const hacker2 = "Marta"
+console.log(`The navigator name is ${hacker2}`)
 
 //Exercise 2
 
 const long1 = hacker1.length
 const long2 = hacker2.length
+
 if (long1 > long2) {
     console.log(`The driver has the longest name, it has ${long1}  characters.`)
 }
@@ -28,16 +30,22 @@ else {
 }
 
 //Exercise 3
+let output = ""
 
 for (let i = 0; i < hacker1.length; i++) {
-    const spell = hacker1[i]
-    console.log(spell)
-
+    const spell = hacker1[i].toUpperCase()
+    output += spell
 }
-console.log("      ")
 
-const lastIndex = hacker1.length - 1
+console.log(output)
+
+console.log("-----------------------------------------")
+
+let outPut = ""
+const lastIndex = hacker2.length - 1
 for (let i = lastIndex; i >= 0; i--) {
-    const char = hacker1[i]
-    console.log(char)
+    const char = hacker2[i]
+    outPut += char
 }
+
+console.log(outPut)
